4efc73d4a3a5078802ed99959cac1efcc3087e89
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-02 17:13:36 -0700

    refact: add router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {
+  Suspense,
+} from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Paths from './shared/utils/paths';
+import HmHome from './Home/components/Home';
+import HmLoading from './shared/components/Loading';
 import './index.css';
 
+
 ReactDOM.render(
-  <App />,
+  (
+    <BrowserRouter>
+      <Suspense fallback={<HmLoading />}>
+        <Switch>
+          <Route exact path={Paths.AppRootPath} component={HmHome} />} />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
+  ),
   document.getElementById('root'),
 );
 
