2f7b75ced640e4e23abd0e290f2574b50bc07b83
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-02 20:53:49 -0700

    feat: add footer

---
+++ src/App/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Footer.css';
+
+
+function Footer() {
+  return (
+    <footer className="footer hm-footer">
+      <div className="hm-copyright">
+        © 2019 H.M.
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
