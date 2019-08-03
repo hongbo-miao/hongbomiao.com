23a33610fe896c9632f780ab0ea30719d228edc5
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-02 17:13:36 -0700

    refact: add router

---
+++ src/shared/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Loading() {
+  return (
+    <div />
+  );
+}
+
+export default Loading;
---
+++ src/shared/utils/paths.js
@@ -0,0 +1,5 @@
+const Paths = {
+  AppRootPath: '/',
+};
+
+export default Paths;
