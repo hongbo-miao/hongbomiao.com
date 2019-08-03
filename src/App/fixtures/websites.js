07026244449f4355862b4b13febe51298e79cb00
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-02 21:55:50 -0700

    feat: add social websites in footer

---
+++ src/App/fixtures/websites.js
@@ -0,0 +1,30 @@
+import githubLogo from '../images/github.svg';
+import linkedinLogo from '../images/linkedin.svg';
+import pinterestLogo from '../images/pinterest.svg';
+import twitterLogo from '../images/twitter.svg';
+
+
+const Websites = [
+  {
+    name: 'Pinterest',
+    src: pinterestLogo,
+    url: 'https://www.pinterest.com/hongbo_miao/',
+  },
+  {
+    name: 'GitHub',
+    src: githubLogo,
+    url: 'https://github.com/hongbo-miao/',
+  },
+  {
+    name: 'Twitter',
+    src: twitterLogo,
+    url: 'https://twitter.com/hongbo_miao/',
+  },
+  {
+    name: 'LinkedIn',
+    src: linkedinLogo,
+    url: 'https://www.linkedin.com/in/hongbomiao/',
+  },
+];
+
+export default Websites;
