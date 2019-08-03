63676af7f84ec39573e402970cc47a13d799d838
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-03 06:33:23 -0700

    style: bigger space between title and content

--- src/Home/components/Home.js
+++ src/Home/components/Home.js
@@ -7,12 +7,14 @@ import './Home.css';
 
 function Home() {
   return (
-    <div className="container hm-container">
-      <h1 className="title">HONGBO MIAO</h1>
-      <div className="hm-content">
-        <img className="hm-emoji" src={hatImage} alt="Magical Hat" />
-        <div className="hm-text">Making magic happen</div>
-        <img className="hm-emoji" src={magicImage} alt="Magic" />
+    <div className="hm-home">
+      <div className="container hm-container">
+        <h1 className="title">HONGBO MIAO</h1>
+        <div className="hm-content">
+          <img className="hm-emoji" src={hatImage} alt="Magical Hat" />
+          <div className="hm-text">Making magic happen</div>
+          <img className="hm-emoji" src={magicImage} alt="Magic" />
+        </div>
       </div>
     </div>
   );
