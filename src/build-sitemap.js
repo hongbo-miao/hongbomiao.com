a8d4d16a6f5406c098692689c92668892f86034b
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-05 17:12:47 -0700

    lint: fix eslint issues

--- src/build-sitemap.js
+++ src/build-sitemap.js
@@ -9,12 +9,10 @@ const OUTPUT_FILE = path.resolve(__dirname, '..', 'public', 'sitemap.xml');
 
 const sitemap = sm.createSitemap({
   hostname: 'https://hongbomiao.com',
-  cacheTime: 10 * 60 * 1000,  // 10 min, cache purge period
+  cacheTime: 10 * 60 * 1000, // 10 min, cache purge period
   urls: [
     { url: Paths.appRootPath, changefreq: 'hourly', priority: 1 },
   ],
 });
 
 fs.writeFileSync(OUTPUT_FILE, sitemap.toString());
-
-console.log(`Sitemap written at ${OUTPUT_FILE}`);
