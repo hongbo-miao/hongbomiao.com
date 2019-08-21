008a8ed66356d98ff0e13e030b7a41cf9086709d
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-21 00:01:43 -0700

    test: add describe for App

--- src/App/components/App.test.jsx
+++ src/App/components/App.test.jsx
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom';
 import HmApp from './App';
 
 
-test('render App', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<HmApp />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  test('render App', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<HmApp />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
