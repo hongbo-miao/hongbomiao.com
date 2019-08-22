d1d9e649995fb7b93b693f5fc4bac2c4868745ea
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-21 19:30:31 -0700

    test: index file

---
+++ src/index.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HmApp from './App/components/App';
+
+
+jest.mock('react-dom', () => ({ render: jest.fn() }));
+
+describe('index', () => {
+  test('render index', () => {
+    const div = document.createElement('div');
+    div.id = 'root';
+    document.body.appendChild(div);
+
+    // eslint-disable-next-line global-require
+    require('./index.jsx');
+    expect(ReactDOM.render).toHaveBeenCalledWith(<HmApp />, div);
+  });
+});
