2c05ff77266be73ca6f9b4581e1125f40be9e5a3
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-21 22:11:43 -0700

    refact: buildSitemap

--- src/shared/utils/buildSitemap.js
+++ src/shared/utils/buildSitemap.js
@@ -5,17 +5,24 @@ import { Sitemap } from 'sitemap';
 import Paths from './paths';
 
 
-async function buildSitemap() {
-  const sitemap = new Sitemap({
-    hostname: 'https://hongbomiao.com',
-    cacheTime: 10 * 60 * 1000, // 10 min, cache purge period
-    urls: [
-      { url: Paths.appRootPath, changefreq: 'hourly', priority: 1 },
-    ],
+export function getSitemap(hostname, cacheTime, urls) {
+  return new Sitemap({
+    hostname,
+    cacheTime,
+    urls,
   });
+}
 
+async function buildSitemap() {
+  const hostname = 'https://hongbomiao.com';
+  const cacheTime = 10 * 60 * 1000; // 10 min, cache purge period
+  const urls = [
+    { url: Paths.appRootPath, changefreq: 'hourly', priority: 1 },
+  ];
   const sitemapPath = path.resolve(__dirname, '..', '..', '..', 'public', 'sitemap.xml');
+
+  const sitemap = getSitemap(hostname, cacheTime, urls);
   await fsp.writeFile(sitemapPath, String(sitemap));
 }
 
-buildSitemap();
+export default buildSitemap;
