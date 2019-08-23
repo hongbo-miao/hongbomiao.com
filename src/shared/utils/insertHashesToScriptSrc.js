929d51e4e274b167ca084bc9aa5bd86b708b4a9a
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-23 11:46:24 -0700

    refact: rename to insertHashesToScriptSrc

--- src/shared/utils/insertHashesToScriptSrc.js
+++ src/shared/utils/insertHashesToScriptSrc.js
@@ -1,6 +1,6 @@
-function getHeaders(headers, hashes) {
+function insertHashesToScriptSrc(headers, hashes) {
   const scriptSrc = `script-src 'self' ${hashes}`;
   return headers.replace('script-src \'self\' ', scriptSrc);
 }
 
-export default getHeaders;
+export default insertHashesToScriptSrc;
