b98774e37452a637d4a214a7e57cebe63ab907bb
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-24 23:28:22 -0700

    feat: set up snapshot test for stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,10 @@
 import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+
 import '../src/index.css';
 
 
-const req = require.context('../src', true, /\.stories.jsx$/);
+const req = requireContext('../src', true, /\.stories\.jsx$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
