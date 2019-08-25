5098e0d2aed8b4a8756ad407676a1542b5b12343
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-25 04:33:00 -0700

    test: add Footer storybook snapshot tests

---
+++ src/App/components/Footer.stories.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import HmFooter from './Footer';
+
+
+storiesOf('Footer', module)
+  .add('default', () => <HmFooter />);
