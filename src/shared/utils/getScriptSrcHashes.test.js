9684a1c1d83108c4ecf687670e79a680fb3a6770
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-24 20:04:34 -0700

    refact: getScriptSrcHashes test

--- src/shared/utils/getScriptSrcHashes.test.js
+++ src/shared/utils/getScriptSrcHashes.test.js
@@ -5,14 +5,12 @@ describe('getScriptSrcHashes', () => {
   test('hash usage for script element with content', () => {
     const index = '<!doctype html><script>a</script>b<script>c</script><script src="/static/js/1.js"></script></html>';
     const hashes = getScriptSrcHashes(index);
-    const expectedHashes = "'sha256-ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs=' 'sha256-Ln0sA6lQeuJl7PW1NWiFpTOTogKdJBOUmXJloaJa78Y=' ";
-    expect(hashes).toBe(expectedHashes);
+    expect(hashes).toBe("'sha256-ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs=' 'sha256-Ln0sA6lQeuJl7PW1NWiFpTOTogKdJBOUmXJloaJa78Y=' ");
   });
 
   test('return empty hash if no script element with content', () => {
     const index = '<!doctype html><script src="/static/js/1.js"></script></html>';
     const hashes = getScriptSrcHashes(index);
-    const expectedHashes = '';
-    expect(hashes).toBe(expectedHashes);
+    expect(hashes).toBe('');
   });
 });
