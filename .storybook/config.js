a65cd985ae42b4f8518e914df2e30f1a192324a4
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-25 04:33:54 -0700

    test: import global css in storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,7 @@
 import { configure } from '@storybook/react';
 import requireContext from 'require-context.macro';
+import 'bulma/css/bulma.css';
+import 'normalize.css';
 
 import '../src/index.css';
 
