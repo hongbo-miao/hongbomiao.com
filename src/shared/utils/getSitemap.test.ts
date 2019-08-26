bdb0f55a79a5bc963ce6cafdd3fe717da10a6cd8
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-25 23:49:08 -0700

    refact: apply typescript on build scripts

---
+++ src/shared/utils/getSitemap.test.ts
@@ -0,0 +1,22 @@
+import {
+  EnumChangefreq,
+  ISitemapOptions,
+} from 'sitemap';
+
+import getSitemap from '../../../bin/getSitemap';
+
+
+describe('getSitemap', () => {
+  test('get sitemap', () => {
+    const sitemapOptions: ISitemapOptions = {
+      hostname: 'https://example.com',
+      cacheTime: 10 * 60 * 1000,
+      urls: [
+        { url: '/', changefreq: EnumChangefreq.HOURLY, priority: 1 },
+      ],
+    };
+
+    const sitemap = getSitemap(sitemapOptions);
+    expect(sitemap).toMatchSnapshot();
+  });
+});
