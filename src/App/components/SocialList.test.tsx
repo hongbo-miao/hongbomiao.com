4279008151af218d08cb527b8cbe897d18cb77f3
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-26 00:57:26 -0700

    chore: uppercase websites array

--- src/App/components/SocialList.test.tsx
+++ src/App/components/SocialList.test.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import WEBSITES from '../fixtures/websites';
 import TestUtils from '../../shared/utils/testUtils';
-import Websites from '../fixtures/websites';
 import HmSocialList from './SocialList';
 
 
 describe('SocialList', () => {
   test('render SocialList', () => {
-    TestUtils.testComponent(<HmSocialList websites={Websites} />);
+    TestUtils.testComponent(<HmSocialList websites={WEBSITES} />);
   });
 
   test('render .hm-social-item', () => {
-    const wrapper = shallow(<HmSocialList websites={Websites} />);
-    expect(wrapper.find('.hm-social-item')).toHaveLength(Websites.length);
+    const wrapper = shallow(<HmSocialList websites={WEBSITES} />);
+    expect(wrapper.find('.hm-social-item')).toHaveLength(WEBSITES.length);
   });
 });
