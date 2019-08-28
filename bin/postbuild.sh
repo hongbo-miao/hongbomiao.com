cdd7b89b18e16480fc2bf3414d993ab8bcb74db9
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-08-27 22:42:13 -0700

    refact: use create-react-app .env to remove sourcemap

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -3,7 +3,5 @@
 set -e
 
 babel-node tmp/postbuild.js
-rm build/static/js/*.map
-rm build/static/css/*.map
 purgecss --css build/static/css/*.css --content build/static/index.html build/static/js/*.js --out build/static/css
 rm -r tmp
