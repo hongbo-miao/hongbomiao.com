034600a3a626f30d88f8173745fc04c37ca6c195
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-05-28 00:37:01 +0800

    refact: import bulma in index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bulma/css/bulma.css';
 
+import './index.css';
 import * as serviceWorker from './shared/lib/serviceWorker';
 import HmApp from './App/components/App';
-import './index.css';
 
 ReactDOM.render(<HmApp />, document.getElementById('root'));
 
