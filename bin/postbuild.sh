4b1e713b205ca24930a837accc7807e969094679
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-05-28 00:51:34 +0800

    chore: disable purgecss

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-purgecss --css build/static/css/*.css --content build/index.html build/static/js/*.js --output build/static/css
+# purgecss --css build/static/css/*.css --content build/index.html build/static/js/*.js --output build/static/css
 babel-node tmp/runUpdateHeaders.js
 rm -r tmp
