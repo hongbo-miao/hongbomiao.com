87a219121fa20158f1a0c3c25e3c9f02988121e6
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-05-28 08:49:17 +0800

    chore: change paths and config from js to ts

--- bin/prebuild.sh
+++ bin/prebuild.sh
@@ -3,4 +3,5 @@
 set -e
 
 yarn run babel scripts --out-dir tmp --extensions '.ts,.tsx'
+yarn run babel src/shared/utils/paths.ts --out-file src/shared/utils/paths.js
 babel-node tmp/runBuildSitemap.js
