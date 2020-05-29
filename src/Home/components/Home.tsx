7a17040fabd8fd9ceec494bfd884769b15f110c8
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-05-30 00:59:47 +0800

    pwa: uses-webp-images

--- src/Home/components/Home.tsx
+++ src/Home/components/Home.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 
 import Config from '../../config';
-import hatImage from '../images/hat.png';
-import magicImage from '../images/magic.png';
+import hatPNG from '../images/hat.png';
+import hatWebP from '../images/hat.webp';
+import magicPNG from '../images/magic.png';
+import magicWebP from '../images/magic.webp';
 import styles from './Home.module.css';
+import LazyComponent from '../../shared/components/LazyComponent';
+import HmImage from '../../shared/components/Image';
 
 const Home: React.FC = () => (
   <div className={styles.hmHome}>
     <div className={`container ${styles.hmContainer}`}>
       <h1 className={styles.hmTitle}>HONGBO MIAO</h1>
       <a className={styles.hmContent} href={Config.githubUrl} target="_blank" rel="noopener noreferrer">
-        <img className={styles.hmEmoji} src={hatImage} alt="Magical Hat" />
+        <LazyComponent>
+          <HmImage className={styles.hmEmoji} alt="Magical Hat" src={hatPNG} webpSrc={hatWebP} />
+        </LazyComponent>
         <div className={styles.hmText}>Making magic happen</div>
-        <img className={styles.hmEmoji} src={magicImage} alt="Magic" />
+        <LazyComponent>
+          <HmImage className={styles.hmEmoji} alt="Magic" src={magicPNG} webpSrc={magicWebP} />
+        </LazyComponent>
       </a>
     </div>
   </div>
