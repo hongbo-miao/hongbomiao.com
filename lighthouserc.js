c556b2f20aa90b1ae0b402cfa0f8205fb93309d4
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-05-30 04:42:20 +0800

    pwa: display image size based on lighthouse suggestion

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,6 @@ module.exports = {
     assert: {
       preset: 'lighthouse:recommended',
       assertions: {
-        'image-size-responsive': ['warn', { minScore: 0 }],
         'unused-css-rules': ['warn', { minScore: 0 }],
         'unused-javascript': ['warn', { minScore: 0 }],
         'uses-rel-preconnect': ['warn', { minScore: 0 }],
