482491a3b1985fa46c493d75bd83f5edcf4bc83f
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-06-01 23:41:17 +0800

    feat: serve client from server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import path from 'path';
 
 const app: express.Application = express();
 const port = 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
