500410bb7d2e2f0d5cb24add12ce4eabfe939464
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-06-02 10:10:18 +0800

    chore: update relative path

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,9 +9,9 @@ const port = 3001;
 
 app.use(cors());
 app.use(helmetMiddleware);
-app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
