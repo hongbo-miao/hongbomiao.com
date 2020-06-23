770f96c9e7efea897d05ea9dbd92497e0320a97b
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-06-24 02:28:50 +0800

    refact: move typescript, eslint, prettier, huksy, lint-staged to the root

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
+    'react',
     '@typescript-eslint',
     'prettier', // eslint-config-prettier
   ],
@@ -9,6 +10,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended', // Not all eslint core rules are compatible with TypeScript, so you need to add both eslint:recommended and plugin:@typescript-eslint/eslint-recommended
     'plugin:@typescript-eslint/recommended', // All the recommended rules for the plugin typescript-eslint
+    'plugin:react/recommended',
     'prettier', // Make sure to put it last in the extends array, so it gets the chance to override other configs.
     'prettier/@typescript-eslint', // Use eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // eslint-plugin-prettier exposes a "recommended" configuration that configures both eslint-plugin-prettier and eslint-config-prettier in a single step. Make sure this is always the last configuration in the extends array.
@@ -21,6 +23,7 @@ module.exports = {
     },
   },
   env: {
+    browser: true,
     jest: true,
   },
   rules: {
@@ -31,11 +34,14 @@ module.exports = {
         'js': 'never',
         'jsx': 'never',
         'ts': 'never',
-        'tsx': 'never',
-      },
+        'tsx': 'never'
+      }
     ],
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: true,
+    }],
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.jsx', '.tsx'],
     }],
     'spaced-comment': ['error', 'always', {
       'markers': ['/'],
