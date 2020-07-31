2b79d42a273469108e658bfda7b09710a9ca7f5b
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-01 04:14:29 +0800

    security: rate limit

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,13 @@ import express from 'express';
 
 import corsMiddleware from './middlewares/cors.middleware';
 import helmetMiddleware from './middlewares/helmet.middleware';
+import rateLimitMiddleware from './middlewares/rate-limit.middleware';
 
 const app: express.Application = express();
 const port = 3001;
 
 app.use(corsMiddleware);
+app.use(rateLimitMiddleware);
 app.use(helmetMiddleware);
 app.use(express.static(path.join(__dirname, '../../client/build')));
 app.get('/', (req, res) => {
