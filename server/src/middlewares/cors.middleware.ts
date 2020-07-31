f7e4a3f3417944e5f95d1ab023ff08b7340e4a55
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-01 01:10:37 +0800

    lint: auto fix lint issues

--- server/src/middlewares/cors.middleware.ts
+++ server/src/middlewares/cors.middleware.ts
@@ -1,22 +1,13 @@
 import cors, { CorsOptions } from 'cors';
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 
-const corsMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const nonProdWhiteList = [
-    'http://localhost:3000',
-    'http://localhost:3001',
-  ];
-  const prodWhiteList = [
-    'https://hongbomiao.com',
-    'https://www.hongbomiao.com',
-  ];
+const corsMiddleware = (req: Request, res: Response, next: NextFunction): RequestHandler => {
+  const nonProdWhiteList = ['http://localhost:3000', 'http://localhost:3001'];
+  // const prodWhiteList = ['https://hongbomiao.com', 'https://www.hongbomiao.com'];
   // const whitelist = process.env.NODE_ENV === 'production' ? prodWhiteList : nonProdWhiteList;
   const whitelist = nonProdWhiteList;
   const corsOptions: CorsOptions = {
-    allowedHeaders: [
-      'Authorization',
-      'Content-Type',
-    ],
+    allowedHeaders: ['Authorization', 'Content-Type'],
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'POST', 'PATCH'],
     optionsSuccessStatus: 200,
