26231b3a96d1673b76c86cfca84770869b5b3f2b
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-01 06:58:51 +0800

    init: add server

---
+++ server/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
