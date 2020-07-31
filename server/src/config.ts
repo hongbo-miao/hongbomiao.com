367609af62f094cd9901acde13904b9aceafd4f7
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-01 05:00:42 +0800

    security: set cors whitelist based on node env

---
+++ server/src/config.ts
@@ -0,0 +1,6 @@
+const Config = {
+  devWhitelist: ['http://localhost:3000', 'http://localhost:3001'],
+  prodWhitelist: ['https://hongbomiao.com', 'https://www.hongbomiao.com'],
+};
+
+export default Config;
