51635576bfb3e85c461e3c383a4d966a61521aa2
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-01 04:25:26 +0800

    chore: rename to rateLimitOptions

--- server/src/middlewares/rate-limit.middleware.ts
+++ server/src/middlewares/rate-limit.middleware.ts
@@ -1,10 +1,10 @@
 import rateLimit, { Options } from 'express-rate-limit';
 
-const options: Options = {
+const rateLimitOptions: Options = {
   windowMs: 60 * 1000, // 60 sec
   max: 100, // requests per IP
   message: 'Sorry, too many requests, please try again later.',
 };
-const rateLimitMiddleware = rateLimit(options);
+const rateLimitMiddleware = rateLimit(rateLimitOptions);
 
 export default rateLimitMiddleware;
