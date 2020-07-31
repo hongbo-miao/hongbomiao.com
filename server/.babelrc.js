b888f8d8623d9a0dc755f79496cf5a0db20e890f
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-01 07:05:54 +0800

    refact: remove babel-node
    # Conflicts:
    #	server/yarn.lock

--- server/.babelrc.js
+++ server/.babelrc.js
@@ -1,6 +1,13 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
     '@babel/preset-typescript',
   ],
 };
