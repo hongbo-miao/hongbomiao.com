b7f1906d1af843b420ac9ff98779dc03acc4a42d
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-01 07:03:50 +0800

    feat: add jest
    # Conflicts:
    #	server/yarn.lock

---
+++ server/src/utils/getScriptSrcHashes.test.ts
@@ -0,0 +1,20 @@
+import getScriptSrcHashes from './getScriptSrcHashes';
+
+describe('getScriptSrcHashes', () => {
+  test('hash usage for script element with content', () => {
+    const index = '<!doctype html><script>a</script>b<script>c</script><script src="/static/js/1.js"></script></html>';
+    const hashes = getScriptSrcHashes(index);
+    expect(hashes).toEqual(
+      [
+        "'sha256-ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs='",
+        "'sha256-Ln0sA6lQeuJl7PW1NWiFpTOTogKdJBOUmXJloaJa78Y='",
+      ],
+    );
+  });
+
+  test('return empty hash if no script element with content', () => {
+    const index = '<!doctype html><script src="/static/js/1.js"></script></html>';
+    const hashes = getScriptSrcHashes(index);
+    expect(hashes).toEqual([]);
+  });
+});
