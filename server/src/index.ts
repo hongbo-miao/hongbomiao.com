4d13223f0014a09259f146dc25ae992e60b665a8
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-01 07:01:37 +0800

    security: add contentSecurityPolicy
    # Conflicts:
    #	server/yarn.lock

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import path from 'path';
 
+import helmetMiddleware from './middlewares/helmet.middleware';
+
 const app: express.Application = express();
 const port = 3001;
 
+app.use(helmetMiddleware);
 app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html'));
