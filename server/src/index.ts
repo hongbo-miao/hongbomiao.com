f89ee4efbfa39b6697a820f1424d386ea25a6c59
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-02 18:02:42 +0800

    feat: add morgan for logs

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,14 @@ import express from 'express';
 import Config from './config';
 import corsMiddleware from './middlewares/cors.middleware';
 import helmetMiddleware from './middlewares/helmet.middleware';
+import morganMiddleware from './middlewares/morgan.middleware';
 import rateLimitMiddleware from './middlewares/rate-limit.middleware';
 import redirectSSLMiddleware from './middlewares/redirect-ssl.middleware';
 
 const app = express();
 const port = Config.port || 3001;
 
+app.use(morganMiddleware);
 app.use(corsMiddleware);
 app.use(rateLimitMiddleware);
 app.use(helmetMiddleware);
