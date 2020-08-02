6d6cd09c9403c87655f118511b0e9d3bcdfa02a5
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-02 17:49:02 +0800

    chore: redirect to https only if in production

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 import express from 'express';
-import redirectSSL from 'redirect-ssl';
 
 import corsMiddleware from './middlewares/cors.middleware';
 import helmetMiddleware from './middlewares/helmet.middleware';
 import rateLimitMiddleware from './middlewares/rate-limit.middleware';
+import redirectSSLMiddleware from './middlewares/redirect-ssl.middleware';
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3001;
 app.use(corsMiddleware);
 app.use(rateLimitMiddleware);
 app.use(helmetMiddleware);
-app.use(redirectSSL);
+app.use(redirectSSLMiddleware);
 app.use(express.static(path.join(__dirname, '../../client/build')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
