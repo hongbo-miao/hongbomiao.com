42647434aec20ccdbbcb1c48ccbc97190485d68c
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-03 01:11:37 +0800

    feat: show different type of logs on different environment

--- server/src/middlewares/morgan.middleware.ts
+++ server/src/middlewares/morgan.middleware.ts
@@ -1,5 +1,9 @@
 import morgan from 'morgan';
 
-const morganMiddleware = morgan('tiny');
+import isProd from '../utils/isProd';
+
+// https://github.com/expressjs/morgan#predefined-formats
+const format = isProd ? 'combined' : 'dev';
+const morganMiddleware = morgan(format);
 
 export default morganMiddleware;
