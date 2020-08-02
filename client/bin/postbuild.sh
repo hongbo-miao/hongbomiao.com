caeaa8866aef3228dd56388afa09478158880170
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-02 11:34:28 +0800

    refact: babel in prebuild and postbuild for client

--- client/bin/postbuild.sh
+++ client/bin/postbuild.sh
@@ -3,5 +3,5 @@
 set -e
 
 # purgecss --css build/static/css/*.css --content build/index.html build/static/js/*.js --output build/static/css
-babel-node tmp/runUpdateHeaders.js
+node tmp/runUpdateHeaders.js
 rm -rf tmp
--- client/bin/prebuild.sh
+++ client/bin/prebuild.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-yarn run babel scripts --out-dir tmp --extensions '.ts,.tsx'
-yarn run babel src/shared/utils/paths.ts --out-file src/shared/utils/paths.js
-babel-node tmp/runBuildSitemap.js
+babel scripts --out-dir tmp --extensions '.ts,.tsx'
+babel src/shared/utils/paths.ts --out-file src/shared/utils/paths.js
+node tmp/runBuildSitemap.js
 rm src/shared/utils/paths.js
