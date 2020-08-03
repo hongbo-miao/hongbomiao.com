24c902ef4c3cd72ff29e836be7132da3ab730a38
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-03 17:49:22 +0800

    feat: add Sentry for error tracking

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,6 +2,8 @@ const Config = {
   nodeEnv: process.env.NODE_ENV,
   port: process.env.PORT,
 
+  sentryDSN: 'https://2f46725646834700b4c2675abbc2da6a@o379185.ingest.sentry.io/5375232',
+
   devWhitelist: ['http://localhost:3000', 'http://localhost:3001'],
   prodWhitelist: ['https://hongbomiao.com', 'https://www.hongbomiao.com'],
 };
