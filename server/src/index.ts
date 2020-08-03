1aa2e80fdc2e4f7ac85230e919e2c59f6f6df4c9
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-03 17:49:22 +0800

    feat: add Sentry for error tracking

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import path from 'path';
+import * as Sentry from '@sentry/node';
 import bodyParser from 'body-parser';
 import express from 'express';
 import Config from './config';
+import handleError from './controllers/error/handleError';
 import corsMiddleware from './middlewares/cors.middleware';
 import helmetMiddleware from './middlewares/helmet.middleware';
 import morganMiddleware from './middlewares/morgan.middleware';
@@ -9,9 +11,15 @@ import rateLimitMiddleware from './middlewares/rateLimit.middleware';
 import redirectSSLMiddleware from './middlewares/redirectSSL.middleware';
 import apiRouter from './routers/api.router';
 
-const app = express();
-const port = Config.port || 3001;
+Sentry.init({
+  dsn: Config.sentryDSN,
+  environment: Config.nodeEnv,
+});
 
+const port = Config.port || 3001;
+const app = express();
+
+app.use(Sentry.Handlers.requestHandler()); // The request handler must be the first middleware on the app
 app.use(morganMiddleware);
 app.use(corsMiddleware);
 app.use(rateLimitMiddleware);
@@ -25,6 +33,8 @@ app.use(express.static(path.join(__dirname, '../../client/build')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
+
+app.use(Sentry.Handlers.errorHandler()); // The error handler must be before any other error middleware and after all controllers
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Listening at port ${port}`));
