ef71a1a9a4225023d375884057cf6020fb8873f7
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-05 16:22:12 +0800

    fix: NODE_ENV, PORT

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,15 +1,15 @@
-const { nodeEnv, port } = process.env;
+const { NODE_ENV, PORT } = process.env;
 
 const Config = {
-  nodeEnv,
-  port: port || 3001,
+  nodeEnv: NODE_ENV,
+  port: PORT || 3001,
 
   devAllowList: ['https://localhost:3000', 'https://localhost:3001'],
   prodAllowList: ['https://hongbomiao.com', 'https://www.hongbomiao.com'],
 
   sentryOptions: {
     dsn: 'https://2f46725646834700b4c2675abbc2da6a@o379185.ingest.sentry.io/5375232',
-    environment: nodeEnv,
+    environment: NODE_ENV,
   },
 };
 
