24b19641426b0772d2538769c42b86800b88d1fb
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-06 16:22:38 +0800

    test: helmetMiddleware

---
+++ server/src/security/middlewares/helmet.middleware.test.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import request from 'supertest';
+import helmetMiddleware from './helmet.middleware';
+
+describe('helmetMiddleware', () => {
+  const app = express()
+    .use(helmetMiddleware())
+    .get('/', (req, res) => {
+      res.send('Hello, World!');
+    });
+
+  test("should include script-src 'self' in content-security-policy", (done) => {
+    request(app)
+      .get('/')
+      .expect('content-security-policy', /script-src 'self'/)
+      .expect(200)
+      .end(done);
+  });
+});
