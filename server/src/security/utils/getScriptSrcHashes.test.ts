9d9c0ef2d5561c870503707135875f498cb8a534
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-06 17:55:06 +0800

    chore: rename index to html

--- server/src/security/utils/getScriptSrcHashes.test.ts
+++ server/src/security/utils/getScriptSrcHashes.test.ts
@@ -2,8 +2,8 @@ import getScriptSrcHashes from './getScriptSrcHashes';
 
 describe('getScriptSrcHashes', () => {
   test('hash usage for script element with content', () => {
-    const index = '<!doctype html><script>a</script>b<script>c</script><script src="/static/js/1.js"></script></html>';
-    const hashes = getScriptSrcHashes(index);
+    const html = '<!doctype html><script>a</script>b<script>c</script><script src="/static/js/1.js"></script></html>';
+    const hashes = getScriptSrcHashes(html);
     expect(hashes).toEqual([
       "'sha256-ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs='",
       "'sha256-Ln0sA6lQeuJl7PW1NWiFpTOTogKdJBOUmXJloaJa78Y='",
@@ -11,8 +11,8 @@ describe('getScriptSrcHashes', () => {
   });
 
   test('return empty hash if no script element with content', () => {
-    const index = '<!doctype html><script src="/static/js/1.js"></script></html>';
-    const hashes = getScriptSrcHashes(index);
+    const html = '<!doctype html><script src="/static/js/1.js"></script></html>';
+    const hashes = getScriptSrcHashes(html);
     expect(hashes).toEqual([]);
   });
 });
