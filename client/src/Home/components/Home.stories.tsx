5560e7629629be7799462d831f8911f5a19d71c7
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-09 16:19:13 +0800

    fix: storybook

--- client/src/Home/components/Home.stories.tsx
+++ client/src/Home/components/Home.stories.tsx
@@ -1,5 +1,21 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { RootState } from '../../reducer';
+import Me from '../types/Me.type';
 import HmHome from './Home';
 
-storiesOf('Home', module).add('default', () => <HmHome />);
+const mockStore = configureStore();
+const me: Me = {
+  name: 'Hongbo Miao',
+  slogan: 'Making magic happen',
+};
+const initialState: RootState = { me };
+const store = mockStore(initialState);
+
+storiesOf('Home', module).add('default', () => (
+  <Provider store={store}>
+    <HmHome />
+  </Provider>
+));
