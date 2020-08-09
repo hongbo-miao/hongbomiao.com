bf5f2ff6b4bfe602e1857d106276a824a07dd402
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-09 10:15:02 +0800

    refact: switch from Enzyme to React Testing Library

--- client/src/App/components/Copyright.test.tsx
+++ client/src/App/components/Copyright.test.tsx
@@ -1,9 +1,9 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import TestUtils from '../../shared/utils/testUtils';
 import HmCopyright from './Copyright';
 
 describe('Copyright', () => {
   test('render Copyright', () => {
-    TestUtils.testComponent(<HmCopyright year={1990} />);
+    render(<HmCopyright year={1990} />);
   });
 });
--- client/src/App/components/SocialList.test.tsx
+++ client/src/App/components/SocialList.test.tsx
@@ -1,16 +1,17 @@
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import React from 'react';
-import TestUtils from '../../shared/utils/testUtils';
 import WEBSITES from '../fixtures/websites';
 import HmSocialList from './SocialList';
 
 describe('SocialList', () => {
+  const component = <HmSocialList websites={WEBSITES} />;
+
   test('render SocialList', () => {
-    TestUtils.testComponent(<HmSocialList websites={WEBSITES} />);
+    render(component);
   });
 
   test('render .level-item', () => {
-    const wrapper = shallow(<HmSocialList websites={WEBSITES} />);
-    expect(wrapper.find('.level-item')).toHaveLength(WEBSITES.length);
+    const { container } = render(component);
+    expect(container.getElementsByTagName('img')).toHaveLength(WEBSITES.length);
   });
 });
