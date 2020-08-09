fb6cf82d565bc89c3a2e1460653b61e4d350fee8
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-09 16:48:10 +0800

    refact: createMockedStore

--- client/src/Home/components/Home.stories.tsx
+++ client/src/Home/components/Home.stories.tsx
@@ -1,21 +1,11 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import { RootState } from '../../reducer';
-import Me from '../types/Me.type';
+import mockedStore from '../../shared/utils/createMockedStore';
 import HmHome from './Home';
 
-const mockStore = configureStore();
-const me: Me = {
-  name: 'Hongbo Miao',
-  slogan: 'Making magic happen',
-};
-const initialState: RootState = { me };
-const store = mockStore(initialState);
-
 storiesOf('Home', module).add('default', () => (
-  <Provider store={store}>
+  <Provider store={mockedStore}>
     <HmHome />
   </Provider>
 ));
