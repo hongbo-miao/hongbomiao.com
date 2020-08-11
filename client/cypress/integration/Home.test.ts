af53498b341ec356aae45eec029fbfa6f59d17cb
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-11 19:05:58 +0800

    test(home): add integration test

--- client/cypress/integration/Home.test.ts
+++ client/cypress/integration/Home.test.ts
@@ -1,6 +1,26 @@
+import meQuery from '../../src/Home/queries/me.query';
+
 describe('Home', () => {
   it('render Home', () => {
     cy.visit('https://localhost:3000/');
     cy.contains('Making magic happen');
+  });
+
+  it('receive GraphQL meQuery data', () => {
+    cy.request('POST', 'https://localhost:3001/graphql', {
+      query: meQuery,
+    }).then((res) => {
+      expect(res).property('status').to.equal(200);
+      expect(res)
+        .property('body')
+        .to.eql({
+          data: {
+            me: {
+              name: 'Hongbo Miao',
+              slogan: 'Making magic happen',
+            },
+          },
+        });
+    });
   });
 });
