cb6eeb0001b61bbd4c4674f1930e1d16648156b0
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-13 22:56:05 +0800

    feat(epic): cancel previous getMe call

--- client/src/Home/epics/fetchMeEpic.ts
+++ client/src/Home/epics/fetchMeEpic.ts
@@ -1,14 +1,14 @@
 import { Epic, ofType } from 'redux-observable';
 import { of } from 'rxjs';
 import { AjaxError, AjaxResponse } from 'rxjs/ajax';
-import { catchError, map, mergeMap } from 'rxjs/operators';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import MeActionType from '../actionTypes/Me.actionType';
 import MeAction from '../actions/Me.action';
 
 const fetchMeEpic: Epic = (action$, state$, { fetchData }) =>
   action$.pipe(
     ofType(MeActionType.FETCH_ME),
-    mergeMap((action) =>
+    switchMap((action) =>
       fetchData(action.payload.query).pipe(
         map((res: AjaxResponse) => MeAction.fetchMeSucceed(res)),
         catchError((err: AjaxError) => of(MeAction.fetchMeFailed(err)))
