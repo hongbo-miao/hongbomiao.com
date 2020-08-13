d8d0e89cfd5520e477ffa7ab33ce000638b2ace1
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-13 23:04:32 +0800

    test(epic): add fetchMeFailed marble test

--- client/src/Home/epics/fetchMeEpic.test.ts
+++ client/src/Home/epics/fetchMeEpic.test.ts
@@ -1,45 +1,82 @@
-import { AjaxResponse } from 'rxjs/ajax';
+import { StateObservable } from 'redux-observable';
+import { Observable, throwError, timer } from 'rxjs';
+import { AjaxResponse, AjaxError } from 'rxjs/ajax';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
+import { switchMap } from 'rxjs/operators';
 import { TestScheduler } from 'rxjs/testing';
+import RootState from '../../shared/types/RootState.type';
 import MeAction from '../actions/Me.action';
 import meQuery from '../queries/me.query';
 import fetchMeEpic from './fetchMeEpic';
 
 describe('fetchMeEpic', () => {
-  test('fetchMeSucceed', () => {
-    const res = {
-      response: {
-        data: {
-          name: 'Hongbo Miao',
-          slogan: 'Making magic happen',
-        },
+  const res = {
+    response: {
+      data: {
+        name: 'Hongbo Miao',
+        slogan: 'Making magic happen',
       },
-    } as AjaxResponse;
+    },
+  } as AjaxResponse;
+  const err = new Error('Test error') as AjaxError;
+
+  test('fetchMeSucceed', () => {
     const marbles = {
-      i: '-i', // input action
-      r: '--r', // mock api response
-      o: '---o', // output action
+      i: '-i', // Input action
+      r: '--r', // Mock API response
+      o: '---o', // Output action
     };
 
-    const testScheduler = new TestScheduler((actual, expected) => {
+    const scheduler = new TestScheduler((actual, expected) => {
       expect(actual).toEqual(expected);
       expect(actual[0].notification.value.payload.res).toEqual(res);
     });
 
-    testScheduler.run(({ hot, cold, expectObservable }) => {
+    scheduler.run(({ hot, cold, expectObservable }) => {
       const action$ = hot(marbles.i, {
         i: MeAction.fetchMe(meQuery),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
       }) as any;
-      const state$ = null as any;
+      const state$ = {} as StateObservable<RootState>;
       const dependencies = {
-        fetchData: () =>
+        fetchData: (): ColdObservable<AjaxResponse> =>
           cold(marbles.r, {
             r: res,
           }),
       };
-
       const output$ = fetchMeEpic(action$, state$, dependencies);
+
       expectObservable(output$).toBe(marbles.o, {
         o: MeAction.fetchMeSucceed(res),
+      });
+    });
+  });
+
+  test('fetchMeFailed', () => {
+    const scheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+      expect(actual[0].notification.value.payload).toEqual(err);
+    });
+    const marbles = {
+      i: '-i', // Input action
+      d: '--|', // Mock API response time duration
+      o: '---o', // Output action
+    };
+
+    scheduler.run(({ hot, expectObservable }) => {
+      const action$ = hot(marbles.i, {
+        i: MeAction.fetchMe(meQuery),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      }) as any;
+      const state$ = {} as StateObservable<RootState>;
+      const duration = scheduler.createTime(marbles.d);
+      const dependencies = {
+        fetchData: (): Observable<never> => timer(duration).pipe(switchMap(() => throwError(err))),
+      };
+      const output$ = fetchMeEpic(action$, state$, dependencies);
+
+      expectObservable(output$).toBe(marbles.o, {
+        o: MeAction.fetchMeFailed(err),
       });
     });
   });
