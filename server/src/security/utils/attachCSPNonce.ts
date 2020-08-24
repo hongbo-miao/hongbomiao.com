f4a168ea3b8d61ae6535f3f9dad2927aacef7523
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-08-24 20:47:10 +0800

    chore(csp): attach nonce to style in html

--- server/src/security/utils/attachCSPNonce.ts
+++ server/src/security/utils/attachCSPNonce.ts
@@ -1,5 +1,5 @@
 const attachCSPNonce = (html: string, cspNonce: string): string => {
-  return html.replace(/<script/g, `<script nonce="${cspNonce}"`);
+  return html.replace(/<script/g, `<script nonce="${cspNonce}"`).replace(/<style/g, `<style nonce="${cspNonce}"`);
 };
 
 export default attachCSPNonce;
