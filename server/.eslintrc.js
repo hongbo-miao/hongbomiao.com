4b6ca77da61255d3afb1ef8b1f0b90f83ebc6b0a
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-02 07:42:56 +0800

    chore(lint): splits .eslintrc.js

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
   env: {
+    jest: true,
     node: true,
   },
   parserOptions: {
@@ -13,6 +15,7 @@ module.exports = {
   plugins: [
     '@babel', // @babel/eslint-plugin
     '@typescript-eslint', // @typescript-eslint/eslint-plugin
+    'jest', // eslint-plugin-jest
     'prettier', // eslint-plugin-prettier
     'security', // eslint-plugin-security
   ],
@@ -21,6 +24,7 @@ module.exports = {
     'airbnb-base', // eslint-config-airbnb-base
     'plugin:@typescript-eslint/eslint-recommended', // @typescript-eslint/eslint-plugin. Not all eslint core rules are compatible with TypeScript, so you need to add both eslint:recommended and plugin:@typescript-eslint/eslint-recommended
     'plugin:@typescript-eslint/recommended', // @typescript-eslint/eslint-plugin
+    'plugin:jest/recommended', // eslint-plugin-jest
     'plugin:security/recommended', // eslint-plugin-security
     'prettier', // eslint-plugin-prettier. Make sure to put it last in the extends array, so it gets the chance to override other configs
     'prettier/@typescript-eslint', // eslint-config-prettier. Disables ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
@@ -60,6 +64,7 @@ module.exports = {
         'newlines-between': 'never',
       },
     ],
+    'jest/expect-expect': 'off',
     'multiline-comment-style': ['error', 'starred-block'],
     'security/detect-non-literal-fs-filename': 'off',
     'spaced-comment': [
