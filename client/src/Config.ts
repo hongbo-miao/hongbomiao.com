9e1459118544e2c83f08b1dd7d6999957a2accd6
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-04 00:08:18 +0800

    refact(trace): change lightstep/opentelemetry-exporter-js to @opentelemetry/exporter-collector

--- client/src/Config.ts
+++ client/src/Config.ts
@@ -6,7 +6,10 @@ const Config = {
   githubURL: 'https://github.com/hongbo-miao/hongbomiao.com',
   graphQLURL: `https://${REACT_APP_SERVER_DOMAIN}:${REACT_APP_SERVER_PORT}/graphql`,
 
-  lightstepToken: REACT_APP_LIGHTSTEP_TOEKN,
+  lightstep: {
+    token: REACT_APP_LIGHTSTEP_TOEKN,
+    traceURL: 'https://ingest.lightstep.com:443/api/v2/otel/trace',
+  },
 
   sentryOptions: {
     dsn: 'https://2f46725646834700b4c2675abbc2da6a@o379185.ingest.sentry.io/5375232',
