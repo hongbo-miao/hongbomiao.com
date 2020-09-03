e83adff84ee5c778e8ac09e8be927fbec63c185b
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-03 19:41:51 +0800

    feat(metric): add cspViolationCounter

--- server/src/security/controllers/reportCSPViolation.ts
+++ server/src/security/controllers/reportCSPViolation.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from 'express';
 import logger from '../../log/utils/logger';
+import meter from '../../log/utils/meter';
+
+const cspViolationCounter = meter.createCounter('cspViolationCounter', {
+  description: 'Count CSP violations',
+});
 
 const reportCSPViolation = (req: Request, res: Response): void => {
   logger.error('reportCSPViolation', req.body);
+  const labels = req.body['csp-report'];
+  cspViolationCounter.bind(labels).add(1);
   res.status(200);
 };
 
