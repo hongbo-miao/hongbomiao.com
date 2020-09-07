28203ffb711725bf15cbca46d017871dcab0ab0b
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-07 15:01:36 +0800

    chore(pm2): change instances to -2

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'server',
       script: './build/index.js',
-      instances: 'max',
+      instances: -2, // https://pm2.keymetrics.io/docs/usage/cluster-mode/#usage
       exec_mode: 'cluster',
       env_development: {
         NODE_ENV: 'development',
