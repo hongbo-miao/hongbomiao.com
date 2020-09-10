eab921be2b04c13ae55484f90b6a81c3ef063367
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-11 00:06:34 +0800

    feat(env): add test env

--- bin/testCypressHeadlessCI.sh
+++ bin/testCypressHeadlessCI.sh
@@ -2,7 +2,13 @@
 
 set -e
 
+# https://create-react-app.dev/docs/adding-custom-environment-variables/
+# In create-react-app, when you run 'yarn build' to make a production bundle, it is always equal to 'production'.
+# You cannot override NODE_ENV manually.
+# So use development version .env as production version
+cp -n client/.env.development.local.example client/.env.production.local || true
+
 cp -n server/private/ssl/hongbomiao.crt.example server/private/ssl/hongbomiao.crt || true
 cp -n server/private/ssl/hongbomiao.key.example server/private/ssl/hongbomiao.key || true
 
-START_SERVER_AND_TEST_INSECURE=1 start-server-and-test 'cd server && yarn dev' https://localhost:5000 'pwd && ./node_modules/.bin/cypress run --config-file ./cypress/cypress.ci.json'
+START_SERVER_AND_TEST_INSECURE=1 start-server-and-test 'cd server && yarn dev:cypress' https://localhost:5000 './node_modules/.bin/cypress run --config-file ./cypress/cypress.ci.json'
