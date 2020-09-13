d17cb0839418423cfb9bcd5d0eb1b63a01c94d34
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-14 05:33:20 +0800

    chore(metric): different interval for dev and prod

--- server/src/log/utils/meter.ts
+++ server/src/log/utils/meter.ts
@@ -1,5 +1,6 @@
 import { CollectorMetricExporter } from '@opentelemetry/exporter-collector';
 import { MeterProvider } from '@opentelemetry/metrics';
+import isProduction from '../../shared/utils/isProduction';
 
 const metricExporter = new CollectorMetricExporter({
   serviceName: 'server-metric-service',
@@ -7,7 +8,7 @@ const metricExporter = new CollectorMetricExporter({
 
 const meter = new MeterProvider({
   exporter: metricExporter,
-  interval: 60 * 1000, // 60 seconds
+  interval: isProduction() ? 60 * 1000 : 5 * 1000, // ms
 }).getMeter('server-meter');
 
 export default meter;
