fd708dd77ed71fab1cf5867488103645273c1988
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-14 18:27:17 +0800

    chore(fluent-bit): update tag name

--- server/src/log/utils/logger.ts
+++ server/src/log/utils/logger.ts
@@ -14,8 +14,7 @@ const logger = createLogger({
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     new SentryTransport(Config.sentryOptions),
-
-    ...(isDevelopment() ? [new FluentTransport('hm-server-tag', Config.fluentBitConfig)] : []),
+    ...(isDevelopment() ? [new FluentTransport('hm-server', Config.fluentBitConfig)] : []),
   ],
 });
 
