c7d08cb2f8f799e6bc0238224143eb8146ad476c
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-14 18:34:44 +0800

    refactor(winston): different transports in different env

--- server/src/log/utils/logger.ts
+++ server/src/log/utils/logger.ts
@@ -3,19 +3,25 @@ import winston, { createLogger } from 'winston';
 import SentryTransport from 'winston-transport-sentry-node';
 import Config from '../../Config';
 import isDevelopment from '../../shared/utils/isDevelopment';
+import isProduction from '../../shared/utils/isProduction';
 
-const FluentTransport = fluentLogger.support.winstonTransport();
+const logger = createLogger();
 
-const logger = createLogger({
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(winston.format.colorize({ all: true })),
-    }),
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    new SentryTransport(Config.sentryOptions),
-    ...(isDevelopment() ? [new FluentTransport('hm-server', Config.fluentBitConfig)] : []),
-  ],
-});
+logger.add(
+  new winston.transports.Console({
+    format: winston.format.combine(winston.format.colorize({ all: true })),
+  })
+);
+
+if (isDevelopment()) {
+  const FluentTransport = fluentLogger.support.winstonTransport();
+  logger.add(new FluentTransport('hm-server', Config.fluentBitConfig));
+}
+
+if (isDevelopment() || isProduction()) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  logger.add(new SentryTransport(Config.sentryOptions));
+}
 
 export default logger;
