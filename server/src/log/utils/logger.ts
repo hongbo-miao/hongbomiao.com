2197ffb5e4644c0d36c01310de0f82c0cd4b6c6d
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-18 04:59:19 +0800

    chore(log): only use Sentry for logger

--- server/src/log/utils/logger.ts
+++ server/src/log/utils/logger.ts
@@ -1,8 +1,18 @@
 import winston from 'winston';
+import SentryTransport from 'winston-transport-sentry-node';
+import Config from '../../Config';
+import isDevelopment from '../../shared/utils/isDevelopment';
+import isProduction from '../../shared/utils/isProduction';
 import winstonTransports from './winstonTransports';
 
 const logger = winston.createLogger({
-  transports: winstonTransports,
+  transports: [
+    ...winstonTransports,
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    ...(isDevelopment() || isProduction() ? [new SentryTransport(Config.sentryOptions)] : []),
+  ],
 });
 
 export default logger;
