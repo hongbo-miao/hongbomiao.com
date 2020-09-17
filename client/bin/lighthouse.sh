b9c42d89e6c16b59387885bd1aad16110dea1356
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-18 06:10:07 +0800

    fix(lighthouse): stop sending data to production server

--- client/bin/lighthouse.sh
+++ client/bin/lighthouse.sh
@@ -4,8 +4,8 @@ set -e
 
 # https://create-react-app.dev/docs/adding-custom-environment-variables/
 # In create-react-app, when you run 'yarn build' to make a production bundle, it is always equal to 'production'.
-# You cannot override NODE_ENV manually.
-# So use development version .env as production version
-cp -n .env.development.local.example .env.production.local || true
+# So using REACT_APP_SERVER_DOMAIN=localhost and REACT_APP_SERVER_PORT=5000 in .env.production.local.example
+# to avoid sending data to production server
+cp -n .env.production.local.example .env.production.local || true
 
 lhci autorun --upload.target=temporary-public-storage
