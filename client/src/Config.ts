050ca626c540c55a4fd1f19ba3c064ac16804ced
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-19 17:08:08 +0800

    chore: remove LogRocket

--- client/src/Config.ts
+++ client/src/Config.ts
@@ -21,8 +21,6 @@ const Config = {
     traceURL: 'https://ingest.lightstep.com:443/api/v2/otel/trace',
   },
 
-  logRocketAppID: 'hongbo-miao/hongbomiao',
-
   sentryOptions: {
     dsn: 'https://2f46725646834700b4c2675abbc2da6a@o379185.ingest.sentry.io/5375232',
     environment: NODE_ENV,
