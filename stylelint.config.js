1e5035c384cb5264421b56f8e09427f25b2c8465
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-22 05:17:11 +0800

    feat(stylelint): add stylelint-config-recess-order

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
     'stylelint-config-standard', // stylelint-config-standard
+    'stylelint-config-recess-order', // stylelint-config-recess-order
     'stylelint-prettier/recommended', // stylelint-config-prettier. Turn off all rules that are unnecessary or might conflict with Prettier. Make sure to put it last, so it will override other configs.
   ],
 };
