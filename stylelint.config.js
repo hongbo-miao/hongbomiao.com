bab3af1a87fb46ba473b28662232d61d0fed4a8c
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-22 05:54:27 +0800

    feat(stylelint): add stylelint-high-performance-animation

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,4 +5,8 @@ module.exports = {
     'stylelint-a11y/recommended', // stylelint-a11y
     'stylelint-prettier/recommended', // stylelint-config-prettier. Turn off all rules that are unnecessary or might conflict with Prettier. Make sure to put it last, so it will override other configs.
   ],
+  plugins: ['stylelint-high-performance-animation'], // stylelint-high-performance-animation
+  rules: {
+    'plugin/no-low-performance-animation-properties': true, // stylelint-high-performance-animation
+  },
 };
