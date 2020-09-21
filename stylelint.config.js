4a1260e3e25eb58fef355a5e5434ab81846c8745
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-22 04:43:04 +0800

    feat(stylelint): extends stylelint-config-standard

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: ['stylelint-prettier/recommended'],
+  extends: [
+    'stylelint-config-standard', // stylelint-config-standard
+    'stylelint-prettier/recommended', // stylelint-config-prettier. Turn off all rules that are unnecessary or might conflict with Prettier. Make sure to put it last, so it will override other configs.
+  ],
 };
