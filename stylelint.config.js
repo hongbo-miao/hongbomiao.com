4e8504acd7e080c25082825f8035142a549a33ae
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-09-22 05:20:36 +0800

    feat(stylelint): add stylelint-a11y

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'stylelint-config-standard', // stylelint-config-standard
     'stylelint-config-recess-order', // stylelint-config-recess-order
+    'stylelint-a11y/recommended', // stylelint-a11y
     'stylelint-prettier/recommended', // stylelint-config-prettier. Turn off all rules that are unnecessary or might conflict with Prettier. Make sure to put it last, so it will override other configs.
   ],
 };
