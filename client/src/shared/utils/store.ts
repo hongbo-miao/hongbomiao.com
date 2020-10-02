4dafb216cb1e7ecb08fe27084a54c75ff5d7e0ba
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-10-02 20:44:07 +0800

    feat(sentry): integrate redux in sentry

--- client/src/shared/utils/store.ts
+++ client/src/shared/utils/store.ts
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { createEpicMiddleware } from 'redux-observable';
@@ -8,10 +9,13 @@ import graphQLFetch from './graphQLFetch';
 const epicMiddleware = createEpicMiddleware({
   dependencies: { fetchData: graphQLFetch },
 });
-
 const middlewares = [epicMiddleware];
 const enhancer = applyMiddleware(...middlewares);
-const store = createStore(rootReducer, composeWithDevTools(enhancer));
+
+// https://docs.sentry.io/platforms/javascript/guides/react/integrations/redux/
+const sentryEnhancer = Sentry.createReduxEnhancer();
+
+const store = createStore(rootReducer, composeWithDevTools(enhancer, sentryEnhancer));
 
 epicMiddleware.run(rootEpic);
 
