98f9d21a717d0dc9c5a1c3a2677fa7002d010782
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-10-04 02:35:22 +0800

    feat(analytics): turn on debug mode when dev

--- client/src/shared/utils/analytics.ts
+++ client/src/shared/utils/analytics.ts
@@ -2,10 +2,12 @@
 // @ts-ignore
 import googleTagManager from '@analytics/google-tag-manager';
 import Analytics from 'analytics';
+import isDevelopment from './isDevelopment';
 import isProduction from './isProduction';
 
 const analytics = Analytics({
   app: 'hm-client-analytics',
+  debug: isDevelopment(),
   plugins: isProduction()
     ? [
         googleTagManager({
