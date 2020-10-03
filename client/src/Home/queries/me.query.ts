c96ff38f50871d21bef058c56ca119abe4112e25
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-10-04 07:35:05 +0800

    feat(client): add GrpahQL fragment

--- client/src/Home/queries/me.query.ts
+++ client/src/Home/queries/me.query.ts
@@ -1,13 +1,16 @@
 import { print } from 'graphql';
 import gql from 'graphql-tag';
+import meNamesFragment from './meNames.fragment';
 
 const meQuery = print(gql`
   query Me {
     me {
       name
       bio
+      ...meNames
     }
   }
+  ${meNamesFragment}
 `);
 
 export default meQuery;
