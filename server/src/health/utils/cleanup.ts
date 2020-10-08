83f46f3ee9528f849e28132a94168847ecb3f44d
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-10-09 00:05:53 +0800

    perf(log): enable async for pino logger

--- server/src/health/utils/cleanup.ts
+++ server/src/health/utils/cleanup.ts
@@ -1,7 +1,12 @@
+import pino from 'pino';
 import logger from '../../log/utils/logger';
 
+const handler = pino.final(logger, (err, finalLogger) => {
+  finalLogger.info('Server is starting cleanup.');
+});
+
 const cleanup = async (): Promise<void> => {
-  logger.info('Server is starting cleanup.');
+  handler(null);
 };
 
 export default cleanup;
