fc6b1860ce14e74d2bc78c62a636782a36b46dda
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-10-14 07:15:11 +0800

    feat(log): add local report-to parser

--- server/src/log/middlewares/reportTo.middleware.ts
+++ server/src/log/middlewares/reportTo.middleware.ts
@@ -1,8 +1,11 @@
 import { RequestHandler } from 'express';
 import reportTo from 'report-to';
 import config from '../../config';
+import isProduction from '../../shared/utils/isProduction';
 
-const REPORT_TO_URL = config.reportURI.reportToURL;
+const REPORT_TO_URL = isProduction()
+  ? config.reportURI.reportToURL
+  : `https://${config.domain}:${config.port}/api/violation/report-to`;
 
 const reportToMiddleware = (reportToURL: string = REPORT_TO_URL): RequestHandler => {
   // Set header 'Report-To'
