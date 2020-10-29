51f3238688f026a4c6e2dcc07141e0518e1a247b
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-10-30 05:21:35 +0800

    feat(log): write circuit breaker status to prometheus

--- server/src/database/swapi/utils/fetchStarshipByIDWithBreaker.ts
+++ server/src/database/swapi/utils/fetchStarshipByIDWithBreaker.ts
@@ -4,6 +4,7 @@ import config from '../../../config';
 // eslint-disable-next-line import/no-cycle
 import starshipDataLoader from '../../../graphQL/dataLoaders/starship.dataLoader';
 import GraphQLStarship from '../../../graphQL/types/GraphQLStarship.type';
+import meter from '../../../log/utils/meter';
 import formatStarship from './formatStarship';
 
 const fetchStarshipByID = async (id: string): Promise<GraphQLStarship | null> => {
@@ -15,6 +16,17 @@ const breaker = new CircuitBreaker(fetchStarshipByID, {
   timeout: 3000, // If our function takes longer than 3s, trigger a failure.
   errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit.
   resetTimeout: 5000, // After 5s, try again.
+});
+
+const starshipBeakerCounter = meter.createCounter('starshipBeakerCounter');
+
+breaker.eventNames().forEach((eventName) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  breaker.on(eventName, () => {
+    const labels = { breakerName: breaker.name, eventName: String(eventName) };
+    starshipBeakerCounter.bind(labels).add(1);
+  });
 });
 
 const fetchStarshipByIDWithBreaker = async (id: string): Promise<GraphQLStarship | null> => {
