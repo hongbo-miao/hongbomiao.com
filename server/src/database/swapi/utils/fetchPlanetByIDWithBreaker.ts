2369f2988f484862520a6213a8a38fcc8f3c23bf
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-11-03 05:33:02 +0800

    refactor(breaker): add Breaker class

--- server/src/database/swapi/utils/fetchPlanetByIDWithBreaker.ts
+++ server/src/database/swapi/utils/fetchPlanetByIDWithBreaker.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import CircuitBreaker from 'opossum';
 import config from '../../../config';
 // eslint-disable-next-line import/no-cycle
 import planetDataLoader from '../../../graphQL/dataLoaders/planet.dataLoader';
 import GraphQLPlanet from '../../../graphQL/types/GraphQLPlanet.type';
-import meter from '../../../log/utils/meter';
+import Breaker from '../../../reliability/utils/Breaker';
 import formatPlanet from './formatPlanet';
 
 const fetchPlanetByID = async (id: string): Promise<GraphQLPlanet | null> => {
@@ -12,31 +11,10 @@ const fetchPlanetByID = async (id: string): Promise<GraphQLPlanet | null> => {
   return formatPlanet(id, swapiPlanet);
 };
 
-const breaker = new CircuitBreaker(fetchPlanetByID, {
-  timeout: 3000, // If our function takes longer than 3s, trigger a failure.
-  errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit.
-  resetTimeout: 5000, // After 5s, try again.
-});
-
-const planetBeakerCounter = meter.createCounter('planetBeakerCounter');
-
-breaker.eventNames().forEach((eventName) => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  breaker.on(eventName, () => {
-    const labels = { breakerName: breaker.name, eventName: String(eventName) };
-    planetBeakerCounter.bind(labels).add(1);
-  });
-});
+const breaker = new Breaker<GraphQLPlanet | null>('planet', fetchPlanetByID);
 
 const fetchPlanetByIDWithBreaker = async (id: string): Promise<GraphQLPlanet | null> => {
-  return breaker
-    .fire(id)
-    .then((res) => res)
-    .catch((err) => {
-      planetDataLoader.clear(id);
-      return err;
-    });
+  return breaker.fire(id, planetDataLoader);
 };
 
 export default fetchPlanetByIDWithBreaker;
--- server/src/database/swapi/utils/fetchStarshipByIDWithBreaker.ts
+++ server/src/database/swapi/utils/fetchStarshipByIDWithBreaker.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import CircuitBreaker from 'opossum';
 import config from '../../../config';
 // eslint-disable-next-line import/no-cycle
 import starshipDataLoader from '../../../graphQL/dataLoaders/starship.dataLoader';
 import GraphQLStarship from '../../../graphQL/types/GraphQLStarship.type';
-import meter from '../../../log/utils/meter';
+import Breaker from '../../../reliability/utils/Breaker';
 import formatStarship from './formatStarship';
 
 const fetchStarshipByID = async (id: string): Promise<GraphQLStarship | null> => {
@@ -12,31 +11,10 @@ const fetchStarshipByID = async (id: string): Promise<GraphQLStarship | null> =>
   return formatStarship(id, swapiStarship);
 };
 
-const breaker = new CircuitBreaker(fetchStarshipByID, {
-  timeout: 3000, // If our function takes longer than 3s, trigger a failure.
-  errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit.
-  resetTimeout: 5000, // After 5s, try again.
-});
-
-const starshipBeakerCounter = meter.createCounter('starshipBeakerCounter');
-
-breaker.eventNames().forEach((eventName) => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  breaker.on(eventName, () => {
-    const labels = { breakerName: breaker.name, eventName: String(eventName) };
-    starshipBeakerCounter.bind(labels).add(1);
-  });
-});
+const breaker = new Breaker<GraphQLStarship | null>('starship', fetchStarshipByID);
 
 const fetchStarshipByIDWithBreaker = async (id: string): Promise<GraphQLStarship | null> => {
-  return breaker
-    .fire(id)
-    .then((res) => res)
-    .catch((err) => {
-      starshipDataLoader.clear(id);
-      return err;
-    });
+  return breaker.fire(id, starshipDataLoader);
 };
 
 export default fetchStarshipByIDWithBreaker;
