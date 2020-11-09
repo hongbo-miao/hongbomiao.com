ac9dc4cbf5a75488dc6a7adcde2fb860e48b7b65
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-11-10 04:09:51 +0800

    fix(locust): ssl

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -5,13 +5,9 @@ class WebsiteUser(HttpUser):
   wait_time = between(5, 15)
 
   def on_start(self):
-    self.client.get('/favicon.png')
-    self.client.get('/favicon.ico')
-    self.client.get('/manifest.json')
-
-  @task
-  def index(self):
-    self.client.get('/')
+    self.client.get('favicon.png', verify=False)
+    self.client.get('favicon.ico', verify=False)
+    self.client.get('manifest.json', verify=False)
 
   @task
   def index(self):
