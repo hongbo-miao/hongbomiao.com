0e7c661b228245baa2c7f545af984127ff3c78d8
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-11-12 04:13:39 +0800

    feat(security): add csrf cookie and token

--- server/src/shared/routers/api.router.ts
+++ server/src/shared/routers/api.router.ts
@@ -1,13 +1,17 @@
+import bodyParser from 'body-parser';
 import timeout from 'connect-timeout';
+import csrf from 'csurf';
 import { Router } from 'express';
 import multer from 'multer';
 import violationRouter from '../../security/routers/violation.router';
 import uploadFile from '../../storage/controllers/uploadFile';
 
+const csrfProtection = csrf({ cookie: true });
+const parseForm = bodyParser.urlencoded({ extended: false });
 const upload = multer();
 
-const apiRouter = Router();
-apiRouter.use('/violation', timeout('5s'), violationRouter);
-apiRouter.use('/upload-file', upload.single('file'), uploadFile);
+const apiRouter = Router()
+  .use('/violation', timeout('5s'), violationRouter)
+  .use('/upload-file', parseForm, csrfProtection, upload.single('file'), uploadFile);
 
 export default apiRouter;
