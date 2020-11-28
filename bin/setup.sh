2d4a0db4fbbbd5f3b68a7dc78e7c8cd7da752005
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-11-29 05:32:46 +0800

    chore(app): update setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+# SSL
+cp -n web/private/ssl/hongbomiao.crt.example web/private/ssl/hongbomiao.crt || true
+cp -n web/private/ssl/hongbomiao.key.example web/private/ssl/hongbomiao.key || true
+
+cp -n server/private/ssl/hongbomiao.crt.example server/private/ssl/hongbomiao.crt || true
+cp -n server/private/ssl/hongbomiao.key.example server/private/ssl/hongbomiao.key || true
+
 # .env
 cp -n web/.env.development.local.example web/.env.development.local || true
 cp -n web/.env.production.local.example web/.env.production.local || true
@@ -11,13 +18,6 @@ cp -n server/.env.production.local.example server/.env.production.local || true
 
 cp -n docker/postgres/.env.development.local.example docker/postgres/.env.development.local || true
 cp -n docker/postgres/.env.production.local.example docker/postgres/.env.production.local || true
-
-# SSL
-cp -n web/private/ssl/hongbomiao.crt.example web/private/ssl/hongbomiao.crt || true
-cp -n web/private/ssl/hongbomiao.key.example web/private/ssl/hongbomiao.key || true
-
-cp -n server/private/ssl/hongbomiao.crt.example server/private/ssl/hongbomiao.crt || true
-cp -n server/private/ssl/hongbomiao.key.example server/private/ssl/hongbomiao.key || true
 
 # Install dependencies
 yarn install
