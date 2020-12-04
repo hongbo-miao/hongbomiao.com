b044aee0c0558d0df69211fbbf6096a578698f77
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-12-05 03:13:28 +0800

    build(app): add HTTP_PROTOCOL and WEBSOCKET_PROTOCOL

--- web/src/shared/utils/graphQLSubscriptionClient.ts
+++ web/src/shared/utils/graphQLSubscriptionClient.ts
@@ -1,9 +1,8 @@
 import { createClient } from 'graphql-ws';
 import config from '../../config';
-import isProduction from './isProduction';
 
 const graphQLSubscriptionClient = createClient({
-  url: isProduction() ? config.prodWebSocketGraphQLURL : config.devWebSocketGraphQLURL,
+  url: config.webSocketGraphQLURL,
 });
 
 export default graphQLSubscriptionClient;
