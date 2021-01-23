3eb91b53adc4b8d6a0e781cf8d0846f8caceffa3
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-01-24 01:14:11 +0800

    build(cypress): run all services in containers

--- cypress/cypress/integration/Home.test.ts
+++ cypress/cypress/integration/Home.test.ts
@@ -1,35 +1,35 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { skipOn } from '@cypress/skip-test';
-import meQuery from '../../../web/src/Home/queries/me.query';
-
 describe('Home', () => {
-  const { ci, domain, serverPort, webPort } = Cypress.env();
+  const { domain, serverPort, webPort } = Cypress.env();
 
   it('render Home', () => {
     cy.visit(`${domain}:${webPort}`);
     cy.contains('Making magic happen');
   });
 
-  skipOn(ci, () => {
-    it('receive GraphQL meQuery data', () => {
-      cy.request('POST', `${domain}:${serverPort}/graphql`, {
-        query: meQuery,
-      }).then((res) => {
-        expect(res).property('status').to.equal(200);
-        expect(res)
-          .property('body')
-          .to.eql({
-            data: {
-              me: {
-                name: 'Hongbo Miao',
-                firstName: 'Hongbo',
-                lastName: 'Miao',
-                bio: 'Making magic happen',
-              },
+  it('receive GraphQL meQuery data', () => {
+    const query = `
+      query Me {
+        me {
+          name
+          bio
+        }
+      }
+    `;
+
+    cy.request('POST', `${domain}:${serverPort}/graphql`, {
+      query,
+    }).then((res) => {
+      expect(res).property('status').to.equal(200);
+      expect(res)
+        .property('body')
+        .to.eql({
+          data: {
+            me: {
+              name: 'Hongbo Miao',
+              bio: 'Making magic happen',
             },
-          });
-      });
+          },
+        });
     });
   });
 });
