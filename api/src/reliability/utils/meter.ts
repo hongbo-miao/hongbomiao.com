c9d0d99ef9ebbbab5ab56a5638905c7878fbb957
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-01-27 00:42:07 +0800

    refactor(app): rename server to api

--- api/src/reliability/utils/meter.ts
+++ api/src/reliability/utils/meter.ts
@@ -3,7 +3,7 @@ import { MeterProvider } from '@opentelemetry/metrics';
 import isDevelopment from '../../shared/utils/isDevelopment';
 
 const metricExporter = new CollectorMetricExporter({
-  serviceName: 'hm-server-metric-service',
+  serviceName: 'hm-api-metric-service',
 });
 
 const metricProvider = isDevelopment()
@@ -13,6 +13,6 @@ const metricProvider = isDevelopment()
     })
   : new MeterProvider();
 
-const meter = metricProvider.getMeter('hm-server-meter');
+const meter = metricProvider.getMeter('hm-api-meter');
 
 export default meter;
