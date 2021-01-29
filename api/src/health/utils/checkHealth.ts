668b910ac179ac5ecd81f8a17af7c7116b5f1af3
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-01-30 07:55:13 +0800

    feat(api): check postgres health

--- api/src/health/utils/checkHealth.ts
+++ api/src/health/utils/checkHealth.ts
@@ -1,9 +1,28 @@
-type Health = {
-  api: string;
-};
+import checkAPIHealth from './checkAPIHealth';
+import checkPostgresHealth from './checkPostgresHealth';
+
+type HealthName = 'api' | 'postgres';
+type HealthStatus = 'ok' | 'error';
+type Health = Record<HealthName, HealthStatus>;
 
 const checkHealth = async (): Promise<Health> => {
-  return Promise.resolve({ api: 'ok' });
+  const services = [
+    { name: 'api', check: checkAPIHealth },
+    { name: 'postgres', check: checkPostgresHealth },
+  ];
+
+  const results = await Promise.allSettled(services.map((service) => service.check()));
+
+  let health = {};
+  results.forEach((res, idx) => {
+    health = {
+      ...health,
+      // eslint-disable-next-line security/detect-object-injection
+      [services[idx].name]: res.status === 'fulfilled' ? 'ok' : 'error',
+    };
+  });
+
+  return Promise.resolve(<Health>health);
 };
 
 export default checkHealth;
