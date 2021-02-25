4ef1ca15fe37d2ea0747b92b4a3a775011f96734
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-02-26 02:16:08 +0800

    fix(deps): update dependency eslint-config-prettier to v8

--- cypress/.eslintrc.js
+++ cypress/.eslintrc.js
@@ -26,7 +26,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended', // @typescript-eslint/eslint-plugin
     'plugin:cypress/recommended', // eslint-plugin-cypress
     'prettier', // eslint-plugin-prettier. Make sure to put it last in the extends array, so it gets the chance to override other configs
-    'prettier/@typescript-eslint', // eslint-config-prettier. Disables ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // eslint-plugin-prettier. Exposes a "recommended" configuration that configures both eslint-plugin-prettier and eslint-config-prettier in a single step. Make sure this is always the last configuration in the extends array
   ],
   settings: {
