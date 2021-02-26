858a864ff7b22c6e6948a3c9e6a543f29da09424
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-02-27 06:57:22 +0800

    perf(mobile): async load fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,26 @@
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
 import AppLoading from 'expo-app-loading';
-import { useFonts } from 'expo-font';
+import * as Font from 'expo-font';
 import React from 'react';
 import HmHome from './Home/components/Home';
 
 const App: React.FC = () => {
-  const [isFontLoaded] = useFonts({
-    // eslint-disable-next-line global-require
-    NeoSansProRegular: require('./assets/fonts/NeoSansProRegular.otf'),
-    // eslint-disable-next-line global-require
-    NeoSansProBold: require('./assets/fonts/NeoSansProBold.otf'),
-  });
+  const [isFontLoaded, setIsFontLoaded] = React.useState(() => false);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      // eslint-disable-next-line global-require
+      NeoSansProRegular: require('./assets/fonts/NeoSansProRegular.otf'),
+      // eslint-disable-next-line global-require
+      NeoSansProBold: require('./assets/fonts/NeoSansProBold.otf'),
+    });
+    setIsFontLoaded(true);
+  };
+
+  React.useEffect(() => {
+    loadFonts();
+  }, []);
 
   if (!isFontLoaded) {
     return <AppLoading />;
