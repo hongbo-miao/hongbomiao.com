3d84bcab24ab4746599f77291cda2e9585dfe7d2
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-02-27 08:18:49 +0800

    test(mobile): Home component

---
+++ mobile/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+  ],
+};
