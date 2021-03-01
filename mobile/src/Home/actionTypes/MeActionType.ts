2eac2125b04208f772cf527581490043647a41ed
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-03-02 01:30:29 +0800

    feat(mobile): add redux and graphql

---
+++ mobile/src/Home/actionTypes/MeActionType.ts
@@ -0,0 +1,11 @@
+const QUERY_ME = '[Me] Get Me';
+const QUERY_ME_SUCCEED = '[Me] Get Me Succeed';
+const QUERY_ME_FAILED = '[Me] Get Me Failed';
+
+const MeActionType = {
+  QUERY_ME,
+  QUERY_ME_SUCCEED,
+  QUERY_ME_FAILED,
+};
+
+export default MeActionType;
---
+++ mobile/src/config.ts
@@ -0,0 +1,9 @@
+type Config = {
+  graphQLURL: string;
+};
+
+const config: Config = {
+  graphQLURL: 'http://localhost:5000/graphql',
+};
+
+export default config;
