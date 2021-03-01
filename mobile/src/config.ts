0b72e4bba60ff766a8718d6b4a1f86c4216f820a
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-03-02 06:56:29 +0800

    feat(mobile): read env

--- mobile/src/config.ts
+++ mobile/src/config.ts
@@ -1,9 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line import/no-unresolved
+import { API_URL } from '@env';
+
+if (API_URL == null || API_URL === '') {
+  throw new Error('Failed to read API_URL.');
+}
+
 type Config = {
   graphQLURL: string;
 };
 
 const config: Config = {
-  graphQLURL: 'http://localhost:5000/graphql',
+  graphQLURL: `${API_URL}/graphql`,
 };
 
 export default config;
