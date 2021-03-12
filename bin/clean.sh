dedf6af2d545ce9a25d499b23a564b6cc7997b0c
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-03-13 00:57:04 +0800

    chore(app): update clean script

--- bin/clean.sh
+++ bin/clean.sh
@@ -8,20 +8,27 @@ rm -rf node_modules
 
 # Web
 cd web
+rm -rf .lighthouseci
 rm -rf build
-rm -rf tmp
 rm -rf coverage
 rm -rf node_modules
+rm -rf tmp
 rm -f public/sitemap.xml
 
 # Mobile
 cd ../mobile
+rm -rf .expo
+rm -rf coverage
 rm -rf node_modules
 
 # API
 cd ../api
 rm -rf .clinic
 rm -rf build
-rm -rf dist
 rm -rf coverage
+rm -rf dist
+rm -rf node_modules
+
+# Cypress
+cd ../cypress
 rm -rf node_modules
