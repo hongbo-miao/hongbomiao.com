5d91f091f33c0a7a3a2f3ad5af16488fe7b6c20a
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-03-14 02:17:14 +0800

    style(code): format by black

--- neural-network/gnn.py
+++ neural-network/gnn.py
@@ -1,24 +1,31 @@
 import torch
-from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool, GlobalAttention, Set2Set
+from torch_geometric.nn import (
+    global_add_pool,
+    global_mean_pool,
+    global_max_pool,
+    GlobalAttention,
+    Set2Set,
+)
 
 from conv import GNN_node, GNN_node_Virtualnode
 
 
 class GNN(torch.nn.Module):
     def __init__(
-            self,
-            num_tasks,
-            num_layer=5,
-            emb_dim=300,
-            gnn_type='gin',
-            virtual_node=True,
-            residual=False,
-            drop_ratio=0.5,
-            JK='last',
-            graph_pooling='mean'):
+        self,
+        num_tasks,
+        num_layer=5,
+        emb_dim=300,
+        gnn_type="gin",
+        virtual_node=True,
+        residual=False,
+        drop_ratio=0.5,
+        JK="last",
+        graph_pooling="mean",
+    ):
         """
-            num_tasks (int): number of labels to be predicted
-            virtual_node (bool): whether to add virtual node or not
+        num_tasks (int): number of labels to be predicted
+        virtual_node (bool): whether to add virtual node or not
         """
 
         super(GNN, self).__init__()
@@ -31,7 +38,7 @@ class GNN(torch.nn.Module):
         self.graph_pooling = graph_pooling
 
         if self.num_layer < 2:
-            raise ValueError('Number of GNN layers must be greater than 1.')
+            raise ValueError("Number of GNN layers must be greater than 1.")
 
         # GNN to generate node embeddings
         if virtual_node:
@@ -41,7 +48,8 @@ class GNN(torch.nn.Module):
                 JK=JK,
                 drop_ratio=drop_ratio,
                 residual=residual,
-                gnn_type=gnn_type)
+                gnn_type=gnn_type,
+            )
         else:
             self.gnn_node = GNN_node(
                 num_layer,
@@ -49,28 +57,31 @@ class GNN(torch.nn.Module):
                 JK=JK,
                 drop_ratio=drop_ratio,
                 residual=residual,
-                gnn_type=gnn_type)
+                gnn_type=gnn_type,
+            )
 
         # Pooling function to generate whole-graph embeddings
-        if self.graph_pooling == 'sum':
+        if self.graph_pooling == "sum":
             self.pool = global_add_pool
-        elif self.graph_pooling == 'mean':
+        elif self.graph_pooling == "mean":
             self.pool = global_mean_pool
-        elif self.graph_pooling == 'max':
+        elif self.graph_pooling == "max":
             self.pool = global_max_pool
-        elif self.graph_pooling == 'attention':
+        elif self.graph_pooling == "attention":
             self.pool = GlobalAttention(
                 gate_nn=torch.nn.Sequential(
                     torch.nn.Linear(emb_dim, 2 * emb_dim),
                     torch.nn.BatchNorm1d(2 * emb_dim),
                     torch.nn.ReLU(),
-                    torch.nn.Linear(2 * emb_dim, 1)))
-        elif self.graph_pooling == 'set2set':
+                    torch.nn.Linear(2 * emb_dim, 1),
+                )
+            )
+        elif self.graph_pooling == "set2set":
             self.pool = Set2Set(emb_dim, processing_steps=2)
         else:
-            raise ValueError('Invalid graph pooling type.')
+            raise ValueError("Invalid graph pooling type.")
 
-        if graph_pooling == 'set2set':
+        if graph_pooling == "set2set":
             self.graph_pred_linear = torch.nn.Linear(2 * self.emb_dim, self.num_tasks)
         else:
             self.graph_pred_linear = torch.nn.Linear(self.emb_dim, self.num_tasks)
@@ -81,5 +92,5 @@ class GNN(torch.nn.Module):
         return self.graph_pred_linear(h_graph)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     GNN(num_tasks=10)
