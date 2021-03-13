c9bbf874000a01795813e1215569bd14a0debea8
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-03-14 01:13:00 +0800

    style(code): formatting

--- neural-network/main_pyg.py
+++ neural-network/main_pyg.py
@@ -1,24 +1,21 @@
-import torch
-from torch_geometric.data import DataLoader
-import torch.optim as optim
-import torch.nn.functional as F
-from gnn import GNN
-
-from tqdm import tqdm
 import argparse
-import time
 import numpy as np
-
-### importing OGB
+import torch
+import torch.optim as optim
 from ogb.graphproppred import PygGraphPropPredDataset, Evaluator
+from torch_geometric.data import DataLoader
+from tqdm import tqdm
+
+from gnn import GNN
 
 cls_criterion = torch.nn.BCEWithLogitsLoss()
 reg_criterion = torch.nn.MSELoss()
 
+
 def train(model, device, loader, optimizer, task_type):
     model.train()
 
-    for step, batch in enumerate(tqdm(loader, desc="Iteration")):
+    for step, batch in enumerate(tqdm(loader, desc='Iteration')):
         batch = batch.to(device)
 
         if batch.x.shape[0] == 1 or batch.batch[-1] == 0:
@@ -26,21 +23,22 @@ def train(model, device, loader, optimizer, task_type):
         else:
             pred = model(batch)
             optimizer.zero_grad()
-            ## ignore nan targets (unlabeled) when computing training loss.
+            # ignore nan targets (unlabeled) when computing training loss.
             is_labeled = batch.y == batch.y
-            if "classification" in task_type: 
+            if 'classification' in task_type:
                 loss = cls_criterion(pred.to(torch.float32)[is_labeled], batch.y.to(torch.float32)[is_labeled])
             else:
                 loss = reg_criterion(pred.to(torch.float32)[is_labeled], batch.y.to(torch.float32)[is_labeled])
             loss.backward()
             optimizer.step()
 
+
 def eval(model, device, loader, evaluator):
     model.eval()
     y_true = []
     y_pred = []
 
-    for step, batch in enumerate(tqdm(loader, desc="Iteration")):
+    for step, batch in enumerate(tqdm(loader, desc='Iteration')):
         batch = batch.to(device)
 
         if batch.x.shape[0] == 1:
@@ -52,10 +50,10 @@ def eval(model, device, loader, evaluator):
             y_true.append(batch.y.view(pred.shape).detach().cpu())
             y_pred.append(pred.detach().cpu())
 
-    y_true = torch.cat(y_true, dim = 0).numpy()
-    y_pred = torch.cat(y_pred, dim = 0).numpy()
+    y_true = torch.cat(y_true, dim=0).numpy()
+    y_pred = torch.cat(y_pred, dim=0).numpy()
 
-    input_dict = {"y_true": y_true, "y_pred": y_pred}
+    input_dict = {'y_true': y_true, 'y_pred': y_pred}
 
     return evaluator.eval(input_dict)
 
@@ -63,61 +61,129 @@ def eval(model, device, loader, evaluator):
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='GNN baselines on ogbgmol* data with Pytorch Geometrics')
-    parser.add_argument('--device', type=int, default=0,
-                        help='which gpu to use if any (default: 0)')
-    parser.add_argument('--gnn', type=str, default='gin-virtual',
-                        help='GNN gin, gin-virtual, or gcn, or gcn-virtual (default: gin-virtual)')
-    parser.add_argument('--drop_ratio', type=float, default=0.5,
-                        help='dropout ratio (default: 0.5)')
-    parser.add_argument('--num_layer', type=int, default=5,
-                        help='number of GNN message passing layers (default: 5)')
-    parser.add_argument('--emb_dim', type=int, default=300,
-                        help='dimensionality of hidden units in GNNs (default: 300)')
-    parser.add_argument('--batch_size', type=int, default=32,
-                        help='input batch size for training (default: 32)')
-    parser.add_argument('--epochs', type=int, default=100,
-                        help='number of epochs to train (default: 100)')
-    parser.add_argument('--num_workers', type=int, default=0,
-                        help='number of workers (default: 0)')
-    parser.add_argument('--dataset', type=str, default="ogbg-molhiv",
-                        help='dataset name (default: ogbg-molhiv)')
-
-    parser.add_argument('--feature', type=str, default="full",
-                        help='full feature or simple feature')
-    parser.add_argument('--filename', type=str, default="",
-                        help='filename to output result (default: )')
+    parser.add_argument(
+        '--device',
+        type=int,
+        default=0,
+        help='which gpu to use if any (default: 0)')
+    parser.add_argument(
+        '--gnn',
+        type=str,
+        default='gin-virtual',
+        help='GNN gin, gin-virtual, or gcn, or gcn-virtual (default: gin-virtual)')
+    parser.add_argument(
+        '--drop_ratio',
+        type=float,
+        default=0.5,
+        help='dropout ratio (default: 0.5)')
+    parser.add_argument(
+        '--num_layer',
+        type=int,
+        default=5,
+        help='number of GNN message passing layers (default: 5)')
+    parser.add_argument(
+        '--emb_dim',
+        type=int,
+        default=300,
+        help='dimensionality of hidden units in GNNs (default: 300)')
+    parser.add_argument(
+        '--batch_size',
+        type=int,
+        default=32,
+        help='input batch size for training (default: 32)')
+    parser.add_argument(
+        '--epochs',
+        type=int,
+        default=100,
+        help='number of epochs to train (default: 100)')
+    parser.add_argument(
+        '--num_workers',
+        type=int,
+        default=0,
+        help='number of workers (default: 0)')
+    parser.add_argument(
+        '--dataset',
+        type=str,
+        default='ogbg-molhiv',
+        help='dataset name (default: ogbg-molhiv)')
+    parser.add_argument(
+        '--feature',
+        type=str,
+        default='full',
+        help='full feature or simple feature')
+    parser.add_argument(
+        '--filename',
+        type=str,
+        default='',
+        help='filename to output result (default: )')
     args = parser.parse_args()
 
-    device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
+    device = torch.device('cuda:' + str(args.device)) if torch.cuda.is_available() else torch.device('cpu')
 
-    ### automatic dataloading and splitting
-    dataset = PygGraphPropPredDataset(name = args.dataset)
+    # automatic dataloading and splitting
+    dataset = PygGraphPropPredDataset(name=args.dataset)
 
     if args.feature == 'full':
-        pass 
+        pass
     elif args.feature == 'simple':
         print('using simple feature')
         # only retain the top two node/edge features
-        dataset.data.x = dataset.data.x[:,:2]
-        dataset.data.edge_attr = dataset.data.edge_attr[:,:2]
+        dataset.data.x = dataset.data.x[:, :2]
+        dataset.data.edge_attr = dataset.data.edge_attr[:, :2]
 
     split_idx = dataset.get_idx_split()
 
-    ### automatic evaluator. takes dataset name as input
+    # automatic evaluator. takes dataset name as input
     evaluator = Evaluator(args.dataset)
 
-    train_loader = DataLoader(dataset[split_idx["train"]], batch_size=args.batch_size, shuffle=True, num_workers = args.num_workers)
-    valid_loader = DataLoader(dataset[split_idx["valid"]], batch_size=args.batch_size, shuffle=False, num_workers = args.num_workers)
-    test_loader = DataLoader(dataset[split_idx["test"]], batch_size=args.batch_size, shuffle=False, num_workers = args.num_workers)
+    train_loader = DataLoader(
+        dataset[split_idx['train']],
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.num_workers)
+    valid_loader = DataLoader(
+        dataset[split_idx['valid']],
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.num_workers)
+    test_loader = DataLoader(
+        dataset[split_idx['test']],
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.num_workers)
 
     if args.gnn == 'gin':
-        model = GNN(gnn_type = 'gin', num_tasks = dataset.num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False).to(device)
+        model = GNN(
+            gnn_type='gin',
+            num_tasks=dataset.num_tasks,
+            num_layer=args.num_layer,
+            emb_dim=args.emb_dim,
+            drop_ratio=args.drop_ratio,
+            virtual_node=False).to(device)
     elif args.gnn == 'gin-virtual':
-        model = GNN(gnn_type = 'gin', num_tasks = dataset.num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True).to(device)
+        model = GNN(
+            gnn_type='gin',
+            num_tasks=dataset.num_tasks,
+            num_layer=args.num_layer,
+            emb_dim=args.emb_dim,
+            drop_ratio=args.drop_ratio,
+            virtual_node=True).to(device)
     elif args.gnn == 'gcn':
-        model = GNN(gnn_type = 'gcn', num_tasks = dataset.num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False).to(device)
+        model = GNN(
+            gnn_type='gcn',
+            num_tasks=dataset.num_tasks,
+            num_layer=args.num_layer,
+            emb_dim=args.emb_dim,
+            drop_ratio=args.drop_ratio,
+            virtual_node=False).to(device)
     elif args.gnn == 'gcn-virtual':
-        model = GNN(gnn_type = 'gcn', num_tasks = dataset.num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True).to(device)
+        model = GNN(
+            gnn_type='gcn',
+            num_tasks=dataset.num_tasks,
+            num_layer=args.num_layer,
+            emb_dim=args.emb_dim,
+            drop_ratio=args.drop_ratio,
+            virtual_node=True).to(device)
     else:
         raise ValueError('Invalid GNN type')
 
@@ -128,7 +194,7 @@ def main():
     train_curve = []
 
     for epoch in range(1, args.epochs + 1):
-        print("=====Epoch {}".format(epoch))
+        print('=====Epoch {}'.format(epoch))
         print('Training...')
         train(model, device, train_loader, optimizer, dataset.task_type)
 
@@ -155,8 +221,13 @@ def main():
     print('Test score: {}'.format(test_curve[best_val_epoch]))
 
     if not args.filename == '':
-        torch.save({'Val': valid_curve[best_val_epoch], 'Test': test_curve[best_val_epoch], 'Train': train_curve[best_val_epoch], 'BestTrain': best_train}, args.filename)
+        torch.save(
+            {'Val': valid_curve[best_val_epoch],
+             'Test': test_curve[best_val_epoch],
+             'Train': train_curve[best_val_epoch],
+             'BestTrain': best_train},
+            args.filename)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
