b24f0885aca6e59a9e714c8b315a56b80a243897
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-03-15 01:23:13 +0800

    refactor(neural): args

--- neural-network/main.py
+++ neural-network/main.py
@@ -1,4 +1,3 @@
-import argparse
 import numpy as np
 import torch
 import torch.optim as optim
@@ -8,6 +7,7 @@ from torch_geometric.data import DataLoader
 from tqdm import tqdm
 
 from gnn import GNN
+from args import get_args
 
 cls_criterion = torch.nn.BCEWithLogitsLoss()
 reg_criterion = torch.nn.MSELoss()
@@ -67,64 +67,7 @@ def eval(model, device, loader, evaluator):
 
 def main():
     # Training settings
-    parser = argparse.ArgumentParser(
-        description="GNN baselines on ogbgmol* data with Pytorch Geometrics"
-    )
-    parser.add_argument(
-        "--device", type=int, default=0, help="which gpu to use if any (default: 0)"
-    )
-    parser.add_argument(
-        "--gnn",
-        type=str,
-        default="gin-virtual",
-        help="GNN gin, gin-virtual, or gcn, or gcn-virtual (default: gin-virtual)",
-    )
-    parser.add_argument(
-        "--lr", type=float, default=0.001, help="dropout ratio (default: 0.5)"
-    )
-    parser.add_argument(
-        "--drop_ratio", type=float, default=0.5, help="dropout ratio (default: 0.5)"
-    )
-    parser.add_argument(
-        "--num_layer",
-        type=int,
-        default=5,
-        help="number of GNN message passing layers (default: 5)",
-    )
-    parser.add_argument(
-        "--emb_dim",
-        type=int,
-        default=300,
-        help="dimensionality of hidden units in GNNs (default: 300)",
-    )
-    parser.add_argument(
-        "--batch_size",
-        type=int,
-        default=32,
-        help="input batch size for training (default: 32)",
-    )
-    parser.add_argument(
-        "--epochs",
-        type=int,
-        default=100,
-        help="number of epochs to train (default: 100)",
-    )
-    parser.add_argument(
-        "--num_workers", type=int, default=0, help="number of workers (default: 0)"
-    )
-    parser.add_argument(
-        "--dataset",
-        type=str,
-        default="ogbg-molhiv",
-        help="dataset name (default: ogbg-molhiv)",
-    )
-    parser.add_argument(
-        "--feature", type=str, default="full", help="full feature or simple feature"
-    )
-    parser.add_argument(
-        "--filename", type=str, default="", help="filename to output result (default: )"
-    )
-    args = parser.parse_args()
+    args = get_args()
 
     device = (
         torch.device("cuda:" + str(args.device))
