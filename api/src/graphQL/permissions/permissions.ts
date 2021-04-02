dfbdad40362703ca6910be732e23814581d87d2c
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-04-03 04:54:48 +0800

    chore(shield): expose errors in dev mode

--- api/src/graphQL/permissions/permissions.ts
+++ api/src/graphQL/permissions/permissions.ts
@@ -1,24 +1,28 @@
 import { rule, shield } from 'graphql-shield';
+import isDevelopment from '../../shared/utils/isDevelopment';
 
 const isAuthenticated = rule()(async (parentValue, args, context) => {
   const { myId } = context;
   return myId != null;
 });
 
-const permissions = shield({
-  Query: {
-    fibonacci: isAuthenticated,
-    user: isAuthenticated,
-    users: isAuthenticated,
-    planet: isAuthenticated,
-    planets: isAuthenticated,
-    starship: isAuthenticated,
-    starships: isAuthenticated,
+const permissions = shield(
+  {
+    Query: {
+      fibonacci: isAuthenticated,
+      user: isAuthenticated,
+      users: isAuthenticated,
+      planet: isAuthenticated,
+      planets: isAuthenticated,
+      starship: isAuthenticated,
+      starships: isAuthenticated,
+    },
+    Mutation: {
+      updateName: isAuthenticated,
+      uploadFile: isAuthenticated,
+    },
   },
-  Mutation: {
-    updateName: isAuthenticated,
-    uploadFile: isAuthenticated,
-  },
-});
+  { debug: isDevelopment() },
+);
 
 export default permissions;
