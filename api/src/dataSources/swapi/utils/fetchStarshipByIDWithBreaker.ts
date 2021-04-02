b2d9b965aa0ee26525ab9edea43edf14bf3ccb92
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-04-03 06:14:28 +0800

    refactor(api): dataloader

--- api/src/dataSources/swapi/utils/fetchStarshipByIDWithBreaker.ts
+++ api/src/dataSources/swapi/utils/fetchStarshipByIDWithBreaker.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 import config from '../../../config';
-// eslint-disable-next-line import/no-cycle
-import starshipDataLoader from '../../../graphQL/dataLoaders/starshipDataLoader';
 import GraphQLStarship from '../../../graphQL/types/GraphQLStarship';
 import createCircuitBreaker from '../../../reliability/utils/createCircuitBreaker';
 import formatStarship from './formatStarship';
@@ -14,13 +12,7 @@ const fetchStarshipByID = async (id: string): Promise<GraphQLStarship | null> =>
 const breaker = createCircuitBreaker(fetchStarshipByID);
 
 const fetchStarshipByIDWithBreaker = async (id: string): Promise<GraphQLStarship | null> => {
-  return breaker
-    .fire(id)
-    .then((res) => res)
-    .catch((err) => {
-      starshipDataLoader.clear(id);
-      return err;
-    });
+  return breaker.fire(id);
 };
 
 export default fetchStarshipByIDWithBreaker;
--- api/src/graphQL/dataLoaders/starshipDataLoader.ts
+++ api/src/graphQL/dataLoaders/starshipDataLoader.ts
@@ -1,12 +1,10 @@
 import DataLoader from 'dataloader';
-// eslint-disable-next-line import/no-cycle
 import fetchStarshipByIDWithBreaker from '../../dataSources/swapi/utils/fetchStarshipByIDWithBreaker';
 import GraphQLStarship from '../types/GraphQLStarship';
 
-const batchGetStarships = async (ids: ReadonlyArray<string>): Promise<(GraphQLStarship | null)[]> => {
-  return Promise.all(ids.map((id) => fetchStarshipByIDWithBreaker(id)));
-};
-
-const starshipDataLoader = new DataLoader(batchGetStarships);
+const starshipDataLoader = (): DataLoader<string, GraphQLStarship | null> =>
+  new DataLoader(async (ids: ReadonlyArray<string>) => {
+    return Promise.all(ids.map((id) => fetchStarshipByIDWithBreaker(id)));
+  });
 
 export default starshipDataLoader;
