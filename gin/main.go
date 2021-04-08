b167d6c0b89d5b92423d981035a6ab75a481cc4f
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-04-08 19:27:13 +0800

    feat(gin): serve static files

--- gin/main.go
+++ gin/main.go
@@ -1,13 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.Use(static.Serve("/", static.LocalFile("./public", true)))
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	router.Run()
 }
