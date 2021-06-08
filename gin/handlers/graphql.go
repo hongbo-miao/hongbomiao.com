c3501437701013b12d3a2d4f3c9713c833356116
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-06-08 18:04:43 +0800

    feat(gin): add graphql

---
+++ gin/handlers/graphql.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"github.com/Hongbo-Miao/hongbomiao.com/gin/schemas"
+	"github.com/gin-gonic/gin"
+	"github.com/graphql-go/handler"
+)
+
+func GraphQLHandler() gin.HandlerFunc {
+	h := handler.New(&handler.Config{
+		Schema:   &schemas.Schema,
+		Pretty:   true,
+		GraphiQL: false,
+	})
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
