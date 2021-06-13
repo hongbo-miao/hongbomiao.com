42f4d493313eeb96fedc55780a60748ceba0b14c
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-06-14 04:27:07 +0800

    feat(gin): read from .env

--- api-go/pkg/utils/init_config.go
+++ api-go/pkg/utils/init_config.go
@@ -1,15 +1,24 @@
 package utils
 
 import (
+	"github.com/joho/godotenv"
 	"go.uber.org/zap"
+	"os"
 )
 
 type Config struct {
 	Logger *zap.Logger
+	Port   string
 }
 
-func InitConfig() Config {
+func InitConfig() *Config {
 	var config Config
 	config.Logger = InitLogger()
-	return config
+
+	err := godotenv.Load()
+	if err != nil {
+		config.Logger.Fatal("Error loading .env file")
+	}
+	config.Port = os.Getenv("PORT")
+	return &config
 }
