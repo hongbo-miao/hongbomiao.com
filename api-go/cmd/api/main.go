6ba5af25dbf88bd3a66f15fa787e79ef9d7340e5
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-06-14 04:09:28 +0800

    refactor(gin): config

--- api-go/cmd/api/main.go
+++ api-go/cmd/api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/Hongbo-Miao/hongbomiao.com/api-go/pkg/config"
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/pkg/controllers"
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/pkg/handlers"
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/pkg/utils"
@@ -10,12 +9,10 @@ import (
 	"go.uber.org/zap"
 )
 
-var app config.AppConfig
-
 func main() {
 	const portNumber = ":8080"
 
-	app.Logger = utils.InitLogger()
+	var config = utils.InitConfig()
 
 	r := gin.Default()
 	r.Use(static.Serve("/", static.LocalFile("./web", true)))
@@ -23,7 +20,7 @@ func main() {
 	r.POST("/graphql", handlers.GraphQLHandler())
 	_ = r.Run(portNumber)
 
-	app.Logger.Info("portNumber",
+	config.Logger.Info("portNumber",
 		// Structured context as strongly typed Field values.
 		zap.String("portNumber", portNumber),
 	)
