8d6e328ef5534ae3d7fb4c01ec6581069508fa65
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-07-03 16:53:27 -0700

    feat(gin): use opencensus to trace

--- api-go/cmd/api/main.go
+++ api-go/cmd/api/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"contrib.go.opencensus.io/exporter/ocagent"
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/routes"
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/utils"
 	"github.com/rs/zerolog/log"
 	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/trace"
 	"net/http"
+	"time"
 )
 
 func main() {
 	utils.InitLogger()
 	var config = utils.GetConfig()
 	log.Info().Str("env", config.Env).Str("port", config.Port).Msg("main")
+
+	oce, err := ocagent.NewExporter(
+		ocagent.WithInsecure(),
+		ocagent.WithReconnectionPeriod(5*time.Second),
+		ocagent.WithAddress(config.OpenCensusAgentHost+":"+config.OpenCensusAgentPort),
+		ocagent.WithServiceName("api-go"))
+	if err != nil {
+		log.Error().Err(err).Msg("ocagent.NewExporter")
+	}
+	trace.RegisterExporter(oce)
 
 	r := routes.SetupRouter()
 	_ = http.ListenAndServe(
--- api-go/internal/routes/setup_router.go
+++ api-go/internal/routes/setup_router.go
@@ -1,28 +1,12 @@
 package routes
 
 import (
-	"contrib.go.opencensus.io/exporter/ocagent"
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/controllers"
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/handlers"
-	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/utils"
 	"github.com/gin-gonic/gin"
-	"github.com/rs/zerolog/log"
-	"go.opencensus.io/trace"
-	"time"
 )
 
 func SetupRouter() *gin.Engine {
-	var config = utils.GetConfig()
-	oce, err := ocagent.NewExporter(
-		ocagent.WithInsecure(),
-		ocagent.WithReconnectionPeriod(5*time.Second),
-		ocagent.WithAddress(config.OpenCensusAgentHost+":"+config.OpenCensusAgentPort),
-		ocagent.WithServiceName("api-go"))
-	if err != nil {
-		log.Error().Err(err).Msg("ocagent.NewExporter")
-	}
-	trace.RegisterExporter(oce)
-
 	r := gin.Default()
 	r.POST("/graphql", handlers.GraphQLHandler())
 	r.GET("/", controllers.Health)
