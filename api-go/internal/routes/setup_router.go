575fae72a25d05e231e433e2d110805334afaeb0
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-07-09 07:35:16 -0700

    feat(gin): add opentelemetry

--- api-go/internal/routes/setup_router.go
+++ api-go/internal/routes/setup_router.go
@@ -4,10 +4,12 @@ import (
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/controllers"
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/handlers"
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(otelgin.Middleware("hm-api-go"))
 	r.POST("/graphql", handlers.GraphQLHandler())
 	r.GET("/", controllers.Health)
 	return r
