466d435578fce76101d6bd93748e42993005a68c
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-07-21 15:24:45 -0700

    chore(opa): change policy in opa to same one with opal

--- api-go/internal/utils/get_opa_decision.go
+++ api-go/internal/utils/get_opa_decision.go
@@ -7,8 +7,8 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-var policyPath = "policy/rbac.authz.rego"
-var defaultQuery = "x = data.rbac.authz.allow"
+var policyPath = "app.rbac.rego"
+var defaultQuery = "x = data.app.rbac.allow"
 
 type OPA struct {
 	Decision bool `json:"decision"`
@@ -22,17 +22,15 @@ func getResult(ctx context.Context, query rego.PreparedEvalQuery, input map[stri
 	return results[0].Bindings["x"].(bool)
 }
 
-func GetOPADecision(user string, action string, object string) (opa OPA, err error) {
+func GetOPADecision(user string, action string, resourceType string, object string) (opa OPA, err error) {
 	input := map[string]interface{}{
 		"user":   user,
 		"action": action,
+		"type":   resourceType,
 		"object": object,
 	}
 
-	p, err := policies.ReadPolicy(policyPath)
-	if err != nil {
-		log.Error().Err(err).Msg("ReadPolicy")
-	}
+	p := policies.ReadPolicy()
 
 	ctx := context.TODO()
 	query, err := rego.New(
