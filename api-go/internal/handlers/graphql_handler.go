c637f53bf0b34b0bfd699223afa3b5a318854f9b
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-07-26 17:01:05 -0700

    fix(gin): context myID

--- api-go/internal/handlers/graphql_handler.go
+++ api-go/internal/handlers/graphql_handler.go
@@ -3,18 +3,17 @@ package handlers
 import (
 	"context"
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/schemas"
+	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/types"
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/graphql-go/handler"
 	"net/http"
 )
 
-type ContextMyIDKey string
-
 func addContext(next *handler.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		myID := utils.VerifyToken(r)
-		ctx := context.WithValue(r.Context(), ContextMyIDKey("myID"), myID)
+		ctx := context.WithValue(r.Context(), types.ContextMyIDKey("myID"), myID)
 		next.ContextHandler(ctx, w, r)
 	})
 }
--- api-go/internal/utils/checkMyID.go
+++ api-go/internal/utils/checkMyID.go
@@ -2,11 +2,12 @@ package utils
 
 import (
 	"errors"
+	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/types"
 	"github.com/graphql-go/graphql"
 )
 
 func CheckGraphQLContextMyID(p graphql.ResolveParams) error {
-	if p.Context.Value("myID").(string) != "" {
+	if p.Context.Value(types.ContextMyIDKey("myID")).(string) != "" {
 		return nil
 	}
 	return errors.New("not authorized")
