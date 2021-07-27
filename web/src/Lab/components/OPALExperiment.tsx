9c1b558ab85193e745ea6c2b6215098dfd74a0c1
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-07-27 07:43:29 -0700

    fix(web): opal decision

--- web/src/Lab/components/OPALExperiment.tsx
+++ web/src/Lab/components/OPALExperiment.tsx
@@ -5,30 +5,22 @@ import styles from './OPALExperiment.module.css';
 const HmPreAuth = React.lazy(() => import('./PreAuth'));
 
 const OPALExperiment: React.VFC = () => {
-  const [isReadDogDisabled, setReadDogIsDisabled] = React.useState(false);
-  const [isAdoptDogDisabled, setIsAdoptDogDisabled] = React.useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [readDogData, setReadDogData] = React.useState<any>(undefined);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [adoptDogData, setAdoptDogData] = React.useState<any>(undefined);
 
   return (
     <div className={styles.hmOPALExperiment}>
       <div className={`container is-max-desktop ${styles.hmContainer}`}>
         <div className="buttons">
-          <HmPreAuth
-            action="read"
-            resourceType="dog"
-            getDecision={getOPALDecision}
-            setIsDisabled={setReadDogIsDisabled}
-          >
-            <button className="button is-link" type="button" disabled={!isReadDogDisabled}>
+          <HmPreAuth action="read" resourceType="dog" getDecision={getOPALDecision} setData={setReadDogData}>
+            <button className="button is-link" type="button" disabled={!readDogData?.data?.opal?.decision}>
               Read Dog
             </button>
           </HmPreAuth>
-          <HmPreAuth
-            action="adopt"
-            resourceType="dog"
-            getDecision={getOPALDecision}
-            setIsDisabled={setIsAdoptDogDisabled}
-          >
-            <button className="button is-link" type="button" disabled={!isAdoptDogDisabled}>
+          <HmPreAuth action="adopt" resourceType="dog" getDecision={getOPALDecision} setData={setAdoptDogData}>
+            <button className="button is-link" type="button" disabled={!adoptDogData?.data?.opal?.decision}>
               Adopt Dog
             </button>
           </HmPreAuth>
