f326c651977eeec0786eba067241570aa2575e2e
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-07-28 13:22:05 -0700

    feat(flink): add Flink

---
+++ streaming/src/main/java/com/hongbomiao/Main.java
@@ -0,0 +1,45 @@
+package com.hongbomiao;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class Main {
+    public static void main(String[] args) {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Person> flintstones = env.fromElements(
+                new Person("Hongbo", 30),
+                new Person("Wilma", 35),
+                new Person("Pebbles", 2));
+
+        DataStream<Person> adults = flintstones.filter(new FilterFunction<Person>() {
+            @Override
+            public boolean filter(Person person) throws Exception {
+                return person.age >= 18;
+            }
+        });
+
+        adults.print();
+
+        try {
+            env.execute();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static class Person {
+        public String name;
+        public Integer age;
+
+        public Person(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        };
+
+        public String toString() {
+            return this.name.toString() + ": age " + this.age.toString();
+        };
+    }
+}
