6c75dbc3b6ad0e9ce9079f2bffdd1a9ec87d87a2
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-02 05:27:25 -0700

    fix(test): set postgres db name

--- docker/postgres/init_up.sql
+++ docker/postgres/init_up.sql
@@ -2,12 +2,12 @@ create extension if not exists pgcrypto;
 
 create table users
 (
-  id         uuid primary key default gen_random_uuid(),
+  id         uuid primary key     default gen_random_uuid(),
   email      text unique not null,
   password   text        not null,
   first_name text        not null,
   last_name  text        not null,
   bio        text,
-  created_on timestamptz      default now(),
-  last_login timestamptz
+  created_on timestamptz not null default now(),
+  last_login timestamptz not null default now()
 );
