4f750e805fb2a133ec3bd7653eaca1911e778463
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-03 10:56:08 -0700

    chore(k3d): update multi cluster script

--- bin/setupK3dMultiCluster.sh
+++ bin/setupK3dMultiCluster.sh
@@ -3,84 +3,106 @@
 set -e
 
 
-# Generate certificates
-cd kubernetes/certificates
-step certificate create root.linkerd.cluster.local root.crt root.key \
-  --profile=root-ca --no-password --insecure
-step certificate create identity.linkerd.cluster.local issuer.crt issuer.key \
-  --profile=intermediate-ca --not-after=8760h --no-password --insecure \
-  --ca=root.crt --ca-key=root.key
+ORG_DOMAIN="k3d.hongbomiao.com"
+CA_DIR="kubernetes/certificates"
 
+# Generate certificates
+step certificate create "identity.linkerd.${ORG_DOMAIN}" \
+  "${CA_DIR}/ca.crt" "${CA_DIR}/ca.key" \
+  --profile=root-ca \
+  --no-password \
+  --insecure
+
+for cluster in west east; do
+  domain="${cluster}.${ORG_DOMAIN}"
+  crt="${CA_DIR}/${cluster}-issuer.crt"
+  key="${CA_DIR}/${cluster}-issuer.key"
+
+  step certificate create "identity.linkerd.${domain}" ${crt} ${key} \
+    --ca="${CA_DIR}/ca.crt" \
+    --ca-key="${CA_DIR}/ca.key" \
+    --profile=intermediate-ca \
+    --not-after=8760h \
+    --no-password \
+    --insecure
+  echo "-------------"
+done
 
 # Create clusters
-cd ../..
 k3d cluster create west --config=kubernetes/k3d/west-cluster-config.yaml
 k3d cluster create east --config=kubernetes/k3d/east-cluster-config.yaml
 #	k3d cluster delete west
 #	k3d cluster delete east
 
- kubectl config use-context k3d-west
+kubectl config use-context k3d-west
 # kubectl config use-context k3d-east
 
 
 # Install Ingress
-for ctx in k3d-west k3d-east; do
-  echo "Installing Ingress on ${ctx}"
+for cluster in west east; do
+  echo "Installing Ingress on "k3d-${cluster}""
   VERSION=$(curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/stable.txt)
-  kubectl --context=${ctx} apply --filename="https://raw.githubusercontent.com/kubernetes/ingress-nginx/${VERSION}/deploy/static/provider/cloud/deploy.yaml"
-  # Local: kubectl --context=${ctx} apply --filename=kubernetes/manifests/ingress-nginx.yaml
+  kubectl --context="k3d-${cluster}" apply --filename="https://raw.githubusercontent.com/kubernetes/ingress-nginx/${VERSION}/deploy/static/provider/cloud/deploy.yaml"
+  # Local: kubectl --context="k3d-${cluster}" apply --filename=kubernetes/manifests/ingress-nginx.yaml
   echo "-------------"
 done
 
 
 # Install Linkerd
-linkerd install \
-  --identity-trust-anchors-file=kubernetes/certificates/root.crt \
-  --identity-issuer-certificate-file=kubernetes/certificates/issuer.crt \
-  --identity-issuer-key-file=kubernetes/certificates/issuer.key |
-  tee \
-    >(kubectl --context=k3d-west apply --filename=-) \
-    >(kubectl --context=k3d-east apply --filename=-)
+for cluster in west east; do
+  domain="${cluster}.${ORG_DOMAIN}"
+  crt="${CA_DIR}/${cluster}-issuer.crt"
+  key="${CA_DIR}/${cluster}-issuer.key"
+
+  linkerd --context="k3d-${cluster}" install \
+    --cluster-domain=${domain} \
+    --identity-trust-domain=${domain} \
+    --identity-trust-anchors-file="${CA_DIR}/ca.crt" \
+    --identity-issuer-certificate-file=${crt} \
+    --identity-issuer-key-file=${key} | \
+    kubectl --context="k3d-${cluster}" apply --filename=-
+  echo "-------------"
+done
 
 
 # Check Linkerd
-for ctx in k3d-west k3d-east; do
-  echo "Checking cluster: ${ctx}"
-  linkerd --context=${ctx} check || break
+for cluster in west east; do
+  echo "Checking cluster: "k3d-${cluster}""
+  linkerd --context="k3d-${cluster}" check || break
   echo "-------------"
 done
 
 
 # Patch Ingress
-for ctx in k3d-west k3d-east; do
-  echo "Patching Ingress on ${ctx}"
-  kubectl --context=${ctx} patch configmap ingress-nginx-controller --namespace=ingress-nginx --patch "$(cat kubernetes/patches/ingress-nginx-controller-configmap-patch.yaml)"
-  kubectl --context=${ctx} patch deployment ingress-nginx-controller --namespace=ingress-nginx --patch "$(cat kubernetes/patches/ingress-nginx-controller-deployment-patch.yaml)"
+for cluster in west east; do
+  echo "Patching Ingress on "k3d-${cluster}""
+  kubectl --context="k3d-${cluster}" patch configmap ingress-nginx-controller --namespace=ingress-nginx --patch "$(cat kubernetes/patches/ingress-nginx-controller-configmap-patch.yaml)"
+  kubectl --context="k3d-${cluster}" patch deployment ingress-nginx-controller --namespace=ingress-nginx --patch "$(cat kubernetes/patches/ingress-nginx-controller-deployment-patch.yaml)"
   echo "-------------"
 done
 
 
 # Install Linkerd multicluster
-for ctx in k3d-west k3d-east; do
-  echo "Installing on cluster: ${ctx}"
-  linkerd --context=${ctx} multicluster install | kubectl --context=${ctx} apply --filename=- || break
+for cluster in west east; do
+  echo "Installing on cluster: "k3d-${cluster}""
+  linkerd --context="k3d-${cluster}" multicluster install | kubectl --context="k3d-${cluster}" apply --filename=- || break
   # linkerd multicluster uninstall | kubectl delete --filename=-
   echo "-------------"
 done
 
 
 # Check Linkerd multicluster
-for ctx in k3d-west k3d-east; do
-  echo "Checking gateway on cluster: ${ctx}"
-  kubectl --context=${ctx} --namespace=linkerd-multicluster rollout status deploy/linkerd-gateway || break
+for cluster in west east; do
+  echo "Checking gateway on cluster: "k3d-${cluster}""
+  kubectl --context="k3d-${cluster}" --namespace=linkerd-multicluster rollout status deploy/linkerd-gateway || break
   echo "-------------"
 done
 
 
 # Check load balancer
-for ctx in k3d-west k3d-east; do
-  echo "Checking load balancer on cluster: ${ctx}"
-  while [ "$(kubectl --context=${ctx} --namespace=linkerd-multicluster get service \
+for cluster in west east; do
+  echo "Checking load balancer on cluster: "k3d-${cluster}""
+  while [ "$(kubectl --context="k3d-${cluster}" --namespace=linkerd-multicluster get service \
     -o 'custom-columns=:.status.loadBalancer.ingress[0].ip' \
     --no-headers)" = "<none>" ]; do
     printf '.'
