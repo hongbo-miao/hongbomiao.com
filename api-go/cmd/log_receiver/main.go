c1be3ad71f4755cb1fdb007a13d0c1448eb8bd9c
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-05 05:30:50 -0700

    feat(go): add logger and graphql error log

--- api-go/cmd/log_receiver/main.go
+++ api-go/cmd/log_receiver/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/log_receiver/utils"
 	sharedUtils "github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/shared/utils"
 	"github.com/gin-contrib/gzip"
+	"github.com/gin-contrib/logger"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 )
@@ -18,6 +19,7 @@ func main() {
 		Msg("main")
 
 	r := gin.Default()
+	r.Use(logger.SetLogger())
 	r.Use(gzip.Gzip(gzip.DefaultCompression, gzip.WithDecompressFn(gzip.DefaultDecompressHandle)))
 	r.POST("/logs", controllers.Logs)
 	err := r.Run(":" + config.Port)
