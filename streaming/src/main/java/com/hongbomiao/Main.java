b48e1a0f7aa9b580b6aba63bf56ac98cdd061cc2
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-11 17:41:23 -0700

    feat(flink): read tweets

--- streaming/src/main/java/com/hongbomiao/Main.java
+++ streaming/src/main/java/com/hongbomiao/Main.java
@@ -1,40 +1,47 @@
 package com.hongbomiao;
 
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.twitter.TwitterSource;
+import org.apache.flink.util.Collector;
+
+import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+  public static void main(String[] args) throws Exception {
+    final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        DataStream<Person> flintstones = env.fromElements(
-            new Person("Hongbo", 30),
-            new Person("Wilma", 35),
-            new Person("Pebbles", 2));
+    Properties props = new Properties();
+    props.setProperty(TwitterSource.CONSUMER_KEY, "");
+    props.setProperty(TwitterSource.CONSUMER_SECRET, "");
+    props.setProperty(TwitterSource.TOKEN, "");
+    props.setProperty(TwitterSource.TOKEN_SECRET, "");
 
-        DataStream<Person> adults = flintstones.filter(new FilterFunction<Person>() {
-            @Override
-            public boolean filter(Person person) throws Exception {
-                return person.age >= 18;
-            }
-        });
+    DataStream<String> streamSource = env.addSource(new TwitterSource(props));
+    DataStream<JsonNode> jsonStream =
+        streamSource
+            .flatMap(
+                new FlatMapFunction<String, JsonNode>() {
+                  @Override
+                  public void flatMap(String value, Collector<JsonNode> out) throws Exception {
+                    ObjectMapper mapper = new ObjectMapper();
+                    JsonNode jsonNode = mapper.readTree(value);
+                    out.collect(jsonNode);
+                  }
+                })
+            .filter(
+                new FilterFunction<JsonNode>() {
+                  @Override
+                  public boolean filter(JsonNode value) throws Exception {
+                    return value.has("text") && !value.has("retweeted_status");
+                  }
+                });
 
-        adults.print();
-        env.execute("My Streaming Job");
-    }
-
-    public static class Person {
-        public String name;
-        public Integer age;
-
-        public Person(String name, Integer age) {
-            this.name = name;
-            this.age = age;
-        };
-
-        public String toString() {
-            return this.name.toString() + ": age " + this.age.toString();
-        };
-    }
+    jsonStream.print();
+    env.execute("Read Tweets");
+  }
 }
