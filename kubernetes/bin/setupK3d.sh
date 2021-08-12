4523bb808c50c09886a5726d7ed443b1da238156
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-12 08:08:49 -0700

    chore(k3d): add dev cluster script

--- kubernetes/bin/setupK3d.sh
+++ kubernetes/bin/setupK3d.sh
@@ -4,11 +4,14 @@ set -e
 
 k3d cluster create west --config=kubernetes/k3d/west-cluster-config.yaml
 k3d cluster create east --config=kubernetes/k3d/east-cluster-config.yaml
-#	Delete: k3d cluster delete west
-#	Delete: k3d cluster delete east
+k3d cluster create dev --config=kubernetes/k3d/dev-cluster-config.yaml
+# Delete: k3d cluster delete west
+# Delete: k3d cluster delete east
+# Delete: k3d cluster delete dev
 
 kubectl config use-context k3d-west
 # kubectl config use-context k3d-east
+# kubectl config use-context k3d-dev
 
 VERSION=$(curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/stable.txt)
 kubectl apply --filename="https://raw.githubusercontent.com/kubernetes/ingress-nginx/${VERSION}/deploy/static/provider/cloud/deploy.yaml"
