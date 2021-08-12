8806e902f159c6b61b37d5cb12c696a9c076371e
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-12 08:08:49 -0700

    chore(k3d): add dev cluster script

--- kubernetes/bin/setup.sh
+++ kubernetes/bin/setup.sh
@@ -46,8 +46,8 @@ k3d cluster create east --config=kubernetes/k3d/east-cluster-config.yaml
 
 sleep 30
 
-#	k3d cluster delete west
-#	k3d cluster delete east
+# k3d cluster delete west
+# k3d cluster delete east
 
 # kubectl config use-context k3d-west
 # kubectl config use-context k3d-east
