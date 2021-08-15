dc3658f99cbdfa7530f007e34dcda6a5ecf5f9d0
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-15 09:12:39 -0700

    feat(flink): find trending Twitter hashtags

--- streaming/src/main/java/com/hongbomiao/Main.java
+++ streaming/src/main/java/com/hongbomiao/Main.java
@@ -1,12 +1,16 @@
 package com.hongbomiao;
 
+import java.util.Objects;
 import java.util.Properties;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.twitter.TwitterSource;
 import org.apache.flink.util.Collector;
 
@@ -22,7 +26,7 @@ public class Main {
     props.setProperty(TwitterSource.TOKEN_SECRET, props.getProperty("twitter.access.token.secret"));
 
     DataStream<String> streamSource = env.addSource(new TwitterSource(props));
-    DataStream<JsonNode> jsonStream =
+    DataStream<JsonNode> tweetStream =
         streamSource
             .flatMap(
                 new FlatMapFunction<String, JsonNode>() {
@@ -37,11 +41,43 @@ public class Main {
                 new FilterFunction<JsonNode>() {
                   @Override
                   public boolean filter(JsonNode value) throws Exception {
-                    return value.has("text") && !value.has("retweeted_status");
+                    // value.has("text")
+                    // !value.has("retweeted_status")
+                    // Objects.equals(value.get("lang").asText(), "en")
+                    return value.has("text")
+                        && !value.has("retweeted_status")
+                        && Objects.equals(value.get("lang").asText(), "en");
                   }
                 });
 
-    jsonStream.print();
-    env.execute("Read Tweets");
+    DataStream<Tuple2<String, Integer>> hashtagStream =
+        tweetStream.flatMap(
+            new FlatMapFunction<JsonNode, Tuple2<String, Integer>>() {
+              @Override
+              public void flatMap(JsonNode value, Collector<Tuple2<String, Integer>> out)
+                  throws Exception {
+                for (JsonNode hashtag : value.get("entities").get("hashtags")) {
+                  out.collect(new Tuple2<String, Integer>(hashtag.get("text").asText(), 1));
+                }
+              }
+            });
+
+    DataStream<Tuple2<String, Integer>> tweetsPerWindow =
+        hashtagStream
+            .keyBy(value -> value.f0)
+            .window(TumblingProcessingTimeWindows.of(Time.seconds(10)))
+            .sum(1);
+
+    tweetsPerWindow.print();
+    env.execute("Find trending Twitter hashtags");
+  }
+
+  public static class Splitter implements FlatMapFunction<JsonNode, Tuple2<String, Integer>> {
+    @Override
+    public void flatMap(JsonNode value, Collector<Tuple2<String, Integer>> out) throws Exception {
+      for (JsonNode hashtag : value.get("entities").get("hashtags")) {
+        out.collect(new Tuple2<String, Integer>(hashtag.get("text").asText(), 1));
+      }
+    }
   }
 }
