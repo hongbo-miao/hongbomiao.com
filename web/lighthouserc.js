ecff4ae594660ce1a4e4d0c524fa7feb815ed896
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-15 09:35:17 -0700

    chore(lighthouse): ignore csp-xss

--- web/lighthouserc.js
+++ web/lighthouserc.js
@@ -6,15 +6,16 @@ module.exports = {
     assert: {
       preset: 'lighthouse:recommended',
       assertions: {
+        'csp-xss': ['warn', { minScore: 0 }],
         'errors-in-console': ['warn', { minScore: 0 }],
         'no-unload-listeners': ['warn', { minScore: 0 }],
+        'offline-start-url': ['warn', { minScore: 0 }], // https://github.com/Hongbo-Miao/hongbomiao.com/issues/824
+        'service-worker': ['warn', { minScore: 0 }],
         'unused-css-rules': ['warn', { minScore: 0 }],
         'unused-javascript': ['warn', { minScore: 0 }],
         'uses-rel-preconnect': ['warn', { minScore: 0 }],
-        'offline-start-url': ['warn', { minScore: 0 }], // https://github.com/Hongbo-Miao/hongbomiao.com/issues/824
-        'service-worker': ['warn', { minScore: 0 }],
-        'works-offline': ['warn', { minScore: 0 }],
         'valid-source-maps': ['warn', { minScore: 0 }],
+        'works-offline': ['warn', { minScore: 0 }],
       },
     },
     upload: {
