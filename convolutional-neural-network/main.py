9f5bdcf627b832631e8abc393477c88191364f4b
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-22 12:10:22 -0700

    feat(cnn): log train_loss, train_acc

--- convolutional-neural-network/main.py
+++ convolutional-neural-network/main.py
@@ -1,9 +1,10 @@
+from args import get_args
 from evaluate import evaluate
 from model.data_loader import train_data_loader, val_data_loader
 from model.net import net
 from torch import nn, optim
+from train import train
 from utils.device import device
-from args import get_args
 import wandb
 
 
@@ -22,35 +23,19 @@ def main():
         # optimizer = optim.Adam(net.parameters(), lr=config.learning_rate)
 
         for epoch in range(config.num_epochs):
-            running_loss = 0.0
-            for i, data in enumerate(train_data_loader, 0):
-                inputs, labels = data
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward + backward + optimize
-                outputs = net(inputs)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
-
-                # print statistics
-                running_loss += loss.item()
-                if i % 2000 == 1999:  # print every 2000 mini-batches
-                    print(
-                        "[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000)
-                    )
-                    running_loss = 0.0
+            train_loss = train(net, train_data_loader, optimizer, criterion)
+            train_acc = evaluate(net, train_data_loader)
             val_acc = evaluate(net, val_data_loader)
+
+            print({"Train": train_acc, "Validation": val_acc})
             wb.log(
                 {
                     "epoch": epoch,
+                    "train_loss": train_loss,
+                    "train_acc": train_acc,
                     "val_acc": val_acc,
                 }
             )
-
-        print("Finished Training")
 
 
 if __name__ == "__main__":
