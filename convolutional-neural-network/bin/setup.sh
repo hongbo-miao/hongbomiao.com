c5328489406c78ccc7bfdb6cefe931c90b0c4768
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-22 14:56:20 -0700

    feat(cml): write reports

--- convolutional-neural-network/bin/setup.sh
+++ convolutional-neural-network/bin/setup.sh
@@ -6,8 +6,9 @@ set -e
 conda create --name hm-cnn python=3.9.6
 conda activate hm-cnn
 
-# Install PyTorch
-conda install pytorch torchvision torchaudio -c pytorch
-
 # Install requirements
+conda install pytorch torchvision torchaudio -c pytorch
+conda install pandas
+conda install tabulate
+
 pip install --requirement requirements.txt
