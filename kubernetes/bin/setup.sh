0b889697f4feecf45aafce582957b978f8e66a41
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-23 08:38:32 -0700

    chore(flink): set pull-policy to always

--- kubernetes/bin/setup.sh
+++ kubernetes/bin/setup.sh
@@ -333,13 +333,13 @@ kubectl apply --filename=kubernetes/config/east
 echo "# West cluster"
 kubectl config use-context k3d-west
 
-flink run-application \
-  --target kubernetes-application \
-  -Dkubernetes.namespace=hm-flink \
-  -Dkubernetes.cluster-id=hm-flink-cluster \
-  -Dkubernetes.container.image=hongbomiao/hm-streaming:latest \
-  -Dkubernetes.jobmanager.service-account=flink-serviceaccount \
-  local:///opt/flink/usrlib/streaming-0.1.jar
-#  -Dkubernetes.container.image.pull-policy=Always \
 
+# flink run-application \
+#   --target kubernetes-application \
+#   -Dkubernetes.namespace=hm-flink \
+#   -Dkubernetes.cluster-id=hm-flink-cluster \
+#   -Dkubernetes.container.image=hongbomiao/hm-streaming:latest \
+#   -Dkubernetes.container.image.pull-policy=Always \
+#   -Dkubernetes.jobmanager.service-account=flink-serviceaccount \
+#   local:///opt/flink/usrlib/streaming-0.1.jar
 # Delete: kubectl delete deployment/hm-flink-cluster --namespace=hm-flink
