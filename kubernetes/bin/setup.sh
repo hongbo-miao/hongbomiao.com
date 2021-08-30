81d514202ee1ed0837a8a29c43ab40dda1b6f117
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-08-30 15:12:36 -0700

    refactor(k8s): wrap lines for setup script

--- kubernetes/bin/setup.sh
+++ kubernetes/bin/setup.sh
@@ -12,6 +12,7 @@ rm -f kubernetes/certificates/east-issuer.key
 rm -f kubernetes/certificates/west-issuer.crt
 rm -f kubernetes/certificates/west-issuer.key
 rm -f kubernetes/data/elastic-apm/tls.crt
+echo "=================================================="
 
 
 # Generate OPAL SSH key
@@ -101,13 +102,21 @@ done
 for cluster in west east; do
   echo "# Install Ingress on: k3d-${cluster}"
   INGRESS_VERSION=$(curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/stable.txt)
-  kubectl apply --context="k3d-${cluster}" --filename="https://raw.githubusercontent.com/kubernetes/ingress-nginx/${INGRESS_VERSION}/deploy/static/provider/cloud/deploy.yaml"
+  kubectl apply \
+    --context="k3d-${cluster}" \
+    --filename="https://raw.githubusercontent.com/kubernetes/ingress-nginx/${INGRESS_VERSION}/deploy/static/provider/cloud/deploy.yaml"
   # Local: kubectl apply --context="k3d-${cluster}" --filename=kubernetes/manifests/ingress-nginx.yaml
   echo "=================================================="
 
   echo "# Patch Ingress on: k3d-${cluster}"
-  kubectl patch configmap ingress-nginx-controller --context="k3d-${cluster}" --namespace=ingress-nginx --patch "$(cat kubernetes/patches/ingress-nginx-controller-configmap-patch.yaml)"
-  kubectl patch deployment ingress-nginx-controller --context="k3d-${cluster}" --namespace=ingress-nginx --patch "$(cat kubernetes/patches/ingress-nginx-controller-deployment-patch.yaml)"
+  kubectl patch configmap ingress-nginx-controller \
+    --context="k3d-${cluster}" \
+    --namespace=ingress-nginx \
+    --patch "$(cat kubernetes/patches/ingress-nginx-controller-configmap-patch.yaml)"
+  kubectl patch deployment ingress-nginx-controller \
+    --context="k3d-${cluster}" \
+    --namespace=ingress-nginx \
+    --patch "$(cat kubernetes/patches/ingress-nginx-controller-deployment-patch.yaml)"
   echo "=================================================="
 done
 sleep 30
@@ -125,7 +134,9 @@ done
 for cluster in west east; do
   echo "# Install Linkerd multicluster on: k3d-${cluster}"
   domain="${cluster}.${ORG_DOMAIN}"
-  linkerd multicluster install --context="k3d-${cluster}" --set=identityTrustDomain=${domain} | \
+  linkerd multicluster install \
+    --context="k3d-${cluster}" \
+    --set=identityTrustDomain=${domain} | \
     kubectl apply --context="k3d-${cluster}" --filename=-
   # linkerd multicluster uninstall | kubectl delete --filename=-
   echo "=================================================="
@@ -136,7 +147,9 @@ sleep 30
 # Check Linkerd gateway
 for cluster in west east; do
   echo "# Check gateway on: k3d-${cluster}"
-  kubectl rollout status deploy/linkerd-gateway --context="k3d-${cluster}" --namespace=linkerd-multicluster
+  kubectl rollout status deploy/linkerd-gateway \
+    --context="k3d-${cluster}" \
+    --namespace=linkerd-multicluster
   echo "=================================================="
 done
 
@@ -160,13 +173,19 @@ done
 echo "# Link the cluster"
 # https://github.com/olix0r/l2-k3d-multi/blob/4cb5e6994671a5f6f507b7ad281b8c008927c9d0/link.sh#L12-L22
 # Unfortunately, the credentials have the API server IP as addressed from localhost and not the docker network, so we have to patch that up.
-EAST_IP=$(kubectl get service --context="k3d-east" --namespace=ingress-nginx ingress-nginx-controller --output="go-template={{ (index .status.loadBalancer.ingress 0).ip }}")
+EAST_IP=$(kubectl get service \
+  --context="k3d-east" \
+  --namespace=ingress-nginx ingress-nginx-controller \
+  --output="go-template={{ (index .status.loadBalancer.ingress 0).ip }}")
 linkerd multicluster link \
   --cluster-name=k3d-east \
   --api-server-address="https://${EAST_IP}:6443" | \
     kubectl apply --context=k3d-west --filename=-
 
-WEST_IP=$(kubectl get service --context="k3d-west" --namespace=ingress-nginx ingress-nginx-controller --output="go-template={{ (index .status.loadBalancer.ingress 0).ip }}")
+WEST_IP=$(kubectl get service \
+  --context="k3d-west" \
+  --namespace=ingress-nginx ingress-nginx-controller \
+  --output="go-template={{ (index .status.loadBalancer.ingress 0).ip }}")
 linkerd multicluster link \
   --cluster-name=k3d-west \
   --api-server-address="https://${WEST_IP}:6443" | \
@@ -192,7 +211,8 @@ echo "=================================================="
 
 # Install Linkerd viz
 echo "# Install Linkerd viz"
-linkerd viz install --set=jaegerUrl=jaeger.linkerd-jaeger:16686,clusterDomain="west.${ORG_DOMAIN}" | \
+linkerd viz install \
+  --set=jaegerUrl=jaeger.linkerd-jaeger:16686,clusterDomain="west.${ORG_DOMAIN}" | \
   kubectl apply --filename=-
 echo "=================================================="
 # linkerd viz dashboard --context=k3d-west
@@ -266,32 +286,52 @@ echo "=================================================="
 
 # Elasticsearch
 # kubectl port-forward service/hm-elasticsearch-es-http --namespace=elastic 9200:9200
-# ELASTIC_PASSWORD=$(kubectl get secret hm-elasticsearch-es-elastic-user --namespace=elastic --output=go-template="{{.data.elastic | base64decode}}")
+# ELASTIC_PASSWORD=$(kubectl get secret hm-elasticsearch-es-elastic-user \
+#   --namespace=elastic \
+#   --output=go-template="{{.data.elastic | base64decode}}")
 # curl -u "elastic:${ELASTIC_PASSWORD}" -k "https://localhost:9200"
 
 # Kibana
 # kubectl port-forward service/hm-kb-http --namespace=elastic 5601:5601
 # Username: elastic
-# Password: kubectl get secret hm-elasticsearch-es-elastic-user --namespace=elastic --output=jsonpath="{.data.elastic}" | base64 --decode; echo
+# Password:
+# kubectl get secret hm-elasticsearch-es-elastic-user \
+#   --namespace=elastic \
+#   --output=jsonpath="{.data.elastic}" | base64 --decode; echo
 
 # Elastic APM
 echo "# Save Elastic APM tls.crt locally"
-kubectl get secret hm-apm-apm-http-certs-public --namespace=elastic --output=go-template='{{index .data "tls.crt" | base64decode }}' > kubernetes/data/elastic-apm/tls.crt
+kubectl get secret hm-apm-apm-http-certs-public \
+  --namespace=elastic \
+  --output=go-template='{{index .data "tls.crt" | base64decode }}' \
+  > kubernetes/data/elastic-apm/tls.crt
 echo "=================================================="
 
 echo "# Save Elastic APM token in Kubernetes secret"
 kubectl create namespace hm
-ELASTIC_APM_TOKEN=$(kubectl get secret hm-apm-apm-token --namespace=elastic --output=go-template='{{index .data "secret-token" | base64decode}}')
-kubectl create secret generic hm-elastic-apm --namespace=hm --from-literal="token=${ELASTIC_APM_TOKEN}"
+ELASTIC_APM_TOKEN=$(kubectl get secret hm-apm-apm-token \
+  --namespace=elastic \
+  --output=go-template='{{index .data "secret-token" | base64decode}}')
+kubectl create secret generic hm-elastic-apm \
+  --namespace=hm \
+  --from-literal="token=${ELASTIC_APM_TOKEN}"
 echo "=================================================="
 
 
 # Install Argo CD
 echo "# Install Argo CD"
 kubectl create namespace argocd
-kubectl apply --namespace=argocd --filename=https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-# Local: kubectl apply --namespace=argocd --filename=kubernetes/manifests/argocd.yaml
-# Delete: kubectl delete --namespace=argocd --filename=https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply \
+  --namespace=argocd \
+  --filename=https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# Local:
+# kubectl apply \
+#   --namespace=argocd \
+#   --filename=kubernetes/manifests/argocd.yaml
+# Delete:
+# kubectl delete \
+#   --namespace=argocd \
+#   --filename=https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 echo "=================================================="
 sleep 30
 
@@ -306,7 +346,10 @@ echo "=================================================="
 # Install the app by Argo CD
 echo "# Install the app"
 kubectl port-forward service/argocd-server --namespace=argocd 31026:443 &
-ARGOCD_PASSWORD=$(kubectl get secret argocd-initial-admin-secret --namespace=argocd --output=jsonpath="{.data.password}" | base64 -d && echo)
+ARGOCD_PASSWORD=$(kubectl get secret argocd-initial-admin-secret \
+  --namespace=argocd \
+  --output=jsonpath="{.data.password}" | \
+  base64 -d && echo)
 argocd login localhost:31026 --username=admin --password="${ARGOCD_PASSWORD}" --insecure
 kubectl apply --filename=kubernetes/config/argocd/hm-application.yaml
 argocd app sync hm-application --local=kubernetes/config/west
@@ -328,7 +371,8 @@ kubectl port-forward service/opal-server-service --namespace=hm-opa 7002:7002 &
 echo "=================================================="
 
 echo "# Create OPAL client secret"
-OPAL_CLIENT_TOKEN=$(opal-client obtain-token "${OPAL_SERVER_MASTER_TOKEN}" --server-url=http://localhost:7002)
+OPAL_CLIENT_TOKEN=$(opal-client obtain-token "${OPAL_SERVER_MASTER_TOKEN}" \
+  --server-url=http://localhost:7002)
 kubectl create secret generic hm-opal-client-secret \
   --namespace=hm \
   --from-literal=opal_client_token="${OPAL_CLIENT_TOKEN}"
@@ -350,9 +394,16 @@ kubectl apply --filename=kubernetes/config/east
 echo "=================================================="
 sleep 30
 
-# Verify Linkerd multicluster setup correctly by checking the endpoints in west and verify that they match the gateway’s public IP address in east
-# kubectl get endpoint grpc-server-service-k3d-east --context=k3d-west --namespace=hm --output="custom-columns=ENDPOINT_IP:.subsets[*].addresses[*].ip"
-# kubectl linkerd-multicluster get service linkerd-gateway --context=k3d-east --namespace=hm --output="custom-columns=GATEWAY_IP:.status.loadBalancer.ingress[*].ip"
+# Verify Linkerd multicluster setup correctly by checking the endpoints in west
+# and verify that they match the gateway’s public IP address in east
+# kubectl get endpoint grpc-server-service-k3d-east \
+#   --context=k3d-west \
+#   --namespace=hm \
+#   --output="custom-columns=ENDPOINT_IP:.subsets[*].addresses[*].ip"
+# kubectl linkerd-multicluster get service linkerd-gateway \
+#   --context=k3d-east \
+#   --namespace=hm \
+#   --output="custom-columns=GATEWAY_IP:.status.loadBalancer.ingress[*].ip"
 
 
 # West cluster
