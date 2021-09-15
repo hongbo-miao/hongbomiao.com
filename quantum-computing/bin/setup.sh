829224e77382472c30b0cfde20c842f6c71e3c45
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-09-15 05:33:11 -0700

    feat(app): add quantum computing

---
+++ quantum-computing/bin/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Creating an environment
+conda create --name=hm-quantum python=3.9
+conda activate hm-quantum
+
+# Install requirements
+pip install qiskit
+pip install 'qiskit[visualization]'
