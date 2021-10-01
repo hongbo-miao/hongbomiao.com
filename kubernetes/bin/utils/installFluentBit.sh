b5cee1672e3219a7945c2ed16c4838bb61743025
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-10-02 05:31:50 +0800

    feat(k8s): add fluent bit

---
+++ kubernetes/bin/utils/installFluentBit.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+echo "# Install Fluent Bit"
+# https://docs.fluentbit.io/manual/installation/kubernetes
+kubectl create namespace logging
+kubectl apply --filename=https://raw.githubusercontent.com/fluent/fluent-bit-kubernetes-logging/master/fluent-bit-service-account.yaml
+kubectl apply --filename=https://raw.githubusercontent.com/fluent/fluent-bit-kubernetes-logging/master/fluent-bit-role-1.22.yaml
+kubectl apply --filename=https://raw.githubusercontent.com/fluent/fluent-bit-kubernetes-logging/master/fluent-bit-role-binding-1.22.yaml
+kubectl apply --filename=https://raw.githubusercontent.com/fluent/fluent-bit-kubernetes-logging/master/output/elasticsearch/fluent-bit-configmap.yaml
+kubectl apply --filename=kubernetes/config/fluentbit
+echo "=================================================="
