a22f9b116bd78bd3ccbf829e78725a52e110a8b5
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-10-27 07:15:00 +0800

    chore(git): ignore opal public key

--- kubernetes/bin/utils/focusOPADebug.sh
+++ kubernetes/bin/utils/focusOPADebug.sh
@@ -12,11 +12,11 @@ perl -p0i -e 's/replicas: 3/replicas: 1/s' kubernetes/config/west/opal-server-de
 # perl -p0i -e 's/# ---- ELASTIC APM BEGIN ----.*?# ---- ELASTIC APM END ----//sg' kubernetes/config/west/config-server-deployment.yaml
 # perl -p0i -e 's/# ---- ELASTIC APM BEGIN ----.*?# ---- ELASTIC APM END ----//sg' kubernetes/config/west/decision-logger-deployment.yaml
 # perl -p0i -e 's/# ---- ELASTIC APM BEGIN ----.*?# ---- ELASTIC APM END ----//sg' kubernetes/config/west/graphql-server-deployment.yaml
+# rm -f kubernetes/config/west/elastic-apm-configmap.yaml
+# rm -f kubernetes/config/west/elastic-apm-pv.yaml
+# rm -f kubernetes/config/west/elastic-apm-pvc.yaml
 
-# Remove Kubernetes yaml
-rm -f kubernetes/config/west/elastic-apm-configmap.yaml
-rm -f kubernetes/config/west/elastic-apm-pv.yaml
-rm -f kubernetes/config/west/elastic-apm-pvc.yaml
+# Remove services
 rm -f kubernetes/config/west/flink-clusterrolebinding.yaml
 rm -f kubernetes/config/west/flink-serviceaccount.yaml
 rm -f kubernetes/config/west/grpc-server-configmap.yaml
