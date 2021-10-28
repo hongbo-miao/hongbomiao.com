6fccab6f18924cef843b217d790602d46d588f93
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-10-29 00:47:53 +0800

    chore(config-server): add SHOULD_ENABLE_SERVER_TLS

--- api-go/internal/config_server/utils/get_config.go
+++ api-go/internal/config_server/utils/get_config.go
@@ -8,6 +8,7 @@ import (
 type Config struct {
 	AppEnv                string
 	Port                  string
+	ShouldEnableServerTLS string
 	ConfigServerCertPath  string
 	ConfigServerKeyPath   string
 	OPALAuthPublicKeyPath string
@@ -27,6 +28,7 @@ func GetConfig() *Config {
 	return &Config{
 		AppEnv:                appEnv,
 		Port:                  os.Getenv("PORT"),
+		ShouldEnableServerTLS: os.Getenv("SHOULD_ENABLE_SERVER_TLS"),
 		ConfigServerCertPath:  os.Getenv("SERVER_CERT_PATH"),
 		ConfigServerKeyPath:   os.Getenv("SERVER_KEY_PATH"),
 		OPALAuthPublicKeyPath: os.Getenv("OPAL_AUTH_PUBLIC_KEY_PATH"),
