5b9b4eb5349d345d5d20b143cfade099b0ebc28c
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-12-10 21:07:37 -0800

    chore(shell): rename variables

--- kubernetes/bin/utils/secureConfigServer.sh
+++ kubernetes/bin/utils/secureConfigServer.sh
@@ -3,13 +3,13 @@
 set -e
 
 
-config_server_data_path="kubernetes/data/config-server"
+CONFIG_SERVER_DATA_PATH="kubernetes/data/config-server"
 
 
 echo "# Clean config server cert and key"
-rm -f "${config_server_data_path}/server.crt"
-rm -f "${config_server_data_path}/server.key"
+rm -f "${CONFIG_SERVER_DATA_PATH}/server.crt"
+rm -f "${CONFIG_SERVER_DATA_PATH}/server.key"
 
 echo "# Generate config server cert and key"
-openssl req -x509 -newkey=rsa:4096 -nodes -out="${config_server_data_path}/server.crt" -keyout="${config_server_data_path}/server.key" -days=3650 -subj="/C=US/ST=CA/CN=hongbomiao.com"
+openssl req -x509 -newkey=rsa:4096 -nodes -out="${CONFIG_SERVER_DATA_PATH}/server.crt" -keyout="${CONFIG_SERVER_DATA_PATH}/server.key" -days=3650 -subj="/C=US/ST=CA/CN=hongbomiao.com"
 echo "=================================================="
