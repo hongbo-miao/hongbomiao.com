e75fdfde179d735f3c4bbe6540e75843a8506407
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-12-10 21:05:48 -0800

    refactor(debezium): opa-db-credentials file name

--- kubernetes/bin/utils/installDebezium.sh
+++ kubernetes/bin/utils/installDebezium.sh
@@ -5,7 +5,7 @@ set -e
 
 echo "# Install Debezium"
 kubectl create secret generic hm-opa-db-credentials \
-  --from-file=kubernetes/config/debezium/debezium-opa-db-credentials.properties \
+  --from-file=kubernetes/config/debezium/opa-db-credentials.properties \
   --namespace=kafka
 
 kubectl apply --filename=kubernetes/config/debezium
