75ae0d3714ff3b637d7cbc096df231482fc835f9
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-12-10 16:14:50 -0800

    feat(k8s): add elasticsearch-sink-connector

--- kubernetes/bin/utils/installDebezium.sh
+++ kubernetes/bin/utils/installDebezium.sh
@@ -3,10 +3,73 @@
 set -e
 
 
-echo "# Install Debezium"
+echo "# Create secret hm-opa-db-credentials"
 kubectl create secret generic hm-opa-db-credentials \
   --from-file=kubernetes/config/debezium/opa-db-credentials.properties \
   --namespace=kafka
+echo "=================================================="
 
+
+echo "# Clean elasticsearch-keystore.jks"
+KAFKACONNECT_DATA_PATH="kubernetes/data/kafkaconnect"
+rm -f "${KAFKACONNECT_DATA_PATH}/elasticsearch-keystore.jks"
+
+echo "# Create elasticsearch-keystore.jks"
+KEYSTORE_P12_PASSWORD="SFLzyT8DPkGGjDtn"
+KEYSTORE_JSK_PASSWORD="MPx57vkACsRWKVap"
+
+kubectl get secret hm-elasticsearch-es-http-certs-public \
+  --namespace=elastic \
+  --output=go-template='{{index .data "ca.crt" | base64decode }}' \
+  > "${KAFKACONNECT_DATA_PATH}/ca.crt"
+
+kubectl get secret hm-elasticsearch-es-http-certs-public \
+  --namespace=elastic \
+  --output=go-template='{{index .data "tls.crt" | base64decode }}' \
+  > "${KAFKACONNECT_DATA_PATH}/tls.crt"
+
+kubectl get secret hm-elasticsearch-es-http-certs-internal \
+  --namespace=elastic \
+  --output=go-template='{{index .data "tls.key" | base64decode }}' \
+  > "${KAFKACONNECT_DATA_PATH}/tls.key"
+
+openssl pkcs12 -export \
+  -in "${KAFKACONNECT_DATA_PATH}/tls.crt" \
+  -inkey "${KAFKACONNECT_DATA_PATH}/tls.key" \
+  -CAfile "${KAFKACONNECT_DATA_PATH}/ca.crt" \
+  -caname root \
+  -out "${KAFKACONNECT_DATA_PATH}/elasticsearch-keystore.p12" \
+  -password "pass:${KEYSTORE_P12_PASSWORD}" \
+  -name hm-elasticsearch-keystore
+
+keytool -importkeystore \
+  -srckeystore  "${KAFKACONNECT_DATA_PATH}/elasticsearch-keystore.p12" \
+  -srcstoretype PKCS12 \
+  -srcstorepass ${KEYSTORE_P12_PASSWORD} \
+  -destkeystore  "${KAFKACONNECT_DATA_PATH}/elasticsearch-keystore.jks" \
+  -destkeypass ${KEYSTORE_JSK_PASSWORD} \
+  -deststorepass ${KEYSTORE_JSK_PASSWORD} \
+  -alias hm-elasticsearch-keystore
+
+rm -f "${KAFKACONNECT_DATA_PATH}/tls.key"
+rm -f "${KAFKACONNECT_DATA_PATH}/tls.crt"
+rm -f "${KAFKACONNECT_DATA_PATH}/ca.crt"
+rm -f "${KAFKACONNECT_DATA_PATH}/elasticsearch-keystore.p12"
+
+echo "# Create secret hm-elasticsearch-keystore"
+kubectl create secret generic hm-elasticsearch-keystore \
+  --from-file="${KAFKACONNECT_DATA_PATH}/elasticsearch-keystore.jks" \
+  --namespace=kafka
+echo "=================================================="
+
+
+echo "# Create secret hm-elasticsearch-credentials"
+kubectl create secret generic hm-elasticsearch-credentials \
+  --from-file=kubernetes/config/debezium/elasticsearch-credentials.properties \
+  --namespace=kafka
+echo "=================================================="
+
+
+echo "# Install Debezium"
 kubectl apply --filename=kubernetes/config/debezium
 echo "=================================================="
