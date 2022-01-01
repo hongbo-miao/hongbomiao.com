856b191b35843cb1184e147d651412f1fb5df8af
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-12-31 22:31:56 -0800

    feat(rasa): add slot for shirt size

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -19,3 +19,21 @@ class ActionShowTime(Action):
         time = now.strftime("%H:%M:%S")
         dispatcher.utter_message(text=f"{time}")
         return []
+
+
+class ActionGetShirtSize(Action):
+    def name(self) -> Text:
+        return "action_get_shirt_size"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        shirt_size = tracker.get_slot("shirt_size")
+        if not shirt_size:
+            dispatcher.utter_message(text="I don't know your shirt size.")
+        else:
+            dispatcher.utter_message(text=f"Your shirt size is {shirt_size}!")
+        return []
