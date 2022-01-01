88eae3bf9ac03874fc8e2c40b29c5635c01155d7
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-12-31 17:20:17 -0800

    feat(k8s): add rasa action server

--- kubernetes/bin/utils/installRasa.sh
+++ kubernetes/bin/utils/installRasa.sh
@@ -1,13 +1,44 @@
 #!/usr/bin/env bash
 set -e
 
-echo "# Install Rasa"
-kubectl create namespace hm-rasa
+echo "# Install Rasa Action Server"
+kubectl apply --filename=kubernetes/config/rasa/hm-rasa-action-server-namespace.yaml
+helm install \
+  --namespace=hm-rasa-action-server \
+  --values=kubernetes/config/rasa/rasa-action-server/values.yaml \
+  hm-release \
+  rasa/rasa-action-server
+
+# Upgrade:
+# helm upgrade \
+#   --namespace=hm-rasa-action-server \
+#   --reuse-values \
+#   --values=kubernetes/config/rasa/values/values.yaml \
+#   hm-release \
+#   rasa/rasa-action-server
+
+# Delete:
+# helm uninstall --namespace=hm-rasa-action-server hm-release
+echo "=================================================="
+
+echo "# Install Rasa Open Source"
+kubectl apply --filename=kubernetes/config/rasa/hm-rasa-namespace.yaml
 helm repo add rasa https://helm.rasa.com
 
 helm install \
   --namespace=hm-rasa \
-  --values=kubernetes/config/rasa/rasa-values.yaml \
+  --values=kubernetes/config/rasa/rasa/values.yaml \
   hm-release \
   rasa/rasa
-# Delete: helm uninstall --namespace=hm-rasa hm-release
+
+# Upgrade:
+# helm upgrade \
+#   --namespace=hm-rasa \
+#   --reuse-values \
+#   --values=kubernetes/config/rasa/rasa/values.yaml \
+#   hm-release \
+#   rasa/rasa
+
+# Delete:
+# helm uninstall --namespace=hm-rasa hm-release
+echo "=================================================="
