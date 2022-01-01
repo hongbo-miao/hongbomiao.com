208b57bf9c066055b8027903b2ca17a318b8b041
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-12-31 22:43:07 -0800

    refactor(rasa): current time

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -7,7 +7,7 @@ from rasa_sdk.executor import CollectingDispatcher
 
 class ActionShowTime(Action):
     def name(self) -> Text:
-        return "action_show_time"
+        return "action_get_current_time"
 
     def run(
         self,
@@ -16,8 +16,7 @@ class ActionShowTime(Action):
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
         now = datetime.now()
-        time = now.strftime("%H:%M:%S")
-        dispatcher.utter_message(text=f"{time}")
+        dispatcher.utter_message(text=f"{now}")
         return []
 
 
