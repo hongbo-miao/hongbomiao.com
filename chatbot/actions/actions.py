6067f911f29953fc746e584578908a5713fff946
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-01-02 19:04:10 -0800

    feat(rasa): add pizza form

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from typing import Any, Dict, List, Text
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, FormValidationAction, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 
 
 class ActionShowTime(Action):
@@ -36,3 +37,42 @@ class ActionGetShirtSize(Action):
         else:
             dispatcher.utter_message(text=f"Your shirt size is {shirt_size}!")
         return []
+
+
+ALLOWED_PIZZA_SIZES = ["small", "medium", "large"]
+ALLOWED_PIZZA_TYPES = ["hawaii", "pepperoni", "veggie"]
+
+
+class ValidateSimplePizzaForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_simple_pizza_form"
+
+    def validate_pizza_size(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        if slot_value.lower() not in ALLOWED_PIZZA_SIZES:
+            dispatcher.utter_message(
+                text=f"We only accept pizza sizes: {'/'.join(ALLOWED_PIZZA_SIZES)}."
+            )
+            return {"pizza_size": None}
+        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} pizza.")
+        return {"pizza_size": slot_value}
+
+    def validate_pizza_type(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        if slot_value not in ALLOWED_PIZZA_TYPES:
+            dispatcher.utter_message(
+                text=f"I don't recognize that pizza. We serve {'/'.join(ALLOWED_PIZZA_TYPES)}."
+            )
+            return {"pizza_type": None}
+        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} pizza.")
+        return {"pizza_type": slot_value}
