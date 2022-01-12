f1c042018ea21bd20573455938f8568b1cce64d1
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-01-11 18:50:39 -0800

    refactor(node): replace faker to @faker-js/faker

--- api-node/src/dataSources/postgres/seeds/initUsers.ts
+++ api-node/src/dataSources/postgres/seeds/initUsers.ts
@@ -1,4 +1,6 @@
-import faker from 'faker';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import faker from '@faker-js/faker';
 import config from '../../../config';
 import findUserByEmail from '../utils/findUserByEmail';
 import formatUser from '../utils/formatUser';
