6d7f1bde98847fab6104b8f2a02d36b0fda08cde
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-01-14 02:07:37 -0800

    feat(ops): add ansible

--- kubernetes/bin/init.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-source kubernetes/bin/utils/initHyperKit.sh
-source kubernetes/bin/utils/initJq.sh
-source kubernetes/bin/utils/initMinIO.sh
--- kubernetes/bin/utils/initJq.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "# Install jq"
-brew install jq
-echo "=================================================="
