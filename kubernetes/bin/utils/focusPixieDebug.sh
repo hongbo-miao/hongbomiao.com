f64d9a6de0e9a24c61cc377326e1e4063c4e1ac5
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-08 12:35:06 -0700

    feat(k8s): add focusHasuraDebug script

--- kubernetes/bin/utils/focusPixieDebug.sh
+++ kubernetes/bin/utils/focusPixieDebug.sh
@@ -43,9 +43,6 @@ rm -f kubernetes/config/west/hm-opa-namespace.yaml
 rm -f kubernetes/config/west/hm-redis-namespace.yaml
 rm -f kubernetes/config/west/model-server-pv.yaml
 rm -f kubernetes/config/west/model-server-pvc.yaml
-rm -f kubernetes/config/west/postgres-configmap.yaml
-rm -f kubernetes/config/west/postgres-service.yaml
-rm -f kubernetes/config/west/postgres-statefulset.yaml
 rm -f kubernetes/config/west/opa-pv.yaml
 rm -f kubernetes/config/west/opa-pvc.yaml
 rm -f kubernetes/config/west/opal-broadcast-channel-configmap.yaml
