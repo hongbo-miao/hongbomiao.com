c01afa74029d67c7b6a790b3464da42335f09cc8
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-15 20:03:16 -0700

    feat(k8s): add superset

---
+++ kubernetes/bin/utils/uninstallSuperset.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+echo "# Uninstall Superset"
+helm uninstall --namespace=hm-superset superset
+kubectl delete jobs superset-init-db --namespace=hm-superset
+kubectl delete --filename=kubernetes/config/superset/hm-superset-namespace.yaml
+echo "=================================================="
+
+echo "# Drop superset_db in Postgres"
+kubectl port-forward service/postgres-service --namespace=hm-postgres 40072:40072 &
+sleep 3
+psql --host=localhost --port=40072 --dbname=postgres --username=admin --command="drop database if exists superset_db with (force);"
+pgrep kubectl | xargs kill -9
+echo "=================================================="
