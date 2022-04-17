8da747ed97820100e1bb672629bfe6711af41f9d
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-16 22:41:45 -0700

    chore(web): enable purgecss

--- web/bin/postbuild.sh
+++ web/bin/postbuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-# purgecss --css build/static/css/*.css --content build/index.html build/static/js/*.js --output build/static/css
+purgecss --css build/static/css/*.css --content build/index.html build/static/js/*.js --output build/static/css
 rm -rf tmp
