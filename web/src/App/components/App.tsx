23e81d38fbbefd48cd19da0f9a54b7ed1df17f47
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-16 23:05:26 -0700

    chore(web): remove top level lazy loading

--- web/src/App/components/App.tsx
+++ web/src/App/components/App.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import HmLazyComponent from '../../shared/components/LazyComponent';
+import HmHome from '../../Home/components/Home';
+import HmLab from '../../Lab/components/Lab';
+import HmSignIn from '../../SignIn/components/SignIn';
 import Paths from '../../shared/utils/paths';
-
-const HmHome = React.lazy(() => import('../../Home/components/Home'));
-const HmSignIn = React.lazy(() => import('../../SignIn/components/SignIn'));
-const HmLab = React.lazy(() => import('../../Lab/components/Lab'));
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <HmLazyComponent>
-      <Routes>
-        <Route path={Paths.appRootPath} element={<HmHome />} />
-        <Route path={Paths.signInPath} element={<HmSignIn />} />
-        <Route path={Paths.labPath} element={<HmLab />} />
-        <Route path="*" element={<Navigate to={Paths.appRootPath} />} />
-      </Routes>
-    </HmLazyComponent>
+    <Routes>
+      <Route path={Paths.appRootPath} element={<HmHome />} />
+      <Route path={Paths.signInPath} element={<HmSignIn />} />
+      <Route path={Paths.labPath} element={<HmLab />} />
+      <Route path="*" element={<Navigate to={Paths.appRootPath} />} />
+    </Routes>
   </BrowserRouter>
 );
 
