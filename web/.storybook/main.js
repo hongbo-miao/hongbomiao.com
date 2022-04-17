ecee7a77c2f8aeeb0d394d5cacd4cbc34c9b0781
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-16 17:10:36 -0700

    chore(deps): update storybook to 6.5.0-alpha.63

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -1,8 +1,7 @@
 module.exports = {
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/preset-create-react-app',
-  ],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/preset-create-react-app'],
   stories: ['../src/**/*.story.tsx'],
+  core: {
+    builder: 'webpack5'
+  }
 };
