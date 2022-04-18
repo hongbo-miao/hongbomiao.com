7b1abde5b936952d35e15307d3cfc8712925d267
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-18 15:04:37 -0700

    feat(hasura): add migrations

---
+++ hasura-graphql-engine/migrations/opa_db/1650319004915_create_opal_clients_table/up.sql
@@ -0,0 +1,24 @@
+create or replace function trigger_update_updated_at()
+  returns trigger as
+$$
+begin
+  new.updated_at = now();
+  return new;
+end;
+$$ language plpgsql;
+
+create table if not exists opal_clients
+(
+  id         uuid                 default gen_random_uuid(),
+  name       text        not null,
+  config     text,
+  created_on timestamptz not null default now(),
+  updated_at timestamptz not null default now(),
+  primary key (id)
+);
+
+create trigger update_updated_at
+  before update
+  on opal_clients
+  for each row
+execute procedure trigger_update_updated_at();
