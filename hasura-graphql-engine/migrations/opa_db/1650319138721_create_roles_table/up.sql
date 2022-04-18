8286b9174816a9d0c614cf24a897930500d8c650
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-18 15:04:37 -0700

    feat(hasura): add migrations

---
+++ hasura-graphql-engine/migrations/opa_db/1650319138721_create_roles_table/up.sql
@@ -0,0 +1,30 @@
+create or replace function trigger_update_updated_at()
+  returns trigger as
+$$
+begin
+  new.updated_at = now();
+  return new;
+end;
+$$ language plpgsql;
+
+create table if not exists roles
+(
+  id        uuid                 default gen_random_uuid(),
+  opal_client_id uuid        not null,
+  role           text        not null,
+  allow          jsonb       not null,
+  created_on     timestamptz not null default now(),
+  updated_at     timestamptz not null default now(),
+  primary key (id),
+  constraint fk_space
+    foreign key (opal_client_id)
+      references opal_clients (id)
+);
+
+alter table roles replica identity full;
+
+create trigger update_updated_at
+  before update
+  on roles
+  for each row
+execute procedure trigger_update_updated_at();
