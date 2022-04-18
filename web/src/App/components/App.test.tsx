a79ba6c9fd8aa62b7109899ea72e6e93303ac838
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-18 15:53:51 -0700

    test(web): remove App.test.tsx

--- web/src/App/components/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import HmApp from './App';
-
-describe('App', () => {
-  test('render App', () => {
-    render(<HmApp />);
-  });
-});
