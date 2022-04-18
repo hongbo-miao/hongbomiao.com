df213f802aa9d6526e9f2dbd584d50bb33c7f547
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-18 15:44:58 -0700

    feat(k8s): add migrations and metadata in hasura

--- kubernetes/bin/utils/installHasura.sh
+++ kubernetes/bin/utils/installHasura.sh
@@ -2,6 +2,17 @@
 set -e
 
 echo "# Install Hasura"
+cp -r hasura-graphql-engine/migrations/ kubernetes/data/hasura/migrations/
+cp -r hasura-graphql-engine/metadata/ kubernetes/data/hasura/metadata/
+
 kubectl apply --filename=kubernetes/config/hasura/hm-hasura-namespace.yaml
 kubectl apply --filename=kubernetes/config/hasura
+echo "=================================================="
+
+echo "# Add seed data in opa_db"
+kubectl port-forward service/postgres-service --namespace=hm-postgres 40072:40072 &
+sleep 5
+cd hasura-graphql-engine
+hasura seed apply
+pgrep kubectl | xargs kill -9
 echo "=================================================="
