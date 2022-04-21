8e92d0b1931704ad3a2cc42c01b81eda49c60f3e
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-21 13:55:29 -0700

    refactor(flink): split class

--- streaming/src/main/java/com/hongbomiao/Main.java
+++ streaming/src/main/java/com/hongbomiao/Main.java
@@ -1,5 +1,8 @@
 package com.hongbomiao;
 
+import com.hongbomiao.utils.Config;
+import com.hongbomiao.utils.Redis.MyRedisMapper;
+import com.hongbomiao.utils.Tweet;
 import java.sql.Timestamp;
 import java.util.Objects;
 import java.util.Properties;
@@ -7,7 +10,6 @@ import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
 import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
 import org.apache.flink.connector.jdbc.JdbcSink;
@@ -19,44 +21,22 @@ import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTime
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.redis.RedisSink;
 import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
-import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
-import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
-import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
 import org.apache.flink.streaming.connectors.twitter.TwitterSource;
 import org.apache.flink.util.Collector;
 
 public class Main {
-  static class Tweet {
-    public Tweet(Timestamp timestamp, String id, String id_str, String text, String lang) {
-      this.timestamp = timestamp;
-      this.id = id;
-      this.id_str = id_str;
-      this.text = text;
-      this.lang = lang;
-    }
-
-    final Timestamp timestamp;
-    final String id;
-    final String id_str;
-    final String text;
-    final String lang;
-  }
-
   public static void main(String[] args) throws Exception {
     final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
     // application-development.properties, application-production.properties
-    String propertiesFilePath = "streaming/src/main/resources/application-development.properties";
-    ParameterTool parameter = ParameterTool.fromPropertiesFile(propertiesFilePath);
-    parameter.getRequired("timescaledb.username");
-    System.out.println(parameter.getRequired("timescaledb.username"));
+    String paramsFilePath = "streaming/src/main/resources/application-production.properties";
+    Config config = new Config(paramsFilePath);
 
     Properties props = new Properties();
-    props.load(TwitterSource.class.getClassLoader().getResourceAsStream("application-development.properties"));
-    props.setProperty(TwitterSource.CONSUMER_KEY, props.getProperty("twitter.api.key"));
-    props.setProperty(TwitterSource.CONSUMER_SECRET, props.getProperty("twitter.api.secret.key"));
-    props.setProperty(TwitterSource.TOKEN, props.getProperty("twitter.access.token"));
-    props.setProperty(TwitterSource.TOKEN_SECRET, props.getProperty("twitter.access.token.secret"));
+    props.setProperty(TwitterSource.CONSUMER_KEY, config.twitterAPIKey);
+    props.setProperty(TwitterSource.CONSUMER_SECRET, config.twitterAPISecretKey);
+    props.setProperty(TwitterSource.TOKEN, config.twitterAccessToken);
+    props.setProperty(TwitterSource.TOKEN_SECRET, config.twitterAccessTokenSecret);
 
     DataStream<String> streamSource = env.addSource(new TwitterSource(props));
     DataStream<JsonNode> tweetStream =
@@ -103,7 +83,7 @@ public class Main {
                 (statement, tweet) -> {
                   statement.setTimestamp(1, tweet.timestamp);
                   statement.setString(2, tweet.id);
-                  statement.setString(3, tweet.id_str);
+                  statement.setString(3, tweet.idStr);
                   statement.setString(4, tweet.text);
                   statement.setString(5, tweet.lang);
                 },
@@ -114,9 +94,9 @@ public class Main {
                     .build(),
                 new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
                     .withDriverName("org.postgresql.Driver")
-                    .withUrl(parameter.getRequired("timescaledb.url"))
-                    .withUsername(parameter.getRequired("timescaledb.username"))
-                    .withPassword(parameter.getRequired("timescaledb.password"))
+                    .withUrl(config.timescaleDBURL)
+                    .withUsername(config.timescaleDBUsername)
+                    .withPassword(config.timescaleDBPassword)
                     .build()));
 
     // Sink to Redis
@@ -137,13 +117,12 @@ public class Main {
             .keyBy(value -> value.f0)
             .window(TumblingProcessingTimeWindows.of(Time.seconds(10)))
             .sum(1);
-
     tweetsPerWindow.print();
 
     FlinkJedisPoolConfig conf =
         new FlinkJedisPoolConfig.Builder()
-            .setHost(parameter.getRequired("redis.host"))
-            .setPort(Integer.parseInt(parameter.getRequired("redis.port")))
+            .setHost(config.redisHost)
+            .setPort(config.redisPort)
             .build();
     tweetsPerWindow
         .map(
@@ -156,31 +135,5 @@ public class Main {
         .addSink(new RedisSink<Tuple2<String, String>>(conf, new MyRedisMapper()));
 
     env.execute("Get trending Twitter hashtags");
-  }
-
-  public static class Splitter implements FlatMapFunction<JsonNode, Tuple2<String, Integer>> {
-    @Override
-    public void flatMap(JsonNode value, Collector<Tuple2<String, Integer>> out) throws Exception {
-      for (JsonNode hashtag : value.get("entities").get("hashtags")) {
-        out.collect(new Tuple2<String, Integer>(hashtag.get("text").asText(), 1));
-      }
-    }
-  }
-
-  public static class MyRedisMapper implements RedisMapper<Tuple2<String, String>> {
-    @Override
-    public RedisCommandDescription getCommandDescription() {
-      return new RedisCommandDescription(RedisCommand.HSET, "trending-twitter-hashtags");
-    }
-
-    @Override
-    public String getKeyFromData(Tuple2<String, String> data) {
-      return data.f0;
-    }
-
-    @Override
-    public String getValueFromData(Tuple2<String, String> data) {
-      return data.f1;
-    }
   }
 }
---
+++ streaming/src/main/java/com/hongbomiao/utils/Config.java
@@ -0,0 +1,30 @@
+package com.hongbomiao.utils;
+
+import java.io.IOException;
+import org.apache.flink.api.java.utils.ParameterTool;
+
+public final class Config {
+  public Config(String paramsFilePath)
+    throws IOException {
+    ParameterTool params = ParameterTool.fromPropertiesFile(paramsFilePath);
+    this.twitterAPIKey = params.getRequired("twitter.api.key");
+    this.twitterAPISecretKey = params.getRequired("twitter.api.secret.key");
+    this.twitterAccessToken = params.getRequired("twitter.access.token");
+    this.twitterAccessTokenSecret = params.getRequired("twitter.access.token.secret");
+    this.timescaleDBURL = params.getRequired("timescaledb.url");
+    this.timescaleDBUsername = params.getRequired("timescaledb.username");
+    this.timescaleDBPassword = params.getRequired("timescaledb.password");
+    this.redisHost = params.getRequired("redis.host");
+    this.redisPort = Integer.parseInt(params.getRequired("redis.port"));
+  }
+
+  public final String twitterAPIKey;
+  public final String twitterAPISecretKey;
+  public final String twitterAccessToken;
+  public final String twitterAccessTokenSecret;
+  public final String timescaleDBURL;
+  public final String timescaleDBUsername;
+  public final String timescaleDBPassword;
+  public final String redisHost;
+  public final int redisPort;
+}
