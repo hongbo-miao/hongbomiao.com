20d1ba2a29d3ec41bd8fc1d33ef75cf2bbb11381
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-22 00:19:05 -0700

    feat(timescale): add docker timescaledb for local development

---
+++ kubernetes/bin/utils/migrateTimescaleDBDocker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+echo "# Create database in TimescaleDB"
+psql postgresql://admin:passw0rd@localhost:16863/postgres --command="create database twitter_db;"
+psql postgresql://admin:passw0rd@localhost:16863/postgres --command="grant all privileges on database twitter_db to admin;"
+echo "=================================================="
+
+echo "# Migrate twitter_db in TimescaleDB"
+TIMESCALEDB_URL="postgresql://admin:passw0rd@localhost:16863/twitter_db?sslmode=disable&search_path=public"
+migrate -database "${TIMESCALEDB_URL}" -path kubernetes/data/timescaledb/twitter_db/migrations up
+echo "=================================================="
