24bb3cd30d6b1fd679a1d081a24ceb4bf0eaa33f
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-22 01:33:10 -0700

    feat(flink): sink to twitter_user table

--- streaming/src/main/java/com/hongbomiao/Main.java
+++ streaming/src/main/java/com/hongbomiao/Main.java
@@ -3,6 +3,7 @@ package com.hongbomiao;
 import com.hongbomiao.utils.Config;
 import com.hongbomiao.utils.Redis.MyRedisMapper;
 import com.hongbomiao.utils.Tweet;
+import com.hongbomiao.utils.TwitterUser;
 import java.sql.Timestamp;
 import java.util.Objects;
 import java.util.Properties;
@@ -66,24 +67,57 @@ public class Main {
     // Sink to TimescaleDB
     tweetStream
         .map(
+            new MapFunction<JsonNode, TwitterUser>() {
+              @Override
+              public TwitterUser map(JsonNode value) throws Exception {
+                return (new TwitterUser(
+                    value.get("user").get("id_str").asText(),
+                    value.get("user").get("name").asText(),
+                    value.get("user").get("profile_image_url_https").asText()));
+              }
+            })
+        .addSink(
+            JdbcSink.sink(
+                "insert into twitter_user (id, name, avatar) values (?, ?, ?);",
+                (statement, tweet) -> {
+                  statement.setString(1, tweet.id);
+                  statement.setString(2, tweet.name);
+                  statement.setString(3, tweet.avatar);
+                },
+                JdbcExecutionOptions.builder()
+                    .withBatchSize(1000)
+                    .withBatchIntervalMs(200)
+                    .withMaxRetries(5)
+                    .build(),
+                new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                    .withDriverName("org.postgresql.Driver")
+                    .withUrl(config.timescaleDBURL)
+                    .withUsername(config.timescaleDBUsername)
+                    .withPassword(config.timescaleDBPassword)
+                    .build()));
+
+    tweetStream
+        .map(
             new MapFunction<JsonNode, Tweet>() {
               @Override
               public Tweet map(JsonNode value) throws Exception {
                 return (new Tweet(
                     new Timestamp(Long.parseLong(value.get("timestamp_ms").asText())),
                     value.get("id_str").asText(),
+                    value.get("user").get("id_str").asText(),
                     value.get("text").asText(),
                     value.get("lang").asText()));
               }
             })
         .addSink(
             JdbcSink.sink(
-                "insert into tweets (timestamp, id, text, lang) values (?, ?, ?, ?, ?)",
+                "insert into tweet (timestamp, id, twitter_user_id, text, lang) values (?, ?, ?, ?, ?)",
                 (statement, tweet) -> {
                   statement.setTimestamp(1, tweet.timestamp);
                   statement.setString(2, tweet.id);
-                  statement.setString(3, tweet.text);
-                  statement.setString(4, tweet.lang);
+                  statement.setString(3, tweet.twitterUserID);
+                  statement.setString(4, tweet.text);
+                  statement.setString(5, tweet.lang);
                 },
                 JdbcExecutionOptions.builder()
                     .withBatchSize(1000)
@@ -121,6 +155,7 @@ public class Main {
         new FlinkJedisPoolConfig.Builder()
             .setHost(config.redisHost)
             .setPort(config.redisPort)
+            .setPassword(config.redisPassword)
             .build();
     tweetsPerWindow
         .map(
---
+++ streaming/src/main/java/com/hongbomiao/utils/TwitterUser.java
@@ -0,0 +1,13 @@
+package com.hongbomiao.utils;
+
+public final class TwitterUser {
+  public TwitterUser(String id, String name, String avatar) {
+    this.id = id;
+    this.name = name;
+    this.avatar = avatar;
+  }
+
+  public final String id;
+  public final String name;
+  public final String avatar;
+}
