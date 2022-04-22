2e3e9fc5b9c44cde6d43da2f924b145f40b868ae
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-22 00:02:11 -0700

    refactor(db): change table name from plural to singular

--- kubernetes/data/postgres/opa_db/migrations/000001_create_opal_clients_table.down.sql
+++
@@ -1,3 +0,0 @@
-drop trigger if exists update_updated_at on opal_clients;
-drop table if exists clients;
-drop function if exists trigger_update_updated_at;
---
+++ kubernetes/data/timescaledb/twitter_db/migrations/000001_create_twitter_user_table.down.sql
@@ -0,0 +1,1 @@
+drop table if exists twitter_user;
