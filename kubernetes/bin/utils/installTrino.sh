97ee2f2d8819c1043c4c518de29b5e662c567d1e
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-26 00:32:41 -0700

    feat(k8s): add Trino

---
+++ kubernetes/bin/utils/installTrino.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+echo "# Install Trino"
+# https://github.com/trinodb/charts
+kubectl apply --filename=kubernetes/manifests/trino/hm-trino-namespace.yaml
+helm repo add trino https://trinodb.github.io/charts
+helm install \
+  trino \
+  trino/trino \
+  --namespace=hm-trino \
+  --values=kubernetes/manifests/trino/helm/my-values.yaml
+
+# Delete:
+# helm uninstall trino --namespace=hm-trino
+echo "=================================================="
