40f351c52fd274d1ccd2d579c0d4e363d6608a0c
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-26 11:28:34 -0700

    refactor(k8s): rasa helm files

--- kubernetes/bin/utils/uninstallRasa.sh
+++ kubernetes/bin/utils/uninstallRasa.sh
@@ -8,5 +8,5 @@ echo "=================================================="
 
 echo "# Uninstall Rasa Action Server"
 helm uninstall hm-release --namespace=hm-rasa-action-server
-kubectl delete --filename=kubernetes/manifests/rasa/hm-rasa-action-server-namespace.yaml
+kubectl delete --filename=kubernetes/manifests/rasa-action-server/hm-rasa-action-server-namespace.yaml
 echo "=================================================="
