1ec4d9ea0db55271f6a4ee531be367288898d99c
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-04-27 01:29:27 -0700

    refactor(hydra): move helm values file

--- trino/query.sql
+++ trino/query.sql
@@ -2,9 +2,14 @@ show catalogs;
 show schemas in tpcds;
 show tables from tpch.sf1;
 
-select count(name) from tpch.sf1.nation;
+select count(name)
+from tpch.sf1.nation;
 
 use tpch.sf1;
-select count(name) from nation;
 
-select nationkey, name, regionkey from tpch.sf1.nation limit 5;
+select count(name)
+from nation;
+
+select nationkey, name, regionkey
+from tpch.sf1.nation
+limit 5;
