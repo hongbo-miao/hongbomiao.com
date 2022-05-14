2a757a452728bd34559ed761011b5c91cf975b2e
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-05-13 23:49:09 -0700

    refactor(app): rename folder keydb to hm-keydb

--- kubernetes/bin/setupMinikube.sh
+++ kubernetes/bin/setupMinikube.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+echo "# Setup minikube"
 minikube start --driver=hyperkit --cpus=2 --memory=8g
 # minikube delete
 
 minikube addons enable ingress
 minikube mount ./kubernetes/data:/data
+echo "=================================================="
