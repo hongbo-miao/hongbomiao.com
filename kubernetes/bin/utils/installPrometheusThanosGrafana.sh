134b567d33ad42e9316b9fd2c773796500f8a71b
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-05-15 01:23:53 -0700

    feat(k8s): add Thanos sidecar

---
+++ kubernetes/bin/utils/installPrometheusThanosGrafana.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+echo "# Install Prometheus and Grafana"
+# https://github.com/prometheus-community/helm-charts/tree/main/charts/kube-prometheus-stack
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+# helm search repo prometheus-community
+helm install \
+  monitoring \
+  prometheus-community/kube-prometheus-stack \
+  --namespace=hm-monitoring \
+  --create-namespace \
+  --values=kubernetes/manifests/prometheus/helm/my-values.yaml
+
+# Upgrade:
+# helm upgrade \
+#   monitoring \
+#   prometheus-community/kube-prometheus-stack \
+#   --namespace=hm-monitoring \
+#   --values=kubernetes/manifests/prometheus/helm/my-values.yaml
+
+# Delete:
+# helm uninstall monitoring --namespace=hm-monitoring
+
+# Grafana
+# Username: admin
+# Password: passw0rd
+echo "=================================================="
