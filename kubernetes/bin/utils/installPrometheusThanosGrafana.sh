56c601cac78c0c52210d1caecc49b96536df0f31
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-05-15 11:52:42 -0700

    docs(readme): update Architecture image

--- kubernetes/bin/utils/installPrometheusThanosGrafana.sh
+++ kubernetes/bin/utils/installPrometheusThanosGrafana.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-echo "# Install Prometheus and Grafana"
+echo "# Install Prometheus with Thanos Sidecar, Grafana"
 # https://github.com/prometheus-community/helm-charts/tree/main/charts/kube-prometheus-stack
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
