7f24f93c73a7ea7aeb91915f68bd4c8175a5d886
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-07-14 02:37:05 -0700

    feat(app): add airflow

---
+++ kubernetes/bin/utils/installAirflow.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+echo "# Install Airflow"
+# https://airflow.apache.org/docs/helm-chart/stable/index.html
+helm repo add apache-airflow https://airflow.apache.org
+helm repo update
+helm install \
+  airflow \
+  apache-airflow/airflow \
+  --namespace=hm-airflow \
+  --create-namespace \
+  --values=kubernetes/manifests/airflow/helm/my-values.yaml
+
+# Upgrade:
+# helm upgrade \
+#   airflow \
+#   apache-airflow/airflow \
+#   --namespace=hm-airflow \
+#   --values=kubernetes/manifests/airflow/helm/my-values.yaml
+
+# Delete:
+# helm uninstall airflow --namespace=hm-airflow
+echo "=================================================="
