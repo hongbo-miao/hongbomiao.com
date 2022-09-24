d17de7b193f9464a9028852d4c98fb3cd023147e
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-09-23 19:45:00 -0700

    feat(airflow): add greet and s3_download dags

---
+++ airflow/dags/greet.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from typing import Dict
+
+from airflow.decorators import task, task_group
+from airflow.operators.bash import BashOperator
+
+from airflow import DAG
+
+with DAG(
+    "greet",
+    start_date=datetime(2022, 1, 1),
+    schedule_interval="@once",
+    catchup=False,
+    params={
+        "first_name": "Hongbo",
+        "last_name": "Miao",
+    },
+) as dag:
+
+    @task(task_id="get_name")
+    def get_name(params=None) -> Dict[str, str]:
+        return {
+            "first_name": params["first_name"],
+            "last_name": params["last_name"],
+        }
+
+    @task(task_id="transform_first_name")
+    def transform_first_name(name: Dict[str, str]) -> str:
+        return name["first_name"][0]
+
+    @task
+    def transform_last_name(name: Dict[str, str]) -> str:
+        return name["last_name"][0]
+
+    @task
+    def get_initials(first_name_initial: str, last_name_initial: str) -> str:
+        return f"{first_name_initial}.{last_name_initial}."
+
+    get_time = BashOperator(
+        task_id="get_time",
+        bash_command="date '+%Y-%m-%d %H:%M:%S'",
+    )
+
+    @task
+    def greet(initials: str, ti=None) -> str:
+        time = ti.xcom_pull(task_ids="get_time")
+        dt = datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+        return f'Hello {initials}, {dt.strftime("at %H:%M on %B %d, %Y")}!'
+
+    @task_group
+    def initials_group_function(name: Dict[str, str]) -> str:
+        return get_initials(transform_first_name(name), transform_last_name(name))
+
+    my_name = get_name()
+    my_initials = initials_group_function(my_name)
+    run_greet = greet(my_initials)
+
+    [my_initials, get_time] >> run_greet
---
+++ airflow/dags/s3_download.py
@@ -0,0 +1,35 @@
+import os
+from datetime import datetime
+from pathlib import Path
+
+from airflow.decorators import task
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
+from airflow import DAG
+
+with DAG(
+    "s3_download",
+    start_date=datetime(2022, 1, 1),
+    schedule_interval="@once",
+    catchup=False,
+) as dag:
+
+    @task
+    def download_from_s3(key: str, bucket: str, local_path: str) -> str:
+        hook = S3Hook("s3_connection")
+        file_path = hook.download_file(
+            key=key, bucket_name=bucket, local_path=local_path
+        )
+        return file_path
+
+    @task
+    def rename_file(file_path: str, new_file_name: str) -> None:
+        path = Path(file_path)
+        os.rename(src=file_path, dst=path.parent.joinpath(new_file_name))
+
+    file_name = download_from_s3(
+        bucket="hongbomiao-bucket",
+        key="hm-airflow/taxi.csv",
+        local_path="/tmp/",
+    )
+    rename_file(file_name, "taxi.csv")
