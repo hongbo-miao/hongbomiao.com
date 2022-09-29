505b5aef84f78c298573cd5e85cdb028f2478d14
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-09-29 01:23:08 -0700

    feat(prefect): add input

--- prefect/get_taxi_statistics.py
+++ prefect/get_taxi_statistics.py
@@ -1,11 +1,23 @@
 import asyncio
 import io
+from enum import Enum
 
 import pandas as pd
 from prefect_aws.credentials import AwsCredentials
 from prefect_aws.s3 import s3_download
+from pydantic import BaseModel
 
 from prefect import flow, get_run_logger, task
+
+
+class CalcMethod(Enum):
+    MEDIAN = "median"
+    MEAN = "mean"
+
+
+class Model(BaseModel):
+    calc_method: CalcMethod
+    trip_id: int
 
 
 @task
@@ -26,13 +38,25 @@ def get_dataframe(data: bytes) -> pd.DataFrame:
 
 
 @task
-def get_average_amount(df: pd.DataFrame) -> float:
-    return df["total_amount"].mean()
+def get_average_amount(df: pd.DataFrame, calc_method: CalcMethod) -> float:
+    match calc_method:
+        case CalcMethod.MEAN:
+            return df["total_amount"].mean()
+        case CalcMethod.MEDIAN:
+            return df["total_amount"].median()
+        case _:
+            raise ValueError(f"Unknown calc_method: {calc_method}")
 
 
 @task
-def get_average_trip_distance(df: pd.DataFrame) -> float:
-    return df["trip_distance"].mean()
+def get_average_trip_distance(df: pd.DataFrame, calc_method: CalcMethod) -> float:
+    match calc_method:
+        case CalcMethod.MEAN:
+            return df["trip_distance"].mean()
+        case CalcMethod.MEDIAN:
+            return df["trip_distance"].median()
+        case _:
+            raise ValueError(f"Unknown calc_method: {calc_method}")
 
 
 @task
@@ -40,8 +64,18 @@ def get_price_per_mile(trip_distance: float, amount: float) -> float:
     return amount / trip_distance
 
 
+@task
+def get_trip(trip_id: int, df: pd.DataFrame) -> pd.Series:
+    return df.loc[trip_id, :]
+
+
+@task
+def get_is_higher_than_unit_price(trip: pd.Series, average_amount: float) -> bool:
+    return trip["total_amount"] / trip["trip_distance"] > average_amount
+
+
 @flow
-async def get_taxi_statistics() -> None:
+async def get_taxi_statistics(model: Model) -> None:
     logger = get_run_logger()
     credentials = await AwsCredentials.load("aws-credentials-block")
     data = await s3_download(
@@ -51,15 +85,20 @@ async def get_taxi_statistics() -> None:
     )
     df = get_dataframe(data)
 
-    average_trip_distance = get_average_trip_distance(df)
+    average_trip_distance = get_average_trip_distance(df, model.calc_method)
     logger.info(average_trip_distance)
 
-    average_amount = get_average_amount(df)
+    average_amount = get_average_amount(df, model.calc_method)
     logger.info(average_amount)
 
     price_per_mile = get_price_per_mile(average_trip_distance, average_amount)
     logger.info(price_per_mile)
 
+    trip = get_trip(model.trip_id, df)
+    is_higher_than_average_amount = get_is_higher_than_unit_price(trip, average_amount)
+    logger.info(is_higher_than_average_amount)
+
 
 if __name__ == "__main__":
-    asyncio.run(get_taxi_statistics())
+    external_model = Model(calc_method=CalcMethod.MEDIAN, trip_id=10)
+    asyncio.run(get_taxi_statistics(external_model))
