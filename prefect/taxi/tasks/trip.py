8eba49ff875ed424651fd013bfd6ab4ea85a20ec
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-09-30 01:11:33 -0700

    feat(prefect): get top routes

---
+++ prefect/taxi/tasks/trip.py
@@ -0,0 +1,54 @@
+import io
+
+import pandas as pd
+from utils.enum import CalcMethod
+
+from prefect import task
+
+
+@task
+def get_trips(data: bytes) -> pd.DataFrame:
+    df = pd.read_csv(
+        io.BytesIO(data), parse_dates=["tpep_pickup_datetime", "tpep_dropoff_datetime"]
+    )
+    df = df.rename(str.lower, axis="columns")
+    df["store_and_fwd_flag"] = df["store_and_fwd_flag"].astype("bool")
+    mask = df["total_amount"] > 0
+    return df[mask]
+
+
+@task
+def get_average_amount(trips: pd.DataFrame, calc_method: CalcMethod) -> float:
+    match calc_method:
+        case CalcMethod.MEAN:
+            return trips["total_amount"].mean()
+        case CalcMethod.MEDIAN:
+            return trips["total_amount"].median()
+        case _:
+            raise ValueError(f"Unknown calc_method: {calc_method}")
+
+
+@task
+def get_average_trip_distance(trips: pd.DataFrame, calc_method: CalcMethod) -> float:
+    match calc_method:
+        case CalcMethod.MEAN:
+            return trips["trip_distance"].mean()
+        case CalcMethod.MEDIAN:
+            return trips["trip_distance"].median()
+        case _:
+            raise ValueError(f"Unknown calc_method: {calc_method}")
+
+
+@task
+def get_price_per_mile(trip_distance: float, amount: float) -> float:
+    return amount / trip_distance
+
+
+@task
+def get_trip(trip_id: int, trips: pd.DataFrame) -> pd.Series:
+    return trips.loc[trip_id, :]
+
+
+@task
+def get_is_higher_than_unit_price(trip: pd.Series, average_amount: float) -> bool:
+    return trip["total_amount"] / trip["trip_distance"] > average_amount
