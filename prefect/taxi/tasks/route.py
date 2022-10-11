766a447da89941d7d7111081cd33be55b25bccaf
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-10-10 18:03:33 -0700

    perf(prefect): change source from csv to parquet

--- prefect/taxi/tasks/route.py
+++ prefect/taxi/tasks/route.py
@@ -4,22 +4,16 @@ from prefect import get_run_logger, task
 
 
 @task
-def get_top_routes(trips: pd.DataFrame, top_count: int) -> pd.DataFrame:
+def get_top_routes(trips: pd.DataFrame, zones: pd.DataFrame) -> pd.DataFrame:
     routes = (
         trips.value_counts(subset=["pulocationid", "dolocationid"], ascending=False)
         .to_frame("count")
         .reset_index()
+        .merge(zones, left_on="pulocationid", right_on="locationid", how="inner")
+        .rename(columns={"zone": "pulocation_zone", "borough": "pulocation_borough"})
+        .drop(columns=["locationid", "shape_area", "shape_leng", "the_geom"])
+        .merge(zones, left_on="dolocationid", right_on="locationid", how="inner")
+        .rename(columns={"zone": "dolocation_zone", "borough": "dolocation_borough"})
+        .drop(columns=["locationid", "shape_area", "shape_leng", "the_geom"])
     )
-    mask = routes["pulocationid"] != routes["dolocationid"]
-    routes = routes[mask].reset_index(drop=True)
-    return routes[:top_count]
-
-
-@task
-def print_routes(routes: pd.DataFrame, zones: pd.DataFrame) -> None:
-    logger = get_run_logger()
-    for _, route in routes.iterrows():
-        u = zones.loc[route["pulocationid"]]["zone"]
-        v = zones.loc[route["dolocationid"]]["zone"]
-        count = route["count"]
-        logger.info(f"{u} -> {v}: {count}")
+    return routes
