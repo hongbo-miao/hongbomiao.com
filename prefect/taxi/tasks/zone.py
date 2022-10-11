cb3c05a0b9b77e988a89e9fa3e5654743b81814f
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-10-10 20:58:19 -0700

    feat(prefect): download multiple parquets from s3

--- prefect/taxi/tasks/zone.py
+++ prefect/taxi/tasks/zone.py
@@ -6,8 +6,7 @@ from prefect import task
 
 
 @task
-def get_zones(data: bytes) -> pd.DataFrame:
-    df = pd.read_csv(io.BytesIO(data))
+def preprocess_zones(df: pd.DataFrame) -> pd.DataFrame:
     df = df.rename(str.lower, axis="columns")
     df["borough"] = df["borough"].astype("category")
     return df.drop(columns=["objectid"])
