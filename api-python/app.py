4d17727977dd9a59b9fa8750b08e90b910b8836d
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-10-31 19:00:53 -0700

    feat(flask): add csrf

--- api-python/app.py
+++ api-python/app.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from flask_sock import Sock
-
-app = Flask(__name__)
-sock = Sock(app)
+from flask_wtf.csrf import CSRFProtect
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+def create_app() -> Flask:
+    app = Flask(__name__)
+    sock = Sock(app)
+    csrf = CSRFProtect()
+    csrf.init_app(app)
 
+    @app.route("/")
+    def hello():
+        return "Hello World!"
 
-@sock.route("/echo")
-def echo(ws):
-    while True:
-        data = ws.receive()
-        ws.send(data)
+    @sock.route("/echo")
+    def echo(ws):
+        while True:
+            data = ws.receive()
+            ws.send(data)
+
+    return app
