139fcaa1927bfab8a28c13565316d3e43b8702ae
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-11-03 11:56:34 -0700

    feat(grafana): add query

--- grafana/hm-panel-plugin/src/components/SimplePanel.tsx
+++ grafana/hm-panel-plugin/src/components/SimplePanel.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 import { css, cx } from '@emotion/css';
-import { useStyles2, useTheme2 } from '@grafana/ui';
+import { Button, useStyles2, useTheme2 } from '@grafana/ui';
 
 interface Props extends PanelProps<SimpleOptions> {}
 
@@ -26,36 +26,53 @@ const getStyles = () => {
   };
 };
 
-export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
+const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
   const theme = useTheme2();
   const styles = useStyles2(getStyles);
-  return (
-    <div
-      className={cx(
-        styles.wrapper,
-        css`
-          width: ${width}px;
-          height: ${height}px;
-        `
-      )}
-    >
-      <svg
-        className={styles.svg}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-      >
-        <g>
-          <circle style={{ fill: theme.colors.primary.main }} r={100} />
-        </g>
-      </svg>
 
-      <div className={styles.textBox}>
-        {options.showSeriesCount && <div>Number of series: {data.series.length}</div>}
-        <div>Text option value: {options.text}</div>
+  console.log('options', options);
+  console.log('data', data);
+  console.log('height', height);
+  console.log('width', width);
+
+  const onClick = () => {
+    console.log('Hi');
+  };
+
+  return (
+    <div>
+      <Button variant="primary" type="button" onClick={onClick}>
+        Click me
+      </Button>
+      <div
+        className={cx(
+          styles.wrapper,
+          css`
+            width: ${width}px;
+            height: ${height}px;
+          `
+        )}
+      >
+        <svg
+          className={styles.svg}
+          width={width}
+          height={height}
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
+        >
+          <g>
+            <circle style={{ fill: theme.colors.primary.main }} r={100} />
+          </g>
+        </svg>
+
+        <div className={styles.textBox}>
+          {options.showSeriesCount && <div>Number of series: {data.series.length}</div>}
+          <div>Text option value: {options.text}</div>
+        </div>
       </div>
     </div>
   );
 };
+
+export default SimplePanel;
--- grafana/hm-panel-plugin/src/module.ts
+++ grafana/hm-panel-plugin/src/module.ts
@@ -1,6 +1,6 @@
 import { PanelPlugin } from '@grafana/data';
 import { SimpleOptions } from './types';
-import { SimplePanel } from './components/SimplePanel';
+import SimplePanel from './components/SimplePanel';
 
 export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions((builder) => {
   return builder
