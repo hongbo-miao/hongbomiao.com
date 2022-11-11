9e76a55859ebd2723868d6bafa2dcc8c75117087
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-11-11 10:37:52 -0800

    fix(lint): 'Element[]' is not a valid JSX element

--- web/src/App/components/SocialList.tsx
+++ web/src/App/components/SocialList.tsx
@@ -8,16 +8,20 @@ type Props = {
 
 function SocialList(props: Props) {
   const { websites } = props;
-  return websites.map((website) => {
-    const { name, src, url } = website;
-    return (
-      <div key={name} className="level-item">
-        <a href={url} target="_blank" rel="noopener noreferrer">
-          <img className={styles.hmLogo} src={src} height="28" width="28" alt={name} />
-        </a>
-      </div>
-    );
-  });
+  return (
+    <>
+      {websites.map((website) => {
+        const { name, src, url } = website;
+        return (
+          <div key={name} className="level-item">
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              <img className={styles.hmLogo} src={src} height="28" width="28" alt={name} />
+            </a>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default SocialList;
