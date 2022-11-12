7e322fdbeac6a5386a266065c9b09a10672c40d8
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-11-11 16:42:42 -0800

    fix(cypress): breaking change

---
+++ cypress/cypress.8080.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  env: {
+    domain: 'http://localhost',
+    webPort: '3000',
+    serverPort: '5000',
+  },
+  video: false,
+  e2e: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
