44b0e66e07b078c1e272294c7b5dfbc0e904785c
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-11-13 16:19:24 -0800

    chore(terraform): update providers

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "instance_id" {
-  value       = aws_instance.hm_cnn_instance.id
+  value = aws_instance.hm_cnn_instance.id
 }
 
 output "instance_public_ip" {
-  value       = aws_instance.hm_cnn_instance.public_ip
+  value = aws_instance.hm_cnn_instance.public_ip
 }
