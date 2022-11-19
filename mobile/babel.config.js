53f139a716309b274ca30d6ef514d1e5d6568649
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-11-19 11:26:57 -0800

    refactor(expo): upgrade version

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,8 +1,6 @@
-// eslint-disable-next-line func-names
-module.exports = function (api) {
+module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['module:react-native-dotenv']],
   };
 };
--- mobile/src/shared/types/RootState.type.ts
+++
@@ -1,5 +0,0 @@
-import rootReducer from '../reducers/rootReducer';
-
-type RootState = ReturnType<typeof rootReducer>;
-
-export default RootState;
