147edf9364864f555c2c1f1d6a13dd9ebe828a09
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-11-20 23:40:34 -0800

    test(flask): health

---
+++ api-python/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from hm_api_python import create_app
+
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({"TESTING": True})
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
