510f2c879778c5083ae56f4b21308006efc7878a
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-11-21 20:29:01 -0800

    feat(ios): init

---
+++ HMMobile/HMMobileUITests/HMMobileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HMMobileUITestsLaunchTests.swift
+//  HMMobileUITests
+//
+//  Created by Hongbo Miao on 11/21/22.
+//
+
+import XCTest
+
+final class HMMobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
