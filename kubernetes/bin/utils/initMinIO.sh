daf0de5372a20913472c6c30875a2fab72a4e283
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-11-28 16:31:30 -0800

    chore(sh): make wget and tar consistent

--- kubernetes/bin/utils/initMinIO.sh
+++ kubernetes/bin/utils/initMinIO.sh
@@ -4,7 +4,7 @@ set -e
 echo "# kubectl-minio"
 # Docs: https://github.com/minio/operator
 # Version: https://github.com/minio/operator/releases
-wget https://github.com/minio/operator/releases/download/v4.2.7/kubectl-minio_4.2.7_darwin_amd64 -O kubectl-minio
+wget --no-verbose --output-document=kubectl-minio https://github.com/minio/operator/releases/download/v4.2.7/kubectl-minio_4.2.7_darwin_amd64
 chmod +x kubectl-minio
 mv kubectl-minio /usr/local/bin/
 kubectl minio version
