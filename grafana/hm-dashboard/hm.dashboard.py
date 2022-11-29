5b5912e784f09e877f57f1eacbe474cacaabdf1f
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-11-28 22:38:47 -0800

    feat(grafana): init grafanalib dashboard

---
+++ grafana/hm-dashboard/hm.dashboard.py
@@ -0,0 +1,52 @@
+from grafanalib.core import (
+    OPS_FORMAT,
+    Dashboard,
+    GaugePanel,
+    GridPos,
+    Target,
+    TimeSeries,
+)
+
+dashboard = Dashboard(
+    title="Python generated example dashboard",
+    description="Example dashboard using the Random Walk and default Prometheus datasource",
+    tags=["example"],
+    timezone="browser",
+    panels=[
+        TimeSeries(
+            title="Random Walk",
+            dataSource="default",
+            targets=[
+                Target(
+                    datasource="grafana",
+                    expr="example",
+                ),
+            ],
+            gridPos=GridPos(h=8, w=16, x=0, y=0),
+        ),
+        GaugePanel(
+            title="Random Walk",
+            dataSource="default",
+            targets=[
+                Target(
+                    datasource="grafana",
+                    expr="example",
+                ),
+            ],
+            gridPos=GridPos(h=4, w=4, x=17, y=0),
+        ),
+        TimeSeries(
+            title="Prometheus http requests",
+            dataSource="prometheus",
+            targets=[
+                Target(
+                    expr="rate(prometheus_http_requests_total[5m])",
+                    legendFormat="{{ handler }}",
+                    refId="A",
+                ),
+            ],
+            unit=OPS_FORMAT,
+            gridPos=GridPos(h=8, w=16, x=0, y=10),
+        ),
+    ],
+).auto_panel_ids()
