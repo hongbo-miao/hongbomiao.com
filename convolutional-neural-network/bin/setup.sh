f5863b42c7853313d22c0f342034d36bf3b895c6
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-11-28 16:28:58 -0800

    chore(conda): add —yes

--- convolutional-neural-network/bin/setup.sh
+++ convolutional-neural-network/bin/setup.sh
@@ -6,9 +6,9 @@ conda create --name=hm-cnn python=3.9 --yes
 conda activate hm-cnn
 
 # Install requirements
-conda install pytorch torchvision torchaudio --channel=pytorch
-conda install dvc dvc-s3 --channel=conda-forge
-conda install pandas
-conda install tabulate
+conda install pytorch torchvision torchaudio --channel=pytorch --yes
+conda install dvc dvc-s3 --channel=conda-forge --yes
+conda install pandas --yes
+conda install tabulate --yes
 
 pip install --requirement=requirements.txt
