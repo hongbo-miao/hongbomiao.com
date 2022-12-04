f4ef7718d492317382a6ba27e4ff3daeb08d6e82
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-04 00:19:04 -0800

    fix(lint): auto fix by Ktlint

--- mobile-android/app/src/main/java/com/example/hmmobile/MainActivity.kt
+++ mobile-android/app/src/main/java/com/example/hmmobile/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.hmmobile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
