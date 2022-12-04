d0fe92793e0b0c27ef32d91e011faefdb37eaf30
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-04 01:02:03 -0800

    refactor(lint): js

--- bin/lintJS.sh
+++ bin/lintJS.sh
@@ -1,22 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-# Web
-cd web
-npm run lint:js
-
-# Mobile (React Native)
-cd ../mobile-react-native
-npm run lint:js
-
-# API (Node.js)
-cd ../api-node
-npm run lint:js
-
-# Cypress
-cd ../web-cypress
-npm run lint:js
-
-# Ethereum
-cd ../ethereum
-npm run lint:js
+cd api-node && npm run lint:js
+cd ../ethereum && npm run lint:js
+cd ../grafana/hm-panel-plugin && npm run lint:js
+cd ../../mobile-react-native && npm run lint:js
+cd ../web && npm run lint:js
+cd ../web-cypress && npm run lint:js
