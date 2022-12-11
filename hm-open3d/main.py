b1edca44cfed08779bc24f65527c77f86d29a37a
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-10 16:57:43 -0800

    feat(open3d): visualize point cloud

--- hm-open3d/main.py
+++ hm-open3d/main.py
@@ -1,5 +1,14 @@
+import numpy as np
 import open3d as o3d
 
-mesh = o3d.geometry.TriangleMesh.create_sphere()
-mesh.compute_vertex_normals()
-o3d.visualization.draw(mesh, raw_mode=True)
+ply_point_cloud = o3d.data.PLYPointCloud()
+pcd = o3d.io.read_point_cloud(ply_point_cloud.path)
+print(pcd)
+print(np.asarray(pcd.points))
+o3d.visualization.draw_geometries(
+    [pcd],
+    zoom=0.3412,
+    front=[0.4257, -0.2125, -0.8795],
+    lookat=[2.6172, 2.0475, 1.532],
+    up=[-0.0694, -0.9768, 0.2024],
+)
