05530112d0fd9fd21cdc5a31205ca3e39ec7f8c4
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-11 20:44:09 -0800

    feat(open3d): crop chair and add axis aligned and oriented bounding box

--- hm-open3d/main.py
+++ hm-open3d/main.py
@@ -1,14 +1,38 @@
 import numpy as np
 import open3d as o3d
 
-ply_point_cloud = o3d.data.PLYPointCloud()
-pcd = o3d.io.read_point_cloud(ply_point_cloud.path)
-print(pcd)
-print(np.asarray(pcd.points))
-o3d.visualization.draw_geometries(
-    [pcd],
-    zoom=0.3412,
-    front=[0.4257, -0.2125, -0.8795],
-    lookat=[2.6172, 2.0475, 1.532],
-    up=[-0.0694, -0.9768, 0.2024],
-)
+
+def main():
+    ply_point_cloud = o3d.data.PLYPointCloud()
+    pcd = o3d.io.read_point_cloud(ply_point_cloud.path)
+    print(pcd)
+    print(np.asarray(pcd.points))
+
+    demo_crop_data = o3d.data.DemoCropPointCloud()
+    vol = o3d.visualization.read_selection_polygon_volume(
+        demo_crop_data.cropped_json_path
+    )
+    chair = vol.crop_point_cloud(pcd)
+
+    dists = pcd.compute_point_cloud_distance(chair)
+    dists = np.asarray(dists)
+    idx = np.where(dists > 0.01)[0]
+    pcd_without_chair = pcd.select_by_index(idx)
+
+    axis_aligned_bounding_box = chair.get_axis_aligned_bounding_box()
+    axis_aligned_bounding_box.color = (1, 0, 0)
+
+    oriented_bounding_box = chair.get_oriented_bounding_box()
+    oriented_bounding_box.color = (0, 1, 0)
+
+    o3d.visualization.draw_geometries(
+        [pcd_without_chair, chair, axis_aligned_bounding_box, oriented_bounding_box],
+        zoom=0.3412,
+        front=[0.4, -0.2, -0.9],
+        lookat=[2.6, 2.0, 1.5],
+        up=[-0.10, -1.0, 0.2],
+    )
+
+
+if __name__ == "__main__":
+    main()
