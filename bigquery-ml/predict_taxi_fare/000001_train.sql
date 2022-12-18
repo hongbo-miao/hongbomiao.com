70f8abdec4e4f537827a97e32c6c901c00dd7954
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-18 00:34:22 -0800

    feat(app): add BigQuery ML

---
+++ bigquery-ml/predict_taxi_fare/000001_train.sql
@@ -0,0 +1,39 @@
+CREATE OR REPLACE MODEL
+  `taxi.taxifare_model` OPTIONS (model_type='linear_reg', labels=['total_fare']) AS (
+    WITH
+      params AS (
+        SELECT
+          1 AS TRAIN,
+          2 AS EVAL),
+      daynames AS (
+        SELECT
+          ['Sun',
+          'Mon',
+          'Tues',
+          'Wed',
+          'Thurs',
+          'Fri',
+          'Sat'] AS daysofweek),
+      taxitrips AS (
+        SELECT
+          (tolls_amount + fare_amount) AS total_fare,
+          daysofweek[ORDINAL(EXTRACT(DAYOFWEEK FROM pickup_datetime))] AS dayofweek,
+          EXTRACT(HOUR FROM pickup_datetime) AS hourofday,
+          pickup_longitude AS pickuplon,
+          pickup_latitude AS pickuplat,
+          dropoff_longitude AS dropofflon,
+          dropoff_latitude AS dropofflat,
+          passenger_count AS passengers
+        FROM
+          `nyc-tlc.yellow.trips`,
+          daynames,
+          params
+        WHERE
+          trip_distance > 0
+          AND fare_amount > 0
+          AND MOD(ABS(FARM_FINGERPRINT(CAST(pickup_datetime AS STRING))), 1000) = params.TRAIN)
+    SELECT
+      *
+    FROM
+      taxitrips
+  );
