456b596bcd3b88b7e91f694afcf04adc0b0f220b
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-18 21:31:19 -0800

    refactor(quantum): use poetry

--- quantum-computing/main.py
+++ quantum-computing/main.py
@@ -1,34 +1,40 @@
 from qiskit import QuantumCircuit, transpile
 from qiskit.providers.aer import QasmSimulator
 
-# Use Aer's qasm_simulator
-simulator = QasmSimulator()
 
-# Create a Quantum Circuit acting on the q register
-circuit = QuantumCircuit(2, 2)
+def main():
+    # Use Aer's qasm_simulator
+    simulator = QasmSimulator()
 
-# Add a H gate on qubit 0
-circuit.h(0)
+    # Create a Quantum Circuit acting on the q register
+    circuit = QuantumCircuit(2, 2)
 
-# Add a CX (CNOT) gate on control qubit 0 and target qubit 1
-circuit.cx(0, 1)
+    # Add a H gate on qubit 0
+    circuit.h(0)
 
-# Map the quantum measurement to the classical bits
-circuit.measure([0, 1], [0, 1])
+    # Add a CX (CNOT) gate on control qubit 0 and target qubit 1
+    circuit.cx(0, 1)
 
-# compile the circuit down to low-level QASM instructions
-# supported by the backend (not needed for simple circuits)
-compiled_circuit = transpile(circuit, simulator)
+    # Map the quantum measurement to the classical bits
+    circuit.measure([0, 1], [0, 1])
 
-# Execute the circuit on the qasm simulator
-job = simulator.run(compiled_circuit, shots=1000)
+    # compile the circuit down to low-level QASM instructions
+    # supported by the backend (not needed for simple circuits)
+    compiled_circuit = transpile(circuit, simulator)
 
-# Grab results from the job
-result = job.result()
+    # Execute the circuit on the qasm simulator
+    job = simulator.run(compiled_circuit, shots=1000)
 
-# Returns counts
-counts = result.get_counts(compiled_circuit)
-print("Total count for 00 and 11 are:", counts)
+    # Grab results from the job
+    result = job.result()
 
-# Draw the circuit
-circuit.draw()
+    # Returns counts
+    counts = result.get_counts(compiled_circuit)
+    print("Total count for 00 and 11 are:", counts)
+
+    # Draw the circuit
+    circuit.draw()
+
+
+if __name__ == "__main__":
+    main()
