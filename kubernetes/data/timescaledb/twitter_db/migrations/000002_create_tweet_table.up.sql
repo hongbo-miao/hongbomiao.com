62ee026c32ae95e99844de6717362cf9e175df7f
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-18 23:15:05 -0800

    fix(lint): format SQL by SQLFluff

--- kubernetes/data/timescaledb/twitter_db/migrations/000002_create_tweet_table.up.sql
+++ kubernetes/data/timescaledb/twitter_db/migrations/000002_create_tweet_table.up.sql
@@ -1,10 +1,10 @@
 create table if not exists tweet
 (
-  timestamp       timestamp without time zone not null,
-  id              text                        not null,
-  twitter_user_id text                        not null,
-  text            text                        not null,
-  lang            text                        not null
+    timestamp timestamp without time zone not null,
+    id text not null,
+    twitter_user_id text not null,
+    text text not null,
+    lang text not null
 );
 
 select create_hypertable('tweet', 'timestamp');
