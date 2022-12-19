565394a5b1c437465f53c7c9ec0a386c53d29ca2
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-18 23:15:05 -0800

    fix(lint): format SQL by SQLFluff

--- hasura-graphql-engine/seeds/opa_db/1650319643177_roles_seed.sql
+++ hasura-graphql-engine/seeds/opa_db/1650319643177_roles_seed.sql
@@ -1,51 +1,62 @@
-with ins (opal_client_name, role, allow) as
-       (values ('hm-opal-client',
-                'customer',
-                '[
-                  {
-                    "action": "read",
-                    "resource": "dog"
-                  },
-                  {
-                    "action": "read",
-                    "resource": "cat"
-                  },
-                  {
-                    "action": "adopt",
-                    "resource": "dog"
-                  },
-                  {
-                    "action": "adopt",
-                    "resource": "cat"
-                  }
-                ]'::json),
-               ('hm-opal-client',
-                'employee',
-                '[
-                  {
-                    "action": "read",
-                    "resource": "dog"
-                  },
-                  {
-                    "action": "read",
-                    "resource": "cat"
-                  }
-                ]'::json),
-               ('hm-opal-client',
-                'billing',
-                '[
-                  {
-                    "action": "read",
-                    "resource": "finance"
-                  },
-                  {
-                    "action": "update",
-                    "resource": "finance"
-                  }
-                ]'::json)
-       )
+with ins (opal_client_name, role, allow) as (
+    values
+    (
+        'hm-opal-client',
+        'customer',
+        '[
+            {
+                "action": "read",
+                "resource": "dog"
+            },
+            {
+                "action": "read",
+                "resource": "cat"
+            },
+            {
+                "action": "adopt",
+                "resource": "dog"
+            },
+            {
+                "action": "adopt",
+                "resource": "cat"
+            }
+        ]'::json
+    ),
+    (
+        'hm-opal-client',
+        'employee',
+        '[
+            {
+                "action": "read",
+                "resource": "dog"
+            },
+            {
+                "action": "read",
+                "resource": "cat"
+            }
+        ]'::json
+    ),
+    (
+        'hm-opal-client',
+        'billing',
+        '[
+            {
+                "action": "read",
+                "resource": "finance"
+            },
+            {
+                "action": "update",
+                "resource": "finance"
+            }
+        ]'::json
+    )
+)
+
 insert
 into role (opal_client_id, role, allow)
-select opal_client.id, ins.role, ins.allow
+select
+    opal_client.id,
+    ins.role,
+    ins.allow
 from opal_client
-       join ins on ins.opal_client_name = opal_client.name;
+inner join ins on ins.opal_client_name = opal_client.name;
--- kubernetes/data/postgres/opa_db/migrations/000003_create_role_table.up.sql
+++ kubernetes/data/postgres/opa_db/migrations/000003_create_role_table.up.sql
@@ -1,5 +1,5 @@
 create or replace function trigger_update_updated_at()
-  returns trigger as
+returns trigger as
 $$
 begin
   new.updated_at = now();
@@ -9,20 +9,18 @@ $$ language plpgsql;
 
 create table if not exists role
 (
-  id        uuid                 default gen_random_uuid(),
-  opal_client_id uuid        not null,
-  role           text        not null,
-  allow          jsonb       not null,
-  created_on     timestamptz not null default now(),
-  updated_at     timestamptz not null default now(),
-  primary key (id),
-  constraint fk_space
-    foreign key (opal_client_id)
-      references opal_client (id)
+    id uuid default gen_random_uuid(),
+    opal_client_id uuid not null,
+    role text not null,
+    allow jsonb not null,
+    created_on timestamptz not null default now(),
+    updated_at timestamptz not null default now(),
+    primary key (id),
+    constraint fk_space foreign key (opal_client_id) references opal_client (id)
 );
 
 create trigger update_updated_at
-  before update
-  on role
-  for each row
+before update
+on role
+for each row
 execute procedure trigger_update_updated_at();
