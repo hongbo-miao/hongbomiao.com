a7a25f81884d5e8434d1efa45e063b503d93429f
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-18 23:15:05 -0800

    fix(lint): format SQL by SQLFluff

--- hasura-graphql-engine/migrations/twitter_db/1650608310921_create_twitter_user_table/up.sql
+++ hasura-graphql-engine/migrations/twitter_db/1650608310921_create_twitter_user_table/up.sql
@@ -1,7 +1,7 @@
 create table if not exists twitter_user
 (
-  id     text not null,
-  name   text not null,
-  avatar text not null,
-  primary key (id)
+    id text not null,
+    name text not null,
+    avatar text not null,
+    primary key (id)
 );
