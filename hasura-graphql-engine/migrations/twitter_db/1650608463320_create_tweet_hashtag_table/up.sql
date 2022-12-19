afed32a05ad23af819106e4c8585d541001f12bf
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-18 23:15:05 -0800

    fix(lint): format SQL by SQLFluff

--- hasura-graphql-engine/migrations/twitter_db/1650608463320_create_tweet_hashtag_table/up.sql
+++ hasura-graphql-engine/migrations/twitter_db/1650608463320_create_tweet_hashtag_table/up.sql
@@ -1,8 +1,6 @@
 create table if not exists tweet_hashtag
 (
-  tweet_id   text not null,
-  hashtag_id uuid not null,
-  constraint fk_space
-    foreign key (hashtag_id)
-      references hashtag (id)
+    tweet_id text not null,
+    hashtag_id uuid not null,
+    constraint fk_space foreign key (hashtag_id) references hashtag (id)
 );
