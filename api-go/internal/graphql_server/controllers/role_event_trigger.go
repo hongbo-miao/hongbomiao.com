8eed6835bf742a6bc1d669af6798623c9173bccb
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-20 10:24:50 -0800

    chore(go): update depreciated io/ioutil

--- api-go/internal/graphql_server/controllers/role_event_trigger.go
+++ api-go/internal/graphql_server/controllers/role_event_trigger.go
@@ -3,12 +3,12 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
 func RoleEventTrigger(c *gin.Context) {
-	bodyBytes, _ := ioutil.ReadAll(c.Request.Body)
+	bodyBytes, _ := io.ReadAll(c.Request.Body)
 	log.Info().Bytes("bodyBytes", bodyBytes).Msg("RoleEventTrigger")
 	c.JSON(http.StatusOK, gin.H{
 		"status": "ok",
