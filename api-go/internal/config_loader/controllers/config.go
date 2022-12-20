b0bf7f5ece2c36e9fcbc6e4075e96945e8d2eee6
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-20 10:04:50 -0800

    chore(go): upgrade pgx to v5

--- api-go/internal/config_loader/controllers/config.go
+++ api-go/internal/config_loader/controllers/config.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"github.com/Hongbo-Miao/hongbomiao.com/api-go/internal/config_loader/utils"
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog/log"
 	"net/http"
 )
