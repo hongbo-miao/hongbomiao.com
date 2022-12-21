741d581197ccceccd732bcc9feacd71760d093f2
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-21 00:49:41 -0800

    chore(bigquery): change anomaly_prob_threshold to 0.99

--- bigquery-ml/detect_sales_anomalies/000002_detect_anomalies_training_data.sql
+++ bigquery-ml/detect_sales_anomalies/000002_detect_anomalies_training_data.sql
@@ -2,5 +2,5 @@ select *
 from
     ml.detect_anomalies(
         model `sales.hm_sales_anomalies_model`,
-        struct(0.8 as anomaly_prob_threshold)
+        struct(0.99 as anomaly_prob_threshold)
     );
