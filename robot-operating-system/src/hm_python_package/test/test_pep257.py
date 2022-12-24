8900547fd1a67dbf8a1b4d0602d536ce89cf95c2
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-24 02:33:01 -0800

    fix(ros): fix lint issues

--- robot-operating-system/src/hm_python_package/test/test_pep257.py
+++ robot-operating-system/src/hm_python_package/test/test_pep257.py
@@ -12,12 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ament_pep257.main import main
 import pytest
+from ament_pep257.main import main
 
 
 @pytest.mark.linter
 @pytest.mark.pep257
 def test_pep257():
-    rc = main(argv=['.', 'test'])
-    assert rc == 0, 'Found code style errors / warnings'
+    rc = main(argv=[".", "test"])
+    assert rc == 0, "Found code style errors / warnings"
