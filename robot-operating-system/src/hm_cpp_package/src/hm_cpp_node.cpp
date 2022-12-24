bb468c21c1130af288e1c16c52114e177eec5858
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-24 02:47:47 -0800

    chore(ros): rename node name

--- robot-operating-system/src/hm_cpp_package/src/hm_cpp_node.cpp
+++ robot-operating-system/src/hm_cpp_package/src/hm_cpp_node.cpp
@@ -1,10 +1,10 @@
 #include "rclcpp/rclcpp.hpp"
 
-class MyNode : public rclcpp::Node {
+class HMNode : public rclcpp::Node {
  public:
-  MyNode() : Node("cpp_test"), counter_(0) {
+  HMNode() : Node("hm_cpp_node"), counter_(0) {
     timer_ = this->create_wall_timer(std::chrono::seconds(1),
-                                     std::bind(&MyNode::timerCallback, this));
+                                     std::bind(&HMNode::timerCallback, this));
   }
 
  private:
@@ -19,7 +19,7 @@ class MyNode : public rclcpp::Node {
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
-  auto node = std::make_shared<MyNode>();
+  auto node = std::make_shared<HMNode>();
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
