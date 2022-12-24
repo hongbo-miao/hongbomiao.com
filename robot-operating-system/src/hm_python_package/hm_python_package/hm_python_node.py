f382f8a741b0599be360653f05d854f6c72093d7
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-24 01:30:53 -0800

    feat(ros): init python package

---
+++ robot-operating-system/src/hm_python_package/hm_python_package/hm_python_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from hm_python_package.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ robot-operating-system/src/hm_python_package/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+package_name = "hm_python_package"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Hongbo Miao",
+    maintainer_email="Hongbo.Miao@outlook.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": ["hm_python_node = hm_python_package.hm_python_node:main"],
+    },
+)
