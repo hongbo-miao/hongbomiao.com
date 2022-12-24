71261e008772314b4d248be040a79942fe20a818
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-12-24 02:15:52 -0800

    feat(ros): count number

--- robot-operating-system/src/hm_cpp_package/src/hm_cpp_node.cpp
+++ robot-operating-system/src/hm_cpp_package/src/hm_cpp_node.cpp
@@ -1,10 +1,26 @@
-#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+class MyNode : public rclcpp::Node {
+ public:
+  MyNode() : Node("cpp_test"), counter_(0) {
+    timer_ = this->create_wall_timer(std::chrono::seconds(1),
+                                     std::bind(&MyNode::timerCallback, this));
+  }
 
-  printf("hello world hm_cpp_package package\n");
+ private:
+  void timerCallback() {
+    counter_++;
+    RCLCPP_INFO(this->get_logger(), "Hello %d", counter_);
+  }
+
+  rclcpp::TimerBase::SharedPtr timer_;
+  int counter_;
+};
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<MyNode>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
   return 0;
 }
--- robot-operating-system/src/hm_python_package/hm_python_package/hm_python_node.py
+++ robot-operating-system/src/hm_python_package/hm_python_package/hm_python_node.py
@@ -1,6 +1,25 @@
-def main():
-    print('Hi from hm_python_package.')
+import rclpy
+from rclpy.node import Node
 
 
-if __name__ == '__main__':
+class MyNode(Node):
+    def __init__(self):
+        super().__init__("py_test")
+        self._count = 0
+        self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        self._count += 1
+        self.get_logger().info(f"Hello {self._count}")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
     main()
