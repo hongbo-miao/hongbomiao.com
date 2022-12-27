297eba974a8206d835fc90878f621473871a835b
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2022-12-26 19:27:40 -0800

    feat(ros): init turtle_controller

---
+++ robot-operating-system/src/hm_turtle/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'hm_turtle'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='parallels',
+    maintainer_email='3375461+Hongbo-Miao@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'turtle_controller = hm_turtle.turtle_controller:main'
+        ],
+    },
+)
