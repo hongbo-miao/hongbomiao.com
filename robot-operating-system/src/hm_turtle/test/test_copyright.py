861e6de465cd1fedd449c2e078be2a96b927e7d9
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2022-12-26 19:30:43 -0800

    feat(ros): add turtle_controller

--- robot-operating-system/src/hm_turtle/test/test_copyright.py
+++ robot-operating-system/src/hm_turtle/test/test_copyright.py
@@ -12,14 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ament_copyright.main import main
 import pytest
+from ament_copyright.main import main
 
 
 # Remove the `skip` decorator once the source file(s) have a copyright header
-@pytest.mark.skip(reason='No copyright header has been placed in the generated source file.')
+@pytest.mark.skip(
+    reason="No copyright header has been placed in the generated source file."
+)
 @pytest.mark.copyright
 @pytest.mark.linter
 def test_copyright():
-    rc = main(argv=['.', 'test'])
-    assert rc == 0, 'Found errors'
+    rc = main(argv=[".", "test"])
+    assert rc == 0, "Found errors"
