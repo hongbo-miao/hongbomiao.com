b71d0b94d736d099e9057a35d3e22f4d752fe4f1
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2022-12-26 19:31:00 -0800

    chore(ros): add turtle_controller commands

--- robot-operating-system/src/hm_python_package/setup.py
+++ robot-operating-system/src/hm_python_package/setup.py
@@ -4,7 +4,7 @@ package_name = "hm_python_package"
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version="1.0.0",
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
@@ -14,8 +14,8 @@ setup(
     zip_safe=True,
     maintainer="Hongbo Miao",
     maintainer_email="Hongbo.Miao@outlook.com",
-    description="TODO: Package description",
-    license="TODO: License declaration",
+    description="HM Python Package",
+    license="MIT",
     tests_require=["pytest"],
     entry_points={
         "console_scripts": ["hm_python_node = hm_python_package.hm_python_node:main"],
