ea51ffab6e20912d0ef040b3639da052fd9affaa
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2022-12-27 01:02:47 -0800

    refactor(ros): rename hm_turtle to turtle_robot (#6483)

---
+++ robot-operating-system/bin/setupTurtleRobot.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+# Setup
+source install/local_setup.bash
+
+# Start the turtlesim
+ros2 run turtlesim turtlesim_node
+
+# Add turtle_robot
+ros2 service call /spawn turtlesim/srv/Spawn "{x: 2.0, y: 2.0, theta: 0.0, name: 'turtle_robot'}"
+
+# Build the turtle_robot
+colcon build --packages-select=turtle_robot --symlink-install
+ros2 run turtle_robot turtle_robot_controller
+
+# Control the target turtle
+ros2 run turtlesim turtle_teleop_key
