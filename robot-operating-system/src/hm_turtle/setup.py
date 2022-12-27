cdb16ad2acddf76eb1bde783614de22f5174d141
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2022-12-26 19:30:43 -0800

    feat(ros): add turtle_controller

--- robot-operating-system/src/hm_turtle/setup.py
+++ robot-operating-system/src/hm_turtle/setup.py
@@ -1,26 +1,23 @@
 from setuptools import setup
 
-package_name = 'hm_turtle'
+package_name = "hm_turtle"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="1.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='parallels',
-    maintainer_email='3375461+Hongbo-Miao@users.noreply.github.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="Hongbo Miao",
+    maintainer_email="Hongbo.Miao@outlook.com",
+    description="HM Turtle",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'turtle_controller = hm_turtle.turtle_controller:main'
-        ],
+        "console_scripts": ["turtle_controller = hm_turtle.turtle_controller:main"],
     },
 )
--- robot-operating-system/src/hm_turtle/test/test_flake8.py
+++ robot-operating-system/src/hm_turtle/test/test_flake8.py
@@ -12,14 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ament_flake8.main import main_with_errors
 import pytest
+from ament_flake8.main import main_with_errors
 
 
 @pytest.mark.flake8
 @pytest.mark.linter
 def test_flake8():
     rc, errors = main_with_errors(argv=[])
-    assert rc == 0, \
-        'Found %d code style errors / warnings:\n' % len(errors) + \
-        '\n'.join(errors)
+    assert rc == 0, "Found %d code style errors / warnings:\n" % len(
+        errors
+    ) + "\n".join(errors)
