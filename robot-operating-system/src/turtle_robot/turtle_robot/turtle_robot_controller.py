772fe38588982f67fce430338dd3fdebbd013170
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2022-12-26 23:50:48 -0800

    refactor(ros): rename hm_turtle to turtle_robot

--- robot-operating-system/src/turtle_robot/turtle_robot/turtle_robot_controller.py
+++ robot-operating-system/src/turtle_robot/turtle_robot/turtle_robot_controller.py
@@ -6,33 +6,33 @@ from rclpy.node import Node
 from turtlesim.msg import Pose
 
 
-class TurtleControllerNode(Node):
+class TurtleRobotControllerNode(Node):
     def __init__(self):
-        super().__init__("turtle_controller")
-        self.get_logger().info("turtle_controller")
-        self._robot_pose = None
+        super().__init__("turtle_robot_controller")
+        self.get_logger().info("turtle_robot_controller")
+        self._turtle_robot_pose = None
         self._target_pose = None
         self._cmd_vel_publisher = self.create_publisher(
-            Twist, "robot_turtle/cmd_vel", 10
+            Twist, "turtle_robot/cmd_vel", 10
         )
         self.create_subscription(
-            Pose, "robot_turtle/pose", self.subscribe_robot_turtle_pose, 10
+            Pose, "turtle_robot/pose", self.subscribe_turtle_robot_pose, 10
         )
         self.create_subscription(Pose, "turtle1/pose", self.subscribe_target_pose, 10)
         self.create_timer(0.01, self.control_loop)
 
-    def subscribe_robot_turtle_pose(self, msg):
-        self._robot_pose = msg
+    def subscribe_turtle_robot_pose(self, msg):
+        self._turtle_robot_pose = msg
 
     def subscribe_target_pose(self, msg):
         self._target_pose = msg
 
     def control_loop(self):
-        if self._robot_pose is None:
+        if self._turtle_robot_pose is None:
             return
 
-        dist_x = self._target_pose.x - self._robot_pose.x
-        dist_y = self._target_pose.y - self._robot_pose.y
+        dist_x = self._target_pose.x - self._turtle_robot_pose.x
+        dist_y = self._target_pose.y - self._turtle_robot_pose.y
         distance = math.sqrt(dist_x**2 + dist_y**2)
 
         msg = Twist()
@@ -42,7 +42,7 @@ class TurtleControllerNode(Node):
 
             # orientation
             goal_theta = math.atan2(dist_y, dist_x)
-            diff = goal_theta - self._robot_pose.theta
+            diff = goal_theta - self._turtle_robot_pose.theta
             if diff > math.pi:
                 diff -= 2 * math.pi
             elif diff < -math.pi:
@@ -58,7 +58,7 @@ class TurtleControllerNode(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = TurtleControllerNode()
+    node = TurtleRobotControllerNode()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
