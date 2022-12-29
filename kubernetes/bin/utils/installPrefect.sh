579b66da0f1c2d3896642375fc3b7c97cf2d8a5b
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2022-12-29 00:46:56 -0800

    feat(k8s): deploy prefect agent (#6501)

--- kubernetes/bin/utils/installPrefect.sh
+++ kubernetes/bin/utils/installPrefect.sh
@@ -9,8 +9,20 @@ helm install \
   prefect/prefect-orion \
   --namespace=hm-prefect \
   --create-namespace \
-  --values=kubernetes/manifests/prefect/helm/my-values.yaml
+  --values=kubernetes/manifests/prefect/helm/prefect-orion/my-values.yaml
 
 # Delete:
 # helm uninstall prefect-orion --namespace=hm-prefect
+echo "=================================================="
+
+echo "# Install Prefect Agent"
+helm install \
+  prefect-agent \
+  prefect/prefect-agent \
+  --namespace=hm-prefect \
+  --create-namespace \
+  --values=kubernetes/manifests/prefect/helm/prefect-agent/my-values.yaml
+
+# Delete:
+# helm uninstall prefect-agent --namespace=hm-prefect
 echo "=================================================="
