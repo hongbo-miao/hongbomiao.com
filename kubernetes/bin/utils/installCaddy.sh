e71d1e3675b2239feef94f191aa120d14999e990
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2022-12-31 01:04:11 -0800

    feat(k8s): add Caddy (#6534)

---
+++ kubernetes/bin/utils/installCaddy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+echo "# Install Caddy Ingress Controller"
+helm install caddy \
+  caddy-ingress-controller \
+  --namespace=hm-caddy \
+  --create-namespace \
+  --repo=https://caddyserver.github.io/ingress \
+  --values=kubernetes/manifests/caddy/helm/my-values.yaml
+
+# Delete:
+# helm uninstall caddy --namespace=hm-caddy
+echo "=================================================="
+
+
+echo "# Install Caddy Ingress"
+kubectl apply --filename=kubernetes/manifests/caddy/hm-prefect-caddy-ingress.yaml
+# kubectl delete --filename=kubernetes/manifests/caddy/hm-prefect-caddy-ingress.yaml
+echo "=================================================="
