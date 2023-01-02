2abdd7cc35c0323d319144d239ddf956c6d7b7bc
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-01-01 21:33:59 -0800

    chore(ros): rename nodes

--- robot-operating-system/bin/launchTurtleRobot.sh
+++ robot-operating-system/bin/launchTurtleRobot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
+colcon build --packages-select=turtle_robot
 colcon build --packages-select=turtle_robot_launch
 ros2 launch turtle_robot_launch turtle_robot.launch.py
