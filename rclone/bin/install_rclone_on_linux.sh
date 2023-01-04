84828abc449b670baf48b67cd4d6e5998c493ec0
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-01-04 14:26:23 -0800

    chore(rclone): add script to install on linux (#6594)

---
+++ rclone/bin/install_rclone_on_linux.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# https://rclone.org/install/#linux
+
+# Fetch and unpack
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+cd rclone-*-linux-amd64
+
+# Copy binary file
+sudo cp rclone /usr/bin/
+sudo chown root:root /usr/bin/rclone
+sudo chmod 755 /usr/bin/rclone
+
+# Install manpage
+sudo mkdir -p /usr/local/share/man/man1
+sudo cp rclone.1 /usr/local/share/man/man1/
+sudo mandb
