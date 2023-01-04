064948cf3052b13c5353d0f577c51706b10087f1
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-01-04 15:38:38 -0800

    chore(bin): rename from camelCase to underscore (#6595)

--- kubernetes/bin/utils/focusHasuraDebug.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "# Focus Hasura Debug"
-source kubernetes/bin/utils/installPostgres.sh
-source kubernetes/bin/utils/installTimescaleDB.sh
-source kubernetes/bin/utils/installDgraph.sh
-source kubernetes/bin/utils/installGraphQLServerWithoutOPAL.sh
-echo "=================================================="
