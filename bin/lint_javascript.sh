22782e71d7c4829fe4c9e080ac2d844253f2a43a
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-01-04 14:29:02 -0800

    chore(bin): rename from camelCase to underscore

---
+++ bin/lint_javascript.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+cd api-node && npm run lint:javascript
+cd ../ethereum && npm run lint:javascript
+cd ../grafana/hm-panel-plugin && npm run lint:javascript
+cd ../../mobile-react-native && npm run lint:javascript
+cd ../web && npm run lint:javascript
+cd ../web-cypress && npm run lint:javascript
