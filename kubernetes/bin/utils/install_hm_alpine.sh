69b1fd6b626bcfc29f539739d2a2be0275d37450
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-01-04 21:25:06 -0800

    chore(bin): rename from camelCase to underscore (#6599)

--- kubernetes/bin/utils/install_hm_alpine.sh
+++ kubernetes/bin/utils/install_hm_alpine.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-echo "# Install HM Alpine"
+echo "# Install hm-alpine"
 kubectl apply --filename=kubernetes/manifests/hm-alpine/hm-alpine-namespace.yaml
 kubectl apply --filename=kubernetes/manifests/hm-alpine
 
