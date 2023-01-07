072608a94ad8049788f74edaad098f5b57f1b6b0
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-01-06 17:30:10 -0800

    chore(feast): change to poetry (#6621)

--- feature-store/inspect_raw_data.py
+++ feature-store/inspect_raw_data.py
@@ -4,5 +4,5 @@ from pandas import option_context
 with option_context(
     "display.max_rows", 10, "display.max_columns", None, "display.width", 500
 ):
-    df = pd.read_parquet("features/data/driver_stats.parquet")
+    df = pd.read_parquet("driver_features/data/driver_stats.parquet")
     print(df)
