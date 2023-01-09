73a691c5e189006a37cc17b727d83b7ebe179bfd
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-01-09 00:13:46 -0800

    fix(timescaledb): fix the script to install timescaledb on ubuntu (#6647)

--- timescaledb/bin/install_timescaledb_on_ubuntu.sh
+++ timescaledb/bin/install_timescaledb_on_ubuntu.sh
@@ -3,18 +3,24 @@ set -e
 
 # https://docs.timescale.com/install/latest/self-hosted/installation-debian/
 
-# Install TimescaleDB
+echo "# Install TimescaleDB"
 sudo apt install gnupg postgresql-common apt-transport-https lsb-release wget
 sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
-echo "deb https://packagecloud.io/timescale/timescaledb/debian/ $(lsb_release -c -s) main" | sudo tee /etc/apt/sources.list.d/timescaledb.list
+echo "deb https://packagecloud.io/timescale/timescaledb/ubuntu/ $(lsb_release -c -s) main" | sudo tee /etc/apt/sources.list.d/timescaledb.list
 wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo apt-key add -
+echo "=================================================="
 
-# Install psql
+echo "# Install psql"
 sudo apt update
-sudo apt install timescaledb-2-postgresql-14
-
-# Set up the TimescaleDB extension
+sudo apt install --yes timescaledb-2-postgresql-14
 sudo systemctl restart postgresql
 sudo -u postgres psql
 # \password postgres
 # \q
+echo "=================================================="
+
+echo "# Set up the TimescaleDB extension"
+sudo nano /etc/postgresql/14/main/postgresql.conf
+# shared_preload_libraries = 'timescaledb'
+sudo systemctl restart postgresql
+echo "=================================================="
--- timescaledb/bin/migrate_timescaledb_iot_db_ubuntu.sh
+++ timescaledb/bin/migrate_timescaledb_iot_db_ubuntu.sh
@@ -6,11 +6,6 @@ psql postgresql://postgres:passw0rd@localhost:5432/postgres --command="create da
 psql postgresql://postgres:passw0rd@localhost:5432/postgres --command="grant all privileges on database iot_db to postgres;"
 echo "=================================================="
 
-# Modify /etc/postgresql/14/main/postgresql.conf
-#   shared_preload_libraries = 'timescaledb'
-# Restart Postgres
-#   sudo systemctl restart postgresql
-
 echo "# Create extension timescaledb in iot_db"
 psql postgresql://postgres:passw0rd@localhost:5432/iot_db --command="create extension if not exists timescaledb;"
 echo "=================================================="
