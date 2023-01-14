ebb792c71d4f49813b2490dd3f551f55dc774b9c
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-01-14 14:41:50 -0800

    chore(timescaledb): add --yes in install script (#6700)

--- timescaledb/bin/install_timescaledb_on_ubuntu.sh
+++ timescaledb/bin/install_timescaledb_on_ubuntu.sh
@@ -4,7 +4,7 @@ set -e
 # https://docs.timescale.com/install/latest/self-hosted/installation-debian/
 
 echo "# Install TimescaleDB"
-sudo apt install gnupg postgresql-common apt-transport-https lsb-release wget
+sudo apt install --yes gnupg postgresql-common apt-transport-https lsb-release wget
 sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
 echo "deb https://packagecloud.io/timescale/timescaledb/ubuntu/ $(lsb_release -c -s) main" | sudo tee /etc/apt/sources.list.d/timescaledb.list
 wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo apt-key add -
