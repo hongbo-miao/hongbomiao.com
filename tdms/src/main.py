da5b0b03cb501277d9059e6594d9f463b1058c17
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-10 01:15:30 -0800

    chore(tdms): update tdms sample url

--- tdms/src/main.py
+++ tdms/src/main.py
@@ -1,7 +1,13 @@
 from nptdms import TdmsFile
 
-tdms_file = TdmsFile.read("data/exampleMeasurements.tdms")
-for group in tdms_file.groups():
-    df = group.as_dataframe()
-    print(group.name)
-    print(df)
+
+def main():
+    tdms_file = TdmsFile.read("data/exampleMeasurements.tdms")
+    for group in tdms_file.groups():
+        df = group.as_dataframe()
+        print(group.name)
+        print(df)
+
+
+if __name__ == "__main__":
+    main()
