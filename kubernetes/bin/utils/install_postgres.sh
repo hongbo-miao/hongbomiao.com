720cb1366ca1f042b804715a6a8c5611ccdcb793
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-11 08:31:32 +0900

    chore(postgres): update postgres port to 5432 (#7294)

--- kubernetes/bin/utils/install_postgres.sh
+++ kubernetes/bin/utils/install_postgres.sh
@@ -7,18 +7,18 @@ echo "=================================================="
 
 kubectl wait pod postgres-statefulset-0 --for=condition=ready --namespace=hm-postgres
 
-kubectl port-forward service/postgres-service --namespace=hm-postgres 40072:40072 &
+kubectl port-forward service/postgres-service --namespace=hm-postgres 5432:5432 &
 sleep 5
 
 echo "# Create database in Postgres"
-psql postgresql://admin@localhost:40072/postgres --command="create database opa_db;"
-psql postgresql://admin@localhost:40072/postgres --command="create database ory_hydra_db;"
-psql postgresql://admin@localhost:40072/postgres --command="grant all privileges on database opa_db to admin;"
-psql postgresql://admin@localhost:40072/postgres --command="grant all privileges on database ory_hydra_db to admin;"
+psql postgresql://admin@localhost:5432/postgres --command="create database opa_db;"
+psql postgresql://admin@localhost:5432/postgres --command="create database ory_hydra_db;"
+psql postgresql://admin@localhost:5432/postgres --command="grant all privileges on database opa_db to admin;"
+psql postgresql://admin@localhost:5432/postgres --command="grant all privileges on database ory_hydra_db to admin;"
 echo "=================================================="
 
 # echo "# Migrate opa_db in Postgres"
-# POSTGRESQL_URL="postgresql://admin:passw0rd@localhost:40072/opa_db?sslmode=disable&search_path=public"
+# POSTGRESQL_URL="postgresql://admin:passw0rd@localhost:5432/opa_db?sslmode=disable&search_path=public"
 # migrate -database "${POSTGRESQL_URL}" -path kubernetes/data/postgres/opa_db/migrations up
 # echo "=================================================="
 
