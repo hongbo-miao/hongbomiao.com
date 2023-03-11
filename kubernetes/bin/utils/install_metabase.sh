1b42a49df84e6ed40e5e4f4ca42f99c8d3685144
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-11 18:30:38 +0900

    feat(k8s): add metabase (#7307)

---
+++ kubernetes/bin/utils/install_metabase.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+echo "# Install Postgres"
+helm repo add pmint93 https://pmint93.github.io/helm-charts
+helm repo update
+helm install \
+  metabase \
+  pmint93/metabase \
+  --namespace=hm-metabase \
+  --create-namespace
+echo "=================================================="
