a87820a601915b4e44277c5374ec1aaac53c5868
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-12 22:26:36 -0700

    feat(k8s): add opencost

--- kubernetes/bin/utils/uninstall_kubecost.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "# Uninstall Kubecost"
-helm uninstall kubecost --namespace=hm-kubecost
-kubectl delete namespace hm-kubecost
-echo "=================================================="
