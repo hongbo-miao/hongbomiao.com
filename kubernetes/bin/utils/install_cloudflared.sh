5c71c958308f0d8510b41770521df9d3cf99b683
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-18 10:50:25 +0900

    chore(k8s): bump cloudflared version to 2023.3.1 (#7453)

--- kubernetes/bin/utils/install_cloudflared.sh
+++ kubernetes/bin/utils/install_cloudflared.sh
@@ -2,11 +2,12 @@
 set -e
 
 echo "# Install cloudflared"
+kubectl create secret generic tunnel-credentials \
+  --from-file=credentials.json=/Users/hongbo-miao/.cloudflared/c9aa4140-fee8-4862-a479-3c1faacbd816.json \
+  --namespace=hm-cloudflared
+# kubectl delete secret tunnel-credentials --namespace=hm-cloudflared
+
 kubectl apply --filename=kubernetes/manifests/cloudflared/hm-clouflared-namespace.yaml
 kubectl apply --filename=kubernetes/manifests/cloudflared
 # kubectl delete --filename=kubernetes/manifests/cloudflared
-
-kubectl create secret generic tunnel-credentials \
-  --from-file=credentials.json=/Users/hongbo-miao/.cloudflared/afccc94c-0065-4e40-832a-a00b1940faaf.json \
-  --namespace=hm-cloudflared
 echo "=================================================="
