f011b803f1a662da576dd11976e6f109f22d9429
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-18 21:21:59 -0700

    chore(k8s): change metabase from h2 to external postgres database

--- kubernetes/bin/utils/install_metabase.sh
+++ kubernetes/bin/utils/install_metabase.sh
@@ -2,11 +2,19 @@
 set -e
 
 echo "# Install Postgres"
+source kubernetes/bin/utils/install_postgres.sh
+psql postgresql://admin@localhost:5432/postgres --command="create database hm_metabase_db;"
+psql postgresql://admin@localhost:5432/postgres --command="grant all privileges on database hm_metabase_db to admin;"
+echo "=================================================="
+
+echo "# Install Metabase"
 helm repo add pmint93 https://pmint93.github.io/helm-charts
 helm repo update pmint93
 helm install \
   metabase \
   pmint93/metabase \
   --namespace=hm-metabase \
-  --create-namespace
+  --create-namespace \
+  --values=kubernetes/manifests/metabase/helm/my-values.yaml
+# helm uninstall metabase --namespace=hm-metabase
 echo "=================================================="
