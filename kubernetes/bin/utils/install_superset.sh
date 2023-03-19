8ce972c504cbb93997713b3e4ba7cd569d6f0342
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-19 00:51:58 -0700

    chore(k8s): clean superset scripts (#7465)

--- kubernetes/bin/utils/install_superset.sh
+++ kubernetes/bin/utils/install_superset.sh
@@ -1,31 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-echo "# Create superset_db in Postgres"
+echo "# Create hm_superset_db in Postgres"
 kubectl port-forward service/postgres-service --namespace=hm-postgres 5432:5432 &
 sleep 5
-psql postgresql://admin@localhost:5432/postgres --command="create database superset_db;"
-psql postgresql://admin@localhost:5432/postgres --command="grant all privileges on database superset_db to admin;"
+psql postgresql://admin@localhost:5432/postgres --command="create database hm_superset_db;"
+psql postgresql://admin@localhost:5432/postgres --command="grant all privileges on database hm_superset_db to admin;"
 pgrep kubectl | xargs kill -9
 echo "=================================================="
 
 echo "# Install Superset"
-kubectl apply --filename=manifests/superset/hm-superset-namespace.yaml
 helm repo add superset https://apache.github.io/superset
+helm repo update superset
 helm install \
   superset \
   superset/superset \
   --namespace=hm-superset \
+  --create-namespace \
   --values=kubernetes/manifests/superset/helm/my-values.yaml
-
-# Upgrade:
-# helm upgrade \
-#   superset \
-#   superset/superset \
-#   --namespace=hm-superset \
-#   --values=kubernetes/manifests/superset/helm/my-values.yaml
-
-# Delete:
 # helm uninstall superset --namespace=hm-superset
 
 # Install database drivers:
