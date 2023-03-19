5510c32a05870ac5908e66a86e11db7881e8194d
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-19 00:51:58 -0700

    chore(k8s): clean superset scripts (#7465)

--- kubernetes/bin/utils/uninstall_superset.sh
+++ kubernetes/bin/utils/uninstall_superset.sh
@@ -4,12 +4,11 @@ set -e
 echo "# Uninstall Superset"
 helm uninstall superset --namespace=hm-superset
 kubectl delete jobs superset-init-db --namespace=hm-superset
-kubectl delete --filename=kubernetes/manifests/superset/hm-superset-namespace.yaml
 echo "=================================================="
 
-echo "# Drop superset_db in Postgres"
+echo "# Drop hm_superset_db in Postgres"
 kubectl port-forward service/postgres-service --namespace=hm-postgres 5432:5432 &
 sleep 3
-psql postgresql://admin@localhost:5432/postgres --command="drop database if exists superset_db with (force);"
+psql postgresql://admin@localhost:5432/postgres --command="drop database if exists hm_superset_db with (force);"
 pgrep kubectl | xargs kill -9
 echo "=================================================="
