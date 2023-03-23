6187ba7b4251dc3b52cf2f14b18b9395fd052bf0
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-23 11:21:13 -0700

    feat(prefect): print python version in print_platform (#7622)

--- hm-prefect/workflows/print-platform/src/main.py
+++ hm-prefect/workflows/print-platform/src/main.py
@@ -6,8 +6,9 @@ from prefect import flow, get_run_logger
 @flow
 def print_platform():
     logger = get_run_logger()
-    logger.info(f"node: {platform.node()}")
-    logger.info(f"platform: {platform.platform()}")
+    logger.info(f"Node: {platform.node()}")
+    logger.info(f"Platform: {platform.platform()}")
+    logger.info(f"Python Version: {platform.python_version()}")
 
 
 if __name__ == "__main__":
