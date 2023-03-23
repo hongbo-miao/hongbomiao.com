9b69f90bd2ff6d26432348394a38c7a2bc126f19
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-22 17:29:36 -0700

    feat(tdms): save tdms as parquet (#7591)

--- tdms/src/main.py
+++ tdms/src/main.py
@@ -7,6 +7,7 @@ def main():
         df = group.as_dataframe()
         print(group.name)
         print(df)
+        df.to_parquet(f"{group.name}.parquet", engine="pyarrow", compression="brotli")
 
 
 if __name__ == "__main__":
