1949ba099c5d9a527eec1d0cad9b47448d669e36
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-23 16:47:00 -0700

    fix(prefect): calculate input (#7640)

--- hm-prefect/workflows/calculate/src/main.py
+++ hm-prefect/workflows/calculate/src/main.py
@@ -1,41 +1,59 @@
+import random
+import time
+
 from prefect import flow, get_run_logger, task
+from pydantic import BaseModel
+
+
+class Model(BaseModel):
+    n: int
 
 
 @task
-def expand(n):
-    return iter(range(n))
+def expand(n: int) -> list[int]:
+    time.sleep(random.uniform(0.5, 5))
+    return [*range(n)]
 
 
 @task
-def power(a, b):
+def power(a: int, b: int) -> int:
+    time.sleep(random.uniform(0.5, 2))
     return a**b
 
 
 @task
-def multiply(a, b):
+def multiply(a: int, b: int) -> int:
+    time.sleep(random.uniform(0.5, 2))
     return a * b
 
 
 @task
-def add(a, b):
+def add(a: int, b: int) -> int:
+    time.sleep(random.uniform(0.5, 2))
     return a + b
 
 
 @task
-def print_nums(nums: list[int]) -> None:
-    logger = get_run_logger()
-    logger.info(nums)
+def sum_up(nums: list[int]) -> int:
+    time.sleep(random.uniform(0.5, 2))
+    return sum(nums)
 
 
 @flow
-def calculate(nums: list[int]) -> None:
+def calculate(model: Model) -> None:
+    logger = get_run_logger()
+    nums = expand(model.n)
     nums = power.map(nums, 2)
-    for i in nums:
-        x = expand(i)
-        x = multiply.map(x, 100)
-        x = add.map(x, 9)
-        print_nums(x)
+    res = []
+    for n in nums:
+        ns = expand(n)
+        ns = multiply.map(ns, 100)
+        ns = add.map(ns, 9)
+        res += ns
+    n = sum_up(res)
+    logger.info(n)
 
 
 if __name__ == "__main__":
-    calculate([*range(1, 4)])
+    external_model = Model(n=4)
+    calculate(external_model)
