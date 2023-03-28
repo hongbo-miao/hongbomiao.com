85da9e7acdc87c1fdc8c78717849b9ab9890ec9d
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-03-28 00:09:34 -0700

    chore(prefect): remove find-taxi-top-routes (#7712)

--- hm-prefect/workflows/find-taxi-top-routes/src/tasks/zone.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from prefect import task
-
-
-@task
-def preprocess_zones(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.rename(str.lower, axis="columns")
-    df["borough"] = df["borough"].astype("category")
-    return df.drop(columns=["objectid"])
