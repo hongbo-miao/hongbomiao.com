503f2335501b3c51b291dc6fe47cb752bcd2d448
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-04-06 22:32:31 -0700

    chore(k8s): rename kafka namespace to hm-kafka (#7898)

--- kubernetes/bin/utils/install_kafka.sh
+++ kubernetes/bin/utils/install_kafka.sh
@@ -4,18 +4,18 @@ set -e
 # Kafka
 # https://strimzi.io/quickstarts
 echo "# Install Strimzi"
-kubectl create namespace kafka
-kubectl apply --filename="https://strimzi.io/install/latest?namespace=kafka" --namespace=kafka
-# kubectl delete --filename="https://strimzi.io/install/latest?namespace=kafka" --namespace=kafka
+kubectl create namespace hm-kafka
+kubectl apply --filename="https://strimzi.io/install/latest?namespace=kafka" --namespace=hm-kafka
+# kubectl delete --filename="https://strimzi.io/install/latest?namespace=kafka" --namespace=hm-kafka
 echo "=================================================="
 
 echo "# Install Kafka"
-kubectl apply --filename=https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml --namespace=kafka
-# kubectl delete --filename=https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml --namespace=kafka
+kubectl apply --filename=https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml --namespace=hm-kafka
+# kubectl delete --filename=https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml --namespace=hm-kafka
 echo "=================================================="
 
 echo "# Check Kafka"
-kubectl wait kafka/my-cluster --for=condition=Ready --timeout=300s --namespace=kafka
-# kubectl run kafka-producer --stdin --tty --namespace=kafka --image=quay.io/strimzi/kafka:0.26.0-kafka-3.0.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --broker-list=my-cluster-kafka-bootstrap:9092 --topic=my-topic
-# kubectl run kafka-consumer --stdin --tty --namespace=kafka --image=quay.io/strimzi/kafka:0.26.0-kafka-3.0.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server=my-cluster-kafka-bootstrap:9092 --topic=my-topic --from-beginning
+kubectl wait kafka/my-cluster --for=condition=Ready --timeout=300s --namespace=hm-kafka
+# kubectl run kafka-producer --stdin --tty --namespace=hm-kafka --image=quay.io/strimzi/kafka:0.26.0-kafka-3.0.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --broker-list=my-cluster-kafka-bootstrap:9092 --topic=my-topic
+# kubectl run kafka-consumer --stdin --tty --namespace=hm-kafka --image=quay.io/strimzi/kafka:0.26.0-kafka-3.0.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server=my-cluster-kafka-bootstrap:9092 --topic=my-topic --from-beginning
 echo "=================================================="
