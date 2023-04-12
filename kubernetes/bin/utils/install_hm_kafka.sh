bb1741e0bedf5274e842b3d65b45121b113e287e
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-04-11 17:41:15 -0700

    chore(kafka): rename cluster to hm-kafka (#8001)

--- kubernetes/bin/utils/install_hm_kafka.sh
+++ kubernetes/bin/utils/install_hm_kafka.sh
@@ -11,12 +11,12 @@ kubectl apply --filename="https://strimzi.io/install/latest?namespace=hm-kafka" 
 echo "=================================================="
 
 echo "# Install Kafka"
-kubectl apply --filename=https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml --namespace=hm-kafka
-# kubectl delete --filename=https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml --namespace=hm-kafka
+kubectl apply --filename=kubernetes/manifests/hm-kafka/kafka-persistent-single.yaml --namespace=hm-kafka
+# kubectl delete --filename=kubernetes/manifestshm-kafka/kafka-persistent-single.yaml --namespace=hm-kafka
 echo "=================================================="
 
 echo "# Check Kafka"
 kubectl wait kafka/my-cluster --for=condition=Ready --timeout=300s --namespace=hm-kafka
-# kubectl run kafka-producer --stdin --tty --namespace=hm-kafka --image=quay.io/strimzi/kafka:0.26.0-kafka-3.0.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --broker-list=my-cluster-kafka-bootstrap:9092 --topic=my-topic
-# kubectl run kafka-consumer --stdin --tty --namespace=hm-kafka --image=quay.io/strimzi/kafka:0.26.0-kafka-3.0.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server=my-cluster-kafka-bootstrap:9092 --topic=my-topic --from-beginning
+# kubectl run kafka-producer --stdin --tty --namespace=hm-kafka --image=quay.io/strimzi/kafka:0.26.0-kafka-3.0.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --broker-list=hm-kafka-kafka-bootstrap:9092 --topic=my-topic
+# kubectl run kafka-consumer --stdin --tty --namespace=hm-kafka --image=quay.io/strimzi/kafka:0.26.0-kafka-3.0.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server=hm-kafka-kafka-bootstrap:9092 --topic=my-topic --from-beginning
 echo "=================================================="
