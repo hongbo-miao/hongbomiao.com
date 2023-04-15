71fa64287c7a7c07c06c28fd7275246efbc98c66
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-04-14 20:08:46 -0700

    refactor(k8s): use helm to install ingress

--- kubernetes/bin/setup_multipass.sh
+++ kubernetes/bin/setup_multipass.sh
@@ -3,15 +3,23 @@ set -e
 
 echo "# Setup Multipass"
 # https://andreipope.github.io/tutorials/create-a-cluster-with-multipass-and-k3s
-multipass launch --name=west-master --cpus=6 --mem=24g --disk=256g
+multipass launch --name=west-master --cpus=8 --memory=56g --disk=512g
+# https://www.rancher.co.jp/docs/k3s/latest/en/installation/
 multipass exec west-master -- \
-  bash -c 'curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -'
+  bash -c 'curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable traefik" K3S_KUBECONFIG_MODE="644" sh -'
 multipass mount "${PWD}/data" west-master:/data
-multipass exec west-master -- \
-  cat /etc/rancher/k3s/k3s.yaml > west-master-k3s.yaml
+
+# multipass info west-master
+# multipass exec west-master -- bash
+
+# multipass delete west-master
+# multipass purge
 echo "=================================================="
 
-echo "# Update kubeconfig"
+echo "# Get kubeconfig"
+multipass exec west-master -- \
+  cat /etc/rancher/k3s/k3s.yaml > k3s-west-master-kubeconfig.yaml
+
 # apiVersion: v1
 # clusters:
 # - cluster:
@@ -33,21 +41,28 @@ echo "# Update kubeconfig"
 #     client-key-data: xxx
 
 WEST_MASTER_IP=$(multipass info west-master | grep IPv4 | awk '{print $2}')
-perl -pi -e "s/127.0.0.1/${WEST_MASTER_IP}/" west-master-k3s.yaml
-perl -p0i -e "s/name: default/name: west-master/" west-master-k3s.yaml
-perl -p0i -e "s/cluster: default/cluster: west-master/" west-master-k3s.yaml
-perl -p0i -e "s/user: default/user: admin/" west-master-k3s.yaml
-perl -p0i -e "s/name: default/name: admin\@west-master/" west-master-k3s.yaml
-perl -p0i -e "s/current-context: default/current-context: admin\@west-master/" west-master-k3s.yaml
-perl -p0i -e "s/name: default/name: admin/" west-master-k3s.yaml
+perl -pi -e "s/127.0.0.1/${WEST_MASTER_IP}/" k3s-west-master-kubeconfig.yaml
+perl -p0i -e "s/name: default/name: west-master/" k3s-west-master-kubeconfig.yaml
+perl -p0i -e "s/cluster: default/cluster: west-master/" k3s-west-master-kubeconfig.yaml
+perl -p0i -e "s/user: default/user: admin/" k3s-west-master-kubeconfig.yaml
+perl -p0i -e "s/name: default/name: admin\@west-master/" k3s-west-master-kubeconfig.yaml
+perl -p0i -e "s/current-context: default/current-context: admin\@west-master/" k3s-west-master-kubeconfig.yaml
+perl -p0i -e "s/name: default/name: admin/" k3s-west-master-kubeconfig.yaml
 echo "=================================================="
 
 echo "# Export kubeconfig"
-export KUBECONFIG=${PWD}/west-master-k3s.yaml
+export KUBECONFIG="${PWD}/k3s-west-master-kubeconfig.yaml"
 
-# multipass info west-master
-# multipass exec west-master -- bash
+# Note this will replace existing kubeconfig
+# cp k3s-west-master-kubeconfig.yaml ~/.kube/config
+echo "=================================================="
 
-# multipass delete west-master
-# multipass purge
+echo "# Install Ingress"
+helm upgrade \
+  ingress-nginx \
+  ingress-nginx \
+  --install \
+  --repo=https://kubernetes.github.io/ingress-nginx \
+  --namespace=ingress-nginx \
+  --create-namespace
 echo "=================================================="
