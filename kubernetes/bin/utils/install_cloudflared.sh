953e1eafde2721e5feb2c73bc9140425b0199ad0
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-04-15 02:06:18 -0700

    chore(k8s): rename to hm-elastic namespace (#8071)

--- kubernetes/bin/utils/install_cloudflared.sh
+++ kubernetes/bin/utils/install_cloudflared.sh
@@ -2,12 +2,13 @@
 set -e
 
 echo "# Install cloudflared"
+kubectl apply --filename=kubernetes/manifests/cloudflared/hm-clouflared-namespace.yaml
+
 kubectl create secret generic tunnel-credentials \
   --from-file=credentials.json=/Users/hongbo-miao/.cloudflared/c9aa4140-fee8-4862-a479-3c1faacbd816.json \
   --namespace=hm-cloudflared
 # kubectl delete secret tunnel-credentials --namespace=hm-cloudflared
 
-kubectl apply --filename=kubernetes/manifests/cloudflared/hm-clouflared-namespace.yaml
 kubectl apply --filename=kubernetes/manifests/cloudflared
 # kubectl delete --filename=kubernetes/manifests/cloudflared
 echo "=================================================="
