51427b68efcdc7b128fc4392c7c7717eebc51541
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-04-18 19:56:03 -0700

    feat(ansible): install vnstat, rclone, pyenv, python, poetry (#8131)

--- rclone/bin/install_rclone_on_linux.sh
+++ rclone/bin/install_rclone_on_linux.sh
@@ -2,18 +2,4 @@
 set -e
 
 # https://rclone.org/install/#linux
-
-# Fetch and unpack
-curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
-unzip rclone-current-linux-amd64.zip
-cd rclone-*-linux-amd64
-
-# Copy binary file
-sudo cp rclone /usr/bin/
-sudo chown root:root /usr/bin/rclone
-sudo chmod 755 /usr/bin/rclone
-
-# Install manpage
-sudo mkdir --parents /usr/local/share/man/man1
-sudo cp rclone.1 /usr/local/share/man/man1/
-sudo mandb
+https://rclone.org/install.sh
