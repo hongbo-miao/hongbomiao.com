bbf225295ec48a952d8175727311405742a6dece
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-04-21 19:03:38 -0700

    fix(heroku): add heroku-postbuild (#8205)

---
+++ api-node/bin/heroku_postbuild.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+cd ../web
+npm install
+npm run build
+
+cd ..
+cp -r web/build/ api-node/public/
+cp -r web/build/ caddy/public/
+
+cd api-node
+npm install
+npm run build
