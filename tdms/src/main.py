12f816e5830cd7a788db33d929313e087a28a535
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-04-22 18:12:36 -0700

    feat(spark): add IngestFromS3ToKafka application (#8218)

--- tdms/src/main.py
+++ tdms/src/main.py
@@ -7,6 +7,7 @@ def main():
         df = group.as_dataframe()
         print(group.name)
         print(df)
+        # df.to_parquet(f"{group.name}.parquet", engine="pyarrow", compression="snappy")
         df.to_parquet(f"{group.name}.parquet", engine="pyarrow", compression="brotli")
 
 
