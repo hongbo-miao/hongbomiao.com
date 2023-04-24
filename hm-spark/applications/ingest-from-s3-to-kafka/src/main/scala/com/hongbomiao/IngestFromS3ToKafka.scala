a019ac42756e142151a900336719e5a43cbdf8d0
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-04-23 22:41:38 -0700

    refactor(spark): switch from typesafe config to sconfig (#8277)

--- hm-spark/applications/ingest-from-s3-to-kafka/src/main/scala/com/hongbomiao/IngestFromS3ToKafka.scala
+++ hm-spark/applications/ingest-from-s3-to-kafka/src/main/scala/com/hongbomiao/IngestFromS3ToKafka.scala
@@ -1,11 +1,11 @@
 package com.hongbomiao
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import com.typesafe.config.ConfigFactory
+import org.ekrich.config.ConfigFactory
 
 object IngestFromS3ToKafka {
   def main(args: Array[String]): Unit = {
-    val config = ConfigFactory.load("application-development.conf")
+    val config = ConfigFactory.load("application.conf")
     val awsAccessKeyID = config.getString("aws-access-key-id")
     val awsSecretAccessKey = config.getString("aws-secret-access-key")
 
