6be74f3e171a5623d1d5639dca4f2b7e92865cb4
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-04-25 10:50:42 -0700

    feat(kafka): add jdbc-sink-kafka-connector

--- kubernetes/bin/utils/install_hm_kafka_sensor_kafka_connect.sh
+++ kubernetes/bin/utils/install_hm_kafka_sensor_kafka_connect.sh
@@ -12,6 +12,13 @@ psql postgresql://admin:passw0rd@localhost:25495/postgres --command="create data
 psql postgresql://admin:passw0rd@localhost:25495/postgres --command="grant all privileges on database hm_sensor_db to admin;"
 echo "=================================================="
 
+echo "# Create secret hm-sensor-db-credentials"
+kubectl create secret generic hm-sensor-db-credentials \
+  --from-file=kubernetes/manifests/hm-kafka/sensor-kafka-connect/jdbc-sink-kafka-connector/sensor-db-credentials.properties \
+  --namespace=hm-kafka
+# kubectl delete secret hm-sensor-db-credentials --namespace=hm-kafka
+echo "=================================================="
+
 echo "# Install hm-kafka-sensor-kafka-connect"
 kubectl apply --filename=kubernetes/manifests/hm-kafka/sensor-kafka-connect
 # kubectl delete --filename=kubernetes/manifests/hm-kafka/sensor-kafka-connect
