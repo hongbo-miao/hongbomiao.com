90b1d9905172d750f2dd3a02afd87d5ba6764606
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-04-26 15:29:22 -0700

    feat(tdms): generate iot tdms file (#8343)

---
+++ tdms/src/utils/convert_tdms_to_parquet.py
@@ -0,0 +1,16 @@
+from nptdms import TdmsFile
+
+
+def convert_tdms_to_parquet(data_dirname: str, tdms_filename: str) -> None:
+    tdms_path = f"{data_dirname}/{tdms_filename}"
+    tdms_file = TdmsFile.read(tdms_path)
+    for group in tdms_file.groups():
+        df = group.as_dataframe()
+        print(group.name)
+        print(df)
+        df.to_parquet(
+            f"{data_dirname}/{group.name}.parquet",
+            engine="pyarrow",
+            # compression="brotli",
+            compression="snappy",
+        )
