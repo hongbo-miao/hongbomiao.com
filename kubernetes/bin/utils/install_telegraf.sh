a9a9b0373a8d880d2635467e839281784414b23a
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-04-28 17:14:53 -0700

    feat(kafka): sink to influxdb (#8399)

--- kubernetes/bin/utils/install_telegraf.sh
+++ kubernetes/bin/utils/install_telegraf.sh
@@ -3,10 +3,11 @@ set -e
 
 echo "# Install Telegraf"
 kubectl apply --filename=kubernetes/manifests/telegraf/hm-telegraf-namespace.yaml
-kubectl apply --filename=kubernetes/manifests/telegraf
 
 INFLUXDB_API_TOKEN="xxx"
 kubectl create secret generic hm-telegraf-secret \
   --namespace=hm-telegraf \
   --from-literal="influxdb_api_token=${INFLUXDB_API_TOKEN}"
+kubectl apply --filename=kubernetes/manifests/telegraf
+# kubectl delete --filename=kubernetes/manifests/telegraf
 echo "=================================================="
