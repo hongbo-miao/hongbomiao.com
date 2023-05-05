0697507c8a14419298bc162024b4f23e4c4fb1b5
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-05-05 14:32:42 -0700

    refactor(kafka): switch to confluent avro

--- hm-spark/applications/ingest-from-s3-to-kafka/src/main/scala/com/hongbomiao/IngestFromS3ToKafka.scala
+++ hm-spark/applications/ingest-from-s3-to-kafka/src/main/scala/com/hongbomiao/IngestFromS3ToKafka.scala
@@ -3,8 +3,8 @@ package com.hongbomiao
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, struct}
 import org.apache.spark.sql.types.{DoubleType, LongType, StructType}
-import org.apache.spark.sql.avro.functions.to_avro
-import sttp.client3.{HttpClientSyncBackend, UriContext, basicRequest}
+import za.co.absa.abris.avro.functions.to_avro
+import za.co.absa.abris.config.{AbrisConfig, ToAvroConfig}
 
 object IngestFromS3ToKafka {
   def main(args: Array[String]): Unit = {
@@ -13,8 +13,6 @@ object IngestFromS3ToKafka {
       .master("local[*]")
       .appName("ingest-from-s3-to-kafka")
       .config("spark.ui.port", "4040")
-      // .config("spark.hadoop.fs.s3a.access.key", "xxx")
-      // .config("spark.hadoop.fs.s3a.secret.key", "xxx")
       .getOrCreate()
 
     val folderPath = "s3a://hongbomiao-bucket/iot/"
@@ -26,20 +24,19 @@ object IngestFromS3ToKafka {
       .add("voltage", DoubleType, nullable = true)
       .add("temperature", DoubleType, nullable = true)
 
-    val backend = HttpClientSyncBackend()
-    val res = basicRequest
-      .get(
-        uri"http://apicurio-registry-apicurio-registry.hm-apicurio-registry.svc:8080/apis/registry/v2/groups/hm-group/artifacts/hm.motor-value"
-      )
-      .send(backend)
-    val kafkaRecordValueSchema = res.body.fold(identity, identity)
+    val toAvroConfig: ToAvroConfig =
+      AbrisConfig.toConfluentAvro.downloadSchemaByLatestVersion
+        .andTopicNameStrategy("hm.motor")
+        .usingSchemaRegistry(
+          "http://confluent-schema-registry.hm-confluent-schema-registry.svc:8081"
+        )
 
     val df = spark.readStream
       .schema(parquetSchema)
       .option("maxFilesPerTrigger", 1)
       .parquet(folderPath)
       .withColumn("timestamp", (col("timestamp") * 1000).cast(LongType))
-      .select(to_avro(struct("*"), kafkaRecordValueSchema).alias("value"))
+      .select(to_avro(struct("*"), toAvroConfig).as("value"))
 
     val query = df.writeStream
       .format("kafka")
