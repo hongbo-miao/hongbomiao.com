cb61b07ceacfbc6d00b42eb77777aca4b07e39c4
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-05-06 01:19:29 -0700

    feat(k8s): add redpanda console (#8563)

---
+++ kubernetes/bin/utils/install_redpanda_console.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+echo "# Install Redpanda Console"
+# https://github.com/redpanda-data/console
+helm upgrade \
+  redpanda-console \
+  console \
+  --install \
+  --repo=https://charts.redpanda.com \
+  --namespace=hm-redpanda-console \
+  --create-namespace \
+  --values=kubernetes/manifests/hm-redpanda-console/helm/my-values.yaml
+# helm uninstall redpanda-console --namespace=hm-redpanda-console
+# kubectl delete namespace hm-redpanda-console
+echo "=================================================="
