c65bf420f814dc3ad6d5b937ed3e7f24260883ce
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-05-06 16:24:36 -0700

    chore(node): remove circuit breaker opossum (#8573)

--- api-node/src/graphQL/dataLoaders/starshipDataLoader.ts
+++ api-node/src/graphQL/dataLoaders/starshipDataLoader.ts
@@ -1,10 +1,10 @@
 import DataLoader from 'dataloader';
-import fetchStarshipByIDWithBreaker from '../../dataSources/swapi/utils/fetchStarshipByIDWithBreaker';
+import fetchStarshipByID from '../../dataSources/swapi/utils/fetchStarshipByID';
 import GraphQLStarship from '../types/GraphQLStarship';
 
 const starshipDataLoader = (): DataLoader<string, GraphQLStarship | null> =>
   new DataLoader(async (ids: ReadonlyArray<string>) => {
-    return Promise.all(ids.map((id) => fetchStarshipByIDWithBreaker(id)));
+    return Promise.all(ids.map((id) => fetchStarshipByID(id)));
   });
 
 export default starshipDataLoader;
