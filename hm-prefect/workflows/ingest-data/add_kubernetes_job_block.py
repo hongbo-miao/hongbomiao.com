785f011d65c315ad290772636fe853fbff0d9061
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-05-20 19:09:14 -0700

    chore(prefect): remove collect-data workflow (#8799)

--- hm-prefect/workflows/ingest-data/add_kubernetes_job_block.py
+++
@@ -1,8 +0,0 @@
-from prefect.infrastructure import KubernetesJob
-
-job = KubernetesJob(
-    image="ghcr.io/hongbo-miao/hm-prefect-ingest-data:latest",
-    namespace="hm-prefect",
-    image_pull_policy="Always",
-)
-job.save("collect-data-kubernetes-job-block", overwrite=True)
