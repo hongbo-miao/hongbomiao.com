c600329508a13d7c5eccb0de0e1d193941c15704
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-05-20 21:57:56 -0700

    refactor(delta): use python-dotenv

--- hm-locust/config.py
+++ hm-locust/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 env_path = Path("../api-node") / ".env.development.local"
-load_dotenv(dotenv_path=env_path)
+load_dotenv(env_path)
 
 seed_user_email = os.getenv("SEED_USER_EMAIL")
 seed_user_password = os.getenv("SEED_USER_PASSWORD")
