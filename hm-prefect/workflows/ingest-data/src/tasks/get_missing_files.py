a3830687c87610ff7603e042029f8decf605ba69
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-05-20 18:21:23 -0700

    feat(prefect): add ingest data workflow (#8796)

---
+++ hm-prefect/workflows/ingest-data/src/tasks/get_missing_files.py
@@ -0,0 +1,29 @@
+import json
+
+from prefect import task
+from prefect_shell import ShellOperation
+
+
+def extract_missing_files(log: list[str]) -> list[str]:
+    missing_files = []
+    for line in log:
+        data = json.loads(line)
+        if "object" in data:
+            missing_files.append(data["object"])
+    return missing_files
+
+
+@task
+async def get_missing_files(
+    source_dirname: str, s3_path: str, delta_table: str
+) -> list[tuple[str, str, str, str]]:
+    log = await ShellOperation(
+        commands=[
+            f'rclone copy --dry-run --include="*.tdms" --use-json-log {source_dirname} {s3_path}',
+        ],
+        stream_output=False,
+    ).run()
+    missing_files = extract_missing_files(log)
+    return [
+        (filename, source_dirname, s3_path, delta_table) for filename in missing_files
+    ]
