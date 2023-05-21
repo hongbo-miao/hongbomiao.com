91f6a3bd92aab5ea5c21a8d028c54311692387c1
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-05-21 03:18:00 -0700

    chore(prefect): different concurrency limit name for each source (#8804)

--- hm-prefect/workflows/ingest-data/src/tasks/get_missing_files.py
+++ hm-prefect/workflows/ingest-data/src/tasks/get_missing_files.py
@@ -15,15 +15,16 @@ def extract_missing_files(log: list[str]) -> list[str]:
 
 @task
 async def get_missing_files(
-    source_dirname: str, s3_path: str, delta_table: str
+    source_dirname: str, s3_raw_path: str, delta_table_path: str, location: str
 ) -> list[tuple[str, str, str, str]]:
     log = await ShellOperation(
         commands=[
-            f'rclone copy --dry-run --include="*.tdms" --use-json-log {source_dirname} {s3_path}',
+            f'rclone copy --dry-run --include="*.tdms" --use-json-log {source_dirname} {s3_raw_path}',
         ],
         stream_output=False,
     ).run()
     missing_files = extract_missing_files(log)
     return [
-        (filename, source_dirname, s3_path, delta_table) for filename in missing_files
+        (filename, source_dirname, s3_raw_path, delta_table_path, location)
+        for filename in missing_files
     ]
