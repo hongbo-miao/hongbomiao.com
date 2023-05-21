10f6fa51bd4b1956bc0bf2541814afedc9cf989d
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-05-21 03:18:00 -0700

    chore(prefect): different concurrency limit name for each source (#8804)

--- hm-prefect/workflows/ingest-data/src/main.py
+++ hm-prefect/workflows/ingest-data/src/main.py
@@ -10,31 +10,43 @@ from utils.write_data import write_data
 
 class DataSource(BaseModel):
     source_dirname: str
-    s3_path: str
-    delta_table_path: str
+    location: str
 
 
 @flow
-async def ingest_data(data_sources: list[DataSource]) -> None:
+async def ingest_data(
+    s3_raw_dirname: str, delta_table_dirname: str, data_sources: list[DataSource]
+) -> None:
     get_missing_files_tasks = []
     for data_source in data_sources:
         source_dirname = data_source.source_dirname
-        s3_path = data_source.s3_path
-        delta_table_path = data_source.delta_table_path
+        location = data_source.location
+        s3_raw_path = f"{s3_raw_dirname}/{data_source.location}"
+        delta_table_path = f"{delta_table_dirname}/{data_source.location}"
         get_missing_files_with_options = get_missing_files.with_options(
             name=f"get-missing-files-{source_dirname}"
         )
         t = asyncio.create_task(
-            get_missing_files_with_options(source_dirname, s3_path, delta_table_path)
+            get_missing_files_with_options(
+                source_dirname, s3_raw_path, delta_table_path, location
+            )
         )
         get_missing_files_tasks.append(t)
     missing_files_list = await asyncio.gather(*get_missing_files_tasks)
 
     write_tasks = []
     for missing_files in missing_files_list:
-        for filename, source_dirname, s3_path, delta_table_path in missing_files:
+        for (
+            filename,
+            source_dirname,
+            s3_raw_path,
+            delta_table_path,
+            location,
+        ) in missing_files:
             t = asyncio.create_task(
-                write_data(filename, source_dirname, s3_path, delta_table_path)
+                write_data(
+                    filename, source_dirname, s3_raw_path, delta_table_path, location
+                )
             )
             write_tasks.append(t)
     await asyncio.gather(*write_tasks)
@@ -42,4 +54,10 @@ async def ingest_data(data_sources: list[DataSource]) -> None:
 
 if __name__ == "__main__":
     params = json.loads(Path("params.json").read_text())
-    asyncio.run(ingest_data(data_sources=params["data_sources"]))
+    asyncio.run(
+        ingest_data(
+            s3_raw_dirname=params["s3_raw_dirname"],
+            delta_table_dirname=params["delta_table_dirname"],
+            data_sources=params["data_sources"],
+        )
+    )
