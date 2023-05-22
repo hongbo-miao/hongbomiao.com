1cc6a5b6d60a4bc0b13e7c2154d8a76c080312d5
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-05-22 00:39:39 -0700

    chore(delta): generate dataframe

--- tdms/src/main.py
+++ tdms/src/main.py
@@ -2,13 +2,13 @@ from utils.convert_tdms_to_parquet import convert_tdms_to_parquet
 from utils.generate_iot_tdms import generate_iot_tdms
 
 
-def main(data_dirname: str, tdms_filename: str, data_point_count: int) -> None:
-    generate_iot_tdms(data_dirname, tdms_filename, data_point_count)
+def main(data_dirname: str, tdms_filename: str, row_count: int) -> None:
+    generate_iot_tdms(data_dirname, tdms_filename, row_count)
     convert_tdms_to_parquet(data_dirname, tdms_filename)
 
 
 if __name__ == "__main__":
     external_data_dirname = "data"
     external_tdms_filename = "iot.tdms"
-    external_data_point_count = 1000000
-    main(external_data_dirname, external_tdms_filename, external_data_point_count)
+    external_row_count = 1000000
+    main(external_data_dirname, external_tdms_filename, external_row_count)
