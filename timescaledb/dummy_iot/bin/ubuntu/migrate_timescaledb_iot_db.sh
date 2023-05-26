8ed55b15b8c9a704250bfeb1d8c133797b2caed5
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-05-26 00:01:23 -0700

    chore(hasura): bump hasura version to v2.25.1-ce.cli-migrations-v3

--- timescaledb/dummy_iot/bin/ubuntu/migrate_timescaledb_iot_db.sh
+++ timescaledb/dummy_iot/bin/ubuntu/migrate_timescaledb_iot_db.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "# Create database iot_db"
 psql postgresql://postgres:passw0rd@localhost:5432/postgres --command="create database iot_db;"
-psql postgresql://postgres:passw0rd@localhost:5432/postgres --command="grant all privileges on database iot_db to postgres;"
+psql postgresql://postgres:passw0rd@localhost:5432/postgres --command="grant all privileges on database iot_db to admin;"
 echo "=================================================="
 
 echo "# Create extension timescaledb in iot_db"
