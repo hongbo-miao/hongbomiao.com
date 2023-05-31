853072446d0e4fed045d3bfb1f7383bdea5aa001
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-05-30 18:52:24 -0700

    chore(spark): clean SparkSession (#8954)

--- hm-spark/applications/ingest-from-s3-to-kafka/src/main/scala/com/hongbomiao/IngestFromS3ToKafka.scala
+++ hm-spark/applications/ingest-from-s3-to-kafka/src/main/scala/com/hongbomiao/IngestFromS3ToKafka.scala
@@ -11,13 +11,11 @@ object IngestFromS3ToKafka {
     val spark: SparkSession = SparkSession
       .builder()
       .master("local[*]")
-      .appName("ingest-from-s3-to-kafka")
       .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
       .config(
         "spark.sql.catalog.spark_catalog",
         "org.apache.spark.sql.delta.catalog.DeltaCatalog"
       )
-      .config("spark.ui.port", "4040")
       .getOrCreate()
 
     val toAvroConfig: ToAvroConfig =
