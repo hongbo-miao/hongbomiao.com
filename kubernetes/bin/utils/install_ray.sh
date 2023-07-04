813e779b70f66159c31d717efbba819832311928
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-07-04 16:09:55 -0700

    feat(k8s): add ray (#9529)

---
+++ kubernetes/bin/utils/install_ray.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+echo "# Install KubeRay operator"
+# https://docs.ray.io/en/latest/cluster/kubernetes/getting-started.html
+helm upgrade \
+  kuberay-operator \
+  kuberay-operator \
+  --install \
+  --repo=https://ray-project.github.io/kuberay-helm \
+  --namespace=hm-ray \
+  --create-namespace
+# helm uninstall kuberay-operator --namespace=hm-ray
+
+echo "# Install Ray cluster"
+helm upgrade \
+  ray-cluster \
+  ray-cluster \
+  --install \
+  --repo=https://ray-project.github.io/kuberay-helm \
+  --namespace=hm-ray \
+  --create-namespace
+# helm uninstall ray-cluster --namespace=hm-ray
+# kubectl delete namespace hm-ray
+echo "=================================================="
