9ca9022a1f15abcea5f1367d07901d9a16dd02fe
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-07-04 02:42:07 -0700

    perf(cnn): add Metal Performance Shaders (MPS) for GPU training acceleration

--- convolutional-neural-network/train.py
+++ convolutional-neural-network/train.py
@@ -1,9 +1,12 @@
-def train(net, data_loader, optimizer, criterion):
+def train(net, data_loader, device, optimizer, criterion):
     net.train()
     running_loss = 0.0
 
     for i, data in enumerate(data_loader, 0):
         inputs, labels = data
+
+        inputs = inputs.to(device)
+        labels = labels.to(device)
 
         optimizer.zero_grad()
 
--- convolutional-neural-network/utils/device.py
+++ convolutional-neural-network/utils/device.py
@@ -1,3 +1,9 @@
 import torch
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
