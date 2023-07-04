195a0b1a8c9e06860f7d54f2b9d9e54abae63928
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-07-04 02:56:16 -0700

    perf(cnn): add Metal Performance Shaders (MPS) for GPU training acceleration (#9506)

--- convolutional-neural-network/evaluate.py
+++ convolutional-neural-network/evaluate.py
@@ -1,17 +1,18 @@
 import torch
 
 
-def evaluate(
-    net,
-    data_loader,
-):
+def evaluate(net, data_loader, device):
     net.eval()
     correct = 0
     total = 0
     with torch.no_grad():
         for data in data_loader:
-            images, labels = data
-            outputs = net(images)
+            inputs, labels = data
+
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = net(inputs)
             # The class with the highest energy is what we choose as prediction
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
