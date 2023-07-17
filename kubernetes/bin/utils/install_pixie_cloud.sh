db1711350bb339c3ef8901109d999beaed77f667
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-07-17 01:58:44 -0700

    chore(shell): use curl -sSL

--- kubernetes/bin/utils/install_pixie_cloud.sh
+++ kubernetes/bin/utils/install_pixie_cloud.sh
@@ -4,7 +4,7 @@ set -e
 # https://docs.px.dev/installing-pixie/install-guides/community-cloud-for-pixie/
 
 echo "# Install the Pixie CLI"
-bash -c "$(curl -fsSL https://withpixie.ai/install.sh)"
+bash -c "$(curl -sSL https://withpixie.ai/install.sh)"
 echo "=================================================="
 
 echo "# Deploy Pixie"
