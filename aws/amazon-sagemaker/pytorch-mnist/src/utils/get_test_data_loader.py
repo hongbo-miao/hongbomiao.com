0668f58e0a76c2ea2b1fb7e6ce9fb86e49217455
Hongbo Miao <3375461+Hongbo-Miao@users.noreply.github.com>
2023-08-13 04:22:33 -0700

    feat(sagemaker): add pytorch mnist (#10354)

---
+++ aws/amazon-sagemaker/pytorch-mnist/src/utils/get_test_data_loader.py
@@ -0,0 +1,22 @@
+import logging
+
+import torch
+import torch.utils.data
+import torch.utils.data.distributed
+from torchvision import datasets, transforms
+
+
+def get_test_data_loader(test_batch_size, training_dir, **kwargs):
+    logging.info("Get test data loader")
+    return torch.utils.data.DataLoader(
+        datasets.MNIST(
+            training_dir,
+            train=False,
+            transform=transforms.Compose(
+                [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+            ),
+        ),
+        batch_size=test_batch_size,
+        shuffle=True,
+        **kwargs
+    )
---
+++ aws/amazon-sagemaker/pytorch-mnist/src/utils/get_train_data_loader.py
@@ -0,0 +1,29 @@
+import logging
+
+import torch
+import torch.utils.data
+import torch.utils.data.distributed
+from torchvision import datasets, transforms
+
+
+def get_train_data_loader(batch_size, training_dir, is_distributed, **kwargs):
+    logging.info("Get train data loader")
+    dataset = datasets.MNIST(
+        training_dir,
+        train=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        ),
+    )
+    train_sampler = (
+        torch.utils.data.distributed.DistributedSampler(dataset)
+        if is_distributed
+        else None
+    )
+    return torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=train_sampler is None,
+        sampler=train_sampler,
+        **kwargs
+    )
