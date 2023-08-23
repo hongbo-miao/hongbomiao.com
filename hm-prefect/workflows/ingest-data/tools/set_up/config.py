65baf244a571fe382885542035c3fbb08a87bf60
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-08-23 01:20:16 -0700

    chore(prefect): verify value in get_env_variable function

--- hm-prefect/workflows/ingest-data/tools/set_up/config.py
+++ hm-prefect/workflows/ingest-data/tools/set_up/config.py
@@ -1,11 +1,10 @@
-import os
-
 from dotenv import load_dotenv
+from utils.get_env_variable import get_env_variable
 
 load_dotenv("tools/set_up/.env.production.local")
 
-aws_default_region = os.getenv("AWS_DEFAULT_REGION")
-aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
-aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+aws_default_region = get_env_variable("AWS_DEFAULT_REGION")
+aws_access_key_id = get_env_variable("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = get_env_variable("AWS_SECRET_ACCESS_KEY")
 
 flow_name = "ingest-data"
