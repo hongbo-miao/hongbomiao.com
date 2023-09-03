c95c4f1fef3051918170db74ded410dd0e3aefac
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-09-03 04:23:31 -0700

    feat(terraform): add aws glue crawler (#10833)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,7 @@
 terraform {
+  # backend "local" {
+  #   path = "terraform.tfstate"
+  # }
   backend "s3" {
     region = "us-west-2"
     bucket = "hongbomiao-bucket"
@@ -18,7 +21,18 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "hm_ec2_module" {
-  source            = "./modules/hm_ec2"
-  ec2_instance_name = "hm-ec2-instance"
+module "hm_amazon_ec2_module" {
+  source            = "./modules/hm_amazon_ec2"
+  ec2_instance_name = "hm-amazon-ec2-instance"
+  ec2_instance_ami  = "ami-0c79a55dda52434da" # Ubuntu Server 22.04 LTS (HVM), SSD Volume Type (64-bit (Arm))
+  ec2_instance_type = "t2.nano"
+}
+module "hm_aws_glue_crawler_module" {
+  source                        = "./modules/hm_aws_glue_crawler"
+  aws_glue_crawler_name         = "hm-delta-lake-crawler-iot"
+  aws_glue_crawler_delta_tables = ["s3://hongbomiao-bucket/delta-tables/motor/"]
+  aws_glue_database             = "hm_delta_db"
+  aws_glue_crawler_role         = "arn:aws:iam::272394222652:role/service-role/AWSGlueServiceRole-hm"
+  environment                   = var.environment
+  team                          = var.team
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "environment" {
+  type = string
+}
+variable "team" {
+  type = string
+}
