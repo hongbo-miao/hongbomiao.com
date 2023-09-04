5a557892fdc58f29bd0f299c478da0d80bf8b984
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-09-03 23:42:07 -0700

    feat(athena): add athena queries (#10882)

---
+++ aws/amazon-athena/queries/delta_lake.sql
@@ -0,0 +1,12 @@
+create external table awsdatacatalog.hm_iot_db.motor
+location 's3://hongbomiao-bucket/delta-tables/motor/'
+tblproperties (
+    'table_type' = 'delta'
+);
+
+select
+    timestamp,
+    current,
+    voltage,
+    temperature
+from awsdatacatalog.hm_iot_db.motor limit 100;
