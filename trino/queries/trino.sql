88c9a646e49ed598e1dd16ceb83aa277a5d29441
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-09-03 23:42:07 -0700

    feat(athena): add athena queries (#10882)

--- trino/queries/trino.sql
+++ trino/queries/trino.sql
@@ -6,3 +6,6 @@ analyze delta.hm_iot_db.motor;
 
 show stats for delta.hm_iot_db.motor;
 show stats for (select * from delta.hm_iot_db.motor where voltage > 10);
+
+-- Show table data description language (DDL)
+show create table delta.hm_iot_db.motor;
