c794d870731bd1a6c9b7dd2c643fe853928d4388
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-09-06 18:29:55 -0700

    chore(terraform): pass database_name as var (#10947)

--- terraform/modules/hm_aws_glue_crawler/main.tf
+++ terraform/modules/hm_aws_glue_crawler/main.tf
@@ -1,8 +1,8 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/glue_crawler
 resource "aws_glue_crawler" "itg_aws_glue_crawler" {
-  database_name = "hm_delta_db"
   name          = var.aws_glue_crawler_name
   role          = var.aws_glue_crawler_role
+  database_name = var.aws_glue_database
   delta_target {
     delta_tables              = var.aws_glue_crawler_delta_tables
     create_native_delta_table = true
