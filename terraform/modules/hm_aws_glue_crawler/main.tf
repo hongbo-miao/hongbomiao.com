af0bb2a1efdb374be1e5421ad11e55395c558437
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-09-07 23:54:44 -0700

    chore(terraform): add schema_change_policy for aws_glue_crawler (#10972)

--- terraform/modules/hm_aws_glue_crawler/main.tf
+++ terraform/modules/hm_aws_glue_crawler/main.tf
@@ -1,5 +1,5 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/glue_crawler
-resource "aws_glue_crawler" "itg_aws_glue_crawler" {
+resource "aws_glue_crawler" "hm_aws_glue_crawler" {
   name          = var.aws_glue_crawler_name
   role          = var.aws_glue_crawler_role
   database_name = var.aws_glue_database
@@ -7,6 +7,10 @@ resource "aws_glue_crawler" "itg_aws_glue_crawler" {
     delta_tables              = var.aws_glue_crawler_delta_tables
     create_native_delta_table = true
     write_manifest            = false
+  }
+  schema_change_policy {
+    update_behavior = "UPDATE_IN_DATABASE"
+    delete_behavior = "DEPRECATE_IN_DATABASE"
   }
   tags = {
     Environment = var.environment
