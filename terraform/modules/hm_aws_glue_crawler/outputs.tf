aabf9e3f6e2df99c78e4a7ebd3d4b4d234675e52
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-09-07 23:44:28 -0700

    chore(terraform): add schema_change_policy for aws_glue_crawler

--- terraform/modules/hm_aws_glue_crawler/outputs.tf
+++ terraform/modules/hm_aws_glue_crawler/outputs.tf
@@ -1,9 +1,9 @@
 output "crawler_id" {
-  value = aws_glue_crawler.itg_aws_glue_crawler.id
+  value = aws_glue_crawler.hm_aws_glue_crawler.id
 }
 output "crawler_name" {
-  value = aws_glue_crawler.itg_aws_glue_crawler.name
+  value = aws_glue_crawler.hm_aws_glue_crawler.name
 }
 output "crawler_arn" {
-  value = aws_glue_crawler.itg_aws_glue_crawler.arn
+  value = aws_glue_crawler.hm_aws_glue_crawler.arn
 }
