7ae3bafb634e275900316f1fdb21737f843d5fe7
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-09-11 01:36:49 -0700

    feat(terraform): support amazon emr cluster (#11019)

--- terraform/modules/hm_amazon_ec2/outputs.tf
+++ terraform/modules/hm_amazon_ec2/outputs.tf
@@ -1,7 +1,6 @@
-output "ec2_instance_id" {
+output "id" {
   value = aws_instance.hm_ec2_instance.id
 }
-
-output "ec2_instance_public_ip" {
+output "public_ip" {
   value = aws_instance.hm_ec2_instance.public_ip
 }
