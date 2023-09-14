84110f9305fad3d83355a61aedaa4f74f0c47044
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-09-14 00:56:08 -0700

    feat(terraform): add aws_s3_object (#11087)

---
+++ terraform/modules/hm_amazon_s3_object/main.tf
@@ -0,0 +1,7 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+resource "aws_s3_object" "hm_amazon_s3_object" {
+  bucket = var.amazon_s3_bucket
+  key    = var.amazon_s3_key
+  source = var.local_file_path
+  etag   = filemd5(var.local_file_path)
+}
