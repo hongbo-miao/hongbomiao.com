88ecd08c6d92c783cac388556b60a147d5d91a5f
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-09-21 23:15:18 -0700

    refactor(hugging-face): move code under src folder

--- hugging-face/sentiment-analysis.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline("sentiment-analysis")
-print(classifier("I've been waiting for this course my whole life."))
-print(classifier("I've been waiting for so long for this package to ship."))
---
+++ hugging-face/src/main.py
@@ -0,0 +1,14 @@
+import logging
+
+from transformers import pipeline
+
+
+def main():
+    classifier = pipeline("sentiment-analysis")
+    logging.info(classifier("I've been waiting for this course my whole life."))
+    logging.info(classifier("I've been waiting for so long for this package to ship."))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
