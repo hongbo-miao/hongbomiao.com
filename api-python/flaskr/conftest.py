1352af147b5024e54d334ecf4a7cf6fb421d3c75
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-10-01 17:47:09 -0700

    refactor(flask): move test files along with files being tested (#11551)

--- api-python/flaskr/conftest.py
+++ api-python/flaskr/conftest.py
@@ -1,19 +1,21 @@
 import pytest
+from flask import Flask
+from flask.testing import FlaskClient, FlaskCliRunner
 from flaskr import create_app
 
 
 @pytest.fixture()
-def app():
+def app() -> Flask:
     app = create_app()
     app.config.update({"TESTING": True})
     yield app
 
 
 @pytest.fixture()
-def client(app):
+def client(app) -> FlaskClient:
     return app.test_client()
 
 
 @pytest.fixture()
-def runner(app):
+def runner(app) -> FlaskCliRunner:
     return app.test_cli_runner()
--- api-python/tests/test_health.py
+++
@@ -1,4 +0,0 @@
-def test_request_example(client):
-    res = client.get("/")
-    assert res.status_code == 200
-    assert res.json["api"] == "ok"
