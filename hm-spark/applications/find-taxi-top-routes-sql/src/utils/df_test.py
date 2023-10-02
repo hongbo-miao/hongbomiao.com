71f45aeed061f37992655a47817bfec23876c8a5
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-10-01 18:05:33 -0700

    test(spark): add tests for pyspark applications

---
+++ hm-spark/applications/find-taxi-top-routes-sql/src/utils/df_test.py
@@ -0,0 +1,14 @@
+from df import union_all
+from pyspark.sql import SparkSession
+
+
+class TestUnionAll:
+    def test_with_two_dataframes(self):
+        spark = SparkSession.builder.getOrCreate()
+        df1 = spark.createDataFrame([(1, "a"), (2, "b")], ["col1", "col2"])
+        df2 = spark.createDataFrame([(3, "c"), (4, "d")], ["col1", "col2"])
+        df = union_all(df1, df2)
+        expected_df = spark.createDataFrame(
+            [(1, "a"), (2, "b"), (3, "c"), (4, "d")], ["col1", "col2"]
+        )
+        assert df.collect() == expected_df.collect()
