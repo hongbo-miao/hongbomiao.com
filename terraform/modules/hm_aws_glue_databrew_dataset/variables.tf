fb5a2dbbb25814de2dbe0f4e91653b7e06bf659b
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-11-19 03:36:53 -0800

    refactor(terraform): split glue_databrew_job module (#12765)

---
+++ terraform/modules/hm_aws_glue_databrew_dataset/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_glue_databrew_dataset_name" {
+  type = string
+}
+variable "input_s3_bucket" {
+  type = string
+}
+variable "input_s3_dir" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "team" {
+  type = string
+}
