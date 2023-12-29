679f38b42a1579172854d9c6bb75932b08c002ec
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2023-12-28 18:44:41 -0800

    feat(emr): set PYSPARK_PYTHON to python 3.11 (#13487)

--- terraform/data/amazon-emr/hm-amazon-emr-cluster-sedona/bootstrap-actions/set_up.sh
+++ terraform/data/amazon-emr/hm-amazon-emr-cluster-sedona/bootstrap-actions/set_up.sh
@@ -6,7 +6,8 @@ echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWhR5NV13iw0X8lKqsrSRqbcIJcA5AVMjyfJjO
 
 echo "# Install Python"
 # https://github.com/aws-samples/aws-emr-utilities/blob/main/utilities/emr-ec2-custom-python3/README.md
-PYTHON_VERSION=3.11.6
+# Update the corresponding hm_sedona_emr -> PYSPARK_PYTHON version in terraform/main.tf
+PYTHON_VERSION=3.11.7
 sudo yum --assumeyes remove openssl-devel*
 sudo yum --assumeyes install \
   bzip2-devel \
@@ -33,10 +34,10 @@ echo "# Install dependencies"
 sudo curl --silent --fail --show-error --location --remote-name --output-dir /usr/lib/spark/jars/ https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-3.4_2.12/1.5.0/sedona-spark-shaded-3.4_2.12-1.5.0.jar
 sudo curl --silent --fail --show-error --location --remote-name --output-dir /usr/lib/spark/jars/ https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.5.0-28.2/geotools-wrapper-1.5.0-28.2.jar
 "/usr/local/python${PYTHON_VERSION}/bin/python${PYTHON_VERSION%.*}" -m pip install \
-  apache-sedona==1.5.0 \
+  apache-sedona[spark]==1.5.0 \
   attrs==23.1.0 \
   descartes==1.1.0 \
   geopandas==0.14.1 \
   matplotlib==3.8.2 \
-  pandas==2.1.3 \
+  pandas==2.1.4 \
   shapely==2.0.2
