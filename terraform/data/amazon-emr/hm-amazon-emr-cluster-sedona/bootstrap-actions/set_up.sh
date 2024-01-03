ac8f588657732d4f18bc78d74527115d553d86d4
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-01-02 23:29:58 -0800

    chore(terraform): upgrade emr version to 7.0.0 (#13626)

--- terraform/data/amazon-emr/hm-amazon-emr-cluster-sedona/bootstrap-actions/set_up.sh
+++ terraform/data/amazon-emr/hm-amazon-emr-cluster-sedona/bootstrap-actions/set_up.sh
@@ -2,21 +2,27 @@
 set -e
 
 echo "# Add SSH keys"
-echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWhR5NV13iw0X8lKqsrSRqbcIJcA5AVMjyfJjOrplwH hongbo-miao >> /home/hadoop/.ssh/authorized_keys
+# An empty line is necessary since the original public key lacks a newline character at the end
+{
+  echo
+  echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWhR5NV13iw0X8lKqsrSRqbcIJcA5AVMjyfJjOrplwH hongbo-miao"
+} >> /home/hadoop/.ssh/authorized_keys
 
 echo "# Install Python"
 # https://github.com/aws-samples/aws-emr-utilities/blob/main/utilities/emr-ec2-custom-python3/README.md
 # Update the corresponding hm_sedona_emr -> PYSPARK_PYTHON version in terraform/main.tf
 PYTHON_VERSION=3.11.7
-sudo yum --assumeyes remove openssl-devel*
+# systemtap-sdt-devel is required for executing `./configure --with-dtrace`, https://docs.python.org/3/howto/instrumentation.html
+# zlib-devel is required for executing `sudo make altinstall`
 sudo yum --assumeyes install \
   bzip2-devel \
   expat-devel \
   gcc \
   libffi-devel \
   make \
-  openssl11-devel \
-  tar
+  systemtap-sdt-devel \
+  tar \
+  zlib-devel
 curl --silent --fail --show-error --location "https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz" | tar -x -J -v
 cd "Python-${PYTHON_VERSION}"
 export CFLAGS="-march=native"
@@ -37,7 +43,6 @@ sudo curl --silent --fail --show-error --location --remote-name --output-dir /us
   apache-sedona[spark]==1.5.0 \
   attrs==23.1.0 \
   descartes==1.1.0 \
-  geopandas==0.14.1 \
   matplotlib==3.8.2 \
   pandas==2.1.4 \
   shapely==2.0.2
