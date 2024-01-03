315550c5cc4ffbeaf668762c5fba44390e5b5ba7
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-01-02 23:29:58 -0800

    chore(terraform): upgrade emr version to 7.0.0 (#13626)

--- terraform/data/amazon-emr/hm-amazon-emr-cluster-trino/bootstrap-actions/set_up.sh
+++ terraform/data/amazon-emr/hm-amazon-emr-cluster-trino/bootstrap-actions/set_up.sh
@@ -2,4 +2,8 @@
 set -e
 
 echo "# Add SSH keys"
-echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWhR5NV13iw0X8lKqsrSRqbcIJcA5AVMjyfJjOrplwH hongbo-miao >> /home/hadoop/.ssh/authorized_keys
+# An empty line is necessary since the original public key lacks a newline character at the end
+{
+  echo
+  echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWhR5NV13iw0X8lKqsrSRqbcIJcA5AVMjyfJjOrplwH hongbo-miao"
+} >> /home/hadoop/.ssh/authorized_keys
