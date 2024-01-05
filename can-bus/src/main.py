8b6007b1a6c7c7192762183764afc3f1fc8b8285
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-01-05 00:08:58 -0800

    chore(can): update to eec1_message (#13671)

--- can-bus/src/main.py
+++ can-bus/src/main.py
@@ -7,11 +7,11 @@ import cantools
 def main() -> None:
     db = cantools.database.load_file("src/dbc/engine.dbc")
     logging.info(db.messages)
-    example_message = db.get_message_by_name("EEC1")
-    logging.info(example_message.signals)
+    eec1_message = db.get_message_by_name("EEC1")
+    logging.info(eec1_message.signals)
 
-    data = example_message.encode({"EngineSpeed": 200.1})
-    message = can.Message(arbitration_id=example_message.frame_id, data=data)
+    data = eec1_message.encode({"EngineSpeed": 200.1})
+    message = can.Message(arbitration_id=eec1_message.frame_id, data=data)
     logging.info(message)
 
     can_bus = can.interface.Bus("vcan0", bustype="socketcan")
