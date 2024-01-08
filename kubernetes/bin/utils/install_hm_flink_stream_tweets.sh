ff4821d6e2c97bc38770ee826ca94f4d162bae06
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-01-07 16:22:57 -0800

    refactor(app): create data-processing folder (#13713)

--- kubernetes/bin/utils/install_hm_flink_stream_tweets.sh
+++ kubernetes/bin/utils/install_hm_flink_stream_tweets.sh
@@ -10,16 +10,16 @@ echo "# Install Redis"
 source kubernetes/bin/utils/install_redis.sh
 echo "=================================================="
 
-echo "# Install hm-streaming"
+echo "# Install hm-flink-stream-tweets"
 kubectl apply --filename=kubernetes/manifests/flink/hm-flink-namespace.yaml
 flink run-application \
   --target kubernetes-application \
   -Dkubernetes.namespace=hm-flink \
   -Dkubernetes.cluster-id=hm-flink-cluster \
-  -Dkubernetes.container.image=ghcr.io/hongbo-miao/hm-streaming:latest \
+  -Dkubernetes.container.image=ghcr.io/hongbo-miao/hm-flink-stream-tweets:latest \
   -Dkubernetes.container.image.pull-policy=Always \
   -Dkubernetes.jobmanager.service-account=flink-serviceaccount \
-  local:///opt/flink/usrlib/streaming-0.1.jar
+  local:///opt/flink/usrlib/flink-stream-tweets-0.1.jar
 
 # Delete:
 # kubectl delete deployment/hm-flink-cluster --namespace=hm-flink
