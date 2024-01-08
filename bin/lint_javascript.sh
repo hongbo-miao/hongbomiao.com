28aa15586901ee3db8a641af9a2398671f424c32
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-01-07 19:40:35 -0800

    refactor(app): create data-visualization folder (#13733)

--- bin/lint_javascript.sh
+++ bin/lint_javascript.sh
@@ -2,8 +2,8 @@
 set -e
 
 cd api-node && npm run lint:javascript
+cd ../data-visualization/grafana/hm-panel-plugin && npm run lint:javascript
 cd ../ethereum && npm run lint:javascript
-cd ../grafana/hm-panel-plugin && npm run lint:javascript
 cd ../../mobile-react-native && npm run lint:javascript
 cd ../web && npm run lint:javascript
 cd ../web-cypress && npm run lint:javascript
