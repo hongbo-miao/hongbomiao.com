a3971e4a41380f844d4fb3cfc9c27eea2a21f1be
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-01-10 21:22:29 -0800

    feat(drone): add px4 (#13801)

---
+++ drone/px4/bin/set_up.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+# https://docs.px4.io/main/en/dev_setup/dev_env_linux_ubuntu.html
+git clone https://github.com/PX4/PX4-Autopilot.git --recursive
+make px4_sitl jmavsim
+
+# Commands
+pxh> commander takeoff
+pxh> commander land
