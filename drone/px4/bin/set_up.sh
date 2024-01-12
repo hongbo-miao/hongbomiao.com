8904afd4c76690e39e8c080fe92bb9c21bb29123
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-01-12 10:57:33 -0800

    fix(px4): fix px4 set up script (#13827)

--- drone/px4/bin/set_up.sh
+++ drone/px4/bin/set_up.sh
@@ -3,6 +3,7 @@ set -e
 
 # https://docs.px4.io/main/en/dev_setup/dev_env_linux_ubuntu.html
 git clone https://github.com/PX4/PX4-Autopilot.git --recursive
+cd PX4-Autopilot
 make px4_sitl jmavsim
 
 # Commands
