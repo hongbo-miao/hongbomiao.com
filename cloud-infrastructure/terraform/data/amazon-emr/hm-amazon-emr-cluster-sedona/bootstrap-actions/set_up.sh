62f192e4cd1e233a8fe549acf07c54f9986b1238
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-01-24 00:41:36 -0800

    chore(emr): bump sedona version to 1.5.1 (#14045)

--- cloud-infrastructure/terraform/data/amazon-emr/hm-amazon-emr-cluster-sedona/bootstrap-actions/set_up.sh
+++ cloud-infrastructure/terraform/data/amazon-emr/hm-amazon-emr-cluster-sedona/bootstrap-actions/set_up.sh
@@ -9,5 +9,6 @@ echo "# Add SSH keys"
 } >> /home/hadoop/.ssh/authorized_keys
 
 echo "# Install Amazon EMR cluster-scoped dependencies"
-sudo curl --silent --fail --show-error --location --remote-name --output-dir /usr/lib/spark/jars/ https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-3.4_2.12/1.5.0/sedona-spark-shaded-3.4_2.12-1.5.0.jar
-sudo curl --silent --fail --show-error --location --remote-name --output-dir /usr/lib/spark/jars/ https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.5.0-28.2/geotools-wrapper-1.5.0-28.2.jar
+# https://sedona.apache.org/latest/setup/emr/
+sudo curl --silent --fail --show-error --location --remote-name --output-dir /usr/lib/spark/jars/ https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-3.5_2.12/1.5.1/sedona-spark-shaded-3.5_2.12-1.5.1.jar
+sudo curl --silent --fail --show-error --location --remote-name --output-dir /usr/lib/spark/jars/ https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.5.1-28.2/geotools-wrapper-1.5.1-28.2.jar
