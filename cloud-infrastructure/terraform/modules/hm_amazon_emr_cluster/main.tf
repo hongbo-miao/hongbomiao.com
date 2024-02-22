c627cbc13baddde23024a0479c464a4fe3b74492
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-02-21 23:05:48 -0800

    feat(emr): set different primary, core, task node sizes (#14751)

--- cloud-infrastructure/terraform/modules/hm_amazon_emr_cluster/main.tf
+++ cloud-infrastructure/terraform/modules/hm_amazon_emr_cluster/main.tf
@@ -3,7 +3,7 @@ resource "aws_emr_cluster" "hm_amazon_emr_cluster" {
   name                              = var.amazon_emr_cluster_name
   release_label                     = var.amazon_emr_version
   applications                      = var.applications
-  termination_protection            = true
+  termination_protection            = false
   keep_job_flow_alive_when_no_steps = true
   log_uri                           = "s3://hongbomiao-bucket/amazon-emr/logs/"
   ec2_attributes {
@@ -16,36 +16,36 @@ resource "aws_emr_cluster" "hm_amazon_emr_cluster" {
   }
   master_instance_fleet {
     name                      = "Primary"
-    target_on_demand_capacity = 1
+    target_on_demand_capacity = var.primary_instance_target_on_demand_capacity
     launch_specifications {
       on_demand_specification {
         allocation_strategy = "lowest-price"
       }
     }
     instance_type_configs {
+      weighted_capacity = var.primary_instance_weighted_capacity
       instance_type     = var.primary_instance_type
-      weighted_capacity = 1
     }
   }
   core_instance_fleet {
     name                      = "Core"
-    target_on_demand_capacity = var.core_target_on_demand_capacity
+    target_on_demand_capacity = var.core_instance_target_on_demand_capacity
     launch_specifications {
       on_demand_specification {
         allocation_strategy = "lowest-price"
       }
     }
     instance_type_configs {
+      weighted_capacity = var.core_instance_weighted_capacity
       instance_type     = var.core_instance_type
-      weighted_capacity = 1
     }
   }
   bootstrap_action {
     name = "set_up"
     path = var.bootstrap_set_up_script_s3_uri
   }
-  configurations_json = var.configurations_json_string
   step                = var.steps
+  configurations_json = var.configurations_json_string
   service_role        = var.iam_role_arn
   tags = {
     for-use-with-amazon-emr-managed-policies = true
@@ -53,9 +53,8 @@ resource "aws_emr_cluster" "hm_amazon_emr_cluster" {
     Team                                     = var.team
     Name                                     = var.amazon_emr_cluster_name
   }
-  # Avoid forcing replacement
-  # https://github.com/hashicorp/terraform-provider-aws/issues/12683#issuecomment-752899019
   lifecycle {
+    # https://github.com/hashicorp/terraform-provider-aws/issues/12683#issuecomment-752899019
     ignore_changes = [
       configurations_json,
       step
--- cloud-infrastructure/terraform/modules/hm_amazon_emr_cluster/variables.tf
+++ cloud-infrastructure/terraform/modules/hm_amazon_emr_cluster/variables.tf
@@ -7,24 +7,33 @@ variable "amazon_emr_version" {
 variable "applications" {
   type = list(string)
 }
+variable "primary_instance_target_on_demand_capacity" {
+  type = number
+}
+variable "primary_instance_weighted_capacity" {
+  type = number
+}
 variable "primary_instance_type" {
   type = string
+}
+variable "core_instance_target_on_demand_capacity" {
+  type = number
+}
+variable "core_instance_weighted_capacity" {
+  type = number
 }
 variable "core_instance_type" {
   type = string
 }
-variable "core_target_on_demand_capacity" {
-  type = number
-}
 variable "bootstrap_set_up_script_s3_uri" {
-  type = string
-}
-variable "configurations_json_string" {
   type = string
 }
 variable "steps" {
   type    = list(any)
   default = []
+}
+variable "configurations_json_string" {
+  type = string
 }
 variable "iam_role_arn" {
   type = string
