cd119e9ab6e0985a0afab145a966ea37c677e8d2
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-04-02 19:33:46 -0700

    feat(terraform): support multiple aws accounts (#15622)

--- cloud-infrastructure/terraform/modules/hm_amazon_ec2/main.tf
+++ cloud-infrastructure/terraform/modules/hm_amazon_ec2/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 resource "aws_instance" "hm_ec2_instance" {
   ami           = var.ec2_instance_ami
--- cloud-infrastructure/terraform/modules/hm_amazon_route_53/main.tf
+++ cloud-infrastructure/terraform/modules/hm_amazon_route_53/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
 resource "aws_route53_record" "hm_amazon_route_53_record" {
   zone_id = "xxxxxxxxxxxxxxxxxxxxx"
