a85ada9e99202f3aec5bfac725eac8f811452eda
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-05-04 19:38:28 -0700

    chore(emr): clean emr config (#16493)

--- cloud-infrastructure/terraform/data/amazon-emr/hm-amazon-emr-cluster-sedona/bootstrap-actions/set_up.sh
+++ cloud-infrastructure/terraform/data/amazon-emr/hm-amazon-emr-cluster-sedona/bootstrap-actions/set_up.sh
@@ -12,3 +12,6 @@ echo "# Install Amazon EMR cluster-scoped dependencies"
 # https://sedona.apache.org/latest/setup/emr/
 sudo curl --silent --fail --show-error --location --remote-name --output-dir /usr/lib/spark/jars/ https://repo1.maven.org/maven2/org/apache/sedona/sedona-spark-shaded-3.5_2.12/1.5.1/sedona-spark-shaded-3.5_2.12-1.5.1.jar
 sudo curl --silent --fail --show-error --location --remote-name --output-dir /usr/lib/spark/jars/ https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.5.1-28.2/geotools-wrapper-1.5.1-28.2.jar
+sudo python3 -m pip install \
+  apache-sedona==1.5.1 \
+  h3==3.7.7
--- cloud-platform/aws/amazon-emr/hm-amazon-emr-cluster-trino/bootstrap-actions/set_up.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# SSH keys
-echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWhR5NV13iw0X8lKqsrSRqbcIJcA5AVMjyfJjOrplwH hongbo-miao >> /home/hadoop/.ssh/authorized_keys
