88ebe5ded4241385bebbc98fda254ef2cf2d2fa5
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-05-13 01:33:22 -0700

    refactor(terraform): seperate providers and versions

--- cloud-infrastructure/terraform/environments/development/aws/data/main.tf
+++ cloud-infrastructure/terraform/environments/development/aws/data/main.tf
@@ -1,33 +1,3 @@
-terraform {
-  backend "s3" {
-    region = "us-west-2"
-    bucket = "hm-terraform-bucket"
-    key    = "development/aws/data/terraform.tfstate"
-  }
-  required_providers {
-    # https://registry.terraform.io/providers/hashicorp/aws/latest
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.49.0"
-    }
-    # https://registry.terraform.io/providers/hashicorp/awscc/latest
-    awscc = {
-      source  = "hashicorp/awscc"
-      version = "0.76.0"
-    }
-  }
-  required_version = ">= 1.7"
-}
-
-provider "aws" {
-  alias  = "development"
-  region = "us-west-2"
-}
-provider "awscc" {
-  alias  = "development"
-  region = "us-west-2"
-}
-
 # Amazon S3 bucket - hm-development-bucket
 module "development_hm_development_bucket_amazon_s3_bucket" {
   providers      = { aws = aws.development }
