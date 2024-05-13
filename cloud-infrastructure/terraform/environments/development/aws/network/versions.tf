19e0efb29e5d130a772c1554f3027b33cb52dff1
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-05-13 01:37:37 -0700

    refactor(terraform): seperate providers and versions (#16640)

---
+++ cloud-infrastructure/terraform/environments/development/aws/network/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    region = "us-west-2"
+    bucket = "hm-terraform-bucket"
+    key    = "development/aws/network/terraform.tfstate"
+  }
+  required_providers {
+    # https://registry.terraform.io/providers/hashicorp/aws/latest
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.49.0"
+    }
+    # https://registry.terraform.io/providers/hashicorp/awscc/latest
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "0.76.0"
+    }
+  }
+  required_version = ">= 1.8"
+}
