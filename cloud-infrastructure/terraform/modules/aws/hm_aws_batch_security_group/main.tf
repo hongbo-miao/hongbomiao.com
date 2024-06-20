6887435f71598ee1994bb2b130ab591c326004b5
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-06-20 02:22:52 -0700

    refactor(terraform): rename security group (#17473)

---
+++ cloud-infrastructure/terraform/modules/aws/hm_aws_batch_security_group/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "hm_aws_batch_security_group" {
+  name   = var.amazon_ec2_security_group_name
+  vpc_id = var.amazon_vpc_id
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = var.amazon_ec2_security_group_name
+  }
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_security_group_egress_rule
+resource "aws_vpc_security_group_egress_rule" "egress_allow" {
+  security_group_id = aws_security_group.hm_aws_batch_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # all ports
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = var.amazon_ec2_security_group_name
+  }
+}
