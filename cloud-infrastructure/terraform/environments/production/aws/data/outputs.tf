b4c0121ff80055059b4c95350c706ec8e9fbf0c1
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-06-21 03:30:57 -0700

    refactor(terraform): remove data folder and move to environments folder (#17522)

--- cloud-infrastructure/terraform/environments/production/aws/data/outputs.tf
+++ cloud-infrastructure/terraform/environments/production/aws/data/outputs.tf
@@ -1,3 +1,6 @@
 output "production_hm_production_bucket_amazon_s3_bucket_name" {
   value = module.production_hm_production_bucket_amazon_s3_bucket.name
 }
+output "production_hm_production_bucket_amazon_s3_bucket_arn" {
+  value = module.production_hm_production_bucket_amazon_s3_bucket.arn
+}
