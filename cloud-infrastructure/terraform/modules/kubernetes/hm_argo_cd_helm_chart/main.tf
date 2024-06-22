a1d4e7f78d7612b8cf84962fd3b9001899781f86
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-06-22 03:01:54 -0700

    feat(k8s): add caddy ingress controller (#17543)

--- cloud-infrastructure/terraform/modules/kubernetes/hm_argo_cd_helm_chart/main.tf
+++ cloud-infrastructure/terraform/modules/kubernetes/hm_argo_cd_helm_chart/main.tf
@@ -8,11 +8,12 @@ terraform {
 
 # https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release
 resource "helm_release" "hm_argo_cd_helm_chart" {
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  name       = var.name
-  version    = var.argo_cd_version
-  values     = [file(var.my_values_yaml_path)]
-  wait       = true
-  namespace  = var.namespace
+  repository   = "https://argoproj.github.io/argo-helm"
+  chart        = "argo-cd"
+  name         = var.name
+  version      = var.argo_cd_version
+  values       = [file(var.my_values_yaml_path)]
+  reset_values = true
+  wait         = true
+  namespace    = var.namespace
 }
