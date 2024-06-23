c33c61dab78132f9074afba4721dbd4a7efe3448
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-06-23 00:32:03 -0700

    chore(snowflake): create kafka read only role (#17547)

--- cloud-infrastructure/terraform/modules/snowflake/hm_snowflake_grant_role_to_role/main.tf
+++ cloud-infrastructure/terraform/modules/snowflake/hm_snowflake_grant_role_to_role/main.tf
@@ -9,5 +9,5 @@ terraform {
 # https://registry.terraform.io/providers/Snowflake-Labs/snowflake/latest/docs/resources/grant_account_role
 resource "snowflake_grant_account_role" "hm_snowflake_grant_role_to_role" {
   role_name        = var.snowflake_role_name
-  parent_role_name = var.snowflake_parent_role_name
+  parent_role_name = var.snowflake_grant_to_role_name
 }
