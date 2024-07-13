c175d842021aa8134cc1599e20bca1270759a10d
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-07-13 04:19:05 -0700

    feat(k8s): add redpanda console

---
+++ cloud-infrastructure/terraform/modules/aws/hm_redpanda_console_iam_role/variables.tf
@@ -0,0 +1,18 @@
+variable "redpanda_console_service_account_name" {
+  type = string
+}
+variable "redpanda_console_namespace" {
+  type = string
+}
+variable "amazon_eks_cluster_oidc_provider" {
+  type = string
+}
+variable "amazon_eks_cluster_oidc_provider_arn" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "team" {
+  type = string
+}
