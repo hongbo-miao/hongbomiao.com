d9eb427c10d92bb23719cd4bc852f607469e3f57
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-07-13 03:27:52 -0700

    refactor(terraform): separate kubernetes

---
+++ cloud-infrastructure/terraform/environments/production/aws/kubernetes/providers.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  alias  = "production"
+  region = "us-west-2"
+}
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
