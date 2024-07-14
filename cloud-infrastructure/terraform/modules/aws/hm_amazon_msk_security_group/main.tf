46573363170b83ccb9eb2fe354b36d339c7f997a
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-07-13 19:31:25 -0700

    chore(msk): allow setting ebs size (#18019)

---
+++ cloud-infrastructure/terraform/modules/aws/hm_amazon_msk_security_group/main.tf
@@ -0,0 +1,166 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "hm_amazon_msk_security_group" {
+  name   = var.amazon_ec2_security_group_name
+  vpc_id = var.amazon_vpc_id
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = var.amazon_ec2_security_group_name
+  }
+}
+# Ingress - On-Site
+locals {
+  ingress_rule_on_site_kafka_broker_tls        = "on_site_kafka_broker_tls"
+  ingress_rule_on_site_kafka_broker_sasl_scram = "on_site_kafka_broker_sasl_scram"
+  ingress_rule_on_site_kafka_broker_iam        = "on_site_kafka_broker_iam"
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_security_group_ingress_rule
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_on_site_kafka_broker_tls" {
+  security_group_id = aws_security_group.hm_amazon_msk_security_group.id
+  description       = local.ingress_rule_on_site_kafka_broker_tls
+  cidr_ipv4         = "10.10.0.0/15"
+  ip_protocol       = "tcp"
+  from_port         = 9094
+  to_port           = 9094
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = local.ingress_rule_on_site_kafka_broker_tls
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_on_site_kafka_broker_sasl_scram" {
+  security_group_id = aws_security_group.hm_amazon_msk_security_group.id
+  description       = local.ingress_rule_on_site_kafka_broker_sasl_scram
+  cidr_ipv4         = "10.10.0.0/15"
+  ip_protocol       = "tcp"
+  from_port         = 9096
+  to_port           = 9096
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = local.ingress_rule_on_site_kafka_broker_sasl_scram
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_on_site_kafka_broker_iam" {
+  security_group_id = aws_security_group.hm_amazon_msk_security_group.id
+  description       = local.ingress_rule_on_site_kafka_broker_iam
+  cidr_ipv4         = "10.10.0.0/15"
+  ip_protocol       = "tcp"
+  from_port         = 9098
+  to_port           = 9098
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = local.ingress_rule_on_site_kafka_broker_iam
+  }
+}
+# Ingress - VPN
+locals {
+  ingress_rule_vpn_kafka_broker_tls        = "vpn_kafka_broker_tls"
+  ingress_rule_vpn_kafka_broker_sasl_scram = "vpn_kafka_broker_sasl_scram"
+  ingress_rule_vpn_kafka_broker_iam        = "vpn_kafka_broker_iam"
+}
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_vpn_kafka_broker_tls" {
+  security_group_id = aws_security_group.hm_amazon_msk_security_group.id
+  description       = local.ingress_rule_vpn_kafka_broker_tls
+  cidr_ipv4         = "10.100.0.0/15"
+  ip_protocol       = "tcp"
+  from_port         = 9094
+  to_port           = 9094
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = local.ingress_rule_vpn_kafka_broker_tls
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_vpn_kafka_broker_sasl_scram" {
+  security_group_id = aws_security_group.hm_amazon_msk_security_group.id
+  description       = local.ingress_rule_vpn_kafka_broker_sasl_scram
+  cidr_ipv4         = "10.100.0.0/15"
+  ip_protocol       = "tcp"
+  from_port         = 9096
+  to_port           = 9096
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = local.ingress_rule_vpn_kafka_broker_sasl_scram
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_vpn_kafka_broker_iam" {
+  security_group_id = aws_security_group.hm_amazon_msk_security_group.id
+  description       = local.ingress_rule_vpn_kafka_broker_iam
+  cidr_ipv4         = "10.100.0.0/15"
+  ip_protocol       = "tcp"
+  from_port         = 9098
+  to_port           = 9098
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = local.ingress_rule_vpn_kafka_broker_iam
+  }
+}
+# Ingress - VPC
+locals {
+  ingress_rule_vpc_kafka_broker_tls        = "vpc_kafka_broker_tls"
+  ingress_rule_vpc_kafka_broker_sasl_scram = "vpc_kafka_broker_sasl_scram"
+  ingress_rule_vpc_kafka_broker_iam        = "vpc_kafka_broker_iam"
+}
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_vpc_kafka_broker_tls" {
+  security_group_id = aws_security_group.hm_amazon_msk_security_group.id
+  description       = local.ingress_rule_vpc_kafka_broker_tls
+  cidr_ipv4         = "172.16.0.0/12"
+  ip_protocol       = "tcp"
+  from_port         = 9094
+  to_port           = 9094
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = local.ingress_rule_vpc_kafka_broker_tls
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_vpc_kafka_broker_sasl_scram" {
+  security_group_id = aws_security_group.hm_amazon_msk_security_group.id
+  description       = local.ingress_rule_vpc_kafka_broker_sasl_scram
+  cidr_ipv4         = "172.16.0.0/12"
+  ip_protocol       = "tcp"
+  from_port         = 9096
+  to_port           = 9096
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = local.ingress_rule_vpc_kafka_broker_sasl_scram
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_vpc_kafka_broker_iam" {
+  security_group_id = aws_security_group.hm_amazon_msk_security_group.id
+  description       = local.ingress_rule_vpc_kafka_broker_iam
+  cidr_ipv4         = "172.16.0.0/12"
+  ip_protocol       = "tcp"
+  from_port         = 9098
+  to_port           = 9098
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = local.ingress_rule_vpc_kafka_broker_iam
+  }
+}
+# Egress
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_security_group_egress_rule
+resource "aws_vpc_security_group_egress_rule" "egress_allow" {
+  security_group_id = aws_security_group.hm_amazon_msk_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+  tags = {
+    Environment  = var.environment
+    Team         = var.team
+    ResourceName = var.amazon_ec2_security_group_name
+  }
+}
