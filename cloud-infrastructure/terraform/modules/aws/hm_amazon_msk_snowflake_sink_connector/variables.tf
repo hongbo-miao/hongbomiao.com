beb22e599df2b2a3d88e08f03cdb655940849b34
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-07-25 14:43:33 -0700

    refactor(msk): rename to snowflake sink connector (#18351)

---
+++ cloud-infrastructure/terraform/modules/aws/hm_amazon_msk_snowflake_sink_connector/variables.tf
@@ -0,0 +1,69 @@
+variable "amazon_msk_connector_name" {
+  type = string
+}
+variable "kafka_topic" {
+  type = string
+}
+variable "max_task_number" {
+  type = number
+}
+variable "max_worker_number" {
+  type = number
+}
+variable "worker_microcontroller_unit_number" {
+  type = number
+}
+variable "snowflake_user_name" {
+  type = string
+}
+variable "snowflake_private_key" {
+  type = string
+}
+variable "snowflake_private_key_passphrase" {
+  type = string
+}
+variable "snowflake_role_name" {
+  type = string
+}
+variable "snowflake_database_name" {
+  type = string
+}
+variable "snowflake_schema_name" {
+  type = string
+}
+variable "confluent_schema_registry_url" {
+  type = string
+}
+variable "kafka_connect_version" {
+  type = string
+}
+variable "amazon_msk_plugin_arn" {
+  type = string
+}
+variable "amazon_msk_plugin_revision" {
+  type = string
+}
+variable "amazon_msk_connector_iam_role_arn" {
+  type = string
+}
+variable "amazon_msk_cluster_bootstrap_servers" {
+  type = string
+}
+variable "amazon_vpc_security_group_id" {
+  type = string
+}
+variable "amazon_vpc_subnet_ids" {
+  type = list(string)
+}
+variable "msk_log_s3_bucket_name" {
+  type = string
+}
+variable "msk_log_s3_key" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "team" {
+  type = string
+}
