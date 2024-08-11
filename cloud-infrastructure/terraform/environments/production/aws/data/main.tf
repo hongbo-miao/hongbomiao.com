b458de0c9b6aa52efc524d9b045ae6d461069218
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-08-11 03:10:29 -0700

    feat(msk): add s3 parquet sink connector (#18674)

--- cloud-infrastructure/terraform/environments/production/aws/data/main.tf
+++ cloud-infrastructure/terraform/environments/production/aws/data/main.tf
@@ -6,6 +6,7 @@ data "terraform_remote_state" "production_aws_network_terraform_remote_state" {
     key    = "production/aws/network/terraform.tfstate"
   }
 }
+data "aws_region" "current" {}
 
 # Amazon S3 bucket - hm-production-bucket
 module "hm_production_bucket" {
@@ -26,7 +27,7 @@ module "kafka_kms_key" {
 
 # IoT data
 locals {
-  iot_data_name = "hm-${var.environment}-iot-data"
+  iot_data_name = "${var.environment}-iot-data"
 }
 # IoT data - S3 bucket
 module "s3_bucket_iot_data" {
@@ -38,7 +39,7 @@ module "s3_bucket_iot_data" {
 }
 # IoT Kafka
 locals {
-  iot_kafka_name                    = "hm-${var.environment}-iot-kafka"
+  iot_kafka_name                    = "${var.environment}-iot-kafka"
   iot_amazon_vpc_private_subnet_ids = slice(data.terraform_remote_state.production_aws_network_terraform_remote_state.outputs.hm_amazon_vpc_private_subnets_ids, 0, 3)
 }
 # IoT Kafka - S3 bucket
@@ -66,11 +67,11 @@ module "iot_kafka_cluster" {
   amazon_msk_cluster_name         = local.iot_kafka_name
   kafka_version                   = "3.7.x.kraft"
   kafka_broker_instance_type      = "kafka.m7g.large"
-  kafka_broker_number             = 3
+  kafka_broker_number             = 60
   kafka_broker_log_s3_bucket_name = module.s3_bucket_iot_kafka.name
   amazon_vpc_security_group_id    = module.iot_kafka_security_group.id
   amazon_vpc_subnet_ids           = local.iot_amazon_vpc_private_subnet_ids
-  amazon_ebs_volume_size_gb       = 32
+  amazon_ebs_volume_size_gb       = 10240
   aws_kms_key_arn                 = module.kafka_kms_key.arn
   is_scram_enabled                = true
   environment                     = var.environment
@@ -86,45 +87,46 @@ module "iot_kafka_sasl_scram_secret_association" {
   amazon_msk_cluster_arn         = module.iot_kafka_cluster.arn
   aws_secrets_manager_secret_arn = data.aws_secretsmanager_secret.iot_kafka_producer_secret.arn
 }
-# IoT Kafka - Kafka sink plugin
+# IoT Kafka - S3 JSON sink plugin
 locals {
-  iot_kafka_sink_plugin_name      = "${var.environment}-iot-kafka-sink-plugin"
-  iot_kafka_sink_plugin_file_name = "${local.iot_kafka_sink_plugin_name}.zip"
+  iot_kafka_s3_json_sink_plugin_name      = "${local.iot_kafka_name}-s3-json-sink-plugin"
+  iot_kafka_s3_json_sink_plugin_file_name = "${local.iot_kafka_s3_json_sink_plugin_name}.zip"
+  iot_kafka_s3_json_sink_plugin_dir_path  = "files/amazon-msk/${local.iot_kafka_name}/plugins/${local.iot_kafka_s3_json_sink_plugin_name}"
 }
-data "external" "local_iot_kafka_sink_plugin" {
-  program = ["bash", "files/amazon-msk/${var.environment}-iot-kafka/plugins/build.sh"]
+data "external" "local_iot_kafka_s3_json_sink_plugin" {
+  program = ["bash", "${local.iot_kafka_s3_json_sink_plugin_dir_path}/build.sh"]
   query = {
-    kafka_plugin_name                            = local.iot_kafka_sink_plugin_name
+    kafka_plugin_name                            = local.iot_kafka_s3_json_sink_plugin_name
     confluent_kafka_connect_s3_converter_version = "10.5.13" # https://www.confluent.io/hub/confluentinc/kafka-connect-s3
-    local_dir_path                               = "files/amazon-msk/${var.environment}-iot-kafka/plugins"
-    local_file_name                              = local.iot_kafka_sink_plugin_file_name
+    local_dir_path                               = local.iot_kafka_s3_json_sink_plugin_dir_path
+    local_file_name                              = local.iot_kafka_s3_json_sink_plugin_file_name
   }
 }
-module "s3_object_iot_kafka_sink_plugin" {
+module "s3_object_iot_kafka_s3_json_sink_plugin" {
   providers       = { aws = aws.production }
   source          = "../../../../modules/aws/hm_amazon_s3_object"
   s3_bucket_name  = module.s3_bucket_iot_kafka.name
-  s3_key          = "plugins/${local.iot_kafka_sink_plugin_file_name}"
-  local_file_path = data.external.local_iot_kafka_sink_plugin.result.local_file_path
+  s3_key          = "plugins/${local.iot_kafka_s3_json_sink_plugin_file_name}"
+  local_file_path = data.external.local_iot_kafka_s3_json_sink_plugin.result.local_file_path
 }
-module "iot_kafka_sink_plugin" {
+module "iot_kafka_s3_json_sink_plugin" {
   providers                = { aws = aws.production }
   source                   = "../../../../modules/aws/hm_amazon_msk_plugin"
-  amazon_msk_plugin_name   = local.iot_kafka_sink_plugin_name
+  amazon_msk_plugin_name   = local.iot_kafka_s3_json_sink_plugin_name
   s3_bucket_arn            = module.s3_bucket_iot_kafka.arn
-  amazon_msk_plugin_s3_key = module.s3_object_iot_kafka_sink_plugin.s3_key
+  amazon_msk_plugin_s3_key = module.s3_object_iot_kafka_s3_json_sink_plugin.s3_key
   depends_on = [
-    module.s3_object_iot_kafka_sink_plugin
+    module.s3_object_iot_kafka_s3_json_sink_plugin
   ]
 }
-# IoT Kafka - Kafka S3 sink connector
+# IoT Kafka - S3 JSON sink connector
 locals {
-  iot_kafka_s3_sink_connector_name = "ProductionIoTKafkaS3SinkConnector"
+  iot_kafka_s3_json_sink_connector_name = "ProductionIoTKafkaS3JSONSinkConnector"
 }
-module "iot_kafka_s3_sink_connector_iam_role" {
+module "iot_kafka_s3_json_sink_connector_iam_role" {
   providers                 = { aws = aws.production }
   source                    = "../../../../modules/aws/hm_amazon_msk_s3_sink_connector_iam_role"
-  amazon_msk_connector_name = local.iot_kafka_s3_sink_connector_name
+  amazon_msk_connector_name = local.iot_kafka_s3_json_sink_connector_name
   amazon_msk_arn            = module.iot_kafka_cluster.arn
   msk_plugin_s3_bucket_name = module.s3_bucket_iot_kafka.name
   msk_log_s3_bucket_name    = module.s3_bucket_iot_kafka.name
@@ -132,40 +134,109 @@ module "iot_kafka_s3_sink_connector_iam_role" {
   environment               = var.environment
   team                      = var.team
 }
-data "aws_secretsmanager_secret" "iot_snowflake_secret" {
-  name = "hm/snowflake/development_hm_kafka_db/product/read_write"
-}
-data "aws_secretsmanager_secret_version" "iot_snowflake_secret_version" {
-  secret_id = data.aws_secretsmanager_secret.iot_snowflake_secret.id
-}
-module "iot_kafka_s3_sink_connector" {
+module "iot_kafka_s3_json_sink_connector" {
   providers                            = { aws = aws.production }
-  source                               = "../../../../modules/aws/hm_amazon_msk_s3_sink_connector"
-  amazon_msk_connector_name            = local.iot_kafka_s3_sink_connector_name
-  kafka_topics                         = "production.iot.device.json"
+  source                               = "../../../../modules/aws/hm_amazon_msk_s3_json_sink_connector"
+  amazon_msk_connector_name            = local.iot_kafka_s3_json_sink_connector_name
+  kafka_topics                         = ["production.iot.device.json"]
+  aws_region                           = data.aws_region.current.name
   s3_bucket_name                       = module.s3_bucket_iot_data.name
   max_task_number                      = 3
   max_worker_number                    = 10
   worker_microcontroller_unit_number   = 1
   kafka_connect_version                = "2.7.1"
-  amazon_msk_plugin_arn                = module.iot_kafka_sink_plugin.arn
-  amazon_msk_plugin_revision           = module.iot_kafka_sink_plugin.latest_revision
-  amazon_msk_connector_iam_role_arn    = module.iot_kafka_s3_sink_connector_iam_role.arn
+  amazon_msk_plugin_arn                = module.iot_kafka_s3_json_sink_plugin.arn
+  amazon_msk_plugin_revision           = module.iot_kafka_s3_json_sink_plugin.latest_revision
+  amazon_msk_connector_iam_role_arn    = module.iot_kafka_s3_json_sink_connector_iam_role.arn
   amazon_msk_cluster_bootstrap_servers = module.iot_kafka_cluster.bootstrap_servers
   amazon_vpc_security_group_id         = module.iot_kafka_security_group.id
   amazon_vpc_subnet_ids                = local.iot_amazon_vpc_private_subnet_ids
   msk_log_s3_bucket_name               = module.s3_bucket_iot_kafka.name
-  msk_log_s3_key                       = "connectors/${local.iot_kafka_s3_sink_connector_name}"
+  msk_log_s3_key                       = "connectors/${local.iot_kafka_s3_json_sink_connector_name}"
   environment                          = var.environment
   team                                 = var.team
   depends_on = [
-    module.iot_kafka_sink_plugin
+    module.iot_kafka_s3_json_sink_plugin
+  ]
+}
+# IoT Kafka - S3 parquet sink plugin
+locals {
+  iot_kafka_s3_parquet_sink_plugin_name      = "${local.iot_kafka_name}-s3-parquet-sink-plugin"
+  iot_kafka_s3_parquet_sink_plugin_file_name = "${local.iot_kafka_s3_parquet_sink_plugin_name}.zip"
+  iot_kafka_s3_parquet_sink_plugin_dir_path  = "files/amazon-msk/${local.iot_kafka_name}/plugins/${local.iot_kafka_s3_parquet_sink_plugin_name}"
+}
+data "external" "local_iot_kafka_s3_parquet_sink_plugin" {
+  program = ["bash", "${local.iot_kafka_s3_parquet_sink_plugin_dir_path}/build.sh"]
+  query = {
+    kafka_plugin_name                              = local.iot_kafka_s3_parquet_sink_plugin_name
+    confluent_kafka_connect_s3_converter_version   = "10.5.13" # https://www.confluent.io/hub/confluentinc/kafka-connect-s3
+    confluent_kafka_connect_avro_converter_version = "7.6.1"   # https://www.confluent.io/hub/confluentinc/kafka-connect-avro-converter
+    local_dir_path                                 = local.iot_kafka_s3_parquet_sink_plugin_dir_path
+    local_file_name                                = local.iot_kafka_s3_parquet_sink_plugin_file_name
+  }
+}
+module "s3_object_iot_kafka_s3_parquet_sink_plugin" {
+  providers       = { aws = aws.production }
+  source          = "../../../../modules/aws/hm_amazon_s3_object"
+  s3_bucket_name  = module.s3_bucket_iot_kafka.name
+  s3_key          = "plugins/${local.iot_kafka_s3_parquet_sink_plugin_file_name}"
+  local_file_path = data.external.local_iot_kafka_s3_parquet_sink_plugin.result.local_file_path
+}
+module "iot_kafka_s3_parquet_sink_plugin" {
+  providers                = { aws = aws.production }
+  source                   = "../../../../modules/aws/hm_amazon_msk_plugin"
+  amazon_msk_plugin_name   = local.iot_kafka_s3_parquet_sink_plugin_name
+  s3_bucket_arn            = module.s3_bucket_iot_kafka.arn
+  amazon_msk_plugin_s3_key = module.s3_object_iot_kafka_s3_parquet_sink_plugin.s3_key
+  depends_on = [
+    module.s3_object_iot_kafka_s3_parquet_sink_plugin
+  ]
+}
+# IoT Kafka - S3 parquet sink connector
+locals {
+  iot_kafka_s3_parquet_sink_connector_name = "ProductionIoTKafkaS3ParquetSinkConnector"
+}
+module "iot_kafka_s3_parquet_sink_connector_iam_role" {
+  providers                 = { aws = aws.production }
+  source                    = "../../../../modules/aws/hm_amazon_msk_s3_sink_connector_iam_role"
+  amazon_msk_connector_name = local.iot_kafka_s3_parquet_sink_connector_name
+  amazon_msk_arn            = module.iot_kafka_cluster.arn
+  msk_plugin_s3_bucket_name = module.s3_bucket_iot_kafka.name
+  msk_log_s3_bucket_name    = module.s3_bucket_iot_kafka.name
+  msk_data_s3_bucket_name   = module.s3_bucket_iot_data.name
+  environment               = var.environment
+  team                      = var.team
+}
+module "iot_kafka_s3_parquet_sink_connector" {
+  providers                            = { aws = aws.production }
+  source                               = "../../../../modules/aws/hm_amazon_msk_s3_parquet_sink_connector"
+  amazon_msk_connector_name            = local.iot_kafka_s3_parquet_sink_connector_name
+  kafka_topics                         = ["production.iot.device.avro"]
+  confluent_schema_registry_url        = "https://confluent-schema-registry.internal.hongbomiao.com"
+  aws_region                           = data.aws_region.current.name
+  s3_bucket_name                       = module.s3_bucket_iot_data.name
+  max_task_number                      = 3
+  max_worker_number                    = 10
+  worker_microcontroller_unit_number   = 1
+  kafka_connect_version                = "2.7.1"
+  amazon_msk_plugin_arn                = module.iot_kafka_s3_parquet_sink_plugin.arn
+  amazon_msk_plugin_revision           = module.iot_kafka_s3_parquet_sink_plugin.latest_revision
+  amazon_msk_connector_iam_role_arn    = module.iot_kafka_s3_parquet_sink_connector_iam_role.arn
+  amazon_msk_cluster_bootstrap_servers = module.iot_kafka_cluster.bootstrap_servers
+  amazon_vpc_security_group_id         = module.iot_kafka_security_group.id
+  amazon_vpc_subnet_ids                = local.iot_amazon_vpc_private_subnet_ids
+  msk_log_s3_bucket_name               = module.s3_bucket_iot_kafka.name
+  msk_log_s3_key                       = "connectors/${local.iot_kafka_s3_parquet_sink_connector_name}"
+  environment                          = var.environment
+  team                                 = var.team
+  depends_on = [
+    module.iot_kafka_s3_parquet_sink_plugin
   ]
 }
 
 # Tracker Kafka
 locals {
-  tracker_kafka_name                    = "hm-${var.environment}-tracker-kakfa"
+  tracker_kafka_name                    = "${var.environment}-tracker-kakfa"
   tracker_amazon_vpc_private_subnet_ids = slice(data.terraform_remote_state.production_aws_network_terraform_remote_state.outputs.hm_amazon_vpc_private_subnets_ids, 0, 3)
 }
 # Tracker Kafka - S3 bucket
@@ -202,39 +273,41 @@ module "tracker_kafka_cluster" {
   environment                     = var.environment
   team                            = var.team
 }
-# Tracker Kafka - Kafka sink plugin
-data "external" "local_tracker_kafka_sink_plugin" {
+# Tracker Kafka - Snowflake sink plugin
+locals {
+  tracker_kafka_snowflake_sink_plugin_name      = "${local.tracker_kafka_name}-snowflake-sink-plugin"
+  tracker_kafka_snowflake_sink_plugin_file_name = "${local.tracker_kafka_snowflake_sink_plugin_name}.zip"
+  tracker_kafka_snowflake_sink_plugin_dir_path  = "files/amazon-msk/${local.tracker_kafka_name}/plugins/${local.tracker_kafka_snowflake_sink_plugin_name}"
+}
+data "external" "local_tracker_kafka_snowflake_sink_plugin" {
   provider = aws.production
-  program  = ["bash", "files/amazon-msk/${var.environment}-tracker-kafka/plugins/build.sh"]
+  program  = ["bash", "${local.tracker_kafka_snowflake_sink_plugin_dir_path}/build.sh"]
   query = {
-    kafka_plugin_name                              = local.tracker_kafka_sink_plugin_name
+    kafka_plugin_name = local.tracker_kafka_snowflake_sink_plugin_name
+    # snowflake-kafka-connector requires bc-fips and bcpkix-fips
     snowflake_kafka_connector_version              = "2.2.2"   # https://mvnrepository.com/artifact/com.snowflake/snowflake-kafka-connector
     bc_fips_version                                = "1.0.2.5" # https://mvnrepository.com/artifact/org.bouncycastle/bc-fips
     bcpkix_fips_version                            = "1.0.7"   # https://mvnrepository.com/artifact/org.bouncycastle/bcpkix-fips
     confluent_kafka_connect_avro_converter_version = "7.6.1"   # https://www.confluent.io/hub/confluentinc/kafka-connect-avro-converter
-    local_dir_path                                 = "files/amazon-msk/${var.environment}-tracker-kafka/plugins"
-    local_file_name                                = local.tracker_kafka_sink_plugin_file_name
+    local_dir_path                                 = local.tracker_kafka_snowflake_sink_plugin_dir_path
+    local_file_name                                = local.tracker_kafka_snowflake_sink_plugin_file_name
   }
 }
-locals {
-  tracker_kafka_sink_plugin_name      = "${var.environment}-tracker-sink-plugin"
-  tracker_kafka_sink_plugin_file_name = "${local.tracker_kafka_sink_plugin_name}.zip"
-}
-module "s3_object_tracker_kafka_sink_plugin" {
+module "s3_object_tracker_kafka_snowflake_sink_plugin" {
   providers       = { aws = aws.production }
   source          = "../../../../modules/aws/hm_amazon_s3_object"
   s3_bucket_name  = module.tracker_kafka_s3_bucket.name
-  s3_key          = "plugins/${local.tracker_kafka_sink_plugin_file_name}"
-  local_file_path = data.external.local_tracker_kafka_sink_plugin.result.local_file_path
+  s3_key          = "plugins/${local.tracker_kafka_snowflake_sink_plugin_file_name}"
+  local_file_path = data.external.local_tracker_kafka_snowflake_sink_plugin.result.local_file_path
 }
-module "hm_amazon_msk_plugin_tracker_kafka_sink_plugin" {
+module "hm_amazon_msk_plugin_tracker_kafka_snowflake_sink_plugin" {
   providers                = { aws = aws.production }
   source                   = "../../../../modules/aws/hm_amazon_msk_plugin"
-  amazon_msk_plugin_name   = local.tracker_kafka_sink_plugin_name
+  amazon_msk_plugin_name   = local.tracker_kafka_snowflake_sink_plugin_name
   s3_bucket_arn            = module.tracker_kafka_s3_bucket.arn
-  amazon_msk_plugin_s3_key = module.s3_object_tracker_kafka_sink_plugin.s3_key
+  amazon_msk_plugin_s3_key = module.s3_object_tracker_kafka_snowflake_sink_plugin.s3_key
   depends_on = [
-    module.s3_object_tracker_kafka_sink_plugin
+    module.s3_object_tracker_kafka_snowflake_sink_plugin
   ]
 }
 # Tracker Kafka - Kafka sink connector
@@ -259,7 +332,7 @@ data "aws_secretsmanager_secret_version" "tracker_snowflake_secret_version" {
   provider  = aws.production
   secret_id = data.aws_secretsmanager_secret.tracker_snowflake_secret.id
 }
-module "tracker_kafka_sink_connector" {
+module "tracker_kafka_snowflake_sink_connector" {
   providers                            = { aws = aws.production }
   source                               = "../../../../modules/aws/hm_amazon_msk_snowflake_sink_connector"
   amazon_msk_connector_name            = local.production_tracker_sink_connector_name
@@ -273,10 +346,10 @@ module "tracker_kafka_sink_connector" {
   snowflake_role_name                  = "HM_KAFKA_DB_PRODUCT_READ_WRITE_ROLE"
   snowflake_database_name              = "HM_KAFKA_DB"
   snowflake_schema_name                = "ENGINEERING"
-  confluent_schema_registry_url        = "https://production-confluent-schema-registry.hongbomiao.com"
+  confluent_schema_registry_url        = "https://confluent-schema-registry.internal.hongbomiao.com"
   kafka_connect_version                = "2.7.1"
-  amazon_msk_plugin_arn                = module.hm_amazon_msk_plugin_tracker_kafka_sink_plugin.arn
-  amazon_msk_plugin_revision           = module.hm_amazon_msk_plugin_tracker_kafka_sink_plugin.latest_revision
+  amazon_msk_plugin_arn                = module.hm_amazon_msk_plugin_tracker_kafka_snowflake_sink_plugin.arn
+  amazon_msk_plugin_revision           = module.hm_amazon_msk_plugin_tracker_kafka_snowflake_sink_plugin.latest_revision
   amazon_msk_connector_iam_role_arn    = module.tracker_kafka_snowflake_sink_connector_iam_role.arn
   amazon_msk_cluster_bootstrap_servers = module.tracker_kafka_cluster.bootstrap_servers
   amazon_vpc_security_group_id         = module.tracker_kafka_security_group.id
@@ -286,6 +359,6 @@ module "tracker_kafka_sink_connector" {
   environment                          = var.environment
   team                                 = var.team
   depends_on = [
-    module.hm_amazon_msk_plugin_tracker_kafka_sink_plugin
+    module.hm_amazon_msk_plugin_tracker_kafka_snowflake_sink_plugin
   ]
 }
---
+++ cloud-infrastructure/terraform/modules/aws/hm_amazon_msk_s3_json_sink_connector/main.tf
@@ -0,0 +1,94 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/mskconnect_connector
+resource "aws_mskconnect_connector" "hm_amazon_msk_s3_json_sink_connector" {
+  name = var.amazon_msk_connector_name
+  # https://docs.confluent.io/kafka-connectors/s3-sink/current/configuration_options.html
+  connector_configuration = {
+    "connector.class"   = "io.confluent.connect.s3.S3SinkConnector"
+    "topics"            = join(",", var.kafka_topics)
+    "tasks.max"         = var.max_task_number
+    "errors.log.enable" = true
+    "errors.tolerance"  = "all"
+    "jmx"               = true
+
+    # Connector
+    "flush.size"                  = "1000000"
+    "rotate.schedule.interval.ms" = "3600000" # every hour
+
+    # Schema
+    "schema.compatibility" = "NONE"
+
+    # S3
+    "s3.region"      = var.aws_region
+    "s3.bucket.name" = var.s3_bucket_name
+    "s3.part.size"   = "104857600" # 100 MiB
+
+    # Storage
+    "storage.class" = "io.confluent.connect.s3.storage.S3Storage"
+
+    # Partition
+    "partitioner.class"     = "io.confluent.connect.storage.partitioner.TimeBasedPartitioner"
+    "path.format"           = "'year'=YYYY/'month'=MM/'day'=dd/'hour'=HH"
+    "partition.duration.ms" = 3600000 # 1 hour
+    "locale"                = "en"
+    "timezone"              = "UTC"
+
+    # JSON
+    "format.class"         = "io.confluent.connect.s3.format.json.JsonFormat"
+    "s3.compression.type"  = "gzip"
+    "s3.compression.level" = 9
+  }
+  kafkaconnect_version = var.kafka_connect_version
+  capacity {
+    # https://docs.aws.amazon.com/MSKC/latest/mskc/API_AutoScaling.html
+    autoscaling {
+      min_worker_count = 1
+      max_worker_count = var.max_worker_number
+      mcu_count        = var.worker_microcontroller_unit_number
+      scale_in_policy {
+        cpu_utilization_percentage = 20
+      }
+      scale_out_policy {
+        cpu_utilization_percentage = 80
+      }
+    }
+  }
+  kafka_cluster {
+    apache_kafka_cluster {
+      bootstrap_servers = var.amazon_msk_cluster_bootstrap_servers
+      vpc {
+        security_groups = [var.amazon_vpc_security_group_id]
+        subnets         = var.amazon_vpc_subnet_ids
+      }
+    }
+  }
+  kafka_cluster_client_authentication {
+    authentication_type = "IAM"
+  }
+  kafka_cluster_encryption_in_transit {
+    encryption_type = "TLS"
+  }
+  plugin {
+    custom_plugin {
+      arn      = var.amazon_msk_plugin_arn
+      revision = var.amazon_msk_plugin_revision
+    }
+  }
+  log_delivery {
+    worker_log_delivery {
+      s3 {
+        bucket  = var.msk_log_s3_bucket_name
+        prefix  = var.msk_log_s3_key
+        enabled = true
+      }
+    }
+  }
+  service_execution_role_arn = var.amazon_msk_connector_iam_role_arn
+}
