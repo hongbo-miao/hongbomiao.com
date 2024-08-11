4d3ea1c38f417fa1749059f390568e47db2627dc
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-08-11 02:58:56 -0700

    feat(msk): add s3 parquet sink connector

---
+++ cloud-infrastructure/terraform/modules/aws/hm_amazon_msk_s3_json_sink_connector/variables.tf
@@ -0,0 +1,54 @@
+variable "amazon_msk_connector_name" {
+  type = string
+}
+variable "kafka_topics" {
+  type = list(string)
+}
+variable "aws_region" {
+  type = string
+}
+variable "s3_bucket_name" {
+  type = string
+}
+variable "max_task_number" {
+  type = number
+}
+variable "max_worker_number" {
+  type = number
+}
+variable "worker_microcontroller_unit_number" {
+  type = number
+}
+variable "kafka_connect_version" {
+  type = string
+}
+variable "amazon_msk_plugin_arn" {
+  type = string
+}
+variable "amazon_msk_plugin_revision" {
+  type = string
+}
+variable "amazon_msk_connector_iam_role_arn" {
+  type = string
+}
+variable "amazon_msk_cluster_bootstrap_servers" {
+  type = string
+}
+variable "amazon_vpc_security_group_id" {
+  type = string
+}
+variable "amazon_vpc_subnet_ids" {
+  type = list(string)
+}
+variable "msk_log_s3_bucket_name" {
+  type = string
+}
+variable "msk_log_s3_key" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "team" {
+  type = string
+}
