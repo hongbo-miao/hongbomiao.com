cdccaf30440ee547615f53b71ad9900bd704912a
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-08-11 20:11:25 -0700

    refactor(kafka-c): clean code (#18681)

--- hm-kafka/kafka-client/kafka-c/json-consumer/main.c
+++ hm-kafka/kafka-client/kafka-c/json-consumer/main.c
@@ -3,25 +3,23 @@
 
 #include "config.c"
 
-static volatile sig_atomic_t run = 1;
+static volatile sig_atomic_t is_running = 1;
 
 // Signal termination of program
-static void stop(int sig) { run = 0; }
+static void stop(int sig) { is_running = 0; }
 
 int main(int argc, char **argv) {
+  const char *topic = "production.iot.device.json";
+
   rd_kafka_t *consumer;
   rd_kafka_conf_t *conf;
-  rd_kafka_resp_err_t err;
-  char errstr[512];
+  rd_kafka_resp_err_t rd_kafka_resp_err;
+  char err_str[4096];
 
-  // Parse the command line.
   if (argc != 2) {
     g_error("Usage: %s <config.ini>", argv[0]);
-    return 1;
   }
 
-  // Parse the configuration
-  // https://github.com/confluentinc/librdkafka/blob/master/CONFIGURATION.md
   const char *config_file = argv[1];
 
   g_autoptr(GError) error = NULL;
@@ -29,48 +27,35 @@ int main(int argc, char **argv) {
   if (!g_key_file_load_from_file(key_file, config_file, G_KEY_FILE_NONE,
                                  &error)) {
     g_error("Error loading config file: %s", error->message);
-    return 1;
   }
 
-  // Load the relevant configuration sections
   conf = rd_kafka_conf_new();
   load_config_group(conf, key_file, "default");
-  load_config_group(conf, key_file, "consumer");
 
-  // Create the consumer instance
-  consumer = rd_kafka_new(RD_KAFKA_CONSUMER, conf, errstr, sizeof(errstr));
+  consumer = rd_kafka_new(RD_KAFKA_CONSUMER, conf, err_str, sizeof(err_str));
   if (!consumer) {
-    g_error("Failed to create new consumer: %s", errstr);
-    return 1;
+    g_error("Failed to create new consumer: %s", err_str);
   }
+  conf = NULL;
   rd_kafka_poll_set_consumer(consumer);
 
-  // Configuration object is now owned, and freed, by the rd_kafka_t instance
-  conf = NULL;
-
   // Convert the list of topics to a format suitable for librdkafka
-  const char *topic = "production.iot.device.json";
   rd_kafka_topic_partition_list_t *subscription =
       rd_kafka_topic_partition_list_new(1);
   rd_kafka_topic_partition_list_add(subscription, topic, RD_KAFKA_PARTITION_UA);
 
   // Subscribe to the list of topics
-  err = rd_kafka_subscribe(consumer, subscription);
-  if (err) {
+  rd_kafka_resp_err = rd_kafka_subscribe(consumer, subscription);
+  if (rd_kafka_resp_err) {
     g_error("Failed to subscribe to %d topics: %s", subscription->cnt,
-            rd_kafka_err2str(err));
-    rd_kafka_topic_partition_list_destroy(subscription);
-    rd_kafka_destroy(consumer);
-    return 1;
+            rd_kafka_err2str(rd_kafka_resp_err));
   }
 
   rd_kafka_topic_partition_list_destroy(subscription);
 
-  // Install a signal handler for clean shutdown
   signal(SIGINT, stop);
 
-  // Start polling for messages
-  while (run) {
+  while (is_running) {
     rd_kafka_message_t *consumer_message;
 
     consumer_message = rd_kafka_consumer_poll(consumer, 500);
@@ -79,14 +64,15 @@ int main(int argc, char **argv) {
       continue;
     }
 
-    if (consumer_message->err) {
-      if (consumer_message->err == RD_KAFKA_RESP_ERR__PARTITION_EOF) {
+    if (consumer_message->rd_kafka_resp_err) {
+      if (consumer_message->rd_kafka_resp_err ==
+          RD_KAFKA_RESP_ERR__PARTITION_EOF) {
         // We can ignore this error - it just means we've read everything and
-        // are waiting for more data
+        // are waiting for more data.
       } else {
         g_message("Consumer error: %s",
                   rd_kafka_message_errstr(consumer_message));
-        return 1;
+        exit(EXIT_FAILURE);
       }
     } else {
       g_message("Consumed event from topic %s: key = %.*s value = %s",
@@ -95,16 +81,11 @@ int main(int argc, char **argv) {
                 (char *)consumer_message->payload);
     }
 
-    // Free the message when we're done
     rd_kafka_message_destroy(consumer_message);
   }
 
-  // Close the consumer: commit final offsets and leave the group
   g_message("Closing consumer");
   rd_kafka_consumer_close(consumer);
-
-  // Destroy the consumer
   rd_kafka_destroy(consumer);
-
-  return 0;
+  exit(EXIT_SUCCESS);
 }
--- hm-kafka/kafka-client/kafka-c/json-producer/main.c
+++ hm-kafka/kafka-client/kafka-c/json-producer/main.c
@@ -5,95 +5,55 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#include "app/utils/app.c"
 #include "config.c"
+#include "kafka/utils/kafka.c"
 
-#define ARR_SIZE(arr) (sizeof((arr)) / sizeof((arr[0])))
-
-// Global variable to manage graceful shutdown
-static volatile bool is_running = true;
-
-static void delivery_report(rd_kafka_t *kafka_handle,
-                            const rd_kafka_message_t *rkmessage, void *opaque) {
-  if (rkmessage->err) {
-    g_error("Message delivery failed: %s", rd_kafka_err2str(rkmessage->err));
-  }
-}
-
-void signal_handler(int signal) {
-  if (signal == SIGINT || signal == SIGTERM) {
-    is_running = false;
-  }
-}
+volatile sig_atomic_t is_running = true;
 
 int main(int argc, char **argv) {
+  const char *topic = "production.iot.device.json";
+
   rd_kafka_t *producer;
   rd_kafka_conf_t *conf;
-  char errstr[512];
+  char err_str[4096];
 
-  // Parse the command line
   if (argc != 2) {
     g_error("Usage: %s <config.ini>", argv[0]);
-    return 1;
   }
 
-  // Parse the configuration
-  // https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
   const char *config_file = argv[1];
-
-  g_autoptr(GError) error = NULL;
+  g_autoptr(GError) err = NULL;
   g_autoptr(GKeyFile) key_file = g_key_file_new();
   if (!g_key_file_load_from_file(key_file, config_file, G_KEY_FILE_NONE,
-                                 &error)) {
-    g_error("Error loading config file: %s", error->message);
-    return 1;
+                                 &err)) {
+    g_error("Error loading config file: %s", err->message);
   }
 
-  // Load the relevant configuration sections
   conf = rd_kafka_conf_new();
   load_config_group(conf, key_file, "default");
-
-  // Increase the internal queue buffer size
-  rd_kafka_conf_set(conf, "queue.buffering.max.messages", "10000000", NULL, 0);
-
-  rd_kafka_conf_set(conf, "queue.buffering.max.kbytes",
-                    "10485760",  // 10 GiB
-                    NULL, 0);
-
-  // Adjust batch size and linger time
-  rd_kafka_conf_set(conf, "batch.size", "65536", NULL, 0);  // 64 KiB
-  rd_kafka_conf_set(conf, "linger.ms", "5", NULL, 0);       // 5 ms
-
-  // Install a delivery-error callback
   rd_kafka_conf_set_dr_msg_cb(conf, delivery_report);
-
-  // Create the Producer instance
-  producer = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
+  producer = rd_kafka_new(RD_KAFKA_PRODUCER, conf, err_str, sizeof(err_str));
   if (!producer) {
-    g_error("Failed to create new producer: %s", errstr);
-    return 1;
+    g_error("Failed to create new producer: %s", err_str);
   }
-
-  // Configuration object is now owned, and freed, by the rd_kafka_t instance
   conf = NULL;
 
-  // Set up signal handling for graceful shutdown
   signal(SIGINT, signal_handler);
   signal(SIGTERM, signal_handler);
 
-  // Produce data continuously
-  const char *topic = "production.iot.device.json";
-  const char *device_ids[6] = {"device1", "device2", "device3",
-                               "device4", "device5", "device6"};
+  const char *device_ids[6] = {"device1", "device2", "device3", "device4",
+                               "device5"};
   const char *status_list[3] = {"online", "offline", "maintenance"};
   const char *locations[3] = {"locationA", "locationB", "locationC"};
   const char *types[3] = {"type1", "type2", "type3"};
 
   while (is_running) {
-    const char *key = device_ids[random() % ARR_SIZE(device_ids)];
+    const char *kafka_key = device_ids[random() % G_N_ELEMENTS(device_ids)];
 
-    const char *status = status_list[random() % ARR_SIZE(status_list)];
-    const char *location = locations[random() % ARR_SIZE(locations)];
-    const char *type = types[random() % ARR_SIZE(types)];
+    const char *status = status_list[random() % G_N_ELEMENTS(status_list)];
+    const char *location = locations[random() % G_N_ELEMENTS(locations)];
+    const char *type = types[random() % G_N_ELEMENTS(types)];
     double temperature =
         ((double)random() / RAND_MAX) * 100.0 - 50.0;  // [-50.0, 50.0]
     double humidity = (random() % 101) / 100.0;        // [0.0, 1.0]
@@ -106,7 +66,6 @@ int main(int argc, char **argv) {
     cJSON *json_obj = cJSON_CreateObject();
     if (!json_obj) {
       g_error("Failed to create JSON object");
-      break;
     }
     cJSON_AddStringToObject(json_obj, "status", status);
     cJSON_AddStringToObject(json_obj, "location", location);
@@ -121,37 +80,30 @@ int main(int argc, char **argv) {
     // Convert JSON object to string
     const char *json_str = cJSON_PrintUnformatted(json_obj);
     if (!json_str) {
-      g_error("Failed to print JSON object");
-      cJSON_Delete(json_obj);
-      break;
+      g_error("Failed to print JSON object\n");
     }
 
-    size_t key_len = strlen(key);
-    size_t value_len = strlen(json_str);
+    size_t key_len = strlen(kafka_key);
+    rd_kafka_resp_err_t rd_kafka_resp_err;
+    rd_kafka_resp_err =
+        rd_kafka_producev(producer, RD_KAFKA_V_TOPIC(topic),
+                          RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
+                          RD_KAFKA_V_KEY((void *)kafka_key, key_len),
+                          RD_KAFKA_V_VALUE((void *)json_str, strlen(json_str)),
+                          RD_KAFKA_V_OPAQUE(NULL), RD_KAFKA_V_END);
 
-    rd_kafka_resp_err_t err;
-
-    err = rd_kafka_producev(producer, RD_KAFKA_V_TOPIC(topic),
-                            RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
-                            RD_KAFKA_V_KEY((void *)key, key_len),
-                            RD_KAFKA_V_VALUE((void *)json_str, value_len),
-                            RD_KAFKA_V_OPAQUE(NULL), RD_KAFKA_V_END);
-
-    if (err) {
+    if (rd_kafka_resp_err) {
       g_error("Failed to produce to topic %s: %s", topic,
-              rd_kafka_err2str(err));
-      return 1;
+              rd_kafka_err2str(rd_kafka_resp_err));
     }
 
-    // Free the JSON object
     cJSON_Delete(json_obj);
-    free((void *)json_str);  // cJSON_PrintUnformatted allocates memory
+    free((void *)json_str);
 
     rd_kafka_poll(producer, 0);
     g_usleep(5);  // μs
   }
 
-  // Block until the messages are all sent
   g_message("Flushing final messages ...");
   rd_kafka_flush(producer, 10 * 1000);
 
@@ -161,5 +113,5 @@ int main(int argc, char **argv) {
 
   g_message("Producer stopped.");
   rd_kafka_destroy(producer);
-  return 0;
+  exit(EXIT_SUCCESS);
 }
