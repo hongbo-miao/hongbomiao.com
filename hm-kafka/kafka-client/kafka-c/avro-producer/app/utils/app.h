4050468ef38eb30523763ba519ff8b81c30305d2
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-08-11 20:11:10 -0700

    chore(kafka-c): support optional field (#18680)

---
+++ hm-kafka/kafka-client/kafka-c/avro-producer/app/utils/app.h
@@ -0,0 +1,6 @@
+#ifndef APP_H
+#define APP_H
+
+extern volatile bool is_running;
+
+#endif
--- hm-kafka/kafka-client/kafka-c/avro-producer/config.c
+++ hm-kafka/kafka-client/kafka-c/avro-producer/config.c
@@ -1,32 +1,26 @@
 #include <glib.h>
 
 static void load_config_group(rd_kafka_conf_t *conf, GKeyFile *key_file,
-                              const char *group) {
-  char errstr[512];
-  g_autoptr(GError) error = NULL;
+                              const char *group_name) {
+  char err_str[4096];
+  g_autoptr(GError) err = NULL;
 
-  gchar **ptr = g_key_file_get_keys(key_file, group, NULL, &error);
-  if (error) {
-    g_error("%s", error->message);
-    exit(1);
+  gchar **ptr = g_key_file_get_keys(key_file, group_name, NULL, &err);
+  if (err) {
+    g_error("%s", err->message);
   }
 
   while (*ptr) {
     const char *key = *ptr;
     g_autofree gchar *value =
-        g_key_file_get_string(key_file, group, key, &error);
-
-    if (error) {
-      g_error("Reading key: %s", error->message);
-      exit(1);
+        g_key_file_get_string(key_file, group_name, key, &err);
+    if (err) {
+      g_error("Reading key: %s", err->message);
     }
-
-    if (rd_kafka_conf_set(conf, key, value, errstr, sizeof(errstr)) !=
+    if (rd_kafka_conf_set(conf, key, value, err_str, sizeof(err_str)) !=
         RD_KAFKA_CONF_OK) {
-      g_error("%s", errstr);
-      exit(1);
+      g_error("%s", err_str);
     }
-
     ptr++;
   }
 }
