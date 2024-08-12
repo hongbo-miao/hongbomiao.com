e30797e150232f91de84696fa74332d5a44a0531
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-08-11 20:11:10 -0700

    chore(kafka-c): support optional field (#18680)

---
+++ hm-kafka/kafka-client/kafka-c/avro-producer/kafka/utils/schema.c
@@ -0,0 +1,30 @@
+static void print_serdes_schema(serdes_schema_t *serdes_schema) {
+  if (!serdes_schema) {
+    g_print("serdes_schema is NULL.\n");
+    return;
+  }
+
+  const char *schema_name = serdes_schema_name(serdes_schema);
+  if (schema_name) {
+    g_print("schema_name: %s\n", schema_name);
+  } else {
+    g_print("Failed to retrieve schema_name.\n");
+  }
+
+  int schema_id = serdes_schema_id(serdes_schema);
+  g_print("schema_id: %d\n", schema_id);
+
+  const char *schema_definition = serdes_schema_definition(serdes_schema);
+  if (schema_definition) {
+    g_print("serdes_schema: %s\n", schema_definition);
+  } else {
+    g_print("Failed to retrieve serdes_schema.\n");
+  }
+}
+
+static void print_avro_schema(avro_schema_t schema) {
+  char schema_str[8192];
+  avro_writer_t writer = avro_writer_memory(schema_str, 8192);
+  avro_schema_to_json(schema, writer);
+  g_print("schema: %s\n", schema_str);
+}
--- hm-kafka/kafka-client/kafka-c/avro-producer/main.c
+++ hm-kafka/kafka-client/kafka-c/avro-producer/main.c
@@ -9,167 +9,152 @@
 #include <string.h>
 #include <time.h>
 
+#include "app/utils/app.c"
 #include "config.c"
+#include "kafka/utils/kafka.c"
+#include "kafka/utils/record.c"
+#include "kafka/utils/schema.c"
 
-#define ARR_SIZE(arr) (sizeof((arr)) / sizeof((arr[0])))
-
-static volatile bool is_running = true;
-
-static void delivery_report(rd_kafka_t *kafka_handle,
-                            const rd_kafka_message_t *rkmessage, void *opaque) {
-  if (rkmessage->err) {
-    g_error("Message delivery failed: %s", rd_kafka_err2str(rkmessage->err));
-  }
-}
-
-void signal_handler(int signal) {
-  if (signal == SIGINT || signal == SIGTERM) {
-    is_running = false;
-  }
-}
+volatile bool is_running = true;
 
 int main(int argc, char **argv) {
+  const char *confluent_schema_registry_url =
+      "https://confluent-schema-registry.internal.hongbomiao.com";
+  const char *topic = "production.iot.device.json";
+  const char *schema_name = "production.iot.device.json-value";
+
   rd_kafka_t *producer;
   rd_kafka_conf_t *conf;
   serdes_conf_t *serdes_conf;
   serdes_t *serdes;
-  char errstr[512];
+  char err_str[4096];
 
   if (argc != 2) {
     g_error("Usage: %s <config.ini>", argv[0]);
-    return 1;
   }
 
   const char *config_file = argv[1];
 
-  g_autoptr(GError) error = NULL;
+  g_autoptr(GError) err = NULL;
   g_autoptr(GKeyFile) key_file = g_key_file_new();
   if (!g_key_file_load_from_file(key_file, config_file, G_KEY_FILE_NONE,
-                                 &error)) {
-    g_error("Error loading config file: %s", error->message);
-    return 1;
+                                 &err)) {
+    g_error("Failed to load config file: %s", err->message);
   }
 
   conf = rd_kafka_conf_new();
   load_config_group(conf, key_file, "default");
-
-  rd_kafka_conf_set(conf, "queue.buffering.max.messages", "10000000", NULL, 0);
-  rd_kafka_conf_set(conf, "queue.buffering.max.kbytes", "10485760", NULL, 0);
-  rd_kafka_conf_set(conf, "batch.size", "65536", NULL, 0);
-  rd_kafka_conf_set(conf, "linger.ms", "5", NULL, 0);
   rd_kafka_conf_set_dr_msg_cb(conf, delivery_report);
-
-  producer = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
+  producer = rd_kafka_new(RD_KAFKA_PRODUCER, conf, err_str, sizeof(err_str));
   if (!producer) {
-    g_error("Failed to create new producer: %s", errstr);
-    return 1;
+    g_error("Failed to create new producer: %s", err_str);
   }
+  conf = NULL;
 
   signal(SIGINT, signal_handler);
   signal(SIGTERM, signal_handler);
 
-  serdes_conf = serdes_conf_new(
-      NULL, 0, "schema.registry.url",
-      "https://confluent-schema-registry.internal.hongbomiao.com", NULL);
+  serdes_conf = serdes_conf_new(NULL, 0, "schema.registry.url",
+                                confluent_schema_registry_url, NULL);
 
-  serdes = serdes_new(serdes_conf, errstr, sizeof(errstr));
+  serdes = serdes_new(serdes_conf, err_str, sizeof(err_str));
   if (!serdes) {
-    g_error("Failed to create serdes instance: %s", errstr);
-    return 1;
+    g_error("Failed to create serdes instance: %s", err_str);
   }
 
-  const char *topic = "production.iot.device.json";
-  const char *schema_name = "production.iot.device.json-value";
   serdes_schema_t *serdes_schema =
-      serdes_schema_get(serdes, schema_name, -1, errstr, sizeof(errstr));
+      serdes_schema_get(serdes, schema_name, -1, err_str, sizeof(err_str));
   if (!serdes_schema) {
-    g_error("Failed to retrieve AVRO schema: %s", errstr);
-    return 1;
+    g_error("Failed to retrieve serdes_schema: %s", err_str);
   }
 
-  const char *device_ids[6] = {"device1", "device2", "device3",
-                               "device4", "device5", "device6"};
+  print_serdes_schema(serdes_schema);
+  avro_schema_t avro_schema = serdes_schema_avro(serdes_schema);
+  // print_avro_schema(avro_schema);
+
+  const char *device_ids[6] = {"device1", "device2", "device3", "device4",
+                               "device5"};
   const char *status_list[3] = {"online", "offline", "maintenance"};
   const char *locations[3] = {"locationA", "locationB", "locationC"};
   const char *types[3] = {"type1", "type2", "type3"};
 
-  srandom(time(NULL));  // Seed the random number generator
+  srandom(time(NULL));
 
   while (is_running) {
-    const char *key = device_ids[random() % ARR_SIZE(device_ids)];
+    const char *record_key = device_ids[random() % G_N_ELEMENTS(device_ids)];
 
-    const char *status = status_list[random() % ARR_SIZE(status_list)];
-    const char *location = locations[random() % ARR_SIZE(locations)];
-    const char *type = types[random() % ARR_SIZE(types)];
-    double temperature = ((double)random() / RAND_MAX) * 100.0 - 50.0;
-    double humidity = ((double)random() / RAND_MAX);
-    int battery = random() % 101;
-    int signal_strength = random() % 101;
+    const char *status = status_list[random() % G_N_ELEMENTS(status_list)];
+    const char *location = locations[random() % G_N_ELEMENTS(locations)];
+    const char *type = types[random() % G_N_ELEMENTS(types)];
+    double temperature =
+        ((double)random() / RAND_MAX) * 100.0 - 50.0;  // [-50.0, 50.0]
+    double humidity = (random() % 101) / 100.0;        // [0.0, 1.0]
+    int battery = random() % 101;                      // [0, 100]
+    int signal_strength = random() % 101;              // [0, 100]
     const char *mode = (random() % 2) ? "manual" : "auto";
-    bool active = (random() % 2);
+    bool active = (random() % 2) ? true : false;
 
-    avro_schema_t schema = serdes_schema_avro(serdes_schema);
-    avro_value_iface_t *record_class = avro_generic_class_from_schema(schema);
+    avro_value_iface_t *record_class =
+        avro_generic_class_from_schema(avro_schema);
 
     avro_value_t record;
     avro_generic_value_new(record_class, &record);
 
-    avro_value_t field;
-    if (avro_value_get_by_name(&record, "status", &field, NULL) == 0) {
-      avro_value_set_string(&field, status);
+    if (set_string_field(&record, "status", status) != 0) {
+      g_error("Failed to set status\n");
     }
-    if (avro_value_get_by_name(&record, "location", &field, NULL) == 0) {
-      avro_value_set_string(&field, location);
+    if (set_string_field(&record, "location", location) != 0) {
+      g_error("Failed to set location\n");
     }
-    if (avro_value_get_by_name(&record, "type", &field, NULL) == 0) {
-      avro_value_set_string(&field, type);
+    if (set_string_field(&record, "type", type) != 0) {
+      g_error("Failed to set type\n");
     }
-    if (avro_value_get_by_name(&record, "temperature", &field, NULL) == 0) {
-      avro_value_set_long(&field, temperature);
+    if (set_long_field(&record, "temperature", temperature) != 0) {
+      g_error("Failed to set temperature\n");
     }
-    if (avro_value_get_by_name(&record, "humidity", &field, NULL) == 0) {
-      avro_value_set_double(&field, humidity);
+    if (set_double_field(&record, "humidity", humidity) != 0) {
+      g_error("Failed to set humidity\n");
     }
-    if (avro_value_get_by_name(&record, "battery", &field, NULL) == 0) {
-      avro_value_set_long(&field, battery);
+    if (set_long_field(&record, "battery", battery) != 0) {
+      g_error("Failed to set battery\n");
     }
-    if (avro_value_get_by_name(&record, "signal_strength", &field, NULL) == 0) {
-      avro_value_set_long(&field, signal_strength);
+    if (set_long_field(&record, "signal_strength", signal_strength) != 0) {
+      g_error("Failed to set signal_strength\n");
     }
-    if (avro_value_get_by_name(&record, "mode", &field, NULL) == 0) {
-      avro_value_set_string(&field, mode);
+    if (set_string_field(&record, "mode", mode) != 0) {
+      g_error("Failed to set mode\n");
     }
-    if (avro_value_get_by_name(&record, "active", &field, NULL) == 0) {
-      avro_value_set_boolean(&field, active);
+    if (set_boolean_field(&record, "active", active) != 0) {
+      g_error("Failed to set active\n");
     }
+    // print_record(&record);
 
     void *avro_payload = NULL;
     size_t avro_size;
-    serdes_err_t serr =
+    serdes_err_t serdes_err =
         serdes_schema_serialize_avro(serdes_schema, &record, &avro_payload,
-                                     &avro_size, errstr, sizeof(errstr));
-    if (serr != SERDES_ERR_OK) {
-      g_error("Failed to serialize data: %s", serdes_err2str(serr));
-      return 1;
+                                     &avro_size, err_str, sizeof(err_str));
+    if (serdes_err != SERDES_ERR_OK) {
+      g_error("Failed to serialize data: %s", serdes_err2str(serdes_err));
     }
 
-    rd_kafka_resp_err_t err;
-    err = rd_kafka_producev(producer, RD_KAFKA_V_TOPIC(topic),
-                            RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
-                            RD_KAFKA_V_KEY((void *)key, strlen(key)),
-                            RD_KAFKA_V_VALUE(avro_payload, avro_size),
-                            RD_KAFKA_V_OPAQUE(NULL), RD_KAFKA_V_END);
+    rd_kafka_resp_err_t rd_kafka_resp_err;
+    rd_kafka_resp_err = rd_kafka_producev(
+        producer, RD_KAFKA_V_TOPIC(topic),
+        RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
+        RD_KAFKA_V_KEY((void *)record_key, strlen(record_key)),
+        RD_KAFKA_V_VALUE(avro_payload, avro_size), RD_KAFKA_V_OPAQUE(NULL),
+        RD_KAFKA_V_END);
 
-    if (err) {
+    if (rd_kafka_resp_err) {
       g_error("Failed to produce to topic %s: %s", topic,
-              rd_kafka_err2str(err));
-      return 1;
+              rd_kafka_err2str(rd_kafka_resp_err));
     }
 
     free(avro_payload);
     avro_value_decref(&record);
     rd_kafka_poll(producer, 0);
-    g_usleep(5);  // μs
+    g_usleep(5000);  // μs
   }
 
   g_message("Flushing final messages ...");
@@ -183,5 +168,5 @@ int main(int argc, char **argv) {
   rd_kafka_destroy(producer);
   serdes_schema_destroy(serdes_schema);
   serdes_destroy(serdes);
-  return 0;
+  exit(EXIT_SUCCESS);
 }
