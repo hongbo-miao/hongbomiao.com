a6038637b3f955a699c911be1771c0c9a111373a
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-08-12 22:32:18 -0700

    chore(clang-format): update to new version (#18695)

--- parallel-computing/cuda/main.cu
+++ parallel-computing/cuda/main.cu
@@ -23,8 +23,7 @@ int main() {
     return 1;
   }
 
-  printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n", c[0], c[1],
-         c[2], c[3], c[4]);
+  printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n", c[0], c[1], c[2], c[3], c[4]);
 
   // cudaDeviceReset must be called before exiting in order for profiling and
   // tracing tools such as Nsight and Visual Profiler to show complete traces
@@ -47,8 +46,7 @@ cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size) {
   // Choose which GPU to run on, change this on a multi-GPU system
   cudaStatus = cudaSetDevice(0);
   if (cudaStatus != cudaSuccess) {
-    fprintf(stderr,
-            "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+    fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
     goto Error;
   }
 
@@ -90,8 +88,7 @@ cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size) {
   // Check for any errors launching the kernel
   cudaStatus = cudaGetLastError();
   if (cudaStatus != cudaSuccess) {
-    fprintf(stderr, "addKernel launch failed: %s\n",
-            cudaGetErrorString(cudaStatus));
+    fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
     goto Error;
   }
 
