6e279e8044c079123d941975bdf31c700d34edb6
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-09-14 06:52:26 -0700

    chore(web): disable lighthouse color-contrast, inspector-issues, third-party-cookies rules (#19335)

--- web/lighthouserc.js
+++ web/lighthouserc.js
@@ -7,13 +7,16 @@ module.exports = {
       preset: 'lighthouse:recommended',
       assertions: {
         'bf-cache': ['warn', { minScore: 0 }],
+        'color-contrast': ['warn', { minScore: 0 }],
         'csp-xss': ['warn', { minScore: 0 }],
         deprecations: ['warn', { minScore: 0 }],
         'errors-in-console': ['warn', { minScore: 0 }],
+        'inspector-issues': ['warn', { minScore: 0 }],
         'landmark-one-main': ['warn', { minScore: 0 }],
         'no-unload-listeners': ['warn', { minScore: 0 }],
         'offline-start-url': ['warn', { minScore: 0 }], // https://github.com/hongbo-miao/hongbomiao.com/issues/824
         'service-worker': ['warn', { minScore: 0 }],
+        'third-party-cookies': ['warn', { minScore: 0 }],
         'total-byte-weight': ['warn', { minScore: 0 }],
         'unused-css-rules': ['warn', { minScore: 0 }],
         'unused-javascript': ['warn', { minScore: 0 }],
