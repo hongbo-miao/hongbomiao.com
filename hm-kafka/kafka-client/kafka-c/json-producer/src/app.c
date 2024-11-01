0b68eda1b548b74c07dbcc3a7d8f93d775b360e4
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-11-01 02:47:48 -0700

    chore(kafka-c): remove json-producer (#20106)

--- hm-kafka/kafka-client/kafka-c/json-producer/src/app.c
+++
@@ -1,9 +0,0 @@
-#include "../include/app.h"
-
-volatile sig_atomic_t is_running = true;
-
-void handle_signal(int signal) {
-  if (signal == SIGINT || signal == SIGTERM) {
-    is_running = false;
-  }
-}
--- hm-kafka/kafka-client/kafka-c/json-producer/src/kafka.c
+++
@@ -1,8 +0,0 @@
-#include "../include/kafka.h"
-
-void delivery_report(rd_kafka_t *kafka_handle __attribute__((unused)),
-                     const rd_kafka_message_t *rkmessage, void *opaque __attribute__((unused))) {
-  if (rkmessage->err) {
-    g_error("Message delivery failed: %s", rd_kafka_err2str(rkmessage->err));
-  }
-}
