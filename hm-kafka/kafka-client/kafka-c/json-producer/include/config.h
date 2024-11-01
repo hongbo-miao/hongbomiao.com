c44ea4af7d6085666987a7025862a49e1936c6e8
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-11-01 02:47:48 -0700

    chore(kafka-c): remove json-producer (#20106)

--- hm-kafka/kafka-client/kafka-c/json-producer/include/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#include <glib.h>
-#include <librdkafka/rdkafka.h>
-
-void load_config_group(rd_kafka_conf_t *conf, GKeyFile *key_file, const char *group_name);
-
-#endif
--- hm-kafka/kafka-client/kafka-c/json-producer/include/kafka.h
+++
@@ -1,9 +0,0 @@
-#ifndef KAFKA_H
-#define KAFKA_H
-
-#include <glib.h>
-#include <librdkafka/rdkafka.h>
-
-void delivery_report(rd_kafka_t *kafka_handle, const rd_kafka_message_t *rkmessage, void *opaque);
-
-#endif
