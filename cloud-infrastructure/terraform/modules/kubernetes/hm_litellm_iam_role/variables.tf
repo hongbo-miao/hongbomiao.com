12925734b3eaf8be91ce81124d92367739cb9498
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-11-09 20:16:29 -0800

    feat(k8s): add litellm

---
+++ cloud-infrastructure/terraform/modules/kubernetes/hm_litellm_iam_role/variables.tf
@@ -0,0 +1,18 @@
+variable "litellm_service_account_name" {
+  type = string
+}
+variable "litellm_namespace" {
+  type = string
+}
+variable "amazon_eks_cluster_oidc_provider" {
+  type = string
+}
+variable "amazon_eks_cluster_oidc_provider_arn" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "team" {
+  type = string
+}
