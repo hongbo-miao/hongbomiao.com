32b9dfb7555ee79151faba3321c8925effb37adc
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-11-10 03:11:41 -0800

    chore(prefect): remove ingest-data workflow (#20354)

--- data-orchestration/hm-prefect/workflows/ingest-data/tools/set_up/utils/get_env_variable.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def get_env_variable(var_name: str) -> str:
-    value = os.getenv(var_name)
-    if value is None:
-        raise ValueError(f"Failed to read {var_name}.")
-    return value
