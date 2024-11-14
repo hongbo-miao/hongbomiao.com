ffd3df17acd847253f638bee2b65152941085e11
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-11-14 02:13:15 -0800

    feat(veristand-controller): send data in protobuf over zeromq (#20474)

--- hardware-in-the-loop/national-instruments/VeriStandZeroMQBridge/VeriStandZeroMQBridge/Properties/AssemblyInfo.cs
+++ hardware-in-the-loop/national-instruments/VeriStandZeroMQBridge/VeriStandZeroMQBridge/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VeriStandController")]
+[assembly: AssemblyTitle("VeriStandZeroMQBridge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VeriStandController")]
+[assembly: AssemblyProduct("VeriStandZeroMQBridge")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
