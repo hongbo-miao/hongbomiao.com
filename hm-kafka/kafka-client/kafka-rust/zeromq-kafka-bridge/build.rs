e27e6154c0733653b1529d11a9ed33470e66948e
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-11-14 14:50:57 -0800

    feat(kafka): add zeromq kafka bridge

---
+++ hm-kafka/kafka-client/kafka-rust/zeromq-kafka-bridge/build.rs
@@ -0,0 +1,12 @@
+use std::io::Result;
+
+fn main() -> Result<()> {
+    println!("cargo:warning=Build script is running!");
+    let mut prost_build = prost_build::Config::new();
+    prost_build.protoc_arg("--experimental_allow_proto3_optional");
+    prost_build.compile_protos(
+        &["src/protos/production.iot.signals.proto"],
+        &["src/protos/"],
+    )?;
+    Ok(())
+}
