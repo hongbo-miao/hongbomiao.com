31e9ffcc5e29e6632a794faafca755ddce044596
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-11-25 00:59:37 -0800

    feat(rust): read from env files (#20751)

--- data-visualization/iads/zeromq-iads-bridge/build.rs
+++ data-visualization/iads/zeromq-iads-bridge/build.rs
@@ -4,6 +4,9 @@ fn main() -> Result<()> {
     println!("cargo:warning=Build script is running!");
     let mut prost_build = prost_build::Config::new();
     prost_build.protoc_arg("--experimental_allow_proto3_optional");
-    prost_build.compile_protos(&["src/protos/production.iot.signals.proto"], &["src/protos/"])?;
+    prost_build.compile_protos(
+        &["src/protos/production.iot.signals.proto"],
+        &["src/protos/"],
+    )?;
     Ok(())
 }
