be98ef5d5f80d86853859e419f5b44f9dc600a65
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-11-30 02:19:18 -0800

    feat(api-rust): add timeout layer (#20937)

--- api-rust/src/main.rs
+++ api-rust/src/main.rs
@@ -1,10 +1,12 @@
 use axum::{routing::get, serve, Router};
 use http::Method;
-use std::env;
+use std::{env, time::Duration};
 use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
 use tower_http::{
     compression::CompressionLayer,
     cors::{Any, CorsLayer},
+    timeout::TimeoutLayer,
 };
 use tracing::info;
 
@@ -24,15 +26,20 @@ async fn main() {
         .parse::<u16>()
         .expect("PORT must be a valid number");
 
+    let compression = CompressionLayer::new();
+    let timeout = TimeoutLayer::new(Duration::from_secs(10));
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
         .allow_origin(Any)
         .allow_headers(Any);
+    let trace = TraceLayer::new_for_http();
 
     let app = Router::new()
         .route("/", get(root))
+        .layer(compression)
+        .layer(timeout)
         .layer(cors)
-        .layer(CompressionLayer::new());
+        .layer(trace);
 
     let listener = TcpListener::bind(format!("0.0.0.0:{}", port))
         .await
