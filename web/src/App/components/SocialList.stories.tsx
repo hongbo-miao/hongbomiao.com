99bc2826735ce8ebaa01d1a5ece0180f8fe952dc
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-12-01 00:25:01 -0800

    chore(web): remove prop-types

--- web/src/App/components/SocialList.stories.tsx
+++ web/src/App/components/SocialList.stories.tsx
@@ -1,6 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react';
 import WEBSITES from '../../Home/fixtures/WEBSITES';
 import HmSocialList from './SocialList';
 
