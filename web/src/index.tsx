e565aedc8a07b1565057f9edc4b3e9d0ff594121
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-12-02 17:09:36 -0800

    chore(web): remove web-vitals (#21102)

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -8,7 +8,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import HmApp from './App/components/App';
-import reportWebVitals from './shared/libs/reportWebVitals';
 import initSentry from './shared/utils/initSentry';
 import queryClient from './shared/utils/queryClient';
 import store from './shared/utils/store';
@@ -29,10 +28,3 @@ root.render(
     </Provider>
   </Sentry.ErrorBoundary>,
 );
-
-/*
- * If you want to start measuring performance in your app, pass a function
- * to log results (for example: reportWebVitals(console.log))
- * or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
- */
-reportWebVitals();
