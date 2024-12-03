148dbdeb963f06e91f9d300c3e7a202b3e8d7404
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-12-02 22:13:14 -0800

    chore(web): clean old create-react-app files (#21117)

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,49 +1,22 @@
-import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          '@babel/plugin-transform-react-display-name',
-          '@babel/plugin-transform-private-property-in-object',
-        ],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      'src': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    port: 3000,
-    host: true, // needed for docker
-  },
+  plugins: [react()],
   build: {
     outDir: 'build',
     sourcemap: true,
   },
-  publicDir: 'public',
+  server: {
+    port: 3000,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
-      exclude: [
-        'node_modules/',
-        'src/setupTests.ts',
-        '**/*.query.ts',
-        '**/*.story.tsx',
-        '**/*.type.ts',
-        'src/shared/libs/*',
-        'src/shared/utils/initSentry.ts',
-      ],
     },
   },
-});
+})
