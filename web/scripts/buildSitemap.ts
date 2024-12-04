3a741c7640470ca41e9c545227cce73763209970
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-12-04 10:46:58 -0800

    chore(web): remove babel

--- web/scripts/buildSitemap.ts
+++ web/scripts/buildSitemap.ts
@@ -1,12 +1,7 @@
 import { promises as fsp } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import { EnumChangefreq, SitemapItemLoose, SitemapStreamOptions } from 'sitemap';
 import Paths from '../src/shared/utils/paths.js';
 import generateSitemap from './generateSitemap.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const buildSitemap = async (): Promise<void> => {
   const options: SitemapStreamOptions = {
@@ -16,7 +11,7 @@ const buildSitemap = async (): Promise<void> => {
     { url: Paths.appRootPath, changefreq: EnumChangefreq.HOURLY, priority: 1 },
   ];
   const sitemap = await generateSitemap(options, links);
-  await fsp.writeFile(path.join(__dirname, '../public/sitemap.xml'), sitemap);
+  await fsp.writeFile('public/sitemap.xml', sitemap);
 };
 
 export default buildSitemap;
