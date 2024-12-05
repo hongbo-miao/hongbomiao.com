939580d00cc02a0d842a84659680983246550c8c
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-12-05 00:20:29 -0800

    chore(web): remove --experimental-specifier-resolution

--- web/bin/prebuild.sh
+++ web/bin/prebuild.sh
@@ -4,5 +4,5 @@ set -e
 rm -r -f tmp
 swc scripts --out-dir=tmp --extensions='.ts,.tsx'
 swc src/shared/utils/paths.ts --out-file=tmp/src/shared/utils/paths.js
-node --experimental-specifier-resolution=node tmp/scripts/runBuildSitemap.js
+node tmp/scripts/runBuildSitemap.js
 rm -r -f tmp
