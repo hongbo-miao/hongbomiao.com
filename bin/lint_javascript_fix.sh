45c9cfdd0c7c1281c01876ed43fdbcad0922209e
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-12-08 03:48:26 -0800

    test(rego): add more test cases for rego files (#21332)

--- bin/lint_javascript_fix.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd api-node && npm run lint:javascript:fix
-cd ../data-visualization/grafana/hm-panel-plugin && npm run lint:javascript:fix
-cd ../ethereum && npm run lint:javascript:fix
-cd ../../mobile-react-native && npm run lint:javascript:fix
-cd ../web && npm run lint:javascript:fix
-cd ../web-cypress && npm run lint:javascript:fix
