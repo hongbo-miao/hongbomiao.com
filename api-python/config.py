e9f0a0a892dc2d211f168aff7c55adc34026c168
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-12-13 20:25:13 -0800

    feat(api-python): load from env file (#21542)

--- api-python/config.py
+++ api-python/config.py
@@ -1,7 +1,18 @@
+import os
+
 from pydantic_settings import BaseSettings
 
 
+def get_env_file() -> str:
+    env = os.getenv("ENV")
+    return ".env.production" if env == "production" else ".env.development"
+
+
 class Settings(BaseSettings):
-    ENV: str = "development"
-    SENTRY_DSN: str | None = None
-    KAFKA_BOOTSTRAP_SERVERS: str = "localhost:9092"
+    ENV: str
+    SENTRY_DSN: str
+    KAFKA_BOOTSTRAP_SERVERS: str
+
+    model_config = {
+        "env_file": get_env_file(),
+    }
