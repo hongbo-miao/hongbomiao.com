46f24cf451b9845ba5d2efe250319e33127c8d4d
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-12-15 18:29:46 -0800

    feat(api-rust): switch to use graphql to upload image (#21612)

--- api-rust/src/graphql.rs
+++ api-rust/src/graphql.rs
@@ -1,34 +1,7 @@
-mod mutation;
-mod query;
-mod subscription;
-use self::mutation::Mutation;
-use self::query::Query;
-use self::subscription::Subscription;
-use async_graphql::Schema;
-use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use axum::{extract::State, response::IntoResponse};
-
-type ApiSchema = Schema<Query, Mutation, Subscription>;
-
-pub async fn graphql_handler(
-    State(schema): State<ApiSchema>,
-    req: GraphQLRequest,
-) -> GraphQLResponse {
-    schema.execute(req.into_inner()).await.into()
-}
-
-pub async fn graphiql() -> impl IntoResponse {
-    axum::response::Html(
-        async_graphql::http::GraphiQLSource::build()
-            .endpoint("/graphql")
-            .subscription_endpoint("/ws")
-            .finish(),
-    )
-}
-
-pub fn create_schema() -> ApiSchema {
-    Schema::build(Query, Mutation, Subscription)
-        .limit_depth(100)
-        .limit_complexity(1000)
-        .finish()
+pub mod mutation;
+pub mod query;
+pub mod schema;
+pub mod subscription;
+pub mod utils {
+    pub mod resnet_18_util;
 }
