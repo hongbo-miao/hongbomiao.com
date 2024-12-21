e03bf64a1fde9341770ea26070269d0bc4d36110
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-12-21 03:28:14 -0800

    refactor(linux): replace from apt to apt-get

--- embedded/yocto-project/bin/build.sh
+++ embedded/yocto-project/bin/build.sh
@@ -4,7 +4,7 @@ set -e
 # https://docs.yoctoproject.org/brief-yoctoprojectqs/index.html
 
 echo "# Clone Poky"
-sudo apt install --yes git
+sudo apt-get install --yes git
 git clone git://git.yoctoproject.org/poky
 cd poky
 git checkout kirkstone
@@ -12,9 +12,8 @@ source oe-init-build-env
 
 echo "# Build"
 # https://wiki.koansoftware.com/index.php/Upgrade_to_Yocto_honister_3.4
-sudo apt update
-sudo apt upgrade
-sudo apt install --yes gawk wget git diffstat unzip texinfo gcc build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev xterm python3-subunit mesa-common-dev zstd lz4
+sudo apt-get update
+sudo apt-get install --yes gawk wget git diffstat unzip texinfo gcc build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev xterm python3-subunit mesa-common-dev zstd lz4
 
 # Speed up the build and guard against fetcher failures by using Shared State Cache mirrors and enabling Hash Equivalence.
 # Use pre-built artifacts rather than building them
