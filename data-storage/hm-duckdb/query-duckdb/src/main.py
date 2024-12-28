1a1d7e1d7bda4e4f001e495ee9c792ee09ecf866
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-12-28 01:11:43 -0800

    perf(duckdb): migrate from pandas to polars (#22075)

--- data-storage/hm-duckdb/query-duckdb/src/main.py
+++ data-storage/hm-duckdb/query-duckdb/src/main.py
@@ -2,41 +2,29 @@ import logging
 from pathlib import Path
 
 import duckdb
-import pandas as pd
+import polars as pl
 
 
-def create_people_dataframe() -> pd.DataFrame:
-    return pd.DataFrame(
+def main(duckdb_path: Path) -> None:
+    people = pl.DataFrame(
         {
             "name": ["Alice", "Bob", "Charlie", "David", "Eve"],
             "age": [25, 30, 35, 28, 22],
             "city": ["New York", "London", "Paris", "Tokyo", "Berlin"],
         }
     )
+    logging.info(people)
+    with duckdb.connect(duckdb_path) as conn:
+        conn.register("people", people)
+        conn.execute("create table if not exists people as select * from people")
 
-
-def save_to_duckdb(df: pd.DataFrame, db_path: Path) -> None:
-    with duckdb.connect(db_path) as conn:
-        conn.register("df", df)
-        conn.execute("create table if not exists people as select * from df")
-
-
-def query_from_duckdb(db_path: Path, age: int) -> pd.DataFrame:
-    with duckdb.connect(db_path) as conn:
-        return conn.execute("select name, age from people where age > ?", [age]).df()
-
-
-def main() -> None:
-    db_path = Path("data/people.duckdb")
-
-    df = create_people_dataframe()
-    logging.info(df)
-    save_to_duckdb(df, db_path)
-
-    df = query_from_duckdb(db_path, age=25)
-    logging.info(df)
+        age = 25
+        df = conn.execute("select name, age from people where age > ?", [age]).pl()
+        logging.info(df)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    main()
+
+    duckdb_path = Path("data/people.duckdb")
+    main(duckdb_path)
