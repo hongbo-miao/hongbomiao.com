8795ef85b9c843c86bd9b93deaf15a3900ac587d
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2024-12-28 03:04:46 -0800

    perf(tdms): migrate from pandas to polars (#22081)

--- hardware-in-the-loop/national-instruments/hm-tdms/src/utils/convert_tdms_to_parquet.py
+++ hardware-in-the-loop/national-instruments/hm-tdms/src/utils/convert_tdms_to_parquet.py
@@ -1,3 +1,4 @@
+import polars as pl
 from nptdms import TdmsFile
 
 
@@ -5,12 +6,11 @@ def convert_tdms_to_parquet(data_dirname: str, tdms_filename: str) -> None:
     tdms_path = f"{data_dirname}/{tdms_filename}"
     tdms_file = TdmsFile.read(tdms_path)
     for group in tdms_file.groups():
-        df = group.as_dataframe()
+        df = pl.from_pandas(group.as_dataframe())
         print(group.name)
         print(df)
-        df.to_parquet(
+        df.write_parquet(
             f"{data_dirname}/{group.name}.parquet",
-            engine="pyarrow",
             compression="zstd",
             compression_level=19,
         )
