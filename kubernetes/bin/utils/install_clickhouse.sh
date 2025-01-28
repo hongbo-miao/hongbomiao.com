a2477054dd41407a1bddb069e382da65a7312a19
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-01-28 02:53:27 -0800

    chore(clickhouse): remove install_clickhouse.sh (#23449)

--- kubernetes/bin/utils/install_clickhouse.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "# Install ClickHouse"
-curl --silent https://raw.githubusercontent.com/Altinity/clickhouse-operator/master/deploy/operator-web-installer/clickhouse-operator-install.sh | OPERATOR_NAMESPACE=hm-clickhouse bash
-kubectl apply --namespace=hm-clickhouse --filename=https://raw.githubusercontent.com/Altinity/clickhouse-operator/master/docs/chi-examples/01-simple-layout-01-1shard-1repl.yaml
-echo "=================================================="
