f778a9d29853cfe65383e2e996ff53bd0b214988
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-02-03 23:16:16 -0800

    chore(ray): add SYS_PTRACE (#23621)

--- parallel-computing/cuda/src/main.cu
+++ parallel-computing/cuda/src/main.cu
@@ -46,7 +46,7 @@ cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size) {
   // Choose which GPU to run on, change this on a multi-GPU system
   cudaStatus = cudaSetDevice(0);
   if (cudaStatus != cudaSuccess) {
-    fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+    fprintf(stderr, "cudaSetDevice failed! Do you have a CUDA-capable GPU installed?");
     goto Error;
   }
 
