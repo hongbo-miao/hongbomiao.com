db96221a9b82baa3812ce4835e2ad75af675c74c
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-02-11 03:30:28 -0800

    feat(harbor): add docker proxy (#23791)

---
+++ cloud-infrastructure/terraform/environments/production/harbor/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    region = "us-west-2"
+    bucket = "hm-terraform-bucket"
+    key    = "production/harbor/terraform.tfstate"
+  }
+  required_providers {
+    # https://registry.terraform.io/providers/goharbor/harbor/latest
+    harbor = {
+      source  = "goharbor/harbor"
+      version = "3.10.19"
+    }
+  }
+  # terraform version
+  required_version = ">= 1.7"
+}
