89ad3390513949d79dda122f61d7ee27424fcd07
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-02-28 17:21:57 -0800

    chore(prefect): upgrade prefect workflow to prefect 3

--- data-orchestration/hm-prefect/workflows/print-platform/add_kubernetes_job_block.py
+++
@@ -1,8 +0,0 @@
-from prefect.infrastructure import KubernetesJob
-
-job = KubernetesJob(
-    image="ghcr.io/hongbo-miao/hm-prefect-print-platform:latest",
-    namespace="hm-prefect",
-    image_pull_policy="Always",
-)
-job.save("print-platform-kubernetes-job-block", overwrite=True)
