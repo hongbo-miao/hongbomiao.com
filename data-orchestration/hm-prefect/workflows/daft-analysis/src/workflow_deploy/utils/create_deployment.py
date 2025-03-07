93559c799fb3e33adc871359a332d8430957a275
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-03-07 00:28:02 -0800

    feat(prefect): use ray to compute

--- data-orchestration/hm-prefect/workflows/daft-analysis/src/workflow_deploy/utils/create_deployment.py
+++ data-orchestration/hm-prefect/workflows/daft-analysis/src/workflow_deploy/utils/create_deployment.py
@@ -11,7 +11,7 @@ async def create_deployment(
     docker_image_name: str,
     deployment: dict[str, Any],
 ) -> None:
-    deployment_name = f"hm-{environment}-{config.BASE_WORKFLOW_NAME}"
+    deployment_name = f"{environment}-hm-{config.BASE_WORKFLOW_NAME}"
     await workflow.deploy(
         name=deployment_name,
         work_pool_name=deployment["work_pool_name"],
