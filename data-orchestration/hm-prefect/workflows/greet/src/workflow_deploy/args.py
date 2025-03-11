5171ef9affc0265a4532297582dac8c0594327ba
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-03-10 21:01:46 -0700

    chore(ec2): add user data checking command (#24967)

--- data-orchestration/hm-prefect/workflows/greet/src/workflow_deploy/args.py
+++ data-orchestration/hm-prefect/workflows/greet/src/workflow_deploy/args.py
@@ -4,5 +4,9 @@ from argparse import Namespace
 
 def get_args() -> Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--environment", type=str)
+    parser.add_argument(
+        "--environment",
+        type=str,
+        choices=["development", "production"],
+    )
     return parser.parse_args()
