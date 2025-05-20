6ce643d2090c2a753ee3852ea15513253b2262fc
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-05-20 02:38:58 -0700

    feat(argo-cd): deploy starrocks (#27513)

---
+++ cloud-infrastructure/terraform/modules/kubernetes/hm_starrocks_iam_role/variables.tf
@@ -0,0 +1,21 @@
+variable "starrocks_service_account_name" {
+  type = string
+}
+variable "starrocks_namespace" {
+  type = string
+}
+variable "iot_data_s3_bucket_name" {
+  type = string
+}
+variable "amazon_eks_cluster_oidc_provider" {
+  type = string
+}
+variable "amazon_eks_cluster_oidc_provider_arn" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "team" {
+  type = string
+}
---
+++ kubernetes/argo-cd/applications/production-hm/starrocks/starrocks/set_up.sql
@@ -0,0 +1,1 @@
+set password for 'root' = password('xxx');
