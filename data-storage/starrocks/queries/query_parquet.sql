8ecf896a98efede32a7d86c834adcf9fcb869f51
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-05-21 01:22:55 -0700

    chore(starrocks): change to iam role to query (#27524)

--- data-storage/starrocks/queries/query_parquet.sql
+++ data-storage/starrocks/queries/query_parquet.sql
@@ -1,8 +1,7 @@
 -- Query metadata
 desc files (
   "aws.s3.region" = "us-west-2",
-  "aws.s3.access_key" = "xxx",
-  "aws.s3.secret_key" = "xxx",
+  "aws.s3.use_aws_sdk_default_behavior" = "true",
   "format" = "parquet",
   "path" = "s3://iot-data-bucket/motor.parquet"
 );
@@ -10,8 +9,7 @@ desc files (
 -- Query data
 select * from files (
   "aws.s3.region" = "us-west-2",
-  "aws.s3.access_key" = "xxx",
-  "aws.s3.secret_key" = "xxx",
+  "aws.s3.use_aws_sdk_default_behavior" = "true",
   "format" = "parquet",
   "path" = "s3://iot-data-bucket/motor.parquet"
 );
