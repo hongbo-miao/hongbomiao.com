00f3055fa6b934aea48c3d9172e1010f37b59038
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-05-24 01:02:59 -0700

    chore(starrocks): load parquet (#27603)

--- data-storage/starrocks/queries/admin.sql
+++ data-storage/starrocks/queries/admin.sql
@@ -1,2 +1,6 @@
 -- Set root password
 set password for 'root' = password('xxx');
+
+-- Max column number per table (default: 10000)
+admin show frontend config like "max_column_number_per_table";
+admin set frontend config ("max_column_number_per_table" = "100000");
