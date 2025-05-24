105670875ace7813b3a9526eee44308cc8b203ad
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-05-24 01:02:59 -0700

    chore(starrocks): load parquet (#27603)

--- data-storage/starrocks/queries/parquet.sql
+++ data-storage/starrocks/queries/parquet.sql
@@ -13,3 +13,16 @@ select * from files (
   "format" = "parquet",
   "path" = "s3://iot-data-bucket/motor.parquet"
 );
+
+-- Load data
+create database if not exists production_hm_iot_db;
+use production_hm_iot_db;
+
+create table motor as
+select * from files
+(
+  "path" = "s3://iot-data-bucket/motor.parquet",
+  "format" = "parquet",
+  "aws.s3.region" = "us-west-2",
+  "aws.s3.use_aws_sdk_default_behavior" = "true"
+);
