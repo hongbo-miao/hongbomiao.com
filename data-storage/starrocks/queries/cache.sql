c24206516dbe8d2f0266b56ace6886d8b40727a4
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-05-25 02:19:28 -0700

    chore(starrocks): set query timeout (#27663)

--- data-storage/starrocks/queries/cache.sql
+++ data-storage/starrocks/queries/cache.sql
@@ -9,3 +9,7 @@ show variables like 'enable_scan_datacache';
 -- Footer cache (default: true)
 -- https://docs.starrocks.io/docs/data_source/data_cache/#footer-cache
 show variables like 'enable_file_metacache';
+
+-- Query timeout (default: 300 seconds)
+show variables like 'query_timeout';
+set global query_timeout=21600; -- 6 hours
