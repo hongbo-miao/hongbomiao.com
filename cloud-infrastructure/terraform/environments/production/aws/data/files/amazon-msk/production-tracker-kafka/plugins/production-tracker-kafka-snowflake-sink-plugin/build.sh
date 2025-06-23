50659acebfa07b178d4abc3c1e027ba0ab7b04b3
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-06-22 17:43:17 -0700

    chore(curl): remove =

--- cloud-infrastructure/terraform/environments/production/aws/data/files/amazon-msk/production-tracker-kafka/plugins/production-tracker-kafka-snowflake-sink-plugin/build.sh
+++ cloud-infrastructure/terraform/environments/production/aws/data/files/amazon-msk/production-tracker-kafka/plugins/production-tracker-kafka-snowflake-sink-plugin/build.sh
@@ -10,10 +10,10 @@ mkdir -p "/tmp/$KAFKA_PLUGIN_NAME/temp/"
 mkdir -p "/tmp/$KAFKA_PLUGIN_NAME/output/"
 
 # Download
-curl --silent --fail --show-error --location --output="/tmp/$KAFKA_PLUGIN_NAME/output/snowflake-kafka-connector-$SNOWFLAKE_KAFKA_CONNECTOR_VERSION.jar" "https://repo1.maven.org/maven2/com/snowflake/snowflake-kafka-connector/$SNOWFLAKE_KAFKA_CONNECTOR_VERSION/snowflake-kafka-connector-$SNOWFLAKE_KAFKA_CONNECTOR_VERSION.jar"
-curl --silent --fail --show-error --location --output="/tmp/$KAFKA_PLUGIN_NAME/output/bc-fips-$BC_FIPS_VERSION.jar" "https://repo1.maven.org/maven2/org/bouncycastle/bc-fips/$BC_FIPS_VERSION/bc-fips-$BC_FIPS_VERSION.jar"
-curl --silent --fail --show-error --location --output="/tmp/$KAFKA_PLUGIN_NAME/output/bcpkix-fips-$BCPKIX_FIPS_VERSION.jar" "https://repo1.maven.org/maven2/org/bouncycastle/bcpkix-fips/$BCPKIX_FIPS_VERSION/bcpkix-fips-$BCPKIX_FIPS_VERSION.jar"
-curl --silent --fail --show-error --location --output="/tmp/$KAFKA_PLUGIN_NAME/raw/confluentinc-kafka-connect-avro-converter-$CONFLUENT_KAFKA_CONNECT_AVRO_CONVERTER_VERSION.zip" "https://d2p6pa21dvn84.cloudfront.net/api/plugins/confluentinc/kafka-connect-avro-converter/versions/$CONFLUENT_KAFKA_CONNECT_AVRO_CONVERTER_VERSION/confluentinc-kafka-connect-avro-converter-$CONFLUENT_KAFKA_CONNECT_AVRO_CONVERTER_VERSION.zip"
+curl --silent --fail --show-error --location --output "/tmp/$KAFKA_PLUGIN_NAME/output/snowflake-kafka-connector-$SNOWFLAKE_KAFKA_CONNECTOR_VERSION.jar" "https://repo1.maven.org/maven2/com/snowflake/snowflake-kafka-connector/$SNOWFLAKE_KAFKA_CONNECTOR_VERSION/snowflake-kafka-connector-$SNOWFLAKE_KAFKA_CONNECTOR_VERSION.jar"
+curl --silent --fail --show-error --location --output "/tmp/$KAFKA_PLUGIN_NAME/output/bc-fips-$BC_FIPS_VERSION.jar" "https://repo1.maven.org/maven2/org/bouncycastle/bc-fips/$BC_FIPS_VERSION/bc-fips-$BC_FIPS_VERSION.jar"
+curl --silent --fail --show-error --location --output "/tmp/$KAFKA_PLUGIN_NAME/output/bcpkix-fips-$BCPKIX_FIPS_VERSION.jar" "https://repo1.maven.org/maven2/org/bouncycastle/bcpkix-fips/$BCPKIX_FIPS_VERSION/bcpkix-fips-$BCPKIX_FIPS_VERSION.jar"
+curl --silent --fail --show-error --location --output "/tmp/$KAFKA_PLUGIN_NAME/raw/confluentinc-kafka-connect-avro-converter-$CONFLUENT_KAFKA_CONNECT_AVRO_CONVERTER_VERSION.zip" "https://d2p6pa21dvn84.cloudfront.net/api/plugins/confluentinc/kafka-connect-avro-converter/versions/$CONFLUENT_KAFKA_CONNECT_AVRO_CONVERTER_VERSION/confluentinc-kafka-connect-avro-converter-$CONFLUENT_KAFKA_CONNECT_AVRO_CONVERTER_VERSION.zip"
 
 # Build
 unzip -q "/tmp/$KAFKA_PLUGIN_NAME/raw/confluentinc-kafka-connect-avro-converter-$CONFLUENT_KAFKA_CONNECT_AVRO_CONVERTER_VERSION.zip" -d "/tmp/$KAFKA_PLUGIN_NAME/temp/"
