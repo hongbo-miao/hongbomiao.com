bd67f4eacfc9a8735470238e22fc27d3269b70e2
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-07-12 21:23:56 -0700

    fix(lint): fix @prettier/plugin-xml (#29576)

--- web/src/shared/components/AudioPlayer.tsx
+++ web/src/shared/components/AudioPlayer.tsx
@@ -1,4 +1,4 @@
-import clsx from 'clsx';
+import { clsx } from 'clsx';
 import React from 'react';
 import { useAudio } from 'react-use';
 import flashingMusicalNoteGIF from '../images/musical-note-flashing.gif';
