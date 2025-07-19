ca69760dfa0c426a047a9f6453f3c5b7cac8a637
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-07-19 14:50:00 -0700

    chore(terraform): update terraform aws to v6.4.0 (#29858)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- cloud-infrastructure/terraform/environments/production/airbyte/versions.tf
+++ cloud-infrastructure/terraform/environments/production/airbyte/versions.tf
@@ -13,7 +13,7 @@ terraform {
     # https://registry.terraform.io/providers/hashicorp/aws/latest
     aws = {
       source  = "hashicorp/aws"
-      version = "6.3.0"
+      version = "6.4.0"
     }
   }
   # terraform version
