2e77256d24df63d9e3b1619c49c42d42bda461c8
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-07-23 01:25:48 -0700

    chore(litellm): add Amazon Titan Text G1 - Premier model (#30026)

--- cloud-infrastructure/terraform/modules/kubernetes/hm_litellm_iam_role/main.tf
+++ cloud-infrastructure/terraform/modules/kubernetes/hm_litellm_iam_role/main.tf
@@ -62,8 +62,13 @@ resource "aws_iam_role_policy" "hm_litellm_iam_role_policy" {
           "arn:aws:bedrock:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:inference-profile/us.anthropic.claude-3-5-haiku-20241022-v1:0",
           "arn:aws:bedrock:*::foundation-model/anthropic.claude-3-5-haiku-20241022-v1:0",
           # Claude Sonnet
-          "arn:aws:bedrock:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:inference-profile/us.anthropic.claude-3-7-sonnet-20250219-v1:0",
-          "arn:aws:bedrock:*::foundation-model/anthropic.claude-3-7-sonnet-20250219-v1:0"
+          "arn:aws:bedrock:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:inference-profile/us.anthropic.claude-sonnet-4-20250514-v1:0",
+          "arn:aws:bedrock:*::foundation-model/anthropic.claude-sonnet-4-20250514-v1:0",
+          # Claude Opus
+          "arn:aws:bedrock:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:inference-profile/us.anthropic.claude-opus-4-20250514-v1:0",
+          "arn:aws:bedrock:*::foundation-model/anthropic.claude-opus-4-20250514-v1:0",
+          # Amazon Titan Text G1 - Premier
+          "arn:aws:bedrock:*::foundation-model/amazon.titan-text-premier-v1:0"
         ]
       }
     ]
