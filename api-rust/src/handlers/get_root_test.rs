947b36b0f70e398a13fb81226efe9e06283ebafa
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-09-15 23:09:05 -0700

    test(axum): add axum-test (#32499)

--- api-rust/src/handlers/get_root_test.rs
+++ api-rust/src/handlers/get_root_test.rs
@@ -1,29 +1,13 @@
 use crate::handlers::get_root::get_root;
-use axum::body::to_bytes;
-use axum::{
-    Router,
-    body::Body,
-    http::{Request, StatusCode},
-};
-use tower::ServiceExt;
+use axum::{Router, routing::get};
+use axum_test::TestServer;
 
 #[tokio::test]
 async fn test_root_handler() {
-    let app = Router::new().route("/", axum::routing::get(get_root));
-    let res = app
-        .oneshot(
-            Request::builder()
-                .uri("/")
-                .method("GET")
-                .body(Body::empty())
-                .unwrap(),
-        )
-        .await
-        .unwrap();
-    assert_eq!(res.status(), StatusCode::OK);
+    let app = Router::new().route("/", get(get_root));
+    let server = TestServer::new(app).expect("Failed to create TestServer");
 
-    // Use a 1MB limit for the res body
-    let body = to_bytes(res.into_body(), 1024 * 1024).await.unwrap();
-    let body_str = String::from_utf8(body.to_vec()).unwrap();
-    assert_eq!(body_str, "ok");
+    let response = server.get("/").await;
+    response.assert_status_ok();
+    response.assert_text("ok");
 }
