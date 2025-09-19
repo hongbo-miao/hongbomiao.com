bf09ed77c86e46c09b899fd5e7fb0c64b8b5c141
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-09-18 21:58:10 -0700

    fix(mobile-react-native): update react native related packages (#32514)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- mobile/mobile-react-native/jest.config.mjs
+++ mobile/mobile-react-native/jest.config.mjs
@@ -9,4 +9,6 @@ export default {
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@ui-kitten)',
   ],
+  // https://github.com/expo/expo/issues/36831#issuecomment-3107047371
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
