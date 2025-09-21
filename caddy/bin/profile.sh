b20b76b6bd7165e116a3433723172b76d982f5ed
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-09-21 15:15:56 -0700

    refactor(app): rename bin to scripts (#32855)

--- caddy/bin/profile.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-node build/scripts/profile.js
--- system-tool/rclone/bin/install_rclone_on_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# https://rclone.org/install/#linux
-sudo -v ; curl https://rclone.org/install.sh | sudo bash
