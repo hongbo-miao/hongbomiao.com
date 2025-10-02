be2022d22ed4726897ec01de04aa5734688a048e
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-10-02 01:17:45 -0700

    refactor(diart): read hugging face hub token from env (#33605)

---
+++ audio/speaker-diarization/hm-diart/src/config.py
@@ -0,0 +1,25 @@
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+def get_env_files() -> list[str]:
+    env = os.getenv("ENV")
+    if env == "production":
+        return [".env.production", ".env.production.local"]
+    if env in {"development", "test"}:
+        return [".env.development", ".env.development.local"]
+    msg = f"Invalid ENV value: {env}."
+    raise ValueError(msg)
+
+
+class Config(BaseSettings):
+    ENV: str
+    HUGGING_FACE_HUB_TOKEN: str
+    SEGMENTATION_MODEL: str
+    EMBEDDING_MODEL: str
+
+    model_config = SettingsConfigDict(env_file=get_env_files())
+
+
+config = Config.model_validate({})
--- audio/speaker-diarization/hm-diart/src/main.py
+++ audio/speaker-diarization/hm-diart/src/main.py
@@ -1,7 +1,9 @@
 import logging
 
-from diart import SpeakerDiarization
+from config import config
+from diart.blocks import SpeakerDiarization, SpeakerDiarizationConfig
 from diart.inference import StreamingInference
+from diart.models import EmbeddingModel, SegmentationModel
 from diart.sinks import RTTMWriter
 from diart.sources import MicrophoneAudioSource
 
@@ -9,7 +11,19 @@ logger = logging.getLogger(__name__)
 
 
 def main() -> None:
-    pipeline = SpeakerDiarization()
+    segmentation = SegmentationModel.from_pretrained(
+        config.SEGMENTATION_MODEL,
+        config.HUGGING_FACE_HUB_TOKEN,
+    )
+    embedding = EmbeddingModel.from_pretrained(
+        config.EMBEDDING_MODEL,
+        config.HUGGING_FACE_HUB_TOKEN,
+    )
+    diarization_config = SpeakerDiarizationConfig(
+        segmentation=segmentation,
+        embedding=embedding,
+    )
+    pipeline = SpeakerDiarization(diarization_config)
     mic = MicrophoneAudioSource()
     inference = StreamingInference(pipeline, mic, do_plot=True)
     inference.attach_observers(RTTMWriter(mic.uri, "output/audio.rttm"))
