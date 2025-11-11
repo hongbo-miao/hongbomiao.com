004a0b6c6b4f2b760fef8f0fbaf477c26319474a
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-11-10 23:33:28 -0800

    chore(rust): add returning types for build.rs (#35538)

--- data-processing/kafka/kafka-client/kafka-rust/zeromq-kafka-bridge/build.rs
+++ data-processing/kafka/kafka-client/kafka-rust/zeromq-kafka-bridge/build.rs
@@ -1,7 +1,6 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    println!("cargo:warning=Build script is running!");
     let mut prost_build = prost_build::Config::new();
     prost_build.protoc_arg("--experimental_allow_proto3_optional");
     prost_build.compile_protos(
