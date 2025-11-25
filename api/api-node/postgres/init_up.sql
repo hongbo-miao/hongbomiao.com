96c1825a7a2702ec7116b4325b43fc92cb303cae
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-11-24 23:16:54 -0800

    feat(lint): lint editorconfig

--- api/api-node/postgres/init_up.sql
+++ api/api-node/postgres/init_up.sql
@@ -2,12 +2,12 @@ create extension if not exists pgcrypto;
 
 create table users
 (
-  id         uuid primary key     default gen_random_uuid(),
-  email      text unique not null,
-  password   text        not null,
-  first_name text        not null,
-  last_name  text        not null,
-  bio        text,
-  created_at timestamptz not null default now(),
-  last_login timestamptz not null default now()
+    id         uuid primary key     default gen_random_uuid(),
+    email      text unique not null,
+    password   text        not null,
+    first_name text        not null,
+    last_name  text        not null,
+    bio        text,
+    created_at timestamptz not null default now(),
+    last_login timestamptz not null default now()
 );
