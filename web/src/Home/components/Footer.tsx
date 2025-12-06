ca8ac4f62c7bc21e1c0858ea85488af5250d62dc
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-12-06 03:17:21 -0800

    style(web): make page responsive on mobile (#36627)

--- web/src/Home/components/Footer.tsx
+++ web/src/Home/components/Footer.tsx
@@ -1,15 +1,14 @@
 import HmCopyright from '@/App/components/Copyright';
 import HmSocialList from '@/App/components/SocialList';
 import WEBSITES from '@/Home/fixtures/WEBSITES';
-import '@/Home/components/Footer.css';
 
 function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer className="bg-background hm-footer">
-      <div className="max-w-4xl mx-auto px-6 hm-footer-container">
-        <nav className="flex justify-between items-center">
-          <div className="flex items-center space-x-4">
+    <footer className="bg-background pb-16 pt-0">
+      <div className="mx-auto max-w-[640px] px-6">
+        <nav className="flex flex-col items-center justify-between gap-12 md:flex-row md:gap-0">
+          <div className="flex flex-col items-center gap-4 md:flex-row">
             <HmSocialList websites={WEBSITES} />
           </div>
           <div className="flex items-center">
