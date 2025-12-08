c0ade3bd2b010883829ff05bbca5eadde38719b3
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-12-07 18:31:43 -0800

    feat(ios): use catboost coreml model (#36752)

--- mobile/mobile-ios/mobile-ios/Sources/Application/Config.swift
+++ mobile/mobile-ios/mobile-ios/Sources/Application/Config.swift
@@ -13,4 +13,11 @@ enum Config {
   static let sileroVadMaxSegmentDurationS: Double = 10.0
   static let realTimeAudioBufferFrameCount: Int = 4096
   static let realTimeAudioProcessingIntervalNanosecondCount: UInt64 = 2 * 1_000_000_000
+  static let breastCancerHighRiskProbabilityThreshold: Double = 0.5
+  // `from sklearn.datasets import load_breast_cancer; print(load_breast_cancer().data[0].tolist())`
+  static let breastCancerSampleFeatureValues: [Double] = [
+    17.99, 10.38, 122.8, 1001.0, 0.1184, 0.2776, 0.3001, 0.1471, 0.2419, 0.07871, 1.095, 0.9053,
+    8.589, 153.4, 0.006399, 0.04904, 0.05373, 0.01587, 0.03003, 0.006193, 25.38, 17.33, 184.6,
+    2019.0, 0.1622, 0.6656, 0.7119, 0.2654, 0.4601, 0.1189,
+  ]
 }
