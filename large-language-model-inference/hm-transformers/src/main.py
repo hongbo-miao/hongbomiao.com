fee60ebe7e24dcee465f344c9aa5808394745436
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2025-12-20 23:39:06 -0800

    feat(machine-learning): add optimum (#37306)

--- large-language-model-inference/hm-transformers/src/main.py
+++ large-language-model-inference/hm-transformers/src/main.py
@@ -1,14 +1,28 @@
 import logging
+import time
 
 from transformers import pipeline
 
 logger = logging.getLogger(__name__)
 
+ITERATION_COUNT = 10000
+
 
 def main() -> None:
-    classifier = pipeline("sentiment-analysis")
-    logger.info(classifier("I've been waiting for this course my whole life."))
-    logger.info(classifier("I've been waiting for so long for this package to ship."))
+    classifier = pipeline(
+        "text-classification",
+        model="distilbert-base-uncased-finetuned-sst-2-english",
+    )
+
+    text = "I've been waiting for this course my whole life."
+
+    start_time = time.time()
+    for _ in range(ITERATION_COUNT):
+        classifier(text)
+    total_time = time.time() - start_time
+
+    logger.info(f"Total time for {ITERATION_COUNT} iterations: {total_time} seconds")
+    logger.info(f"Average time per iteration: {total_time / ITERATION_COUNT} seconds")
 
 
 if __name__ == "__main__":
