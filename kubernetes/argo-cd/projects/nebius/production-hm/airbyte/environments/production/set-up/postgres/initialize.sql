a54013a6d7c3463aea4e5be1c9a283f89642be6e
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2026-03-29 00:09:03 -0700

    feat(nebius): add airbyte (#40490)

---
+++ kubernetes/argo-cd/projects/nebius/production-hm/airbyte/environments/production/set-up/postgres/initialize.sql
@@ -0,0 +1,17 @@
+create user airbyte_user with encrypted password 'xxx';
+
+create database airbyte_db;
+create database temporal_db;
+create database temporal_visibility_db;
+
+grant all privileges on database airbyte_db to airbyte_user;
+grant all privileges on database temporal_db to airbyte_user;
+grant all privileges on database temporal_visibility_db to airbyte_user;
+
+\connect airbyte_db
+grant all privileges on schema public to airbyte_user;
+\connect temporal_db
+grant all privileges on schema public to airbyte_user;
+create extension if not exists btree_gin;
+\connect temporal_visibility_db
+grant all privileges on schema public to airbyte_user;
