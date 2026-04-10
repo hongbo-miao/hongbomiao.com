e9255353dbc09a4abc2a27db460a918957cde44f
Hongbo Miao <3375461+hongbo-miao@users.noreply.github.com>
2026-04-09 22:14:32 -0700

    chore(proto): remove experimental_allow_proto3_optional arg

--- data-visualization/iads/iads-rtstation/zeromq-iads-bridge/build.rs
+++ data-visualization/iads/iads-rtstation/zeromq-iads-bridge/build.rs
@@ -1,9 +1,7 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    let mut prost_build = prost_build::Config::new();
-    prost_build.protoc_arg("--experimental_allow_proto3_optional");
-    prost_build.compile_protos(
+    prost_build::compile_protos(
         &["src/protos/production.iot.signals.proto"],
         &["src/protos/"],
     )?;
